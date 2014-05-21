ce6a81fe2eb63390832ddc366a1395492866bbd7
Ville Tirronen <ville.tirronen+tim@jyu.fi>
2014-05-21 09:44:53 +0000

    Works behind reverse proxy

--- flask-py/hello-world.py
+++ flask-py/hello-world.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from ReverseProxied import ReverseProxied
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,4 +7,5 @@ def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+     app.wsgi_app = ReverseProxied(app.wsgi_app)	
+     app.run(host='0.0.0.0',port=50000)
