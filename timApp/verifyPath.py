2ec7322c21da12eaa8e4402bd569b27861037548
Mika Lehtinen <mikkalle@student.jyu.fi>
2014-05-30 10:43:32 +0300

    Works with Python 3.

--- timApp/verifyPath.py
+++ timApp/verifyPath.py
@@ -4,10 +4,10 @@ STATIC_PATH = "./static"
 # TODO: TESTSS
 def verifyPath(name, file):
     thisPath = STATIC_PATH + "/" + file.strip()
-    print os.path.dirname(thisPath) + "\n" + STATIC_PATH
+    print(os.path.dirname(thisPath) + "\n" + STATIC_PATH)
     if (os.path.dirname(thisPath) == STATIC_PATH):
-        print "Path successfully verified"
+        print("Path successfully verified")
         return True
     else: 
-        print "Path may be corrupt"
+        print("Path may be corrupt")
         return False
