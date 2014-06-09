ae3cbc5fd48b629e4daa13164fccd26164001bd7
Ville Tirronen <ville.tirronen+tim@jyu.fi>
2014-06-09 11:45:44 +0000

    Shovel file for linting

---
+++ shovel.py
@@ -0,0 +1,15 @@
+from shovel import task
+import subprocess
+
+@task
+def lint():
+    res = ""
+    try:
+        res = subprocess.check_output("pylint --output-format=html timApp/*.py",shell=True)
+        with open("/var/TimDev/lint.html","wb") as file:
+                file.write(res)
+    except subprocess.CalledProcessError as e:
+        with open("/var/TimDev/lint.html","wb") as file:
+                file.write(e.output)
+        
+
