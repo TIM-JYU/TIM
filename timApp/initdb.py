7a1bda438e4db9bf7f0be2bfb0a0616981e2867d
Mika Lehtinen <mikkalle@student.jyu.fi>
2014-06-09 15:13:21 +0300

    Added initdb.py for initializing the database.

---
+++ timApp/initdb.py
@@ -0,0 +1,9 @@
+from timdb import TimDb
+import os
+
+if __name__ == "__main__":
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+    os.chdir(dname)
+    timdb = TimDb(db_path='tim.db', files_root_path='tim_files')
+    timdb.init()
