b3b734cb717cb320062f4aee348a9c657ba05f60
Ville Tirronen <ville.tirronen+tim@jyu.fi>
2014-06-09 11:57:21 +0000

    Added linters

--- shovel.py
+++ shovel.py
@@ -1,8 +1,14 @@
 from shovel import task
 import subprocess
 
+
 @task
 def lint():
+    pylint()
+    jslint()
+
+@task
+def pylint():
     res = ""
     try:
         res = subprocess.check_output("pylint --output-format=html timApp/*.py",shell=True)
@@ -12,4 +18,14 @@ def lint():
         with open("/var/TimDev/lint.html","wb") as file:
                 file.write(e.output)
         
+@task
+def jslint():
+    res = ""
+    try:
+        res = subprocess.check_output("jshint timApp/static/scripts/*.js",shell=True)
+        with open("/var/TimDev/jslint.txt","wb") as file:
+                file.write(res)
+    except subprocess.CalledProcessError as e:
+        with open("/var/TimDev/jslint.txt","wb") as file:
+                file.write(e.output)
 
