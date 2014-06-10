2d1dc8b8028d890b66b51ecc3d41ea314eb0809b
Mika Lehtinen <mikkalle@student.jyu.fi>
2014-06-10 13:45:54 +0300

    Added initial unit tests for timdb.

--- timApp/timdbtest.py
+++ timApp/timdbtest.py
@@ -1,7 +1,53 @@
-from timdb import TimDb
-import os
+"""Unit tests for TimDb."""
 
-abspath = os.path.abspath(__file__)
-dname = os.path.dirname(abspath)
-os.chdir(dname)
-testdb = TimDb(db_path='test.db', files_root_path='test_files')
+import shutil
+import unittest
+from timdb import TimDb
+
+TEST_DB_NAME = 'test.db'
+TEST_FILES_PATH = 'test_files'
+
+
+class TestTimDb(unittest.TestCase):
+
+    def setUp(self):
+        self.db = TimDb(TEST_DB_NAME, TEST_FILES_PATH)
+        self.db.clear()
+
+    def test_user(self):
+        TEST_USER_NAME = 'test_name'
+        user_id = self.db.createUser(TEST_USER_NAME)
+        self.assertIsNotNone(user_id, "user_id was None")
+        self.assertEqual(TEST_USER_NAME, self.db.getUser(user_id)['name'], 'User name was not saved properly')
+    
+    def test_document(self):
+        TEST_DOCUMENT_NAME = 'test_document'
+        document_id = self.db.createDocument(TEST_DOCUMENT_NAME)
+        self.assertIsNotNone(document_id, "document_id was None")
+        self.assertEqual(TEST_DOCUMENT_NAME, self.db.getDocument(document_id)['name'], 'Document name was not saved properly')
+    
+    def test_blocks(self):
+        TEST_CONTENT = 'test content!'
+        TEST_CONTENT2 = 'second block'
+        document_id = self.db.createDocument('test_document')
+        
+        self.db.addMarkDownBlock(document_id, TEST_CONTENT, None)
+        block_ids = self.db.getDocumentBlockIds(document_id)
+        self.assertEqual(1, len(block_ids), 'Block count was wrong')
+        blocks = self.db.getDocumentBlocks(document_id)
+        self.assertEqual(TEST_CONTENT, blocks[0]['text'], 'Block content was wrong')
+        
+        self.db.addMarkDownBlock(document_id, TEST_CONTENT2, block_ids[0])
+        block_ids = self.db.getDocumentBlockIds(document_id)
+        self.assertEqual(2, len(block_ids), 'Block count was wrong')
+        blocks = self.db.getDocumentBlocks(document_id)
+        self.assertEqual(TEST_CONTENT2, blocks[0]['text'], 'Block content was wrong')
+        self.assertEqual(TEST_CONTENT, blocks[1]['text'], 'Block content was wrong')
+        
+    def tearDown(self):
+        shutil.rmtree(TEST_FILES_PATH)
+        self.db.close()
+        return
+    
+if __name__ == '__main__':
+    unittest.main()
