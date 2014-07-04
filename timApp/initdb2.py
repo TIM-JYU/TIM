9e6c33e61a47fa40e8fd038893e5ea44e90dd343
Mika Lehtinen <mikkalle@student.jyu.fi>
2014-07-04 14:06:04 +0300

    initdb2.py now adds some test notes to the document.

--- timApp/initdb2.py
+++ timApp/initdb2.py
@@ -9,8 +9,17 @@ if __name__ == "__main__":
     timdb = TimDb(db_path='tim_files/tim.db', files_root_path='tim_files')
     timdb.initRepo()
     timdb.create()
-    timdb.importDocument('lecture.markdown', 'Ohjelmointi 1 (ei testimuokkauksia!)')
+    doc_id = timdb.importDocument('lecture.markdown', 'Ohjelmointi 1 (ei testimuokkauksia!)')
     timdb.importDocument('lecture.markdown', 'Ohjelmointi 1 (saa testailla vapaasti)')
     uid = timdb.createUser('testikäyttäjä')
     gid = timdb.createUserGroup('testikäyttäjän ryhmä')
     timdb.addUserToGroup(user_id=uid, group_id=gid)
+    timdb.addNote(gid, 'Tämä on testimuistiinpano.', doc_id, 2)
+    timdb.addNote(gid, 'Tämä on toinen testimuistiinpano samassa kappaleessa.', doc_id, 2)
+    timdb.addNote(gid, """Vielä kolmas muistiinpano, jossa on pitkä teksti.
+                        Vielä kolmas muistiinpano, jossa on pitkä teksti.
+                        Vielä kolmas muistiinpano, jossa on pitkä teksti.
+                        Vielä kolmas muistiinpano, jossa on pitkä teksti.
+                        Vielä kolmas muistiinpano, jossa on pitkä teksti.
+                        Vielä kolmas muistiinpano, jossa on pitkä teksti.""", doc_id, 4)
+    
