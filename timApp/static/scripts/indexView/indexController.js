7fc5509a5c95bbdbb8930d14933fdbe85dc6a9f7
Mika Lehtinen <mikkalle@student.jyu.fi>
2014-07-09 09:42:10 +0300

    New documents can now be created from the index page.

--- timApp/static/scripts/indexView/indexController.js
+++ timApp/static/scripts/indexView/indexController.js
@@ -1,19 +1,30 @@
 var TimCtrl = angular.module('controllers', []);
 
-TimCtrl.controller("IndexCtrl", [ '$scope', '$http', '$q', function(sc, http, q) {
-    sc.getDocIds = function() {
-        http( {
-            method : 'GET',
-            url : '/getDocuments/'
-        }).success(function(data, status, headers, config) {
-            sc.documentList = data;
-            sc.displayIndex = true;
-        }).error(function(data, status, headers, config) {
+TimCtrl.controller("IndexCtrl", [ '$scope', '$http', '$q',
+        function(sc, http, q) {
+            sc.createDocument = function(name) {
+                http.post('/createDocument', {
+                    "doc_name" : name
+                }).success(function(data, status, headers, config) {
+                    window.location.href = "/documents/" + data.id;
+                }).error(function(data, status, headers, config) {
+                    alert("Could not create the document.");
+                });
+            }
+
+            sc.getDocIds = function() {
+                http( {
+                    method : 'GET',
+                    url : '/getDocuments/'
+                }).success(function(data, status, headers, config) {
+                    sc.documentList = data;
+                    sc.displayIndex = true;
+                }).error(function(data, status, headers, config) {
+                    sc.documentList = [];
+                    // TODO: Show some error message.
+                    });
+            };
             sc.documentList = [];
-            //TODO: Show some error message.
-        });
-    };
-    sc.documentList = [];
-    sc.getDocIds();
-    sc.displayIndex = false;
-} ]);
+            sc.getDocIds();
+            sc.displayIndex = false;
+        } ]);
