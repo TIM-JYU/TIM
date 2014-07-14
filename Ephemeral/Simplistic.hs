9ba429bfec5716c78e845dbd8ff10054b078c03d
Ville Tirronen <aleator@gmail.com>
2014-07-14 12:22:28 +0300

    First version of block matcher

--- Ephemeral/Simplistic.hs
+++ Ephemeral/Simplistic.hs
@@ -10,6 +10,7 @@ import qualified Data.Text.Encoding as T
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as LBS
 import Control.Monad.Trans
+import Control.Monad.Trans.Either
 import Snap.Core
 import Data.Aeson
 import GHC.Generics
@@ -22,6 +23,8 @@ import qualified Data.Sequence as Seq
 import Data.Sequence (Seq)
 import EphemeralPrelude
 import Text.Blaze.Html.Renderer.Text
+import qualified Data.HashSet as Set
+import Data.HashSet (HashSet)
 
 newtype DocID = DocID T.Text deriving(Eq,Ord,Show)
 
@@ -34,7 +37,7 @@ instance ToJSON DocID where
 instance ToJSON a => ToJSON (Seq a) where
     toJSON = toJSON . F.toList
 
-data Block = Block {markdown::T.Text, html::LT.Text} deriving Eq
+data Block = Block {markdown::T.Text, html::LT.Text, bagOfWords :: HashSet T.Text} deriving Eq
 newtype Doc = Doc {fromDoc::Seq Block}
 
 convert :: BS.ByteString -> Doc
@@ -43,8 +46,9 @@ convert bs = case (PDC.readMarkdown PDC.def . T.unpack . T.decodeUtf8 $ bs) of
             where 
                 convertBlock t = let  pdc = PDC.Pandoc mempty . box $ t
                                  in Block
-                                     (T.pack     . PDC.writeMarkdown PDC.def $ pdc)
-                                     (renderHtml . PDC.writeHtml PDC.def     $ pdc)
+                                     (T.pack       . PDC.writeMarkdown PDC.def $ pdc)
+                                     (renderHtml   . PDC.writeHtml PDC.def     $ pdc)
+                                     (Set.fromList . map T.pack . words . PDC.writeMarkdown PDC.def $ pdc)
                             
 
 box :: t -> [t]
@@ -68,16 +72,22 @@ instance Ord a => Ord (Labeled x a) where
 labelList :: (Enum t1, Num t1) => t -> [a] -> [Labeled (t, t1) a]
 labelList lbl lst = [Labeled (lbl,i) x | (i,x) <- zip [0..] lst]
 
+-- Return the list of affinities for given block of a given document when compared to
+-- all blocks of another document
+performMatch :: MonadIO m => (DocID,Int) -> DocID -> State -> EitherT Value m [(Double,Int)]
+performMatch (d1ID,i1) d2ID state = do
+   Doc d2 <- fetchDoc d2ID state
+   block  <- fetchBlock d1ID i1 state
+   return $ zip (map (textAffinity block) (F.toList d2)) [0..]
+
 -- TODO: PerformDiff is really, really slow!
-performDiff :: MonadIO m => DocID -> DocID -> State -> m (Maybe [(DocID,Int)])
-performDiff parentID childID (State st) = liftIO $ do
-   parent <- LRU.lookup parentID st 
-   child <- LRU.lookup childID st 
-   case (parent,child) of
-     (Just (Doc a),Just (Doc b)) 
-       -> return $ Just (execDiff (labelList parentID (F.toList a)) 
-                                  (labelList childID (F.toList b)))
-     _ ->  return Nothing
+performDiff :: MonadIO m => DocID -> DocID -> State -> EitherT Value m [(DocID,Int)]
+performDiff parentID childID state@(State st) = do
+   Doc parent <- fetchDoc parentID state 
+   Doc child  <- fetchDoc childID  state 
+   return $ execDiff (labelList parentID (F.toList parent)) 
+                     (labelList childID  (F.toList child))
+
 
 -- TODO: PerformDiff3 is really, really slow!
 performDiff3 :: MonadIO m => DocID -> DocID -> DocID -> State -> m (Maybe [D3.Hunk (DocID,Integer)])
@@ -106,43 +116,44 @@ execDiff a b = reverse $ foldl' op [] (getDiff a b)
     op acc (Second bl) = getLabel bl:acc
     op acc (Both bl _) = getLabel bl:acc
 
--- Ops
+-- Operations
+
+-- Load a document from a given bytestring and insert it to the LRU
 loadDocument :: MonadIO m => DocID -> BS.ByteString -> State -> m ()
 loadDocument docID bs (State st) = liftIO $ 
     LRU.insert docID (convert bs) st
 
-fetchDoc :: MonadIO m => DocID -> State -> m (Maybe Doc)
-fetchDoc docID (State st) = liftIO $ do
-    doc <- LRU.lookup docID st
-    case doc of
-        Just d -> return (Just d)
-        _ -> return Nothing
+-- Retrieve a document
+fetchDoc :: MonadIO m => DocID -> State -> EitherT Value m Doc
+fetchDoc docID (State st) =  hoistMaybe (jsErr $ "No such document: "<> (T.pack $ show docID))
+                                        (liftIO $ LRU.lookup docID st)
 
 
-fetchBlock :: MonadIO m => DocID -> Int -> State -> m (Maybe Block)
-fetchBlock docID index (State st) = liftIO $ do
-    doc <- LRU.lookup docID st
-    case doc of
-        Just (Doc d) 
-         | index >= 0 && index < Seq.length d 
-          -> return (Just $ Seq.index d index)
-        _ -> return Nothing
+fetchBlock :: MonadIO m => DocID -> Int -> State -> EitherT Value m Block
+fetchBlock docID index state@(State st) = do
+    Doc d <- fetchDoc docID state
+    abortIf (index >= 0 && index < Seq.length d)
+            (jsErr "Index out of bounds")
+    return (Seq.index d index)
 
+replace :: MonadIO m => DocID -> Int -> BS.ByteString -> State -> EitherT Value m ()
+replace docID index bs state@(State st) = do
+    (Doc d) <- fetchDoc docID state
+    liftIO $ LRU.insert docID (Doc $ insertRange d index (fromDoc (convert bs))) st
 
---fetchBlocks :: MonadIO m => DocID -> State -> m ([Block])
---fetchBlocks docID (State st) = liftIO $ do
---    doc <- LRU.lookup docID st
---    case doc of
---        Just (Doc d) -> return (map (Seq.index d) [0..Seq.length d]) --(Just $ Seq.index d index)
---        _ -> return []
+jsErr :: T.Text -> Value
+jsErr e = object ["error" .= e] 
 
+jsOk :: T.Text -> Value
+jsOk e = object ["ok" .= e] 
 
-replace :: MonadIO m => DocID -> Int -> BS.ByteString -> State -> m ()
-replace docID index bs (State st) = liftIO $ do
-    doc <- LRU.lookup docID st
-    case doc of 
-        Nothing       -> LRU.insert docID (convert bs) st
-        Just (Doc d)  -> LRU.insert docID (Doc $ insertRange d index (fromDoc (convert bs))) st
+abortIf :: Monad m => Bool -> e -> EitherT e m ()
+abortIf True  e = left e
+abortIf False e = right ()
+
+hoistMaybe :: Monad m => e -> m (Maybe a) -> EitherT e m a
+hoistMaybe str op = EitherT $ op >>= return . maybe (Left str) Right
+        
 
 addParagraph :: MonadIO m => DocID -> Int -> BS.ByteString -> State -> m ()
 addParagraph docID idx bs (State st) = liftIO $ do
@@ -159,52 +170,51 @@ insertRange orig index source =
      (s,e) = Seq.splitAt index orig
     in s <> source <> (Seq.drop 1 e)
 
+textAffinity :: Block -> Block -> Double
+textAffinity d1 d2 
+ | d1 == d2  = 1
+ | otherwise = Set.size (Set.intersection (bagOfWords d1) (bagOfWords d2))
+               `fdiv`
+               Set.size (Set.union        (bagOfWords d1) (bagOfWords d2))
+ where fdiv a b = fromIntegral a / fromIntegral b
+
 main :: IO ()
 main = do
     state <- initialize
+    let withDoc op = do
+            docID <- requireParam "docID"
+            runFailing (fetchDoc docID state>>=lift.op)
+        withBlock op =  do
+            docID <- requireParam "docID"
+            idx   <- requireParam "idx"
+            runFailing (fetchBlock docID idx state >>= lift.op)
+        runFailing :: EitherT Value Snap a -> Snap ()
+        runFailing op = eitherT (writeLBS.encode) -- In case of failure, send the encoded error
+                                (\_ -> return ()) -- In case of success, assume that data has been already sent
+                                op
+
     quickHttpServe $ route 
         [
-         (":docID", method GET $ do
-            docID <- requireParam "docID"
-            doc <- fetchDoc docID state
-            case doc of
-                Just  (Doc d) -> traverse (\x -> writeText (markdown x) >> writeText "\n\n") d >> return ()
-                Nothing -> writeBS "{\"Error\":\"No doc found\"}" 
-         ),
-         ("/json-html/:docID", method GET $ do
-            docID <- requireParam "docID"
-            doc <- fetchDoc docID state
-            case doc of
-                Just  (Doc d) -> writeLBS . encode . fmap html $ d
-                Nothing -> writeBS "{\"Error\":\"No doc found\"}" 
-         ),
-         ("/html/:docID", method GET $ do
-            docID <- requireParam "docID"
-            doc <- fetchDoc docID state
-            case doc of
-                Just  (Doc d) -> traverse (\x -> writeLazyText (html x) >> writeText "\n\n") d >> return ()
-                Nothing -> writeBS "{\"Error\":\"No doc found\"}" 
-         ),
-         (":docID/:idx", method GET $ do
-            docID <- requireParam "docID"
-            idx   <- requireParam "idx"
-            block <- fetchBlock docID idx state
-            case block of 
-                Nothing -> writeBS "{\"Error\":\"No block found\"}" 
-                Just  r -> writeText (markdown r)
-         ),
-         (":docID/:idx/html", method GET $ do
-            docID <- requireParam "docID"
-            idx   <- requireParam "idx"
-            block <- fetchBlock docID idx state
-            case block of 
-                Nothing -> writeBS "{\"Error\":\"No block found\"}" 
-                Just  r -> writeLazyText (html r)
-         ),
-         (":docID/:idx", method PUT $ do
-            docID <- requireParam "docID"
-            idx   <- requireParam "idx"
-            bd    <- readRequestBody (1024*2000)
+         -- Send the whole doc as markdown. Required: [?]
+         (":docID", method GET . withDoc $ \(Doc d) ->
+                traverse (\x -> writeText (markdown x) >> writeText "\n\n") d >> return ())
+         
+         -- Send the whole doc as json containing html for the blocks. Required: [X]
+         ,("/json-html/:docID", method GET . withDoc $ \(Doc d) -> 
+                writeLBS . encode . fmap html $ d),
+         
+         -- Send the whole doc as html. Required: [?]
+         ("/html/:docID", method GET . withDoc $ \(Doc d) ->
+                traverse (\x -> writeLazyText (html x) >> writeText "\n\n") d >> return ()),
+         
+         -- Send a single block as markdown. Required [?]
+         (":docID/:idx", method GET . withBlock $ \block -> writeText (markdown block)),
+
+         -- Replace a block. Required [X]
+         (":docID/:idx", method PUT . runFailing $ do
+            docID <- requireParamE "docID"
+            idx   <- requireParamE "idx"
+            bd    <- lift (readRequestBody (1024*2000))
             replace docID idx (LBS.toStrict bd) state
          ),
          ("/new/:docID/:idx", method POST $ do
@@ -214,17 +224,25 @@ main = do
             addParagraph docID idx (LBS.toStrict bd) state
             
          ),
+         -- Load an entire markdown document into cache. Required [X]
          ("load/:docID/", method POST $ do
             docID <- requireParam "docID"
             bd    <- readRequestBody (1024*2000)
             loadDocument docID (LBS.toStrict bd) state
             writeBS "{\"Ok\":\"Document loaded\"}" 
          ),
-         ("diff/:parentID/:childID/", method GET $ do
-            parentID <- requireParam "parentID"
-            childID  <- requireParam "childID"
+         ("match/:doc1ID/:doc1idx/:doc2ID", method GET . runFailing $ do
+            d1ID  <- requireParamE "doc1ID"
+            d2ID  <- requireParamE "doc2ID"
+            d1idx <- requireParamE "doc1idx"
+            aff   <- performMatch (d1ID,d1idx) d2ID state 
+            lift (writeLBS . encode $ aff)
+         ),
+         ("diff/:parentID/:childID/", method GET . runFailing $ do
+            parentID <- requireParamE "parentID"
+            childID  <- requireParamE "childID"
             diffed <- performDiff parentID childID state
-            writeLBS (encode diffed) 
+            lift (writeLBS (encode diffed))
          ),
          ("diff3/:parentID/:childID1/:childID2", method GET $ do
             parentID <- requireParam "parentID"
@@ -235,6 +253,11 @@ main = do
          )
  
         ]
+
+requireParamE :: (MonadSnap m, Readable b) => BS.ByteString -> EitherT Value m b
+requireParamE p = lift (getParam p) >>= \x -> case x of
+                    Just val -> lift $ fromBS val
+                    Nothing  -> left . jsErr $ "Parameter " <> T.decodeUtf8 p <> " needed"
 
 requireParam :: (MonadSnap m, Readable b) => BS.ByteString -> m b
 requireParam p = getParam p >>= \x -> case x of
