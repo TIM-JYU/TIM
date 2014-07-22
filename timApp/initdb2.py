ad3eb0ab4c0b742e387cc22f8dd1726dd8709221
Mika Lehtinen <mikkalle@student.jyu.fi>
2014-07-22 11:26:09 +0300

    Changed encoding of initdb2 to match other files.

--- timApp/initdb2.py
+++ timApp/initdb2.py
@@ -17,12 +17,12 @@ if __name__ == "__main__":
     doc_id = timdb.documents.importDocument('lecture.markdown', 'Ohjelmointi 1 (ei testimuokkauksia!)', 0)
     timdb.documents.importDocument('lecture.markdown', 'Ohjelmointi 1 (saa testailla vapaasti)', 0)
     timdb.users.grantViewAccess(0, doc_id.id)
-    timdb.notes.addNote(0, 'T√§m√§ on testimuistiinpano.', doc_id.id, 2)
-    timdb.notes.addNote(0, 'T√§m√§ on toinen testimuistiinpano samassa kappaleessa.', doc_id.id, 2)
-    timdb.notes.addNote(0, """Viel√§ kolmas muistiinpano, jossa on pitk√§ teksti.
-                        Viel√§ kolmas muistiinpano, jossa on pitk√§ teksti.
-                        Viel√§ kolmas muistiinpano, jossa on pitk√§ teksti.
-                        Viel√§ kolmas muistiinpano, jossa on pitk√§ teksti.
-                        Viel√§ kolmas muistiinpano, jossa on pitk√§ teksti.
-                        Viel√§ kolmas muistiinpano, jossa on pitk√§ teksti.""", doc_id.id, 4)
+    timdb.notes.addNote(0, 'T‰m‰ on testimuistiinpano.', doc_id.id, 2)
+    timdb.notes.addNote(0, 'T‰m‰ on toinen testimuistiinpano samassa kappaleessa.', doc_id.id, 2)
+    timdb.notes.addNote(0, """Viel‰ kolmas muistiinpano, jossa on pitk‰ teksti.
+                        Viel‰ kolmas muistiinpano, jossa on pitk‰ teksti.
+                        Viel‰ kolmas muistiinpano, jossa on pitk‰ teksti.
+                        Viel‰ kolmas muistiinpano, jossa on pitk‰ teksti.
+                        Viel‰ kolmas muistiinpano, jossa on pitk‰ teksti.
+                        Viel‰ kolmas muistiinpano, jossa on pitk‰ teksti.""", doc_id.id, 4)
     
