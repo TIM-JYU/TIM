a9469146fc18e7e04a1ad7974bc55e7aa37aceb9
Mika Lehtinen <mikkalle@student.jyu.fi>
2014-08-08 10:21:19 +0300

    Bugfix in rendering view template.

--- timApp/tim.py
+++ timApp/tim.py
@@ -320,7 +320,7 @@ def viewDocument(doc_id):
     versions = timdb.documents.getDocumentVersions(doc_id)
     xs = timdb.documents.getDocumentAsHtmlBlocks(DocIdentifier(doc_id, versions[0]['hash']))
     doc = timdb.documents.getDocument(DocIdentifier(doc_id, versions[0]['hash']))
-    fullHtml = pluginControl.pluginify(xs, getCurrentUserName())
+    plugins, fullHtml = pluginControl.pluginify(xs, getCurrentUserName())
     return render_template('view.html', docID=doc['id'], docName=doc['name'], text=json.dumps(fullHtml), version=versions[0])
 
 @app.route("/postNote", methods=['POST'])
