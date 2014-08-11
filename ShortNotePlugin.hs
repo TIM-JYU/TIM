98f75ddcc15404b7f310b856b6b7a30048ac56f7
Ville Tirronen <aleator@gmail.com>
2014-08-11 17:19:16 +0300

    Reorganization + prototype Haskell interpreter

---
+++ ShortNotePlugin.hs
@@ -0,0 +1,36 @@
+{-#LANGUAGE OverloadedStrings, ScopedTypeVariables, RecordWildCards, DeriveGeneric#-}
+module Main where
+
+import Data.Aeson
+import GHC.Generics
+import qualified Data.Text as T
+import qualified Data.Text.Lazy.Encoding as LT
+import Snap.Http.Server
+
+import PluginType
+
+data NoteMarkup = NM {width,height::Int,prompt::T.Text} deriving (Show,Generic)
+data NoteCommands = SaveNote T.Text | SubmitNote T.Text deriving (Show,Generic)
+instance FromJSON NoteMarkup where
+instance ToJSON NoteMarkup where
+
+shortNote :: Plugin NoteMarkup T.Text T.Text
+shortNote = Plugin{..}
+  where 
+    requirements = [JS "shortNote.js"
+                   ,NGModule "Note"]
+    additionalFiles = ["ShortNoteTemplate.html"]
+    initial = ""
+    update markup _ i = return $ [save i,web "content" (encodeState markup i)]
+    encodeState markup state =  object ["width"   .= width markup
+                                       ,"height"  .= height markup
+                                       ,"prompt"  .= prompt markup
+                                       ,"content" .= state]
+    render markup state = return . LT.decodeUtf8 $ ngDirective "shortNote" $ encodeState markup state
+    additionalRoutes = noRoutes
+                                
+main :: IO ()
+main = quickHttpServe $ serve shortNote
+
+testNote :: NoteMarkup 
+testNote = NM 80 15 "Kerro kissasta"
