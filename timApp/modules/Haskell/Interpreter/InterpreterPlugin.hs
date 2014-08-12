3464066669122050beeedfa2858559d4aa5b65be
Ville Tirronen <aleator@gmail.com>
2014-08-12 10:31:31 +0300

    HsInterpreter, the `:t` command

--- timApp/modules/Haskell/Interpreter/InterpreterPlugin.hs
+++ timApp/modules/Haskell/Interpreter/InterpreterPlugin.hs
@@ -6,6 +6,7 @@ import GHC.Generics
 import qualified Data.Text as T
 import qualified Data.Text.Lazy.Encoding as LT
 import Snap.Http.Server
+import Data.List
 import Control.Monad.Trans.Either
 
 import PluginType
@@ -33,14 +34,16 @@ mkInterpreter = do
         additionalFiles = ["NewConsole/Console.template.html"]
         initial = ()
         update markup _ (Evaluate expr) = do
-                             res <- callApiHec evaluator $ Input (Eval expr) (Main.context markup)
-                             case res of
-                                H.Plain s  -> return $ [web "reply" $ "<span>"++s++"</span>"] -- TODO: SANITIZE!!
-                                H.Html  s  -> return $ [web "reply" $ s] -- TODO: SANITIZE!!
-                                H.Error ss -> return $ [web "reply" $ "<pre class='error'>"++unlines ss++"</pre>"] -- TODO: SANITIZE!! 
-                                H.NonTermination -> return $ [web "reply" ("<span class='warning'>Your expression is diverging (ie. does not terminate)</span>"::String)] 
-                                H.TimeOut -> return $ [web "reply" ("<span class='warning'>Your expression did not terminate on time</span>"::String)] 
-                                H.ProtocolError e -> return $ [web "reply" $ "<span class='warning'>Interactive interpreter is broken. Please report this to a human:"++e++"</span>"]  -- TODO: SANITIZE!!
+             res <- if ":t" `isPrefixOf` expr 
+                     then callApiHec evaluator $ Input (TypeOf $ drop 2 expr) (Main.context markup)
+                     else callApiHec evaluator $ Input (Eval expr) (Main.context markup)
+             case res of
+                H.Plain s  -> return $ [web "reply" $ "<span>"++s++"</span>"] -- TODO: SANITIZE!!
+                H.Html  s  -> return $ [web "reply" $ s] -- TODO: SANITIZE!!
+                H.Error ss -> return $ [web "reply" $ "<pre class='error'>"++unlines ss++"</pre>"] -- TODO: SANITIZE!! 
+                H.NonTermination -> return $ [web "reply" ("<span class='warning'>Your expression is diverging (ie. does not terminate)</span>"::String)] 
+                H.TimeOut -> return $ [web "reply" ("<span class='warning'>Your expression did not terminate on time</span>"::String)] 
+                H.ProtocolError e -> return $ [web "reply" $ "<span class='warning'>Interactive interpreter is broken. Please report this to a human:"++e++"</span>"]  -- TODO: SANITIZE!!
         render markup state = return . LT.decodeUtf8 . ngDirective "console" . object $
                                     ["examples" .= examples markup]
         additionalRoutes = noRoutes
