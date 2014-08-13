6f13070806a3cccf62221b2ce9e8bb6b507c3273
Mika Lehtinen <mikkalle@student.jyu.fi>
2014-08-13 14:48:42 +0300

    Updated saveAnswer route (added some checks to make sure the plugin response is correct).

--- timApp/tim.py
+++ timApp/tim.py
@@ -449,9 +449,21 @@ def saveAnswer(doc_id, plugintype, task_id):
     
     # Assuming the JSON is in a string
     jsonresp = json.loads(pluginResponse)
+    if type(jsonresp) is not dict:
+        return jsonResponse({'error' : 'The plugin response was not a dictionary.'}, 400)
     
-    #Save the new state
-    timdb.answers.saveAnswer([getCurrentUserId()], doc_task_id, json.dumps(jsonresp['state']), jsonresp['state']['points'])
+    if not 'save' in jsonresp:
+        return jsonResponse({'error' : 'The key "save" was not found from plugin response.'}, 400)
+    
+    if not 'web' in jsonresp:
+        return jsonResponse({'error' : 'The key "web" was not found from plugin response.'}, 400)
+    
+    points = jsonresp['state']['points'] if 'points' in jsonresp['save'] else None
+    
+    # Save the new state
+    # TODO: Save tags
+    timdb.answers.saveAnswer([getCurrentUserId()], doc_task_id, json.dumps(jsonresp['save']), points)
+    
     return jsonResponse(jsonresp['web'])
 
 def getPluginMarkup(doc_id, plugintype, task_id):
