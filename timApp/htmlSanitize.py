14c0e34ed42df37c85e6b1f9a1247f1748473645
Mika Lehtinen <mikkalle@timbeta.it.jyu.fi>
2014-08-14 12:49:35 +0300

    Samuel: changes

--- timApp/htmlSanitize.py
+++ timApp/htmlSanitize.py
@@ -1,7 +1,8 @@
 import bleach
 
-tags = bleach.ALLOWED_TAGS + ['video']
+tags = bleach.ALLOWED_TAGS + ['video', 'http', 'p']
 
 # Sanitize html, bleach is whitelist based, see https://github.com/jsocol/bleach for more information
 def sanitize_html(html_string):
-    return bleach.clean(html_string, tags)
+    return bleach.linkify(html_string)
+    
