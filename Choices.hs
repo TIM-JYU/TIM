4482790976c1af4b8883d46f0dc7c2a79f15a396
Ville Tirronen <aleator@gmail.com>
2014-08-14 12:57:12 +0300

    Split plugin to a library

---
+++ Choices.hs
@@ -0,0 +1,61 @@
+{-#LANGUAGE OverloadedStrings, ScopedTypeVariables, RecordWildCards, DeriveGeneric#-}
+module Choice where
+
+import Data.Aeson
+import GHC.Generics
+import qualified Data.Text as T
+import qualified Data.Text.Lazy.Encoding as LT
+
+import PluginType
+
+data Choice = Choice {text :: T.Text, correct :: Bool, reason :: T.Text} deriving (Show,Generic)
+instance ToJSON   Choice where
+
+newtype Blind = Blind T.Text deriving (Show, Generic)
+
+instance ToJSON Blind where
+    toJSON (Blind t) = object ["text" .= t]
+
+blind :: MCQMarkup Choice -> MCQMarkup Blind
+blind MCM{..} = MCM ident stem (map hide choices) 
+hide :: Choice -> Blind
+hide = Blind . text
+
+data MCQMarkup choice 
+    = MCM {ident   :: T.Text
+          ,stem    :: T.Text
+          ,choices :: [choice]} 
+      deriving (Show,Generic)
+
+instance ToJSON a => ToJSON (MCQMarkup a) where
+instance ToJSON (MCQState) where
+instance FromJSON Choice where
+instance FromJSON a => FromJSON (MCQMarkup a) where
+instance FromJSON MCQState where
+
+
+data MCQState = Revealed Integer | Unchecked deriving (Eq,Show,Generic)
+
+simpleMultipleChoice :: Plugin (MCQMarkup Choice) (Maybe Integer) Integer
+simpleMultipleChoice 
+   = Plugin{..}
+  where 
+    requirements = [JS "script.js"
+                  ,NGModule "MCQ"]
+    additionalFiles = ["MCQTemplate.html"]
+    initial = Nothing
+    update mcm _ i = return $ 
+                       [save (Just i)
+                       ,web  "state"  i
+                       ,web  "question" mcm
+                       ]
+    render mcm state = return . LT.decodeUtf8 $
+                        case state of
+                             Just i  -> ngDirective "mcq" 
+                                            $ object ["question" .= mcm 
+                                                     ,"state"    .= Just i]
+                             Nothing -> ngDirective "mcq"
+                                            $ object ["question" .= blind mcm 
+                                                     ,"state"    .= (Nothing :: Maybe ()) ]
+    additionalRoutes = noRoutes
+                                
