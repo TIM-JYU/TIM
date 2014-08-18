d0d373ff065e4e6af6b79545260e5db8ec4bf1a2
Ville Tirronen <aleator@gmail.com>
2014-08-18 15:37:27 +0300

    Fixed a bug in MCQ

--- timApp/modules/Haskell/Choices/script2.js
+++ timApp/modules/Haskell/Choices/script2.js
@@ -4,12 +4,14 @@ angular.module('MCQ', [])
 					$scope.active  = false;
 					$scope.checked = true;
 					$scope.answer = $scope.content.state; //new Array($scope.content.question.length);
-					if ($scope.answer==null) 
+					if ($scope.answer==null||$scope.answer==undefined) 
                         {
                         $scope.checked = false;
                         $scope.active  = true;
+                        $scope.answer = new Array($scope.content.question.choices.length);
     					for(var j=0;j++;j<$scope.answer.length) {$scope.answer[j]=false;};
 	    			    }
+                    console.log(["ANSWER",$scope.answer]);
                     }
                     , function(scope){scope.active=false;return scope.answer;})) //TODO: cleanup
   .directive('mcq', standardDirective("MCQTemplate.html"
