5b8a2a630c4110044bc073a233e251f5f24eb79d
Mika Lehtinen <mikkalle@student.jyu.fi>
2014-09-01 14:49:26 +0300

    Added sup tag to the list of allowed tags.

--- timApp/htmlSanitize.py
+++ timApp/htmlSanitize.py
@@ -2,7 +2,7 @@
 import bleach
 
 
-tags = bleach.ALLOWED_TAGS + ['video','p','code','div','span','br','pre','img','h1','h2','h3','h4','h5','h6','h7','table','tbody','thead','tfoot','td','tr','th','caption','colgroup','col','sub']
+tags = bleach.ALLOWED_TAGS + ['video','p','code','div','span','br','pre','img','h1','h2','h3','h4','h5','h6','h7','table','tbody','thead','tfoot','td','tr','th','caption','colgroup','col','sub','sup']
 
 tim_attrs = {'*':['class', 'id','align'], 'video':['src','controls'], 'img':['src','width','height']}
 bleach.ALLOWED_ATTRIBUTES.update(tim_attrs)
