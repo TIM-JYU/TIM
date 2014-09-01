965f6f0b79b6eeaa8bdff3996e398beffb45f6d6
Mika Lehtinen <mikkalle@timbeta.it.jyu.fi>
2014-09-01 15:07:37 +0300

    Fix in shortNotePlugin: Answer was sent to a wrong URL.

--- timApp/modules/Haskell/Choices/shortNote.js
+++ timApp/modules/Haskell/Choices/shortNote.js
@@ -4,23 +4,23 @@ function standardDirective(template,extract) {
       restrict: 'E',
       templateUrl: function(elem,attrs) {
                     return elem.parent().attr('data-plugin')+"/"+template;
-                   }, 
+                   },
       replace: true,
       scope: {},
       controller: function($scope, $element, $http) {
-        $scope.ident     = $element.parent().attr("id"); 
+        $scope.ident = $element.parent().attr("id");
         $scope.content = JSON.parse($element.attr("data-content"));
         console.log(["initial",$scope.content]);
         $scope.plugin = $element.parent().attr("data-plugin");
         $scope.submit = function () {
             var message = {input:extract($scope)};
             console.log(["sent",$scope.content.content]);
-            var localState  = JSON.parse($element.parent().attr("data-state")  || "null");
+            var localState = JSON.parse($element.parent().attr("data-state") || "null");
             var localMarkup = JSON.parse($element.parent().attr("data-markup") || "null");
             if (localState!==null) {message.state = localState};
             if (localMarkup!==null) {message.markup = localMarkup};
             $http({method:'PUT'
-                  ,url:$scope.plugin+"/answer/"
+                  ,url:$scope.plugin+"/" + $scope.ident + "/answer/"
                   ,data:message})
              .success(function(data){
                   $scope.content = data.web;
@@ -34,8 +34,7 @@ function standardDirective(template,extract) {
     }
   }
 }
-
 angular.module('Note', [])
-  .directive('shortnote', 
+  .directive('shortnote',
              standardDirective("ShortNoteTemplate.html"
                               ,function(scope){return scope.content.content}));
