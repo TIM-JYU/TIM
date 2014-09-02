a2add696e1f9ce8a97eae74f155fa925b5b8ceff
Mika Lehtinen <mikkalle@student.jyu.fi>
2014-09-02 12:58:35 +0300

    Added default, debug and production configuration files.

---
+++ timApp/defaultconfig.py
@@ -0,0 +1,9 @@
+DATABASE           = './tim_files/tim.db'
+DEBUG              = False
+FILES_PATH         = 'tim_files'
+LOG_PATH           = "../tim_logs/timLog.log"
+MAX_CONTENT_LENGTH = 16 * 1024 * 1025
+PASSWORD           = '4t95MHJj9h89y'
+SECRET_KEY         = '85db8764yhfZz7-U.-y968buyn89b54y8y45tg'
+UPLOAD_FOLDER      = "./media/images/"
+USERNAME           = 'admin'
