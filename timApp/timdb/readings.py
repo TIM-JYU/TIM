c949eea329ab6a94c8b5ceae6526b642a77d374f
Mika Lehtinen <mikkalle@student.jyu.fi>
2014-09-07 13:06:28 +0300

    Added timestamp saving for reading info.

--- timApp/timdb/readings.py
+++ timApp/timdb/readings.py
@@ -11,12 +11,16 @@ class Readings(TimDbBase):
         :param block_id: The id of the block whose readings will be fetched.
         """
         rows = self.getOwnedBlockRelations(document_id, user_id, blocktypes.READING)
-        return [{'id' : row['id'], 'specifier' : row['parent_block_specifier'], 'text' : row['description']} for row in rows]
+        return [{'id' : row['id'],
+                 'specifier' : row['parent_block_specifier'],
+                 'text' : row['description'],
+                 'created' : row['created']} for row in rows]
 
     @contract
     def setAsRead(self, usergroup_id : 'int', document_id : 'int', paragraph_specifier : 'int', current_text : 'str'):
         cursor = self.db.cursor()
-        cursor.execute('insert into Block (description, UserGroup_id, type_id) values (?, ?, ?)', [current_text, usergroup_id, blocktypes.READING])
+        cursor.execute('insert into Block (description, UserGroup_id, type_id, created) values (?, ?, ?, CURRENT_TIMESTAMP)',
+                       [current_text, usergroup_id, blocktypes.READING])
         reading_id = cursor.lastrowid
         assert reading_id is not None, 'reading_id was None'
         cursor.execute('insert into BlockRelation (block_id, parent_block_specifier, parent_block_id, parent_block_revision_id) values (?,?,?,?)',
