90477a109e2df7c36ace25c9ab18d3409f38c479
Mika Lehtinen <mikkalle@student.jyu.fi>
2015-01-25 10:55:24 +0200

    Don't try to re-launch Ephemeral when Flask reloads.

--- timApp/launch.py
+++ timApp/launch.py
@@ -1,10 +1,12 @@
+import os
 import tim
 import ephemeralclient
 import sys
 
 if __name__ == '__main__':
     try:
-        p = ephemeralclient.launch_ephemeral(ignore_signals='pudb' in sys.modules)
+        if not os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+            p = ephemeralclient.launch_ephemeral(ignore_signals='pudb' in sys.modules)
         tim.startApp()
     finally:
         p.kill()
