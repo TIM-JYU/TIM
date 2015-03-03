8c8c4edfe3a3c1754d66fa5446a43ac1e4248268
Mika Lehtinen <mikkalle@student.jyu.fi>
2015-03-03 15:59:16 +0200

    Added a helper route for developers for logging in as another user.

--- timApp/routes/login.py
+++ timApp/routes/login.py
@@ -279,3 +279,23 @@ def finishLogin(ready=True):
         session.pop('anchor', '')
         session.pop('came_from', '/')
     return redirect(came_from + anchor)
+
+
+@login_page.route("/quickLogin/<username>")
+def quickLogin(username):
+    """A debug helping method for logging in as another user.
+       For developer use only.
+    """
+    if getCurrentUserName() not in ['mikkalle', 'tojukarp', 'vesal']:
+        abort(403)
+    timdb = getTimDb()
+    user = timdb.users.getUserByName(username)
+    if user is None:
+        abort(404, 'User not found.')
+    user = timdb.users.getUser(user)
+    session['user_id'] = user['id']
+    session['user_name'] = user['name']
+    session['real_name'] = user['real_name']
+    session['email'] = user['email']
+    flash("Logged in as: {}".format(username))
+    return redirect(url_for('indexPage'))
