a1c537f8fdae08e1016f3a45fd5f5193a22166d8
Ville Tirronen <aleator@gmail.com>
2015-04-07 14:17:43 +0300

    SecSplit handles code blocks
    This is a tentative attempt at making section splitting
    aware of code blocks. Unlike the previous splitting,
    this has way more special cases so it might not work
    right away.

--- Ephemeral/SecSplit.hs
+++ Ephemeral/SecSplit.hs
@@ -1,6 +1,6 @@
-{-#LANGUAGE OverloadedStrings#-}
+{-#LANGUAGE OverloadedStrings, TupleSections #-}
 module SecSplit where
-import Data.Attoparsec.Text hiding (feed)
+import Data.Attoparsec.Text as A hiding (feed)
 import Control.Applicative
 import Data.List
 import Control.Monad
@@ -13,9 +13,13 @@ feed :: SrcPos -> T.Text -> SrcPos
 feed pos x = pos <> Sum (T.length x) 
 feeds :: SrcPos -> [T.Text] -> SrcPos
 feeds pos = foldl' feed pos 
+
 data Tokened = Heading SrcPos Int T.Text 
              | Body SrcPos T.Text
              deriving (Eq,Show,Ord)
+
+body :: T.Text -> Tokened
+body content = Body (mempty`feed`content) content
 
 contents :: Tokened -> T.Text
 contents (Heading _ _ t) = t
@@ -38,9 +42,28 @@ eol = (string "\n") <|> (string "\r\n")
 
 document :: Parser [Tokened]
 document = do 
-            parts <- many (heading <|> notHeading) <|> (endOfInput>>return [])
+            parts <- many (heading <|> codeBlock <|> notHeading) <|> (endOfInput>>return [])
             return $ reverse $ foldPos parts
 
+--- | Parse a block of code into a Body element
+codeBlock :: Parser Tokened
+codeBlock = do
+    (sym,st) <- (('~',) <$> "~~~") <|> (('`',) <$> "```")
+    additional <- A.takeWhile (== sym)
+    restOfHeader <- mappend <$> takeTill isEndOfLine <*> eol
+    let blockTerm = do
+                term   <- string (st <> additional)
+                extras <- A.takeWhile (== sym)
+                end <- eol
+                return (term<>extras<>end)
+    let bodyContent = do
+                content <- takeTill (==sym)
+                more <- eitherP blockTerm (T.cons <$> char sym <*> bodyContent)
+                case more of
+                    Left  theEnd  -> return (content<>theEnd)
+                    Right hadMore -> return (content<>hadMore) 
+    bodyContent' <- bodyContent
+    return $ body (st<>additional<>restOfHeader<>bodyContent')
 
 heading :: Parser Tokened
 heading = do
@@ -54,6 +77,8 @@ line = do
     crud     <- takeTill (=='\n')
     e        <- eol<|>(endOfInput*>return "")
     when (T.isPrefixOf "#" crud) (fail "Unexpected header")
+    when (T.isPrefixOf "~~~" crud) (fail "Unexpected codeblock")
+    when (T.isPrefixOf "```" crud) (fail "Unexpected codeblock")
     when (T.null crud&&T.null e) (fail "expected a line")
     return (mempty`feed`crud`feed`e,crud<>e) 
 
