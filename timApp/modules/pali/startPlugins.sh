e65d2ee20ca57f3c523f26a155aea2ef05e763c1
Vesa Lappalainen <vesal@jyu.fi>
2015-04-21 00:53:08 +0300

    muotoilua

--- timApp/modules/pali/startPlugins.sh
+++ timApp/modules/pali/startPlugins.sh
@@ -5,4 +5,8 @@ docker stop pali
 docker rm pali 
 
 # Start showFile-plugin
-docker run --name pali -p 61000:5000 -v /opt/tim/timApp/modules/pali:/pali/:ro -d -t -i pali /bin/bash -c 'cd /pali && python3 pali.py ; /bin/bash'
+docker run --name pali\
+           -p 61000:5000\
+           -v /opt/tim/timApp/modules/pali:/pali/:ro\
+           -d -t -i pali \
+           /bin/bash -c 'cd /pali && python3 pali.py ; /bin/bash'
