4f9864e30489f55f5d7292d028d032edd30ec8fd
Vesa Lappalainen <vesal@jyu.fi>
2015-04-22 20:04:27 +0300

    cs/startPlugins

--- timApp/modules/cs/startPlugins.sh
+++ timApp/modules/cs/startPlugins.sh
@@ -16,6 +16,8 @@ mkdir /tmp/uhome/cs
 chmod 775 /tmp/uhome/cs
 mkdir /tmp/uhome/run
 chmod 777 /tmp/uhome/run
+mkdir /tmp/uhome/tmp
+chmod 777 /tmp/uhome/tmp
 
 sudo setfacl  -R -d -m m::rwx -m group::rwx -m other::rwx /tmp/uhome
 sudo setfacl  -R -d -m m::rwx -m group::rwx -m other::rwx /tmp/uhome/cs
@@ -36,6 +38,6 @@ sudo cp /opt/cs/jypeli/* /tmp/uhome/cs
 
 cd /opt/cs
 
-nohup /opt/cs/csdaemon.sh &
+# nohup /opt/cs/csdaemon.sh &
 # docker run --name csPlugin -d -p 56000:5000 -v /opt/cs:/cs/  -v /opt/cs/images/cs:/csimages/ -v /tmp/uhome:/tmp/ -w /cs cs3 /bin/bash -c '/cs/startAll.sh ; /bin/bash'
 docker run --name csPlugin -d -p 56000:5000 -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/bin/docker -v /opt/cs:/cs/:ro -v /opt/cs/images/cs:/csimages/ -v /tmp/uhome:/tmp/ -w /cs cs3 /bin/bash -c '/cs/startAll.sh ; /bin/bash'
