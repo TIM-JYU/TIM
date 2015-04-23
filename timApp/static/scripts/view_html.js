022ea487579afd16cdfa9e29d71f033e042e6f2d
Bek Eljurkaev <kaeljurk@student.jyu.fi>
2015-04-23 12:06:41 +0300

    the question creation form will now keep the question and answer values after the type of the question is changed

--- timApp/static/scripts/view_html.js
+++ timApp/static/scripts/view_html.js
@@ -717,28 +717,36 @@ timApp.controller("QuestionController", ['$scope', '$http', function (scope, htt
 
     scope.createRowMatrix = function (rowsCount, columnsCount, type) {
 
+        if (scope.rows.length > rowsCount) rowsCount = scope.rows.length;
+
         for (var i = 0; i < rowsCount; i++) {
             var columns = [];
-            for (var j = 0; j < columnsCount; j++) {
+            if (scope.rows[i]) columns = scope.rows[i].columns;
+            if (columnsCount < columns.length) columns.splice(columnsCount, columns.length);
+
+            for (var j = columns.length; j < columnsCount; j++) {
                 columns[j] = {
                     id: j,
                     rowId: i,
-                    text: 'test',
+                    text: '',
                     questionPlaceholder: 'column',
                     type: 'answer',
                     value: 'textfield'
                 }
             }
+
+            var value = '';
+            if (i < scope.rows.length) value = scope.rows[i].value;
+
             scope.rows[i] = {
                 id: i,
                 text: 'test',
                 type: 'question',
-                //TODO get question text
-                value: '',
+                value: value,
                 columns: columns
             }
-
         }
+
     };
 
     scope.createColumnMatrix = function (rowsCount, columnsCount, type) {
