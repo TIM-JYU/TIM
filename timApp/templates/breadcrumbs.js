08b0e29378bb0f0e62a1c69f5924b4ed34357141
Tomi Karppinen <tomi.j.karppinen@jyu.fi>
2015-05-06 03:22:11 +0300

    Breadcrumb label bug fix

--- timApp/templates/breadcrumbs.js
+++ timApp/templates/breadcrumbs.js
@@ -3,7 +3,7 @@ function splitPath(path) {
     var relstart = 0;
     for (i = 0; i < path.length; i++) {
         if (path[i] == '/') {
-            result.push({name: path.substr(relstart, i), fullname: path.substr(0, i)});
+            result.push({name: path.substr(relstart, i - relstart), fullname: path.substr(0, i)});
             relstart = i + 1;
         }
     }
