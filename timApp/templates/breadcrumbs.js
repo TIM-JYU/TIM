e60cfb6466e71c186d9098bd5b9a2e358f4db156
Tomi Karppinen <tomi.j.karppinen@jyu.fi>
2015-05-06 03:04:07 +0300

    Added missing files

---
+++ timApp/templates/breadcrumbs.js
@@ -0,0 +1,19 @@
+function splitPath(path) {
+    var result = [];
+    var relstart = 0;
+    for (i = 0; i < path.length; i++) {
+        if (path[i] == '/') {
+            result.push({name: path.substr(relstart, i), fullname: path.substr(0, i)});
+            relstart = i + 1;
+        }
+    }
+    if (relstart < i)
+        result.push({name: path.substr(relstart, i), fullname: path.substr(0, i)});
+    return result;
+}
+
+function getBreadcrumbs(folder) {
+   var crumbs = splitPath(folder);
+   crumbs.unshift({name: '[root]', fullname: ''});
+   return crumbs;
+}
