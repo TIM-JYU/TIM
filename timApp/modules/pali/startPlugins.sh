f747dbbb6fedf2e0cadc2eba9eb9e8233852e2b4
Vesa Lappalainen <vesal@jyu.fi>
2015-05-19 00:31:29 +0300

    ro away from log

--- timApp/modules/pali/startPlugins.sh
+++ timApp/modules/pali/startPlugins.sh
@@ -12,6 +12,6 @@ docker run --name pali\
            -p 61000:5000\
            -v /opt/tim/timApp/modules/pali:/pali/:ro\
            -v /opt/tim/timApp/modules/py:/py/:ro\
-           -v /var/log/pali:/var/opt/:ro\
+           -v /var/log/pali:/var/opt/\
            -d -t -i pali \
            /bin/bash -c 'cd /pali && python3 pali.py ; /bin/bash'
