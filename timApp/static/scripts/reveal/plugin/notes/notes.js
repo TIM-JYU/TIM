6f3803708f0536a43cf0ea5904e2fb67e31bbd43
Juuso Valkeejärvi <juvalkee@student.jyu.fi>
2015-06-02 18:55:04 +0300

    Avoiding unnescessary updating of slides when using a remote

--- timApp/static/scripts/reveal/plugin/notes/notes.js
+++ timApp/static/scripts/reveal/plugin/notes/notes.js
@@ -12,17 +12,8 @@
 var RevealNotes = (function() {
 
 	function openNotes() {
-		var jsFileLocation = document.querySelector('script[src$="notes.js"]').src;  // this js file path
-		jsFileLocation = jsFileLocation.replace(/notes\.js(\?.*)?$/, '');   // the js folder path
-		var notesPopup = window.open( jsFileLocation + 'notes.html', 'reveal.js - Notes', 'width=1100,height=700' );
 
-		/**
-		 * Connect to the notes window through a postmessage handshake.
-		 * Using postmessage enables us to work in situations where the
-		 * origins differ, such as a presentation being opened from the
-		 * file system.
-		 */
-		function connect() {
+        function connect() {
 			// Keep trying to connect until we get a 'connected' message back
 			var connectInterval = setInterval( function() {
 				notesPopup.postMessage( JSON.stringify( {
@@ -41,6 +32,18 @@ var RevealNotes = (function() {
 				}
 			} );
 		}
+
+        var now = Date.now();
+		var jsFileLocation = document.querySelector('script[src$="notes.js"]').src;  // this js file path
+		jsFileLocation = jsFileLocation.replace(/notes\.js(\?.*)?$/, '');   // the js folder path
+		var notesPopup = window.open( jsFileLocation + 'notes.html?time=' + now, 'reveal.js - Notes', 'width=1100,height=700' );
+		/**
+		 * Connect to the notes window through a postmessage handshake.
+		 * Using postmessage enables us to work in situations where the
+		 * origins differ, such as a presentation being opened from the
+		 * file system.
+		 */
+
 
 		/**
 		 * Posts the current slide data to the notes window
