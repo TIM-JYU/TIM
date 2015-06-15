89b56588896eed896cc6c139d2034b09bfc3de18
Matias Berg <majukabe@student.jyu.fi>
2015-06-15 15:43:00 +0300

    Attempt #1 at making the popups undraggable on WP

--- timApp/static/scripts/directives/popUpDialog.js
+++ timApp/static/scripts/directives/popUpDialog.js
@@ -11,11 +11,18 @@
  */
 
 timApp.directive('popUpDialog', function () {
+    var toDragOrNot = function(){
+        var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+        if(width> 500)
+        {
+            return "tim-draggable-fixed";
+        }
+    };
     return {
         restrict: 'E',
         template: "<div class='pop-up' ng-show='show' id='popUpBack'>"+
         "<div class='pop-up-overlay'></div> " +
-        "<div class='pop-up-dialog' tim-draggable-fixed ng-mousedown='checkDown($event)' ng-mouseup='checkUp($event)' style='top:0px; left:0px;'>" +
+        "<div class='pop-up-dialog' " + toDragOrNot() + " ng-mousedown='checkDown($event)' ng-mouseup='checkUp($event)' style='top:0px; left:0px;'>" +
         "<div class='pop-up-dialog-content' ng-transclude></div>" +
         "</div>" +
         "</div>",
