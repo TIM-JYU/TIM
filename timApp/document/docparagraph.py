334473cca41529d07a1e47c60c5b2b4a9d221f2f
Mika Lehtinen <mikkalle@student.jyu.fi>
2015-06-16 15:54:56 +0300

    Added a constructor argument for attrs.

--- timApp/document/docparagraph.py
+++ timApp/document/docparagraph.py
@@ -2,11 +2,13 @@ from contracts import new_contract
 
 
 class DocParagraph:
-    def __init__(self, md, html, par_id, t):
+    def __init__(self, md, html, par_id, t, attrs=None):
+        if not attrs:
+            attrs = {}
         self.md = md
         self.html = html
         self.id = par_id
         self.t = t
-        self.attrs = {}
+        self.attrs = attrs
 
 new_contract('DocParagraph', DocParagraph)
