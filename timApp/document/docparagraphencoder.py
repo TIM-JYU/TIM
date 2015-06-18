9d134301a3344717b1834b8181fefa75cb385a09
Mika Lehtinen <mikkalle@student.jyu.fi>
2015-06-18 16:06:19 +0300

    Fixed DocParagraphEncoder to work with the updated DocParagraph.

--- timApp/document/docparagraphencoder.py
+++ timApp/document/docparagraphencoder.py
@@ -5,4 +5,4 @@ from document.docparagraph import DocParagraph
 class DocParagraphEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, DocParagraph):
-            return {'md': o.md, 'html': o.html, 't': o.t, 'id': o.id}
+            return {'md': o.getMarkdown(), 'html': o.getHtml(), 't': o.getHash(), 'id': o.getId()}
