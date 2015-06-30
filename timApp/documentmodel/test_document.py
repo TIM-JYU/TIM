5bbf6ca58903f724b2d02077d5a8b5850d919bbd
Mika Lehtinen <mikkalle@student.jyu.fi>
2015-06-30 14:05:08 +0300

    Added initial test for Document class.

---
+++ timApp/documentmodel/test_document.py
@@ -0,0 +1,26 @@
+import os
+import unittest
+import shutil
+from documentmodel.document import Document
+from filemodehelper import change_permission_and_retry
+
+
+class DocumentTest(unittest.TestCase):
+    files_root = 'doctest_files'
+
+    @classmethod
+    def setUpClass(cls):
+        if os.path.exists(DocumentTest.files_root):
+            shutil.rmtree(DocumentTest.files_root, onerror=change_permission_and_retry)
+
+    def test_document(self):
+        d = Document(doc_id=1, files_root=DocumentTest.files_root)
+        self.assertFalse(Document.exists(1))
+        self.assertEqual(2, Document.getNextFreeId(self.files_root))
+        par = d.addParagraph('testing')
+        self.assertTrue(d.hasParagraph(par.getId()))
+        d.deleteParagraph(par.getId())
+        self.assertFalse(d.hasParagraph(par.getId()))
+
+if __name__ == '__main__':
+    unittest.main()
