e122a0b4c191f60d366eb181f79b4503cdd46588
Mika Lehtinen <mikkalle@student.jyu.fi>
2015-07-21 11:21:48 +0300

    Added u to the list of allowed tags.

--- timApp/htmlSanitize.py
+++ timApp/htmlSanitize.py
@@ -42,7 +42,8 @@ TIM_SAFE_TAGS = ['a',
                  'colgroup',
                  'col',
                  'sub',
-                 'sup']
+                 'sup',
+                 'u']
 
 TIM_SAFE_ATTRS_MAP = {'*': ['class', 'id', 'align'],
                       'video': ['src', 'controls'],
