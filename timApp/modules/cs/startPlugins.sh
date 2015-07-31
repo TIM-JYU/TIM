d06523ce6e0daa600dd5aba0264c2a8136d4c6fc
Vesa Lappalainen <vesal@jyu.fi>
2015-07-31 12:38:16 +0300

    mkdir komentoihin -p

--- timApp/modules/cs/startPlugins.sh
+++ timApp/modules/cs/startPlugins.sh
@@ -8,18 +8,17 @@ docker rm csPlugin
 sudo setfacl  -R -d -m m::rwx -m group::rwx -m other::rwx /tmp
 
 sudo chmod 777 /tmp
-sudo mkdir /tmp/uhome
+sudo mkdir -p /tmp/uhome
 sudo chmod 777 /tmp/uhome 
 # sudo mkdir /tmp/uhome/cs
 # sudo chmod 777 /tmp/uhome/cs
-sudo mkdir /tmp/uhome/run
+sudo mkdir -p /tmp/uhome/run
 sudo chmod 777 /tmp/uhome/run
-sudo mkdir /tmp/uhome/tmp
+sudo mkdir -p /tmp/uhome/tmp
 sudo chmod 777 /tmp/uhome/tmp
-sudo mkdir /tmp/uhome/user
+sudo mkdir -p /tmp/uhome/user
 sudo chmod 777 /tmp/uhome/user
-sudo mkdir /opt/cs/images
-sudo mkdir /opt/cs/images/cs
+sudo mkdir -p /opt/cs/images/cs
 sudo chmod 777 /opt/cs/images/cs
 
 sudo setfacl  -R -d -m m::rwx -m group::rwx -m other::rwx /tmp/uhome
@@ -32,9 +31,9 @@ sudo chmod 766 /var/run/docker.sock
 
 sudo chmod 777 r
 
-mkdir /opt/cs/jypeli
+sudo mkdir -p /opt/cs/jypeli
 cd /opt/cs/jypeli
-curl http://kurssit.it.jyu.fi/npo/MonoJypeli/TIM/Jypeli.headless.tar.gz | sudo tar -xz --overwrite
+curl http://kurssit.it.jyu.fi/npo/MonoJypeli/TIM/Jypeli.headless.tar.gz | sudo tar -xz --overwrite --warning=none
 
 cd /opt/cs/java
 rm -f comtest*.jar*
@@ -42,11 +41,11 @@ wget https://svn.cc.jyu.fi/srv/svn/comtest/proto/vesa/trunk/comtest.jar -O comte
 wget https://svn.cc.jyu.fi/srv/svn/comtest/proto/vesa/trunk/comtestcpp.jar -O comtestcpp.jar -nv
 
 rm -f Graphics.jar*
-wget https://svn.cc.jyu.fi/srv/svn/ohj1/graphics/trunk/Graphics.jar -O Graphics.jar - nv
+wget https://svn.cc.jyu.fi/srv/svn/ohj1/graphics/trunk/Graphics.jar -O Graphics.jar -nv
 wget https://svn.cc.jyu.fi/srv/svn/ohj2/Ali/trunk/Ali.jar -O Ali.jar -nv
 
 
-sudo mkdir cs
+sudo mkdir -p cs
 sudo chmod 777 cs
 cd cs
 wget https://svn.cc.jyu.fi/srv/svn/comtest/proto/tojukarp/trunk/dist/ComTest.jar -O ComTest.jar -nv
