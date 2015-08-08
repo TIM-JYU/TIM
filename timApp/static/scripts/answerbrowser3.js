b6cd780d3ce97dfec15de135cd850ada9966202a
Vesa Lappalainen <vesal@jyu.fi>
2015-08-08 10:44:51 +0300

    Laiska answerbrowser ja ajoitus

---
+++ timApp/static/scripts/answerbrowser3.js
@@ -0,0 +1,231 @@
+timLogTime("answerbrowser3 load","answ");
+
+var angular, Waypoint;
+var timApp = angular.module('timApp');
+
+timApp.directive("answerbrowser", ['$upload', '$http', '$sce', '$compile', '$window',
+    function ($upload, $http, $sce, $compile, $window) {
+        "use strict";
+        timLogTime("answerbrowser directive function","answ");
+        return {
+            //templateUrl: "/static/templates/answerBrowser2.html",
+       		// template: timApp.directiveTemplateAnswerBrowser(),
+
+            restrict: 'E',
+            scope: {
+                taskId: '@'
+            },
+            
+            controller: function ($scope) {
+                timLogTime("answerbrowser ctrl function","answ",1);
+                $scope.compiled = false;
+            },
+            
+            link: function ($scope, $element, $attrs) {
+                timLogTime("answerbrowser link function","answ",1);
+                // $element.parents('.par').find('.parContent').html($compile(data.html)($scope));
+                var plugin = $element.parents('.par').find('.parContent')[0];
+                var origHtml = plugin.innerHTML;
+                plugin.innerHTML = "Kukkuu";
+                
+                $element.parent().on('mouseenter touchstart', function () {
+                    if ( $scope.compiled ) return;
+                    $scope.compiled = true;
+                    var newScope = $scope;
+                    var newHtml = '<answerbrowserreal task-id="' + $scope.taskId + '"></answerbrowserreal>';
+                    var newElement = $compile(newHtml);
+                    // $element.parentElement.insertBefore(newElement,$element.parentElement.firstChild);
+                    // $element.parents('.par').find('.parContent').html(newElement($scope));
+                    //$element.html(newElement($scope));
+                    var parent = $element.parents(".par")[0];
+                    parent.replaceChild(newElement($scope.$parent)[0],$element[0]);
+                    //parent.insertBefore(newElement($scope),parent.firstChild);
+                    //parent.firstChild = newElement($scope);
+                    /*
+                    $scope.answerBrowserScope.loadIfChanged();
+                    if ($scope.$parent.teacherMode && $scope.users === null) {
+                        $scope.users = [];
+                        if ($scope.$parent.users.length > 0) {
+                            $scope.answerBrowserScope.getAvailableUsers();
+                        }
+                    }
+                    */
+                });
+            }
+            
+            //function ($scope, $element, $attrs) {
+            //}
+        };
+    }]);
+
+
+
+timApp.directive("answerbrowserreal", ['$upload', '$http', '$sce', '$compile', '$window',
+    function ($upload, $http, $sce, $compile, $window) {
+        "use strict";
+        timLogTime("answerbrowserreal directive function","answ");
+        return {
+            templateUrl: "/static/templates/answerBrowser.html",
+            restrict: 'E',
+            scope: {
+                taskId: '@'
+            },
+            controller: function ($scope) {
+            },
+            link: function ($scope, $element, $attrs) {
+                //$scope.$parent = $scope.$parent; // muutos koska scope on syntynyt tuon toisen lapseksi
+                timLogTime("answerbrowserreal link","answ");
+                
+                $scope.$watch("taskId", function (newValue, oldValue) {
+                    if (newValue === oldValue) {
+                        return;
+                    }
+                    if ($scope.$parent.teacherMode) {
+                        $scope.getAvailableUsers();
+                    }
+                    $scope.getAvailableAnswers();
+                });
+
+                $scope.loading = 0;
+                $scope.changeAnswer = function () {
+                    $scope.points = $scope.selectedAnswer.points;
+                    var $par = $element.parents('.par');
+                    var par_id = $scope.$parent.getParIndex($par);
+                    $scope.loading++;
+                    $http.get('/getState', {
+                        params: {
+                            doc_id: $scope.$parent.docId,
+                            par_id: par_id,
+                            user_id: $scope.user.id,
+                            state: $scope.selectedAnswer.content
+                        }
+                    }).success(function (data, status, headers, config) {
+                        $element.parents('.par').find('.parContent').html($compile(data.html)($scope));
+                    }).error(function (data, status, headers, config) {
+                        $window.alert('Error getting answers: ' + data.error);
+                    }).finally(function () {
+                        $scope.loading--;
+                    });
+                };
+
+                $scope.next = function () {
+                    var newIndex = $scope.answers.indexOf($scope.selectedAnswer) - 1;
+                    if (newIndex < 0) {
+                        newIndex = $scope.answers.length - 1;
+                    }
+                    $scope.selectedAnswer = $scope.answers[newIndex];
+                    $scope.changeAnswer();
+                };
+
+                $scope.previous = function () {
+                    var newIndex = $scope.answers.indexOf($scope.selectedAnswer) + 1;
+                    if (newIndex >= $scope.answers.length) {
+                        newIndex = 0;
+                    }
+                    $scope.selectedAnswer = $scope.answers[newIndex];
+                    $scope.changeAnswer();
+                };
+
+                $scope.setNewest = function () {
+                    $scope.selectedAnswer = $scope.answers[0];
+                    $scope.changeAnswer();
+                };
+
+                $scope.getTeacherData = function () {
+                    if ($scope.answers.length > 0)
+                        return {
+                            answer_id: $scope.selectedAnswer.id,
+                            saveTeacher: $scope.saveTeacher,
+                            teacher: true,
+                            points: $scope.points
+                        };
+                    else
+                        return {
+                            saveTeacher: false,
+                            teacher: true
+                        };
+                };
+
+                $scope.getAvailableUsers = function () {
+                    $scope.loading++;
+                    $http.get('/getTaskUsers/' + $scope.taskId, {params: {group: $scope.$parent.group}})
+                        .success(function (data, status, headers, config) {
+                            $scope.users = data;
+                        }).error(function (data, status, headers, config) {
+                            $window.alert('Error getting users: ' + data.error);
+                        }).finally(function () {
+                            $scope.loading--;
+                        });
+                };
+
+                $scope.getAvailableAnswers = function (updateHtml) {
+                    updateHtml = (typeof updateHtml === "undefined") ? true : updateHtml;
+                    if (!$scope.$parent.rights.browse_own_answers) {
+                        return;
+                    }
+                    if ($scope.user === null) {
+                        return;
+                    }
+                    $scope.loading++;
+                    $http.get('/answers/' + $scope.taskId + '/' + $scope.user.id)
+                        .success(function (data, status, headers, config) {
+                            $scope.answers = data;
+                            if ($scope.answers.length > 0) {
+                                $scope.selectedAnswer = $scope.answers[0];
+                                $scope.points = $scope.selectedAnswer.points;
+                                if (updateHtml) {
+                                    $scope.changeAnswer();
+                                }
+                            }
+                        }).error(function (data, status, headers, config) {
+                            $window.alert('Error getting answers: ' + data.error);
+                        }).finally(function () {
+                            $scope.loading--;
+                        });
+                };
+
+                $scope.$on('answerSaved', function (event, args) {
+                    if (args.taskId === $scope.taskId) {
+                        $scope.getAvailableAnswers(false);
+                    }
+                });
+
+                $scope.$on('userChanged', function (event, args) {
+                    $scope.user = args.user;
+                    $scope.changed = true;
+                    $scope.firstLoad = false;
+                    $scope.shouldUpdateHtml = true;
+                });
+
+                $scope.loadIfChanged = function () {
+                    if ($scope.changed) {
+                        $scope.getAvailableAnswers($scope.shouldUpdateHtml);
+                        $scope.changed = false;
+                        $scope.firstLoad = false;
+                        $scope.shouldUpdateHtml = false;
+                    }
+                };
+
+                if ($scope.$parent.users.length > 0) {
+                    $scope.user = $scope.$parent.users[0];
+                } else {
+                    $scope.user = null;
+                }
+
+                $scope.changed = true;
+                $scope.firstLoad = true;
+                $scope.shouldUpdateHtml = false;
+                $scope.saveTeacher = false;
+                $scope.users = null;
+                //$element.parent().on('mouseenter touchstart', function () {
+                    $scope.loadIfChanged();
+                    if ($scope.$parent.teacherMode && $scope.users === null) {
+                        $scope.users = [];
+                        if ($scope.$parent.users.length > 0) {
+                            $scope.getAvailableUsers();
+                        }
+                    }
+                // });
+            }
+        };
+    }]);
---
+++ timApp/static/scripts/timTiming.js
@@ -0,0 +1,100 @@
+/*
+  To use timTiming, add to meta part of the document:
+  
+    <script src="{{ url_for('static', filename='scripts/timTiming.js') }}"></script>
+    <script>timLogInit("{{ request.query_string }}");</script>
+
+  before any othe script loadings.  Then when you want log timing, add:
+
+    <script>timLogTime("Document end","base");</script>
+  
+  to html (f.ex end of the whole document before </body>-tag
+  To JavaScript codes add the bare call to the timLogTime-function.
+  First parameter is printed as it is and secod is for filtering the output,
+  see timLogTime comments.  
+  
+*/  
+var timJavaScriptStartTime = new Date();
+var timJavaScriptLastTime = timJavaScriptStartTime;
+var timDivLogger;
+var timDivPreLogger = document.createElement("pre");
+
+document.addEventListener('DOMContentLoaded', function () {
+    timLogTime("Document loaded","main");
+});
+
+
+
+document.onreadystatechange = function () {
+    timLogTime("ready changed " + document.readyState,"main");
+    if ( timTimingDiv & !timDivLogger ) {
+        timDivLogger = document.createElement("div")
+        timDivLogger.appendChild(timDivPreLogger);
+        //document.body.appendChild(timDivLogger);
+        document.body.insertBefore(timDivLogger,document.body.firstChild);
+    }    
+};
+
+
+var timTimingDiv = false;
+var timTimingLog = false;
+var timTimingFilter;
+var timTiming = false;
+var timTimingSummary = false;
+var timTimingLevel = 0;
+
+function timLogInit(params) {
+    /*
+    .../view/1?timing=div    - logittaa vain diviin (vain taso 0 tai ei tasoa)
+    .../view/1?timing=log    - logittaa vain console.log
+    .../view/1?timing=log&timingLevel=1 - logittaa vain console.log, tasot 0-1
+    .../view/1?timing=log,div,summary - logit div ja konsolit ja vain summat
+    .../view/1?timing=log,div&timingFilter=cs - logit div,konsoli ja vain cs id:llä olevat.  filter on regexp
+    */
+    var p = params.replace("b&#39;","");
+    p = p.replace("&#39;","");
+    p = p.replace("&amp;","&");
+    var ps = p.split("&");
+    for (var i=0; i<ps.length; i++) {
+        var k = (ps[i]+"=").split("=");
+        var key = k[0];
+        var val = k[1];
+        if ( key === "timing" ) { 
+           if ( val.indexOf("div") >= 0 ) timTimingDiv = true;
+           if ( val.indexOf("log") >= 0 ) timTimingLog = true;
+           if ( val.indexOf("summary") >= 0 ) timTimingSummary = true;
+        }   
+        if ( key === "timingFilter" ) timTimingFilter = val;
+        if ( key === "timingLevel" ) timTimingLevel = val;
+    }
+    timTiming = timTimingDiv || timTimingLog;
+    timLogTime("Tim log initialized","main");
+}    
+
+var timLogText = "";
+
+function timLogTime(msg,id,level) {
+    if ( !timTiming ) return;
+    if ( timTimingFilter && !id.match(timTimingFilter) ) return;
+    var tlevel = level | 0;
+    if ( tlevel > timTimingLevel ) return;
+    var sid = "    ";
+    if ( id ) sid = (id+"   ").substring(0, 4);
+    var d = new Date();       
+    var diff = d - timJavaScriptStartTime;
+    var diffLast = d - timJavaScriptLastTime;
+    if ( timTimingSummary ) {
+        timLogText = "";
+        str = diff.valueOf();
+    }    
+    else 
+        str = sid+": " + d.toLocaleTimeString()+ " " + diff.valueOf() + " " + diffLast.valueOf() + " - " + msg;
+    if ( timTimingLog ) console.log(str);  
+    if ( timTimingDiv ) {
+        timLogText += str +"\r\n";     
+        timDivPreLogger.innerHTML = "<code>"+timLogText+"</code>"; // IE:n takia pitää kikkailla näin, innerText ei toimi.
+    }    
+    timJavaScriptLastTime = d;
+}   
+
+
