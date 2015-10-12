1bc79b97f5ceb10b699216ede5b985070e930468
Mika Lehtinen <mikkalle@student.jyu.fi>
2015-10-12 21:57:58 +0300

    gunicorn: Don't limit request line

--- timApp/gunicornconf.py
+++ timApp/gunicornconf.py
@@ -3,3 +3,4 @@ import multiprocessing
 bind = "0.0.0.0:5000"
 workers = multiprocessing.cpu_count() * 2 + 1  # Recommended value is cpu_count() * 2 + 1
 worker_class = "gevent"  # Possible values: sync, gevent, eventlet, tornado, gaiohttp, gthread
+limit_request_line = 0
