3674e522910c52d585c1986307f280bc486ed85e
Juuso Valkeejärvi <juvalkee@student.jyu.fi>
2015-10-24 23:23:23 +0300

    Fix: postgre_query.sh not working with older docker version in tim-beta

--- postgre_query.sh
+++ postgre_query.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm --link postgre -t -i postgre /bin/bash -c 'psql -h postgre -p 5432 -d docker -U docker --password'
+docker run --rm --link postgre:postgre -t -i postgre /bin/bash -c 'psql -h postgre -p 5432 -d docker -U docker --password'
