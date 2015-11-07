02b5fd4f2bdead5350553d2b135d3070b652f209
Tomi Karppinen <tomi.j.karppinen@jyu.fi>
2015-11-07 09:54:45 +0200

    Updated dockerfiles & run_tests.sh
    TIM dockerfile: use older pycontracts because of a bug in the
    latest one's install script.
    
    Postgre dockerfile: set & generate utf8 locale. Default might be
    set to POSIX/C which means the server can't start...
    
    run_tests.sh: TIM_NAME environment variable set to timtest.

--- run_tests.sh
+++ run_tests.sh
@@ -2,5 +2,6 @@
 
 docker run --rm -v /opt/tim/:/service -t -i tim:$(./get_latest_date.sh) /bin/bash -c \
  "cd /service/timApp &&
+ export TIM_NAME=timtest &&
  python3 -c 'import dumboclient; dumboclient.launch_dumbo()' &&
  python3 -m unittest"
