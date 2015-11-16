a83db020694d4b938e45a4d38264bf8cf35c101e
Mika Lehtinen <mikkalle@student.jyu.fi>
2015-11-16 15:59:52 +0200

    Improve unit test
    * Add a failing test for checking {% ... %} macro expansion
    * Refactoring

--- timApp/test_tim.py
+++ timApp/test_tim.py
@@ -2,13 +2,15 @@
 
 import json
 import unittest
-
 from flask import session
-
 import tim
 from documentmodel.document import Document
 from markdownconverter import md_to_html
 from timdbtest import TimDbTest
+
+
+def load_json(resp):
+    return json.loads(resp.get_data(as_text=True))
 
 
 class TimTest(TimDbTest):
@@ -23,11 +25,16 @@ class TimTest(TimDbTest):
         cls.app.jreq = lambda url, data=None, method='GET': cls.json_req(cls.app, url, data, method)
 
     def assertResponseStatus(self, resp, expect_status=200):
-        self.assertEqual(expect_status, resp.status_code)
+        self.assertEqual(expect_status, resp.status_code, msg=resp.get_data(as_text=True))
 
-    def assertInResponse(self, expected, resp, expect_status=200):
+    def assertInResponse(self, expected, resp, expect_status=200, json_key=None):
+        resp_text = resp.get_data(as_text=True)
         self.assertResponseStatus(resp, expect_status)
-        self.assertIn(expected, resp.get_data(as_text=True))
+        if json_key is not None:
+            resp_text = json.loads(resp_text)[json_key]
+        self.assertIn(expected, resp_text,
+                      msg="""\n--------THIS TEXT:--------\n{}\n--------WAS NOT FOUND IN:---------\n{}""".format(
+                          expected, resp_text))
 
     def assertManyInResponse(self, expecteds, resp, expect_status=200):
         for e in expecteds:
@@ -39,7 +46,7 @@ class TimTest(TimDbTest):
 
     def assertDictResponse(self, expected, resp, expect_status=200):
         self.assertEqual(expect_status, resp.status_code)
-        self.assertDictEqual(expected, json.loads(resp.get_data(as_text=True)))
+        self.assertDictEqual(expected, load_json(resp))
 
     @staticmethod
     def json_put(app, url, json_data=None):
@@ -60,9 +67,7 @@ class TimTest(TimDbTest):
         timdb = self.get_db()
         a = TimTest.app
 
-        login_resp = a.post('/altlogin',
-                            data={'email': 'test1@example.com', 'password': 'test1pass'},
-                            follow_redirects=True)
+        login_resp = self.login_test1(a)
         self.assertInResponse('Logged in as: Test user 1 (testuser1)', login_resp)
         doc_names = ['users/testuser1/testing',
                      'users/testuser1/testing2',
@@ -100,38 +105,19 @@ class TimTest(TimDbTest):
         self.assertInResponse(html_comment_of_test1, a.get('/view/' + doc_name))
         edit_text = 'testing editing now...\nnew line\n'
         par_html = md_to_html(edit_text)
-        self.assertInResponse(par_html, a.jpost('/postParagraph/', {
-            "text": edit_text,
-            "docId": doc_id,
-            "par": first_id,
-            "par_next": None
-        }))
+        self.assertInResponse(par_html, self.post_par(a, doc_id, edit_text, first_id))
         self.assertDictResponse({'text': edit_text}, a.get('/getBlock/{}/{}'.format(doc_id, first_id)))
-        self.assertInResponse(par_html, a.jpost('/postParagraph/', {
-            "text": edit_text,
-            "docId": doc_id,
-            "par": first_id,
-            "par_next": None
-        }))
+        self.assertInResponse(par_html, self.post_par(a, doc_id, edit_text, first_id))
         par2_text = 'new par'
         par2_html = md_to_html(par2_text)
-        self.assertManyInResponse([par_html, par2_html], a.jpost('/postParagraph/', {
-            "text": edit_text + '#-\n' + par2_text,
-            "docId": doc_id,
-            "par": first_id,
-            "par_next": None
-        }))
+        self.assertManyInResponse([par_html, par2_html],
+                                  self.post_par(a, doc_id, edit_text + '#-\n' + par2_text, first_id))
         pars = Document(doc_id).get_paragraphs()
         self.assertEqual(2, len(pars))
         second_par_id = pars[1].get_id()
         par2_new_text = '    ' + par2_text
         par2_new_html = md_to_html(par2_new_text)
-        self.assertInResponse(par2_new_html, a.jpost('/postParagraph/', {
-            "text": par2_new_text,
-            "docId": doc_id,
-            "par": second_par_id,
-            "par_next": None
-        }))
+        self.assertInResponse(par2_new_html, self.post_par(a, doc_id, par2_new_text, second_par_id))
         self.assertResponseStatus(a.post('/logout', follow_redirects=True))
         self.assertResponseStatus(a.get('/settings/'), 403)
         for d in doc_ids - {3}:
@@ -165,9 +151,7 @@ class TimTest(TimDbTest):
             self.assertEqual(1, len(notes))
             test2_note_id = notes[0]['id']
 
-        a.post('/altlogin',
-               data={'email': 'test1@example.com', 'password': 'test1pass'},
-               follow_redirects=True)
+        self.login_test1(a)
         self.assertInResponse(comment_of_test2,
                               a.get('/note/{}'.format(test2_note_id)))
         with a as a:
@@ -186,6 +170,59 @@ class TimTest(TimDbTest):
                                       query_string={'docId': doc_id,
                                                     'area_start': first_id,
                                                     'area_end': second_par_id}))
+
+    def post_par(self, a, doc_id, text, par_id):
+        return a.jpost('/postParagraph/', {
+            "text": text,
+            "docId": doc_id,
+            "par": par_id,
+            "par_next": None
+        })
+
+    def new_par(self, a, doc_id, text, next_id):
+        return a.jpost('/newParagraph/', {
+            "text": text,
+            "docId": doc_id,
+            "par_next": next_id
+        })
+
+    def login_test1(self, a):
+        return a.post('/altlogin',
+                      data={'email': 'test1@example.com', 'password': 'test1pass'},
+                      follow_redirects=True)
+
+    def test_macro_doc(self):
+        a = TimTest.app
+        self.login_test1(a)
+        docname = 'users/testuser1/macrotest'
+        resp = load_json(a.jpost('/createDocument', {
+            'doc_name': docname
+        }))
+        doc = Document(resp['id'])
+        self.new_par(a, doc.doc_id, """
+``` {settings=""}
+macro_delimiter: "%%"
+macros:
+ rivi: kerros
+```
+""", None)
+        table_text = """
+{% set sarakeleveys = 50 %}
+{% set sarakkeet = ['eka', 'toka', 'kolmas', 'neljäs'] %}
+{% set rivit = ['eka', 'toka', 'kolmas', 'neljäs', 'viides'] %}
+
+{% for x in sarakkeet %} %%'-'*sarakeleveys%% {% endfor %}
+
+{% for r in rivit %}
+{% for s in sarakkeet %}%%('{} {}, {} sarake').format(r,rivi,s).rjust(sarakeleveys)%% {% endfor %}
+
+{% endfor %}
+{% for x in sarakkeet %} %%'-'*sarakeleveys%% {% endfor %}
+        """
+        table_html = md_to_html(table_text, sanitize=True, macros={'rivi': 'kerros'}, macro_delimiter='%%')
+
+        self.assertInResponse(table_html, self.new_par(a, doc.doc_id, table_text, None), json_key='texts')
+        self.assertInResponse(table_html, a.get('/view/' + docname))
 
 
 if __name__ == '__main__':
