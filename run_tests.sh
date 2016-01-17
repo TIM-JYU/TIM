5849f4dff6ecdb28119c839c975a87443e6c2be2
Mika Lehtinen <mikkalle@student.jyu.fi>
2016-01-17 14:06:33 +0200

    Add an explanation about the docker run parameters in run_tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# The cap-add and security-opt are for enabling ramdisk mounting inside the container
 docker run --cap-add SYS_ADMIN --security-opt apparmor:unconfined --rm -v /opt/tim/:/service -t -i tim:$(./get_latest_date.sh) /bin/bash -c \
  "cd /service/timApp &&
  export TIM_NAME=timtest &&
