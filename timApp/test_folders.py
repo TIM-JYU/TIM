e0015515af1b1a881a6dcc1a6018a4179fa645d1
Mika Lehtinen <mikkalle@student.jyu.fi>
2016-01-27 17:58:58 +0200

    Improve folder test
    Fetch folders both as owner and as anonymous user.

--- timApp/test_folders.py
+++ timApp/test_folders.py
@@ -6,15 +6,41 @@ class FolderTest(TimRouteTest):
     def test_folders(self):
         self.login_test1()
         db = self.get_db()
-        fname = "users/{}/testing".format(session['user_name'])
+        user_folder = 'users/{}'.format(session['user_name'])
+        fname = "{}/testing".format(user_folder)
+        personal_group_id = db.users.get_personal_usergroup_by_id(session['user_id'])
         resp = self.json_post('/createFolder',
                               {"name": fname,
-                               "owner": db.users.get_personal_usergroup_by_id(session['user_id'])})
+                               "owner": personal_group_id})
         j = self.assertResponseStatus(resp, return_json=True)
         self.assertEqual(fname, j['name'])
         self.assertIsInstance(j['id'], int)
         new_name = fname + '1'
         resp = self.json_put('/rename/{}'.format(j['id']), {"new_name": new_name})
-        j = self.assertResponseStatus(resp, return_json=True)
-        self.assertEqual(new_name, j['new_name'])
+        j2 = self.assertResponseStatus(resp, return_json=True)
+        self.assertEqual(new_name, j2['new_name'])
+
+        # Create another folder and give access to anonymous users
+        fname2 = "{}/{}".format(user_folder, 'testing2')
+        resp = self.json_post('/createFolder',
+                              {"name": fname2,
+                               "owner": personal_group_id})
+        j3 = self.assertResponseStatus(resp, return_json=True)
+        db.users.grant_access(db.users.get_anon_group_id(), j3['id'], 'view')
+
+        self.assertListResponse([{'id': j['id'], 'name': 'testing1',
+                                  'isOwner': True,
+                                  'fullname': new_name,
+                                  'owner': {'id': 7, 'name': 'testuser1'}},
+                                 {'id': j3['id'], 'name': 'testing2',
+                                  'isOwner': True,
+                                  'fullname': fname2,
+                                  'owner': {'id': 7, 'name': 'testuser1'}}],
+                                self.json_req('/getFolders', query_string={'root_path': user_folder}))
+        self.logout()
+        self.assertListResponse([{'id': j3['id'], 'name': 'testing2',
+                                  'isOwner': False,
+                                  'fullname': fname2,
+                                  'owner': {'id': 7, 'name': 'testuser1'}}],
+                                self.json_req('/getFolders', query_string={'root_path': user_folder}))
         db.close()
