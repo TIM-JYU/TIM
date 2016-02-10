be1bbfbf8e2664665cffe9c21c0925492fa7a065
Mika Lehtinen <mikkalle@student.jyu.fi>
2016-02-11 00:25:37 +0200

    Fix search route not working due to missing template parameter, add tests

---
+++ timApp/test_search.py
@@ -0,0 +1,16 @@
+from timroutetest import TimRouteTest
+
+
+class SearchTest(TimRouteTest):
+    def test_search(self):
+        self.login_test1()
+        text_to_search = 'Text to search'
+        self.create_doc(initial_par=text_to_search)
+        resp = self.get('/search/' + text_to_search, as_tree=True)
+        pars = resp.findall('.//div[@class="par"]/div[@class="parContent"]/p')
+        self.assertEqual(1, len(pars))
+        self.assertEqual(text_to_search, pars[0].text)
+
+    def test_failed_search(self):
+        self.login_test1()
+        self.assertResponseStatus(self.app.get('/search/aa'), expect_status=400)
