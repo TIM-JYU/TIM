f0999381b50ef6d8406cb91ccc888c516937a94b
Tomi Karppinen <tomi.j.karppinen@jyu.fi>
2016-02-18 18:23:39 +0200

    run_tests.sh (funnel) runs the tests in container

--- funnel/run_tests.sh
+++ funnel/run_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 -m unittest test_mailer.py
+docker run --net=timnet --rm -v $PWD:/service -t -i funnel:$(../get_latest_date.sh) /bin/bash -c "cd /service && python3 -m unittest"
