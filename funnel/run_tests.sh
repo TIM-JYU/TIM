4dfd8ea613e9abf2e59536631c5b4d33a8b898d1
Tomi Karppinen <tomi.j.karppinen@jyu.fi>
2016-02-18 19:11:34 +0200

    Fixed image version in test run script

--- funnel/run_tests.sh
+++ funnel/run_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --net=timnet --rm -v $PWD:/service -t -i funnel:$(../get_latest_date.sh) /bin/bash -c "cd /service && python3 -m unittest"
+docker run --net=timnet --rm -v $PWD:/service -t -i funnel /bin/bash -c "cd /service && python3 -m unittest"
