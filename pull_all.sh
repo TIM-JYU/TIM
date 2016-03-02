9692dae57c9d949099fb56dc105fdda3423a11d6
Mika Lehtinen <mikkalle@student.jyu.fi>
2016-03-02 14:43:11 +0200

    Include funnel image in push and pull scripts

--- pull_all.sh
+++ pull_all.sh
@@ -13,6 +13,7 @@ docker pull timimages/stackage_builder
 docker pull timimages/postgre
 docker pull timimages/pali
 docker pull timimages/local_nginx
+docker pull timimages/funnel
 
 docker tag timimages/tim:$(./get_latest_date.sh) tim:$(./get_latest_date.sh)
 docker tag timimages/cs3 cs3
@@ -23,6 +24,7 @@ docker tag timimages/stackage_builder stackage_builder
 docker tag timimages/postgre postgre
 docker tag timimages/pali pali
 docker tag timimages/local_nginx local_nginx
+docker tag timimages/funnel funnel
 
 # Remove leftover untagged images
 docker rmi $(docker images | grep "^<none>" | awk '{print $3}')
--- push_all.sh
+++ push_all.sh
@@ -8,6 +8,7 @@ docker tag stackage_builder timimages/stackage_builder
 docker tag postgre timimages/postgre
 docker tag pali timimages/pali
 docker tag local_nginx timimages/local_nginx
+docker tag funnel timimages/funnel
 
 docker push timimages/tim:$(./get_latest_date.sh)
 docker push timimages/svn
@@ -18,3 +19,4 @@ docker push timimages/stackage_builder
 docker push timimages/postgre
 docker push timimages/pali
 docker push timimages/local_nginx
+docker push timimages/funnel
