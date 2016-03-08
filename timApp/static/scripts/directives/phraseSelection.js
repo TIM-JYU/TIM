1eb7a108fd8457f0096fb5a3e3718ca256583bf9
Tarvainen <sevitarv@jyu.fi>
2016-03-08 15:31:16 +0200

    phrase selection directive updated

--- timApp/static/scripts/directives/phraseSelection.js
+++ timApp/static/scripts/directives/phraseSelection.js
@@ -8,6 +8,8 @@
 var angular;
 var timApp = angular.module('timApp');
 
+var colorPalette = ["blueviolet", "darkcyan", "orange", "darkgray", "lightblue", "coral", "goldenrod", "blue"];
+
 /**
  * Angular directive for phrase selection
  */
@@ -18,9 +20,15 @@ timApp.directive('phraseSelection', function () {
     }
 });
 
+/**
+ * Controller for phrase selection
+ */
 timApp.controller('PhraseSelectionController', ['$scope', '$http', function ($scope, $http) {
     "use strict";
-    $scope.testi = "Oikea testi";
+
+    // Data
+    $scope.orderPhrase = 'tag';
+    $scope.labelsOn = true;
 
     $http.get('http://192.168.99.100/static/test_data/phrases.json').success(function (data) {
         $scope.phrases = data;
@@ -30,4 +38,49 @@ timApp.controller('PhraseSelectionController', ['$scope', '$http', function ($sc
         $scope.tags = data;
     });
 
+    // Methods
+    $scope.getColor = function (index) {
+        return colorPalette[index];
+    };
+
+    $scope.toggleTag = function (tag) {
+        tag.selected = !tag.selected;
+    };
+
+    $scope.toggleLabelShow = function () {
+        $scope.labelsOn = !$scope.labelsOn;
+    };
+
+    $scope.showLabels = function(){
+        if (!$scope.labelsOn)
+            return "hide";
+        return "";
+    }
+
 }]);
+
+
+timApp.filter('selectedTags', function () {
+    return function (phrases, tags) {
+        var selectedPhrases = new Set();
+        var selectedTags = [];
+        if (typeof tags != "undefined" && typeof phrases != "undefined") {
+
+            for (var i = 0; i < tags.length; i++) {
+                if (tags[i].selected)
+                    selectedTags.push(tags[i].id);
+            }
+            // return all phrases if no tags selected
+            if (selectedTags.length == 0)
+                return phrases;
+
+            for (var i = 0; i < phrases.length; i++) {
+                for (var j = 0; j < selectedTags.length; j++) {
+                    if (phrases[i].tags.indexOf(selectedTags[j]) != -1)
+                        selectedPhrases.add(phrases[i]);
+                }
+            }
+        }
+        return Array.from(selectedPhrases);
+    };
+});
