5a30f6883d7a2c153f0b5b787cd32dd0657b309d
Tomi Karppinen <tomi.j.karppinen@jyu.fi>
2016-03-15 21:26:16 +0200

    Edit workaround for reference heavy documents
    get_referenced_document_ids now returns just the source document
    id instead of recursively going through every paragraph.
    
    This may temporarily make some read markings and notes
    "disappear" but they're still there, the function just
    needs a faster implementation (caching?)

--- timApp/documentmodel/document.py
+++ timApp/documentmodel/document.py
@@ -643,6 +643,13 @@ class Document:
         """Gets all the document ids that are referenced from this document recursively.
         :return: The set of the document ids.
         """
+
+        # begin workaround
+        # todo: find a faster way to implement this function
+        src_doc = self.get_settings().get_source_document()
+        return set() if src_doc is None else {src_doc}
+        # end workaround
+
         refs = set()
         for p in self:
             if p.is_reference():
