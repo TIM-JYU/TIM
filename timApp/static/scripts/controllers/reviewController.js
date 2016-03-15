00e05547cdb0e86975e01f94996a5091fc2b9347
Tarvainen <sevitarv@jyu.fi>
2016-03-15 12:01:03 +0200

    Markings loaded from test data in UI

--- timApp/static/scripts/controllers/reviewController.js
+++ timApp/static/scripts/controllers/reviewController.js
@@ -1,15 +1,79 @@
 /**
  * Created by sevitarv on 8.3.2016.
- * This controller requires phraseSelection directive to work.
+ * NOTE: This controller requires phraseSelection directive to work.
  */
 
 var angular;
 var timApp = angular.module('timApp');
-var markingColorPalette = ["palegreen", "khaki", "lightpink"];
 console.log("reviewController.js added");
 
 timApp.controller("ReviewController", ['$scope', '$http', '$window', '$compile', function ($scope, $http, $window, $compile) {
     "use strict";
+
+    $scope.markingsAdded = false;
+
+    $http.get('http://192.168.99.100/static/test_data/markings.json').success(function (data) {
+        $scope.markings = data;
+       //$scope.loadMarkings();
+    });
+
+
+    /**
+     * Loads used markings into view
+     */
+    $scope.loadMarkings = function() {
+        var elements = document.getElementById("previewContent").getElementsByTagName("p");
+
+        if (elements.length > 0 && !$scope.markingsAdded && typeof $scope.markings != "undefined" && $scope.markings.length > 0) {
+
+            for (var i=$scope.markings.length-1;i>=0; i--){
+                var placeInfo = $scope.markings[i]["coord"];
+                console.log($scope.markings[i]);
+                var el = elements.item(placeInfo["el"]).childNodes[0];
+                var range = document.createRange();
+
+                range.setStart(el, placeInfo["start"]);
+                range.setEnd(el, placeInfo["end"]);
+
+                $scope.addMarkingToCoord(range, $scope.markings[i]["points"]);
+            }
+
+
+            /*
+            console.log($scope.markings[0]["coord"]);
+            var coord = $scope.markings[0]["coord"];
+
+            var startOffset = 0;
+
+            console.log(elements.length);
+                console.log(elements[0]);
+            console.log(elements[0].html());
+
+            range.setEnd(elements[0], 2);
+
+
+            $scope.addMarkingToCoord(elements[0].innerHTML, range);
+            */
+            $scope.markingsAdded = true;
+
+        }
+    };
+
+
+    /**
+     * Adds marking to given element on given coordinate
+     * @param el element
+     * @param start start coordinate
+     * @param end end coordinate
+     */
+    $scope.addMarkingToCoord = function(range, points){
+        var span = document.createElement("span");
+        span.classList.add("marking");
+        span.classList.add($scope.getMarkingHighlight(points));
+        $compile(span)($scope);
+
+        range.surroundContents(span);
+    };
 
     /**
      * Select text range
@@ -23,7 +87,11 @@ timApp.controller("ReviewController", ['$scope', '$http', '$window', '$compile',
         } else {
             $scope.selectedArea = undefined;
         }
+
+
     };
+
+
 
     /**
      * Get marking highlight style
@@ -48,19 +116,16 @@ timApp.controller("ReviewController", ['$scope', '$http', '$window', '$compile',
             /*
              var span = angular.element("<span></span>");
              console.log(span);
-             */
+            */
             //var span = $scope.createPopOverElement(phrase.content);
             var span = document.createElement("span");
             span.classList.add("marking");
             span.classList.add($scope.getMarkingHighlight(phrase.points));
-
             $compile(span)($scope);
 
             $scope.selectedArea.surroundContents(span);
             $scope.selectedArea = undefined;
             phrase.used += 1;
-
-            $scope.open(phrase.id);
         }
     };
 
