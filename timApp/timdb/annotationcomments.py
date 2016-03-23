167a7c3fe9aa6c25a5d33a7e1d11a1ba5f44ce05
Joonas Sakari Lattu <joonas.s.lattu@student.jyu.fi>
2016-03-23 14:19:15 +0200

    Add functions for basic table operations needed

--- timApp/timdb/annotationcomments.py
+++ timApp/timdb/annotationcomments.py
@@ -1,7 +1,20 @@
 from contracts import contract
 from timdb.timdbbase import TimDbBase
 
-class AnnotationAnswers(TimDbBase):
+class AnnotationComments(TimDbBase):
     @contract
-    def add_comment(self):
-        return
+    def add_comment(self, annotation_id: 'int', commenter_id: 'int', content: 'str'):
+        """
+        Adds new comment to annotation
+        :param annotation_id:
+        :param commenter_id:
+        :param content:
+        :return:
+        """
+        cursor = self.db.cursor()
+        cursor.execute("""
+                      INSERT INTO
+                      Comment(annotation_id, commenter_id, content)
+                      VALUES (?, ?, ?)
+                      """, [annotation_id, commenter_id, content]
+        )
