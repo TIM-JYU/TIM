85275f0de5f0ac26dc6209957a57beefc4cab621
Panu Petteri Palojarvi <petteri.p.palojarvi@student.jyu.fi>
2016-04-07 15:22:39 +0300

    new velp adding route now replaces the old

--- timApp/routes/velp.py
+++ timApp/routes/velp.py
@@ -88,7 +88,7 @@ def add_velp(velp_content: str = "MOIMOI", default_points: int = -5.0, language_
 
 
 # Might replace /addvelp route as the actual route.
-@velps.route("/addvelp2", methods=['POST'])
+@velps.route("/addvelp", methods=['POST'])
 def add_velp2():
     velp_content = request.form.get('content')
     default_points = request.form.get('points')
