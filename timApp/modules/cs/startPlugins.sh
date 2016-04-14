c54ca7c88446b31e0af41ee877b6cfbcaad41c49
Panu Petteri Palojarvi <petteri.p.palojarvi@student.jyu.fi>
2016-04-14 17:21:57 +0300

    Somehow there were git conflict markers commited to this file. They caused syntax errors when running it.

--- timApp/modules/cs/startPlugins.sh
+++ timApp/modules/cs/startPlugins.sh
@@ -3,14 +3,7 @@
 # run with option i to get interactive mode
 # option p for pure start (no wget for files)
 dockername="csPlugin"
-<<<<<<< HEAD
 dockerOptions="--name $dockername --net=timnet -p 56000:5000 -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/bin/docker -v /opt/cs:/cs/:ro -v /opt/cs/images/cs:/csimages/ -v /tmp/uhome:/tmp/ -w /cs cs3 /bin/bash"
-# dockerOptions="--name $dockername --net=timnet -p 56000:5000 -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/bin/docker -v /opt/cs:/cs/:ro -v /opt/cs/images/cs:/csimages/ -v /tmp/uhome:/tmp/ -w /cs cs3 /bin/bash"
-=======
-dockerOptions="--name $dockername --net=timnet -p 56000:5000 -v /lib/x86_64-linux-gnu:/lib/x86_64-linux-gnu:ro -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/bin/docker -v /opt/cs:/cs/:ro -v /opt/cs/images/cs:/csimages/ -v /tmp/uhome:/tmp/ -w /cs cs3 /bin/bash"
-# dockerOptions="--name $dockername --net=timnet -p 56000:5000                                                 -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/bin/docker -v /opt/cs:/cs/:ro -v /opt/cs/images/cs:/csimages/ -v /tmp/uhome:/tmp/ -w /cs cs3 /bin/bash"
-# dockerOptions="--name $dockername --net=timnet -p 56000:5000  -v /lib/x86_64-linux-gnu/libdevmapper.so.1.02.1:/lib/x86_64-linux-gnu/libdevmapper.so.1.02.1   -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/bin/docker -v /opt/cs:/cs/:ro -v /opt/cs/images/cs:/csimages/ -v /tmp/uhome:/tmp/ -w /cs cs3 /bin/bash"
->>>>>>> 5d750fa96795a21926add4f1b6ed207ce90481f1
 
 docker stop $dockername > /dev/null 2>&1
 docker rm $dockername > /dev/null 2>&1
