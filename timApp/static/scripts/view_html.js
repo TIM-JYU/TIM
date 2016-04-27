326b669ed414dad2a0efb60d04c060b179ae6763
Tomi Karppinen <tomi.j.karppinen@jyu.fi>
2016-04-28 00:36:04 +0300

    Moved menu items around, added Copy to normal mode

--- timApp/static/scripts/view_html.js
+++ timApp/static/scripts/view_html.js
@@ -1720,14 +1720,14 @@ timApp.controller("ViewCtrl", [
             } else {
                 return [
                     {func: sc.showNoteWindow, desc: 'Comment/note', show: sc.rights.can_comment},
-                    {func: sc.cutPar, desc: 'Cut paragraph', show: $window.editMode === 'par'},
-                    {func: sc.copyPar, desc: 'Copy paragraph', show: $window.editMode === 'par'},
                     {func: sc.showEditWindow, desc: 'Edit', show: sc.rights.editable},
-                    {func: sc.showAddParagraphAbove, desc: 'Add paragraph above', show: sc.rights.editable},
+                    {func: sc.cutPar, desc: 'Cut paragraph', show: $window.editMode === 'par'},
+                    {func: sc.copyPar, desc: 'Copy paragraph', show: $window.editMode !== 'area'},
                     {func: sc.cutArea, desc: 'Cut area', show: $window.editMode === 'area'},
                     {func: sc.copyArea, desc: 'Copy area', show: $window.editMode === 'area'},
                     {func: sc.showPasteMenu, desc: 'Paste...', show: $window.editMode && (sc.allowPasteRef || sc.allowPasteContent)},
                     {func: sc.showMoveMenu, desc: 'Move here...', show: $window.editMode && sc.allowPasteContent && sc.allowPasteRef},
+                    {func: sc.showAddParagraphAbove, desc: 'Add paragraph above', show: sc.rights.editable},
                     {func: sc.addQuestion, desc: 'Create question', show: sc.lectureMode && sc.rights.editable},
                     {
                         func: sc.startArea,
