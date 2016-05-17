9385eb12031eee66787ceedb46f1914e96a03a44
Tomi Karppinen <tomi.j.karppinen@jyu.fi>
2016-05-17 14:48:05 +0300

    Revert "Removed par_index attribute"
    This reverts commit 7a703a826dc0171e4fc0ef6fe4ed40dce059f5fc.
    Some experimental code was committed from the working directory
    by mistake.

--- timApp/static/scripts/view_html.js
+++ timApp/static/scripts/view_html.js
@@ -1025,50 +1025,25 @@ timApp.controller("ViewCtrl", [
             return sc.markParRead($this, $this.parents('.par'));
         });
 
-        sc.getParIndex = function($par) {
-            var par_id = sc.getParId($par);
-            var $pars = $('.par');
-            var realIndex = 0;
-
-            if ($par.find('.parContent').not(':empty').length === 0)
-                return null;
-
-            for (var i = 0; i < $pars.length; i++) {
-                var $node = $pars.eq(i);
-                if ($node.find('.parContent').not(':empty').length === 0)
-                    continue;
-
-                if (sc.getParId($node) == par_id) {
-                    console.log('sc.getParIndex(' + par_id + ') = ' + realIndex);
-                    return realIndex;
-                }
-
-                realIndex++;
-            }
-
-            console.log('sc.getParIndex(' + par_id + ') = (null)');
-            return null;
-        };
-
         sc.extendSelection = function ($par, allowShrink) {
             if (sc.selection.start === null) {
                 sc.selection.start = $par;
                 sc.selection.end = $par;
             } else {
                 var n = sc.selection.pars.length;
-                var startIndex = sc.getParIndex(sc.selection.pars.eq(0));
-                var endIndex = sc.getParIndex(sc.selection.pars.eq(n - 1));
+                var startIndex = $(sc.selection.pars[0]).attr('data-index');
+                var endIndex = $(sc.selection.pars[n - 1]).attr('data-index');
                 var areaLength = endIndex - startIndex + 1;
-                var newIndex = sc.getParIndex($par);
+                var newIndex = $par.attr('data-index');
 
                 if (newIndex < startIndex) {
                     sc.selection.start = $par;
                 } else if (newIndex > endIndex) {
                     sc.selection.end = $par;
                 } else if (allowShrink && areaLength > 1 && newIndex == startIndex) {
-                    sc.selection.start = sc.selection.pars.eq(1);
+                    sc.selection.start = $(sc.selection.pars[1]);
                 } else if (allowShrink && areaLength > 1 && newIndex == endIndex) {
-                    sc.selection.end = sc.selection.pars.eq(n - 2);
+                    sc.selection.end = $(sc.selection.pars[n - 2]);
                 }
             }
         };
@@ -1603,7 +1578,7 @@ timApp.controller("ViewCtrl", [
                     continue;
                 }
 
-                if ($next.hasClass('par') && sc.getParIndex($next) != null) {
+                if ($next.hasClass('par') && $next.attr('data-index') !== undefined) {
                     pars.push($next);
                     if ($next.is($par_last))
                         break;
