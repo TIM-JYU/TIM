2eaadd86e9ac91e82b84c5cdabf109dc344d1ff4
Panu Petteri Palojarvi <petteri.p.palojarvi@student.jyu.fi>
2016-05-20 15:53:48 +0300

    Fixed annotations not showing when not in answers.

--- timApp/timdb/annotations.py
+++ timApp/timdb/annotations.py
@@ -173,17 +173,18 @@ class Annotations(TimDbBase):
                        FROM Annotation
                          INNER JOIN VelpVersion ON VelpVersion.id = Annotation.version_id
                          INNER JOIN VelpContent ON VelpContent.version_id = Annotation.version_id
+                         LEFT JOIN UserAnswer ON UserAnswer.answer_id = Annotation.answer_id
                        WHERE VelpContent.language_id = ? AND
                              (Annotation.valid_until ISNULL OR
                                 Annotation.valid_until >= CURRENT_TIMESTAMP) AND
                              Annotation.document_id = ? AND (
                                 Annotation.annotator_id = ? OR (
-                                   """ + check_annotation_access_right_sql +
-                                """
+                                """ + check_annotation_access_right_sql +
+                                """ AND (? OR UserAnswer.user_id = ? OR UserAnswer.user_id ISNULL)
                              )
                           )
            ORDER BY depth_start DESC, node_start DESC, offset_start DESC""",
-                       [language_id, document_id, user_id] + annotation_access_levels)
+                       [language_id, document_id, user_id] + annotation_access_levels + [user_has_see_answers, user_id])
         results = self.resultAsDictionary(cursor)
         for result in results:
             start_path = [int(i) for i in result['element_path_start'][1:-1].split(',')]
