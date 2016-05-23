7d09f6cec8a1e0364e0afc0b44f288e7cd920da9
Joonas Sakari Lattu <joonas.s.lattu@student.jyu.fi>
2016-05-23 14:11:37 +0300

    Clean up code base by adding comments and moving methods around

--- timApp/timdb/annotations.py
+++ timApp/timdb/annotations.py
@@ -61,20 +61,6 @@ class Annotations(TimDbBase):
         :param answer_id: ID of answer if annotation is located within one.
         :return:
         """
-        """
-        Create new annotation
-        :param version_id: version of the velp that the annotation uses
-        :param document_id: Document id for convenience, saved for answers as well.
-        :param paragraph_id: Paragraph id   \ one of these must be null
-        :param answer_id: Answer id         /
-        :param element_number Number of the html element from which we start counting.
-        :param place_start: start
-        :param place_end: end
-        :param annotator_id: id of user who left the annotation
-        :param points: Points given, overrides velp's default and can be null
-        :param icon_id: Icon id, can be null
-        :return id of the new annotation.
-        """
         cursor = self.db.cursor()
         cursor.execute("""
                       INSERT INTO
@@ -115,9 +101,132 @@ class Annotations(TimDbBase):
         self.db.commit()
         return
 
+    def get_annotation(self, annotation_id: int) -> List[Dict]:
+        """Gets an annotation.
+
+        :param annotation_id: ID of annotation
+        :return: A list of dictionaries with the fields of individual annotations. List is empty if nothing is found and has several rows if there are duplicate ids(not good).
+        """
+        cursor = self.db.cursor()
+        cursor.execute("""
+                       SELECT *
+                       FROM Annotation
+                       WHERE Annotation.id = ?
+        """, [annotation_id])
+        return self.resultAsDictionary(cursor)
+
+    def invalidate_annotation(self, annotation_id: int, valid_until: Optional[str] = None):
+        """Invalidates and thus hides annotation
+
+        :param annotation_id: Id of annotation
+        :param valid_until: Time when annotation will be invalidated
+        :return:
+        """
+        cursor = self.db.cursor()
+
+        if valid_until is None:  # None if we want to invalidate immediately
+            cursor.execute("""
+                          UPDATE
+                          Annotation
+                          SET
+                          valid_until = current_timestamp
+                          WHERE Annotation.id = ?
+                          """, [annotation_id]
+                           )
+        else:  # Else invalidate at some specific time
+            cursor.execute("""
+                          UPDATE
+                          Annotation
+                          SET
+                          valid_until = ?
+                          WHERE Annotation.id = ?
+                          """, [valid_until, annotation_id]
+                           )
+        self.db.commit()
+
+    def add_comment(self, annotation_id: int, commenter_id: int, content: str) -> int:
+        """Adds new comment to an annotation
+
+        :param annotation_id:
+        :param commenter_id:
+        :param content:
+        :return: id of the new comment.
+        """
+        cursor = self.db.cursor()
+        cursor.execute("""
+                      INSERT INTO
+                      AnnotationComment(annotation_id, commenter_id, content)
+                      VALUES (?, ?, ?)
+                      """, [annotation_id, commenter_id, content]
+                       )
+        self.db.commit()
+        return cursor.lastrowid
+
+
+    def get_annotations_with_comments_in_document(self, user_id: int, user_has_see_answers: bool, user_has_teacher: bool,
+                                                  user_has_owner: bool, document_id: int):
+        annotations = self.get_annotations_in_document(user_id, user_has_see_answers, user_has_teacher, user_has_owner,
+                                                       document_id)
+        annotation_ids = []
+        for annotation in annotations:
+            annotation_ids.append(annotation['id'])
+
+        comment_data = self.get_comments_for_annotations(annotation_ids)
+
+        comment_dict = {}
+
+        if comment_data:
+            user_dict = {}
+            user_info = {}
+            annotation_id = comment_data[0]['annotation_id']
+            list_help = []
+            dict_help = {}
+            # comment_list = []
+            for i in range(len(comment_data)):
+                next_id = comment_data[i]['annotation_id']
+                if next_id != annotation_id:
+                    # comment_dict['id'] = annotation_id
+                    comment_dict[annotation_id] = copy.deepcopy(list_help)
+                    # comment_list.append(copy.deepcopy(comment_dict))
+                    annotation_id = next_id
+                    dict_help.clear()
+                    del list_help[:]
+                    dict_help['comment_time'] = comment_data[i]['comment_time']
+                    dict_help['commenter_id'] = comment_data[i]['commenter_id']
+                    dict_help['content'] = comment_data[i]['content']
+                    dict_help['commenter_username'] = comment_data[i]['username']
+                    dict_help['commenter_real_name'] = comment_data[i]['real_name']
+                    dict_help['commenter_email'] = comment_data[i]['user_email']
+                    list_help.append(copy.deepcopy(dict_help))
+                else:
+                    dict_help['comment_time'] = comment_data[i]['comment_time']
+                    dict_help['commenter_id'] = comment_data[i]['commenter_id']
+                    dict_help['content'] = comment_data[i]['content']
+                    dict_help['commenter_username'] = comment_data[i]['username']
+                    dict_help['commenter_real_name'] = comment_data[i]['real_name']
+                    dict_help['commenter_email'] = comment_data[i]['user_email']
+                    list_help.append(copy.deepcopy(dict_help))
+                if i == len(comment_data) - 1:
+                    # comment_dict['id'] = annotation_id
+                    comment_dict[annotation_id] = copy.deepcopy(list_help)
+                    # comment_list.append(copy.deepcopy(comment_dict))
+                    annotation_id = next_id
+                    dict_help.clear()
+
+        for annotation in annotations:
+            annotation_id = annotation['id']
+            if annotation_id in comment_dict:
+                annotation['comments'] = copy.deepcopy(comment_dict[annotation_id])
+            else:
+                annotation['comments'] = []
+                print("No comments for annotation id: " + str(annotation_id))
+
+        return annotations
+
     def get_annotations_in_document(self, user_id: int, user_has_see_answers, user_has_teacher: bool,
                                     user_has_owner: bool, document_id: int) -> List[Dict]:
         """Gets all annotations made in a document. Both in document and in answers.
+
         :param user_has_see_answers:
         :param user_id: user that is viewing annotations. Affects which annotations are returned.
         :param user_has_teacher:
@@ -194,6 +303,34 @@ class Annotations(TimDbBase):
             result['coord'] = coord
         return results
 
+    def get_comments_for_annotations(self, annotation_ids: List[int]) -> List[dict]:
+        """Gets comments for annotations given as a list
+
+        :param annotation_ids: List of annotation IDs
+        :return:
+        """
+        cursor = self.db.cursor()
+        cursor.execute("""
+                      SELECT
+                        AnnotationComment.annotation_id,
+                        AnnotationComment.comment_time,
+                        AnnotationComment.commenter_id,
+                        AnnotationComment.content,
+                        User.name as username, User.real_name,
+                        User.email as user_email
+                      FROM AnnotationComment
+                      JOIN User ON AnnotationComment.commenter_id = User.id
+                      WHERE AnnotationComment.annotation_id IN ({})
+                      ORDER BY AnnotationComment.annotation_id ASC;
+                      """.format(self.get_sql_template(annotation_ids)), annotation_ids
+                      )
+        comment_data = self.resultAsDictionary(cursor)
+
+        return comment_data
+
+
+    # TODO Outdated methods
+
     def get_annotations_in_answer(self, answer_id: int) -> List[Dict]:
         """
         Get all annotations made in a given answer.
@@ -226,150 +363,6 @@ class Annotations(TimDbBase):
                        """, [answer_id]
                        )
         return self.resultAsDictionary(cursor)
-
-    def get_annotation(self, annotation_id: int) -> List[Dict]:
-        """Gets an annotation.
-
-        :param annotation_id:
-        :return: A list of dictionaries with the fields of individual annotations. List is empty if nothing is found and has several rows if there are duplicate ids(not good).
-        """
-        cursor = self.db.cursor()
-        cursor.execute("""
-                       SELECT *
-                       FROM Annotation
-                       WHERE Annotation.id = ?
-        """, [annotation_id])
-        return self.resultAsDictionary(cursor)
-
-    def invalidate_annotation(self, annotation_id: int, valid_until: Optional[str] = None):
-        cursor = self.db.cursor()
-
-        if valid_until is None:  # None if we want to invalidate immediately
-            cursor.execute("""
-                          UPDATE
-                          Annotation
-                          SET
-                          valid_until = current_timestamp
-                          WHERE Annotation.id = ?
-                          """, [annotation_id]
-                           )
-        else:  # Else invalidate at some specific time
-            cursor.execute("""
-                          UPDATE
-                          Annotation
-                          SET
-                          valid_until = ?
-                          WHERE Annotation.id = ?
-                          """, [valid_until, annotation_id]
-                           )
-        self.db.commit()
-
-    def add_comment(self, annotation_id: int, commenter_id: int, content: str) -> int:
-        """Adds new comment to an annotation
-
-        :param annotation_id:
-        :param commenter_id:
-        :param content:
-        :return: id of the new comment.
-        """
-        cursor = self.db.cursor()
-        cursor.execute("""
-                      INSERT INTO
-                      AnnotationComment(annotation_id, commenter_id, content)
-                      VALUES (?, ?, ?)
-                      """, [annotation_id, commenter_id, content]
-                       )
-        self.db.commit()
-        return cursor.lastrowid
-
-    def get_annotations_with_comments_in_document(self, user_id: int, user_has_see_answers: bool, user_has_teacher: bool,
-                                                  user_has_owner: bool, document_id: int):
-        annotations = self.get_annotations_in_document(user_id, user_has_see_answers, user_has_teacher, user_has_owner,
-                                                       document_id)
-        annotation_ids = []
-        for annotation in annotations:
-            annotation_ids.append(annotation['id'])
-
-        comment_data = self.get_comments_for_annotations(annotation_ids)
-
-        comment_dict = {}
-
-        if comment_data:
-            user_dict = {}
-            user_info = {}
-            annotation_id = comment_data[0]['annotation_id']
-            list_help = []
-            dict_help = {}
-            # comment_list = []
-            for i in range(len(comment_data)):
-                next_id = comment_data[i]['annotation_id']
-                if next_id != annotation_id:
-                    # comment_dict['id'] = annotation_id
-                    comment_dict[annotation_id] = copy.deepcopy(list_help)
-                    # comment_list.append(copy.deepcopy(comment_dict))
-                    annotation_id = next_id
-                    dict_help.clear()
-                    del list_help[:]
-                    dict_help['comment_time'] = comment_data[i]['comment_time']
-                    dict_help['commenter_id'] = comment_data[i]['commenter_id']
-                    dict_help['content'] = comment_data[i]['content']
-                    dict_help['commenter_username'] = comment_data[i]['username']
-                    dict_help['commenter_real_name'] = comment_data[i]['real_name']
-                    dict_help['commenter_email'] = comment_data[i]['user_email']
-                    list_help.append(copy.deepcopy(dict_help))
-                else:
-                    dict_help['comment_time'] = comment_data[i]['comment_time']
-                    dict_help['commenter_id'] = comment_data[i]['commenter_id']
-                    dict_help['content'] = comment_data[i]['content']
-                    dict_help['commenter_username'] = comment_data[i]['username']
-                    dict_help['commenter_real_name'] = comment_data[i]['real_name']
-                    dict_help['commenter_email'] = comment_data[i]['user_email']
-                    list_help.append(copy.deepcopy(dict_help))
-                if i == len(comment_data) - 1:
-                    # comment_dict['id'] = annotation_id
-                    comment_dict[annotation_id] = copy.deepcopy(list_help)
-                    # comment_list.append(copy.deepcopy(comment_dict))
-                    annotation_id = next_id
-                    dict_help.clear()
-
-        for annotation in annotations:
-            annotation_id = annotation['id']
-            if annotation_id in comment_dict:
-                annotation['comments'] = copy.deepcopy(comment_dict[annotation_id])
-            else:
-                annotation['comments'] = []
-                print("No comments for annotation id: " + str(annotation_id))
-
-        return annotations
-
-
-    def get_comments_for_annotations(self, annotation_ids: List[int]) -> List[dict]:
-        """
-
-        :param annotation_ids:
-        :return:
-        """
-        cursor = self.db.cursor()
-        cursor.execute("""
-                      SELECT
-                        AnnotationComment.annotation_id,
-                        AnnotationComment.comment_time,
-                        AnnotationComment.commenter_id,
-                        AnnotationComment.content,
-                        User.name as username, User.real_name,
-                        User.email as user_email
-                      FROM AnnotationComment
-                      JOIN User ON AnnotationComment.commenter_id = User.id
-                      WHERE AnnotationComment.annotation_id IN ({})
-                      ORDER BY AnnotationComment.annotation_id ASC;
-                      """.format(self.get_sql_template(annotation_ids)), annotation_ids
-                      )
-        comment_data = self.resultAsDictionary(cursor)
-
-        return comment_data
-
-
-
 
     # Todo write support for answer_id.
     def get_comments_in_document(self, document_id: int) -> List[Dict]:
