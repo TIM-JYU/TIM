3710091c97f281a9b962a50983776d250a7b384e
Mika Lehtinen <mikkalle@student.jyu.fi>
2016-06-09 00:33:49 +0300

    Copy bower libs when running tests if needed

--- run_tests.sh
+++ run_tests.sh
@@ -7,9 +7,11 @@ docker run \
  -v $PWD:/service:ro \
  -v $PWD/timApp/static/testgen:/service/timApp/static/testgen:rw \
  -v $PWD/timApp/static/.webassets-cache:/service/timApp/static/.webassets-cache:rw \
+ -v $PWD/timApp/static/scripts:/service/timApp/static/scripts:rw \
  -t -i \
  tim:$(./get_latest_date.sh) /bin/bash -c \
  "cd /service/timApp &&
  export TIM_SETTINGS=testconfig.py &&
+ python3 bower_helper.py &&
  python3 -c 'import dumboclient; dumboclient.launch_dumbo()' &&
  python3 -m unittest"
