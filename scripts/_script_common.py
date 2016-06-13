1f25e6e6439669589b4661d08e0ad407faa1d0af
Tomi Karppinen <tomi.j.karppinen@jyu.fi>
2016-06-13 20:01:10 +0300

    Revert "Scripts dir moved under timApp"
    This reverts commit 1ccf8c3922beb4d9698b5357036c6563e1f96c93.

--- scripts/_script_common.py
+++ scripts/_script_common.py
@@ -1,22 +1,8 @@
 import sys
 
-from timdb.timdb2 import TimDb
-
-
-TIM_FILES = '../tim_files'
-DBFILE = TIM_FILES + '/tim.db'
+DBFILE = '../timApp/tim_files/tim.db'
 STDOUT = sys.stdout
 STDERR = sys.stderr
-
-__TIMDB = None
-
-
-def get_tim_db():
-    global __TIMDB
-    if __TIMDB is None:
-        global DBFILE, TIM_FILES
-        __TIMDB = TimDb(DBFILE, TIM_FILES)
-    return __TIMDB
 
 
 def stdout(s=''):
--- timApp/installMain.sh
+++ timApp/installMain.sh
@@ -89,4 +89,4 @@ apt-get install -y openssh-server
 run echo "Europe/Helsinki" > /etc/timezone; dpkg-reconfigure -f noninteractive tzdata
 
 # Default startup command
-# cmd cd /service/timApp && source scripts/_initenv.sh && python3 launch.py
+# cmd cd /service/timApp && source /service/scripts/_initenv.sh && python3 launch.py
