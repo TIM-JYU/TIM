5c471f35560ab1fd734f52941759db4954263848
Joonas Sakari Lattu <joonas.s.lattu@student.jyu.fi>
2016-06-17 11:25:57 +0300

    Add console.log('ANNOTATIONS') to help Ismo's testing

--- timApp/static/scripts/directives/velpSelection.js
+++ timApp/static/scripts/directives/velpSelection.js
@@ -69,7 +69,7 @@ timApp.controller('VelpSelectionController', ['$scope', '$http', function ($scop
             default_velp_group.selected = true;
         console.log(data);
     });
-
+a
     // Get velpgroup data
     $http.get('/{0}/get_velp_groups'.replace('{0}', doc_id)).success(function (data) {
         $scope.velpGroups = data;
@@ -114,6 +114,7 @@ timApp.controller('VelpSelectionController', ['$scope', '$http', function ($scop
         $http.get('/{0}/get_annotations'.replace('{0}', doc_id)).success(function (data) {
             $scope.annotations = data;
             $scope.loadDocumentAnnotations();
+            console.log("ANNOTATIONS");
             console.log(data);
         });
 
