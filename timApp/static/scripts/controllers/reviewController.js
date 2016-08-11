675fef665d1a1a15a45e6eb744d8b4b0cfa5df15
Seppo Tarvainen <seppo.v.tarvainen@jyu.fi>
2016-08-11 11:55:21 +0300

    Teacher annotation allowed only with points fixing.

--- timApp/static/scripts/controllers/reviewController.js
+++ timApp/static/scripts/controllers/reviewController.js
@@ -219,7 +219,7 @@ timApp.controller("ReviewController", ['$scope', '$http', '$window', '$compile',
                 range.setStart(element, placeInfo.start.offset);
                 range.setEnd(element, placeInfo.end.offset);
                 $scope.addAnnotationToCoord(range, annotations[i], false);
-                //addAnnotationToElement(par, annotations[i], false, "Added also margin annotation");
+                addAnnotationToElement(par, annotations[i], false, "Added also margin annotation");
             }
 
         }
@@ -703,7 +703,7 @@ timApp.controller("ReviewController", ['$scope', '$http', '$window', '$compile',
      */
 
     $scope.notAnnotationRights = function (points) {
-    if (parent.teacherMode) {
+    if ($scope.$parent.rights.teacher) {
         return false;
     } else {
         if (points === null) {
@@ -726,8 +726,13 @@ timApp.controller("ReviewController", ['$scope', '$http', '$window', '$compile',
         }
     }
 
+    /**
+     * Return true if user has teacher rights.
+     * @returns {boolean}
+     */
+
     $scope.allowChangePoints = function () {
-        if (parent.teacherMode) return true;
+        return $scope.$parent.rights.teacher;
     }
 
     /**
