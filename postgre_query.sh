56af8d4ca4377e2b05adeba2268ba7ddea2fe101
Mika Lehtinen <mikkalle@student.jyu.fi>
2016-09-01 22:30:42 +0300

    Fix PostgreSQL console script

--- postgre_query.sh
+++ postgre_query.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run --rm --net=timnet -t -i postgre /bin/bash -c 'psql -h postgre -p 5432 -d docker -U docker --password'
+. ./variables.sh
+
+docker run --rm --net timnet -ti postgres:9.5 /bin/bash -c "psql -h postgresql-$TIM_NAME -p 5432 -d $TIM_NAME -U postgres"
