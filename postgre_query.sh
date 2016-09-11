f699a089a682442fbb30c6c51a9f4e87f757fea8
Vesa Lappalainen <vesal@jyu.fi>
2016-09-12 02:57:29 +0300

    sql scripts to postgre_query.sh

--- postgre_query.sh
+++ postgre_query.sh
@@ -2,15 +2,21 @@
 
 . ./variables.sh
 
-if [ $# -ne "1" ]; then
+if [ $# -lt 1 ]; then
  echo Usage:
- echo To connect to main database: ./postgre_query 1
- echo To connect to tempdb database: ./postgre_query 2
+ echo To connect to main database: ./postgre_query 1  [sql/script]
+ echo To connect to tempdb database: ./postgre_query 2 [sql/script]
  exit
 fi
 
+fileOpt=''
+
+if [ $# -ge 2 ]; then
+   fileOpt=" -f /$2"
+fi
+
 if [ $1 = "2" ]; then
-  docker run --rm --net timnet -ti postgres:9.5 /bin/bash -c "psql -h postgresql-tempdb-$TIM_NAME -p 5432 -d tempdb_$TIM_NAME -U postgres"
+  docker run --rm --net timnet -v /opt/tim/sql:/sql/:ro  -ti postgres:9.5 /bin/bash -c "psql -h postgresql-tempdb-$TIM_NAME -p 5432 -d tempdb_$TIM_NAME -U postgres $fileOpt"
 else
-  docker run --rm --net timnet -ti postgres:9.5 /bin/bash -c "psql -h postgresql-$TIM_NAME -p 5432 -d $TIM_NAME -U postgres"
+  docker run --rm --net timnet -v /opt/tim/sql:/sql/:ro  -ti postgres:9.5 /bin/bash -c "psql -h postgresql-$TIM_NAME        -p 5432 -d        $TIM_NAME -U postgres $fileOpt"
 fi
