a2ec3b1e1c015aca00511b9baf699e47e8ba07db
Mika Lehtinen <mikkalle@student.jyu.fi>
2016-09-21 15:20:56 +0300

    Fix admin user script run command

--- create_admin_user.sh
+++ create_admin_user.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-docker run --net=timnet --rm -v ${PWD}:/service -t -i timimages/tim:$(./get_latest_date.sh) \
-  /bin/bash -c 'cd /service/timApp && PYTHONPATH=${PYTHONPATH}:/service/timApp python3 sql/create_admin_user.py'
+. ./variables.sh
+
+docker run \
+ --net=timnet \
+ --env TIM_NAME=${TIM_NAME} \
+ --env TIM_HOST=${TIM_HOST} \
+ -v ${PWD}:/service \
+ -t -i \
+ timimages/tim:$(./get_latest_date.sh) \
+ /bin/bash -c 'cd /service/timApp && PYTHONPATH=${PYTHONPATH}:/service/timApp python3 sql/create_admin_user.py'
