5eb1c62f1ef3d7ea6274c5b891eb0332e6a7bb90
Mika Lehtinen <mikkalle@student.jyu.fi>
2016-09-21 15:16:12 +0300

    Fix incorrect path and env variable in create_admin_user.sh

--- create_admin_user.sh
+++ create_admin_user.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run --net=timnet --rm -v /opt/tim/:/service -t -i timimages/tim:$(./get_latest_date.sh) \
-  /bin/bash -c 'cd /service/timApp && PYTHONPATH=PYTHONPATH:/service/timApp python3 sql/create_admin_user.py'
+docker run --net=timnet --rm -v ${PWD}:/service -t -i timimages/tim:$(./get_latest_date.sh) \
+  /bin/bash -c 'cd /service/timApp && PYTHONPATH=${PYTHONPATH}:/service/timApp python3 sql/create_admin_user.py'
