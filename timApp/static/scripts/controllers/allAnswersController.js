a5c396865e3b95ad9a1a364a2d5cd254ef697541
Mika Lehtinen <mikkalle@student.jyu.fi>
2016-11-02 16:57:47 +0200

    Add initial UI for choosing period for all answers

--- timApp/static/scripts/controllers/allAnswersController.js
+++ timApp/static/scripts/controllers/allAnswersController.js
@@ -1,9 +1,17 @@
-var angular;
+var angular, moment;
 var timApp = angular.module('timApp');
 
 timApp.controller('AllAnswersCtrl', ['$uibModalInstance', '$window', '$httpParamSerializer', '$localStorage', 'options', function ($uibModalInstance, $window, $httpParamSerializer, $localStorage, options) {
     "use strict";
     var $ctrl = this;
+    $ctrl.datePickerOptionsFrom = {
+        format: 'D.M.YYYY HH:mm:ss',
+        defaultDate: moment().subtract({weeks: 1})
+    };
+    $ctrl.datePickerOptionsTo = {
+        format: 'D.M.YYYY HH:mm:ss',
+        defaultDate: moment().add({minutes: 10})
+    };
     $ctrl.showSort = options.allTasks;
     $ctrl.options = {};
     $ctrl.options.age = 'max';
