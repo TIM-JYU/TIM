af848ee30cbca01a4be53ce2ce1c6961e899b6a4
Jukka Koskela <jukka.t.koskela@student.jyu.fi>
2016-11-03 13:13:26 +0200

    Fixed gamification YAML

--- timApp/timdb/gamificationdata.py
+++ timApp/timdb/gamificationdata.py
@@ -1,4 +1,4 @@
-from timdb.timdbbase import TimDbBase
+"Collection of gamification functions"
 from timdb.models.docentry import DocEntry
 import yaml
 import json
@@ -6,7 +6,8 @@ import json
 
 def gamify(initial_data):
     initial_json = convert_to_json(initial_data)
-    parse_json(initial_json)
+    checked_json = check_for_duplicate_docs(initial_json)
+    parse_json(checked_json)
     get_doc_name()
     #get_maximum_points()
     #get_url()
@@ -14,11 +15,24 @@ def gamify(initial_data):
 
 def convert_to_json(md_data):
     """Converts the YAML paragraph in gamification document to JSON
-    :param initial_data = the data read from paragraph in YAML
+    :param md_data = the data read from paragraph in YAML
     :returns: same data in JSON
     """
     temp = yaml.load(md_data[3:len(md_data) - 3])
     return json.dumps(temp)
+
+
+def check_for_duplicate_docs(json_to_check):
+    """Checks that all documents in json are unique
+    :param json = Checked documents in JSON
+    :returns: same data in JSON
+    """
+    is_valid = True
+
+    if json_to_check is None:
+        raise GamificationException('JSON is None')
+
+    return is_valid
 
 
 def parse_json(initial_json):
@@ -39,7 +53,12 @@ def parse_json(initial_json):
 
 def get_doc_name(initial_yaml):
     """Converts the gamification
-    :param initial_data = the data read from block in yaml
+    :param initial_yaml = the data read from block in yaml
     :returns: Updated JSON, ready for map generator
     """
     DocEntry.find_by_path()
+
+
+class GamificationException(Exception):
+    """The exception that is thrown when an error occurs during a gamification check."""
+    pass
