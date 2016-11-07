9ce02118b71c49df5c2d42d3299678a6918a760d
Mika Lehtinen <mikkalle@student.jyu.fi>
2016-11-07 12:59:57 +0200

    Require username in CSV file when importing accounts (can be empty in which case email will be the username)

--- timApp/import_accounts.py
+++ timApp/import_accounts.py
@@ -10,7 +10,13 @@ def import_accounts(file, password):
     with open(file) as csvfile:
         reader = csv.reader(csvfile, delimiter=';')
         for row in reader:
-            timdb.users.create_user_with_group(row[0], row[1], row[0], password, commit=False)
+            if len(row) != 3:
+                raise Exception('All rows must have 3 fields, found a row with {} fields: {}'.format(len(row), row))
+            timdb.users.create_user_with_group(name=row[2] or row[0],
+                                               real_name=row[1],
+                                               email=row[0],
+                                               password=password,
+                                               commit=False)
     timdb.commit()
 
 
--- timApp/test_import_accounts.py
+++ timApp/test_import_accounts.py
@@ -7,7 +7,17 @@ from timdbtest import TimDbTest
 
 class AccountImportTest(TimDbTest):
     def test_import_accounts(self):
-        accounts = [(name + '@example.com', name) for name in ['testimport{}'.format(i) for i in range(1, 100)]]
+        num_accounts = 100
+        accounts = [(name + '@example.com', name, name + 'uname') for name in
+                    ['testimport{}'.format(i) for i in range(0, num_accounts)]]
+        self.assertEqual(num_accounts, len(accounts))
+        self.write_and_test(accounts)
+        accounts = [(name + '@example.com', name, '') for name in
+                    ['testimport_2_{}'.format(i) for i in range(0, num_accounts)]]
+        self.assertEqual(num_accounts, len(accounts))
+        self.write_and_test(accounts, username_is_email=True)
+
+    def write_and_test(self, accounts, username_is_email=False):
         csv_path = os.path.join(self.test_files_path, 'import.csv')
         with open(csv_path, 'w') as f:
             w = csv.writer(f, delimiter=';')
@@ -16,4 +26,10 @@ class AccountImportTest(TimDbTest):
         import_accounts(csv_path, 'testpass')
         db = self.get_db()
         for a in accounts:
-            self.assertEqual(a[1], db.users.get_user_by_email(a[0])['real_name'])
+            u = db.users.get_user_by_email(a[0])
+            self.assertEqual(a[1], u['real_name'])
+            self.assertEqual(a[0], u['email'])
+            if username_is_email:
+                self.assertEqual(a[0], u['name'])
+            else:
+                self.assertEqual(a[2], u['name'])
