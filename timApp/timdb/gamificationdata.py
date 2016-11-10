4cedb093e96008f785d3fc12bfbed722d9354e30
Jukka Koskela <jukka.t.koskela@student.jyu.fi>
2016-11-10 13:07:23 +0200

    Can now find doc id, name, url and place them in a dictionary with separate lectures and demos

--- timApp/timdb/gamificationdata.py
+++ timApp/timdb/gamificationdata.py
@@ -2,16 +2,20 @@
 from timdb.models.docentry import DocEntry
 import yaml
 import json
+from flask import request
 
 
 def gamify(initial_data):
+
+    # Convert initial data to JSON format
     initial_json = convert_to_json(initial_data)
-    checked_json = check_for_duplicate_docs(initial_json)
-    return
-    parse_json(checked_json)
-    get_doc_name()
-    get_maximum_points()
-    get_url()
+
+    # Find document IDs from json, and place them in their appropriate arrays(lectures and demos separately)
+    lecture_table, demo_table = get_doc_ids(initial_json)
+
+    # Insert document, IDs, paths, and points in a dictionary
+    place_in_dict(lecture_table, demo_table)
+    # get_demo_maxpoints(demo_name)
 
 
 def convert_to_json(md_data):
@@ -23,48 +27,61 @@ def convert_to_json(md_data):
     return json.loads(json.dumps(temp))
 
 
-def check_for_duplicate_docs(json_to_check):
-    """Checks that all documents in json are unique
-    :param json = Checked documents in JSON
-    :returns: same data in JSON
+def get_doc_ids(json_to_check):
+    """Parses json to find names of lecture and demo documents
+    :param json_to_check = Checked documents in JSON
+    :returns:
     """
-    is_valid = True
     if json_to_check is None:
         raise GamificationException('JSON is None')
 
-    paths = json_to_check['lectures']
+    lecture_paths = json_to_check['lectures']
+    demo_paths = json_to_check['demos']
 
-    for path in paths:
-        print(path['path'].split('/')[-1])
+    lectures = []
+    for path in lecture_paths:
+        lecture = DocEntry.find_by_path(path['path'])
+        if lecture is not None:
+            temp_dict1 = {}
+            temp_dict1['id']   = lecture.id
+            temp_dict1['path'] = lecture.get_short_name()
+            temp_dict1['url']  = request.url_root+'/view/'+ lecture.get_path()
+            lectures.append(temp_dict1)
+
+    demos = []
+    for path in demo_paths:
+        demo = DocEntry.find_by_path(path['path'])
+        if demo is not None:
+            temp_dict2 = {}
+            temp_dict2['id']   = demo.id
+            temp_dict2['path'] = demo.get_short_name()
+            temp_dict2['url']  = request.url_root+'/view/'+ demo.get_path()
+            demos.append(temp_dict2)
+
+    return lectures, demos
 
 
-    return is_valid
-
-
-def parse_json(initial_json):
-    """Parses JSON for document ids, document points and user points
-    :param initial_json = the JSON to be parsed
-    :returns: Updated JSON, ready for map generator
+def place_in_dict(l_table, d_table):
     """
-    lectures = initial_json[1]['lectures']
-    index = 0
-    for lecture in lectures:
-        name = lectures[index]['name']
-        maximum_points = lectures[index]['maxPoints']
-        got_points = lectures[index]['gotPoints']
-        # self.addPointsToDocument(name, maxPoints)
-        index += 1
-        print(name, maximum_points, got_points)
-
-
-def get_doc_name(initial_yaml):
-    """Converts the gamification
-    :param initial_yaml = the data read from block in yaml
-    :returns: Updated JSON, ready for map generator
+    :param l_table Array of lecture IDs
+    :param d_table Array of demo IDs
+    :returns: maxPoints of the demo document
     """
-    DocEntry.find_by_path()
+    document_dict = {'lectures': [], 'demos': []}
+
+    temp1 = document_dict['lectures']
+    for i in range(len(l_table)):
+        temp1.append(l_table[i])
+
+    temp2 = document_dict['demos']
+    for j in range(len(d_table)):
+        temp2.append(d_table[j])
+
+    print(document_dict)
+    return
 
 
 class GamificationException(Exception):
     """The exception that is thrown when an error occurs during a gamification check."""
     pass
+
