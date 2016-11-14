be582285d664818574a9c2a21322296a1f25848c
Mika Lehtinen <mikkalle@student.jyu.fi>
2016-11-14 18:06:02 +0200

    Download prebuilt binaries for Dumbo and Haskell plugins when using pull_all.sh

--- pull_all.sh
+++ pull_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Pulls the latest TIM images from the timimages repository.
+# Pulls the latest TIM images from the timimages repository and downloads prebuilt binaries.
 
 docker pull ubuntu
 
@@ -8,8 +8,6 @@ docker pull timimages/tim:$(./get_latest_date.sh)
 docker pull timimages/cs3
 docker pull timimages/svn
 docker pull timimages/haskellrun
-docker pull timimages/haskelldev
-docker pull timimages/stackage_builder
 docker pull timimages/pali
 docker pull timimages/local_nginx
 docker pull timimages/funnel
@@ -17,10 +15,16 @@ docker pull timimages/funnel
 # Remove leftover untagged images
 docker rmi $(docker images | grep "^<none>" | awk '{print $3}')
 
-# Haskell plugin binaries are not included directly in the images, so we need to build them
-(cd timApp/modules/Haskell && ./launch_sandbox_build.sh $@)
+# download prebuilt Dumbo
+mkdir -p Ephemeral/Dumbo/dist/build/Dumbo
+docker run --rm -v ${PWD}/Ephemeral/Dumbo/dist/build/Dumbo:/target timimages/prebuilt /bin/bash -c "cp /bin/Dumbo /target/"
 
-# Same with Dumbo
-(cd Ephemeral/Dumbo && ./build_dumbo.sh $@)
+# download prebuilt Haskell plugins (ChoicesPlugin, MultipleChoicesPlugin, ShortNotePlugin, GraphVizPlugin)
+mkdir -p timApp/modules/Haskell/bin
+docker run --rm -v ${PWD}/timApp/modules/Haskell/bin:/target timimages/prebuilt /bin/bash -c "cp /bin/*Plugin /target/"
+
+(cd timApp/modules/Haskell \
+ && git clone git://yousource.it.jyu.fi/ties343-funktio-ohjelmointi/MultipleChoicePlugin.git Choices 2> /dev/null \
+ ; cd Choices && git pull)
 
 ./create_network.sh
