28ffea62066d27842680d96b3b969389282e1784
Jukka Koskela <jukka.t.koskela@student.jyu.fi>
2016-11-17 12:04:11 +0200

    Add max_points_key to document settings

--- timApp/documentmodel/docsettings.py
+++ timApp/documentmodel/docsettings.py
@@ -23,6 +23,7 @@ class DocSettings:
     lazy_key = 'lazy'
     hide_links_key = 'hide_links'
     point_sum_rule_key = 'point_sum_rule'
+    max_points_key = 'max_points'
 
     @classmethod
     def is_valid_paragraph(cls, par):
@@ -175,3 +176,6 @@ class DocSettings:
 
     def point_sum_rule(self, default=None):
         return self.__dict.get(self.point_sum_rule_key, default)
+
+    def max_points(self, default=None):
+        return self.__dict.get(self.max_points_key, default)
