f9884115b2776cbb6ea2332d2dc9ad3bcd3ecbf6
Jukka Koskela <jukka.t.koskela@student.jyu.fi>
2016-12-01 12:14:58 +0200

    Fix more merge conflicts

--- timApp/timdb/gamification_models/gamificationdocumenttype.py
+++ timApp/timdb/gamification_models/gamificationdocumenttype.py
@@ -1,13 +1,14 @@
 from tim_app import db
 
-
 class GamificationDocumentType(db.model):
+    """
+    Created by TIMG
+    This class represents the GamificationDocumentType database table, that differentiates documents for gamification
+    point purposes. Currently only two types exist (Lectures = 1, Demos = 2).
+    """
     __bind_key__ = 'tim_main'
     __tablename__ = 'gamificationdocumenttype'
     document_type_id = db.Column (db.Integer, primary_key = True)
     document_type_name = db.Column (db.Text)
 
-    @staticmethod
-    def create() -> 'GamificationDocumentType':
-        """Creates a new entry into GamificationDocumentType table"""
 
