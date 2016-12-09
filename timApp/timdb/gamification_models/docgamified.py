231a74ee4a6ccfca4b257e31ea4497593370d8e6
Jukka Koskela <jukka.t.koskela@student.jyu.fi>
2016-12-09 17:08:18 +0200

    Remove unnecessary db functions

--- timApp/timdb/gamification_models/docgamified.py
+++ timApp/timdb/gamification_models/docgamified.py
@@ -26,33 +26,3 @@ class DocGamified(db.Model):
         db.session.commit()
 
         return dgamified
-
-    # #Kokeillaan etsiä block selffin avulla.
-    # def get_docgamified_block(self):
-    #     g = block.query.filter_by(id=self.doc_id).first()
-    #     if g:
-    #         return g
-    #     raise TimDbException('docgamified_block not found')
-    #
-    # #Kokeillaan etsiä gamificationdocumenttype selffin avulla.
-    # def get_gamificationdocumenttype(self):
-    #     g = gamificationdocumenttype.query.filter_by(document_type_id=self.doc_type_id).first()
-    #     if g:
-    #         return g
-    #     raise TimDbException('gamificationdocumenttype not found')
-    #
-    # #Revitään gamificationdocument selffin avulla.
-    # def get_gamificationdocument(self):
-    #     g = gamificationdocument.query.filter_by(id=self.gamification_doc_id).first()
-    #     if g:
-    #         return g
-    #     raise TimDbException('gamificationdocumenttype not found')
-    #
-    #
-    # @staticmethod
-    # def create(gamification_doc_id,doc_id,doc_type_id) -> 'DocGamified':
-    #     """Creates a new entry into DocGamified table"""
-    #     docgamified = DocGamified(gamification_doc_id=gamification_doc_id,doc_id=doc_id,doc_type_id=doc_type_id)
-    #     db.session.add(docgamified)
-    #     db.session.commit()
-    #     return docgamified
