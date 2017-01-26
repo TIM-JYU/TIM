0fea21d7dd1ef42b1a1bbeb5a73e5fc165e13eaf
Mika Lehtinen <mikkalle@student.jyu.fi>
2017-01-26 15:08:55 +0200

    Update Dumbo's Pandoc to version 1.19

--- Ephemeral/Dumbo/buildScript.sh
+++ Ephemeral/Dumbo/buildScript.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -exu
 cd /build
-stack build --allow-different-user
-mkdir -p dist/build/Dumbo
-cp /build/.stack-work/install/x86_64-linux/lts-3.1/7.10.2/bin/Dumbo dist/build/Dumbo/
+stack solver --system-ghc --update-config --allow-different-user
+stack build --copy-bins --system-ghc --allow-different-user
--- Ephemeral/Dumbo/build_dumbo.sh
+++ Ephemeral/Dumbo/build_dumbo.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -v $PWD:/build/ -w /build/ timimages/haskelldev /build/buildScript.sh
+docker run --rm -v $PWD/dist/build/Dumbo/:/root/.local/bin/ -v $PWD:/build/ -w /build/ timimages/haskelldev /build/buildScript.sh
