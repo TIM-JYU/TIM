1d432d9d4bdce7300c500d1ee2cf4fcd7bf2040d
Vesa Lappalainen <vesal@jyu.fi>
2017-03-05 23:19:21 +0200

    add octave dir

---
+++ timApp/modules/cs/octave/urso.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+dockername="cso"
+dockerImage="timimages/octave"
+ 
+dockerOptions="--name $dockername --net=timnet -p 56001:5000 -v /var/run/docker.sock:/var/run/docker.sock   -v /opt/cs:/cs/:ro -v /opt/cs/images/cs:/csimages/ -v /tmp/uhome:/tmp2/ -w /cs $dockerImage " 
+
+docker run  --rm=true  -t -i $dockerOptions 
