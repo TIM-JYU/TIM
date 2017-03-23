01748971fab2d8219693f2f69120300dda90911f
Mika Lehtinen <mikkalle@student.jyu.fi>
2017-03-23 15:36:33 +0200

    Improve tslint script
    Multiple arguments can now be passed to tslint, not just the filename.

---
+++ run_command_workdir.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Runs the given command in the given working directory inside TIM container with the repository's root path being mapped at /service inside container.
+# Usage: ./run_command_workdir.sh <working dir> <command> [arguments...]
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+. ${DIR}/variables.sh
+
+docker run \
+ --rm \
+ --net=timnet \
+ --env TIM_NAME=${TIM_NAME} \
+ --env TIM_HOST=${TIM_HOST} \
+ --env FLASK_APP=tim_app.py \
+ --env PYTHONPATH=/service/timApp \
+ -v ${DIR}:/service \
+ -t -i \
+ -w "/service/$1" \
+ timimages/tim:$(${DIR}/get_latest_date.sh) \
+ "${@:2}"
--- timApp/static/scripts/tslint.sh
+++ timApp/static/scripts/tslint.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-../../../run_command.sh tslint "static/scripts/$1"
+../../../run_command_workdir.sh timApp/static/scripts tslint "$@"
