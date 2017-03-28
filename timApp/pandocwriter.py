5c97db29301de7b850ff30c831365663641a7178
Topi Latva-Salo <tohesala@student.jyu.fi>
2017-03-28 15:05:42 +0300

    Change to use pypandoc library

--- timApp/pandocwriter.py
+++ timApp/pandocwriter.py
@@ -1,33 +1,20 @@
 """
 Functions for calling pandoc and constructing the calls
 """
-import subprocess
+
+import pypandoc
 
 
-def call_pandoc(markdown: str) -> bytes:
+def call_pandoc(markdown: str, to_format: str) -> str:
     """
     Calls for a new system subprocess to run pandoc.
 
+    :param to_format: Specifies the format to which the input is converted
     :param markdown: The documents markdown
     :return: LaTeX produced by pandoc
     """
 
-    def construct_call() -> [str]:
-        """
-        Constructs the subprocess call with the given options
-
-        """
-
-        call = []
-        call.append("pandoc")
-        for opt in get_print_options():
-            call.append(opt)
-        return call
-
-    try:
-        return subprocess.check_output(construct_call())
-    except OSError as e:
-        return bytes("Requested command does not exist", 'utf-8')
+    return pypandoc.convert_text(markdown, to_format, format='md')
 
 
 def get_print_options() -> [str]:
