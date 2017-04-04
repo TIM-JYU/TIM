744146a60d6137dff0acda9c1a81a303ec89191a
Mika Lehtinen <mikkalle@student.jyu.fi>
2017-04-04 15:55:21 +0300

    Fix video.js define call

--- timApp/modules/svn/js/video.js
+++ timApp/modules/svn/js/video.js
@@ -1,4 +1,4 @@
-﻿define("require", "exports", "angular", "tim/timHelper", function (require, exports, angular, timHelper) {
+﻿define(["require", "exports", "angular", "tim/timHelper"], function (require, exports, angular, timHelper) {
 
 // Workaround for lazy loading: the directive definition functions below are
 // not re-run when reloading the module
