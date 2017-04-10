9ec7b8f9cfb101af63c23b5fc1a60ae7218dccc8
Topi Latva-Salo <tohesala@student.jyu.fi>
2017-04-10 22:49:48 +0300

    Add functionality
    The printing routes are now able to output a document at a given path to both latex and
    pdf, which are stored in the tim database as bytearrays.
    
    PrintSettings object handles standardizing the handling of print settings, providing default values and function to hash the settings.

--- timApp/routes/print.py
+++ timApp/routes/print.py
@@ -1,21 +1,17 @@
 """
 Routes for printing a document
 """
-import datetime
-import string
 
 from flask import Blueprint
-from flask import abort
 from flask import Response
-from flask import stream_with_context
 from flask import g
+from flask import abort
 
 from accesshelper import verify_logged_in
 from dbaccess import get_timdb
 from timdb.models.docentry import DocEntry
-from sessioninfo import get_current_user_object
-from documentprinter import DocumentPrinter
-from timdb.printsettings import PrintSettings, PrintFormats
+from documentprinter import DocumentPrinter, PrintingError
+from timdb.printsettings import PrintSettings
 from timdb.tim_models import PrintedDoc
 
 print_blueprint = Blueprint('print',
@@ -30,11 +26,27 @@ def do_before_requests():
     g.PrintSettings = PrintSettings()
 
 
-@print_blueprint.route("/<path:doc_path>", methods=['GET'])
-def print_document(doc_path):
+@print_blueprint.route("/latex/<path:doc_path>", methods=['GET'])
+def print_document_to_latex(doc_path):
     doc = DocEntry.find_by_path(doc_path)
-    printed_doc = add_printed_doc_to_db(doc_entry=doc, file_type=PrintFormats.LATEX, temp=True, settings=PrintSettings())
+    settings_used = PrintSettings()
+
+    printed_doc = fetch_document_from_db(doc_entry=doc, file_type='latex', settings=settings_used)
+    if printed_doc is None:
+        printed_doc = add_printed_doc_to_db(doc_entry=doc, file_type='latex', temp=True, settings=PrintSettings())
     return Response(printed_doc, mimetype='text/plain')
+
+
+@print_blueprint.route("/pdf/<path:doc_path>", methods=['GET'])
+def print_document_to_pdf(doc_path):
+    doc = DocEntry.find_by_path(doc_path)
+    settings_used = PrintSettings()
+
+    printed_doc = fetch_document_from_db(doc_entry=doc, file_type='pdf', settings=settings_used)
+    if printed_doc is None:
+        printed_doc = add_printed_doc_to_db(doc_entry=doc, file_type='pdf', temp=True, settings=settings_used)
+    return Response(printed_doc, mimetype='application/pdf')
+
 
 
 @print_blueprint.route("/edit_settings", methods=['POST'])
@@ -42,49 +54,52 @@ def edit_settings():
     return
 
 
-def get_markdown(doc_path):
-    # Define a generator that assembles the response content piece by piece.
-    """Returns the paragraphs as plain markdown for the specified document.
+def fetch_document_from_db(doc_entry: DocEntry, file_type: str, settings: PrintSettings) -> bytearray:
+    """
+    Fetches the given document from the database.
 
-    :param doc_path: The document path.
-    :return: The document markdown.
-
+    :param doc_entry:
+    :param file_type:
+    :param settings:
+    :return:
     """
 
-    # Search for the document in the database based on its path. We also search the translations.
-    d = DocEntry.find_by_path(doc_path, try_translation=True)
-    # If the document is not found, an error is returned.
-    if not d:
-        abort(404)
-
-    doc = '\n\n'.join(par.get_markdown() for par in d.document.get_paragraphs())
-
-    return doc
+    db = get_timdb()
+    identical_document = db.session.query(PrintedDoc).\
+        filter(PrintedDoc.doc_id == doc_entry.document.doc_id).\
+        filter(PrintedDoc.filetype == file_type).\
+        filter(PrintedDoc.settings_hash == settings.hash_value).first()
+    return None if identical_document is None else identical_document.content
 
 
-def add_printed_doc_to_db(doc_entry: DocEntry, file_type: PrintFormats, temp: bool, settings: PrintSettings) -> bytearray:
+def add_printed_doc_to_db(doc_entry: DocEntry, file_type: str, temp: bool, settings: PrintSettings) -> bytearray:
     """
     Adds a marking for a printed document to the db
 
 
     :param doc_entry: Document that is being printed
     :param file_type: File type for the document
-    :param temp: Is the doc saved on a temporary basis
+    :param temp: Is the document stored only temporarily (gets deleted after some time)
     :param settings: The settings object of the printing transaction
     :return: id of the newly-created db-entry
     """
 
-    p_doc = PrintedDoc(doc_id=doc_entry.document.doc_id,
-                       temp=temp,
-                       content=DocumentPrinter(doc_entry=doc_entry, print_settings=settings).write_tex(),
-                       filetype=file_type,
-                       settings_hash=settings.hash_value,
-                       created=datetime.date())
-    db = get_timdb()
-    db.session.add(p_doc)
-    db.commit()
+    try:
+        doc_content = DocumentPrinter(doc_entry=doc_entry, print_settings=settings).write_to_format(file_type=file_type)
 
-    return p_doc.content
+        p_doc = PrintedDoc(doc_id=doc_entry.document.doc_id,
+                           content=doc_content,
+                           filetype=str(file_type),
+                           temp=True,
+                           settings_hash=settings.hash_value)
+        db = get_timdb()
+        db.session.add(p_doc)
+        db.commit()
+
+        return p_doc.content
+
+    except PrintingError as err:
+        abort(403, str(err))
 
 
 def get_printed_document_from_db(printed_doc_id: int):
--- timApp/timdb/printsettings.py
+++ timApp/timdb/printsettings.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+from documentmodel.randutils import hashfunc
 from timdb.models.user import User
 
 
@@ -10,8 +11,15 @@ class PrintFormats(Enum):
 
 class PrintSettings:
 
-    def __init(self):
-        return
+    def __init__(self, font_size: int = 12, margins: float = 2.5):
+        """
+
+        :param font_size:
+        """
+        self.settings = {}
+
+        self.settings.pop('font_size', font_size)
+        self.settings.pop('margins', margins)
 
     # def __init__(self, user: User):
     #    self.user = user
@@ -22,4 +30,4 @@ class PrintSettings:
 
     @property
     def hash_value(self) -> int:
-        return hash(self) #TODO: sensible behaviour
+        return hashfunc(','.join([pair for pair in self.settings]))
