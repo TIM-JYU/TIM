5316338463ed5a73afcd4e8a25f3e24a5a866705
Mika Lehtinen <mikkalle@student.jyu.fi>
2017-04-13 19:08:22 +0300

    Add required commands to build_js script

--- build_js.sh
+++ build_js.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 # Builds the NPM package at timApp/static/scripts.
 
+./run_command_workdir.sh timApp/static/scripts npm install
+./run_command_workdir.sh timApp/static/scripts jspm install
 ./run_command_workdir.sh timApp/static/scripts npm run build
