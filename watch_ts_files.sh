12cb3bfb7f74303764195f631ba789ad2f985d5f
Mika Lehtinen <mikkalle@student.jyu.fi>
2017-04-28 19:31:59 +0300

    Improve comments and help messages for TS file watcher

--- watch_ts_files.sh
+++ watch_ts_files.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+# Activates a file watcher that triggers JS rebuild whenever a file belonging to the bundle changes.
+# This includes all TypeScript files except tim/ace.ts and tim/imagex.ts.
 
 . ./variables.sh
 
 docker rm -f ts-watcher-${TIM_NAME} 2> /dev/null
-
-# Launches a file watcher for TypeScript files and rebuilds JS on the fly.
 DOCKERFLAGS="-d --name ts-watcher-${TIM_NAME}" ./run_command_workdir.sh timApp/static/scripts jspm bundle tim/main + tim/slide - "(tim/main + tim/slide - [tim/**/*])" build/tim.js -wm
+echo It takes a while for the watcher to do the initial build, after which it starts watching. Check the status with: docker logs ts-watcher-${TIM_NAME}.
