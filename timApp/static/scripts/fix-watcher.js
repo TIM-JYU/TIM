90d61cd8646510210ebf03367c9ff2779ae2b0ca
Mika Lehtinen <mikkalle@student.jyu.fi>
2017-05-11 15:59:20 +0300

    Use polling by default in JSPM watcher

--- timApp/static/scripts/fix-watcher.js
+++ timApp/static/scripts/fix-watcher.js
@@ -3,8 +3,9 @@ const replace = require('replace-in-file');
 // Fixes some issues with JSPM watcher:
 // - watchman is off by default
 // - config files are excluded from the set of watched files because otherwise the underlying fs.watch call will also
-//   watch node_modules and jspm_packages
+//   watch node_modules and jspm_packages that have lots of files
 // - fixes apparently incorrect value passed as glob
+// - uses polling (it is more reliable; the non-poll method sometimes misses changes)
 replace.sync({
     files: 'node_modules/jspm/lib/bundle.js',
     from: [
@@ -15,6 +16,6 @@ replace.sync({
     to: [
         'files = files.concat([]);',
         'var watchman = false;',
-        'glob: relFiles'
+        'glob: relFiles, poll: true, interval: 500'
     ]
 });
