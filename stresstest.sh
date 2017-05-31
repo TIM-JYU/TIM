c27bf0ecac1f9ec37d91e710c8d3f07ae0a8c8f9
Mika Lehtinen <mikkalle@student.jyu.fi>
2017-05-31 14:25:03 +0300

    Fix stress test script

---
+++ stresstest.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [ $# -ne "1" ]; then
+ echo Stress test for TIM instance. Makes n simultaneous wget calls.
+ echo Usage examples:
+ echo ./stresstest.sh 1
+ echo ./stresstest.sh 128
+ exit
+fi
+
+read -r -d '' SCRIPT <<'EOF'
+dir=/tmp/stresstest
+mkdir -p ${dir}
+rm ${dir}/*
+time (
+eval "for i in {1..$1};do wget --timeout=0 tim:5000/view/1 -O ${dir}/stresstest.\$i & done" 2>/dev/null
+wait
+)
+echo "Errors: $(ls -la ${dir} | grep -c "  0")"
+EOF
+
+./docker-compose.sh exec csplugin /bin/bash -c "${SCRIPT}" "" "$@"
