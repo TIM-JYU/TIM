36ecdb0a8460e3eab6e287480b55c5891116c47e
Mika Lehtinen <mikkalle@student.jyu.fi>
2017-06-12 10:42:32 +0300

    Fix 3rdparty blueprint path

--- timApp/routes/static_3rdparty.py
+++ timApp/routes/static_3rdparty.py
@@ -2,5 +2,5 @@ from flask import Blueprint
 
 static_blueprint = Blueprint('3rdparty',
                              __name__,
-                             static_folder='static/scripts/jspm_packages/npm',
+                             static_folder='../static/scripts/jspm_packages/npm',
                              static_url_path='/static/scripts/jspm_packages/npm')
