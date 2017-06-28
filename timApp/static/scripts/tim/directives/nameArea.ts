f729ad761b59db33ae3d253d6cea03fe69bdb2d8
Mika Lehtinen <mikkalle@student.jyu.fi>
2017-06-28 11:14:19 +0300

    Make nameArea a component

--- timApp/static/scripts/tim/directives/nameArea.ts
+++ timApp/static/scripts/tim/directives/nameArea.ts
@@ -1,71 +1,75 @@
-
+import {IRootElementService} from "angular";
 import $ from "jquery";
 import {timApp} from "tim/app";
+
+class NameAreaController {
+    private static $inject = ["$element"];
+    private $area: JQuery;
+    private areaName: string;
+    private options: {collapse: boolean; hlevel: number};
+    private datePickerOptions: {format: string; showTodayButton: boolean};
+    private element: IRootElementService;
+    private onClose: (e: JQuery) => void;
+    private onCancel: (e: JQuery) => void;
+    private onOk: (area: JQuery, name: string, options) => void;
+
+    constructor(element: IRootElementService) {
+        this.element = element;
+        this.$area = element.parents(".area").first();
+
+        const area = $("#areaname");
+        area.keypress(function(e) {
+            if (e.which == 13) {
+                this.addArea();
+            }
+        });
+
+        this.areaName = "";
+        this.options = {collapse: true, hlevel: 0};
+        element.css("position", "absolute"); // IE needs this
+        this.datePickerOptions = {
+            format: "D.M.YYYY HH:mm:ss",
+            showTodayButton: true,
+        };
+
+        area.focus();
+    }
+
+    closePopup() {
+        this.element.remove();
+
+        if (this.onClose) {
+            this.onClose(this.$area);
+        }
+    }
+
+    addArea() {
+        this.closePopup();
+
+        if (this.onOk) {
+            this.onOk(this.$area, this.areaName, this.options);
+        }
+    }
+
+    cancelAdd() {
+        this.closePopup();
+
+        if (this.onCancel) {
+            this.onCancel(this.$area);
+        }
+    }
+}
+
 /**
  * A popup window directive that is used in the document view
  * when creating a new area.
  */
-timApp.directive("nameArea", [function() {
-    return {
-        restrict: "E",
-        scope: true, // we need functions from parent scope
-        templateUrl: "/static/templates/nameArea.html",
-        replace: true,
-
-        link($scope, $element, $attrs) {
-            $scope.$area = $element.parents(".area").first();
-            if ($attrs.onclose) {
-                $scope.onClose = eval("$scope." + $attrs.onclose);
-            }
-            if ($attrs.onok) {
-                $scope.onOk = eval("$scope." + $attrs.onok);
-            }
-            if ($attrs.oncancel) {
-                $scope.onCancel = eval("$scope." + $attrs.oncancel);
-            }
-
-            $("#areaname").keypress(function(e) {
-                if (e.which == 13) {
-                    $scope.addArea();
-                }
-            });
-        },
-
-        controller: ["$scope", "$element", function($scope, $element) {
-            $scope.closePopup = function() {
-                $scope.$destroy();
-                $element.remove();
-
-                if ($scope.onClose) {
-                    $scope.onClose($scope.$area);
-                }
-            };
-
-            $scope.addArea = function() {
-                $scope.closePopup();
-
-                if ($scope.onOk) {
-                    $scope.onOk($scope.$area, $scope.areaName, $scope.options);
-                }
-            };
-
-            $scope.cancelAdd = function() {
-                $scope.closePopup();
-
-                if ($scope.onCancel) {
-                    $scope.onCancel($scope.$area);
-                }
-            };
-
-            $scope.areaName = "";
-            $scope.options = {collapse: true, hlevel: 0};
-            $element.css("position", "absolute"); // IE needs this
-            $scope.datePickerOptions = {
-                format: "D.M.YYYY HH:mm:ss",
-                showTodayButton: true,
-            };
-
-            $("#areaname").focus();
-        }],
-    };
-}]);
+timApp.component("nameArea", {
+    bindings: {
+        onCancel: "&",
+        onClose: "&",
+        onOk: "&",
+    },
+    controller: NameAreaController,
+    templateUrl: "/static/templates/nameArea.html",
+});
