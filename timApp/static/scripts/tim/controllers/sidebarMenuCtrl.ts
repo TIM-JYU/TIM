1982906e33303c0bc5823e254d16c126c3e6dd2e
Mika Lehtinen <mikkalle@student.jyu.fi>
2017-07-05 15:45:07 +0300

    Make sidebar menu a component

--- timApp/static/scripts/tim/controllers/sidebarMenuCtrl.ts
+++ timApp/static/scripts/tim/controllers/sidebarMenuCtrl.ts
@@ -1,11 +1,7 @@
 import $ from "jquery";
 import {timApp} from "tim/app";
-import * as userService from "tim/services/userService";
-import {markAsUsed} from "tim/utils";
-import {Users} from "../services/userService";
+import {Users, UserService} from "../services/userService";
 import {$http, $log, $window} from "../ngimport";
-
-markAsUsed(userService);
 
 /**
  * FILL WITH SUITABLE TEXT
@@ -19,124 +15,144 @@ markAsUsed(userService);
  * @copyright 2015 Timppa project authors
  */
 
-timApp.controller("SidebarMenuCtrl", ["$scope",
+export class SidebarMenuCtrl {
+    private currentLecturesList: {}[];
+    private futureLecturesList: {}[];
+    private pastLecturesList: {}[];
+    private lectureQuestions: {}[];
+    private materialQuestions: {}[];
+    private users: UserService;
+    private leftSide: JQuery;
+    private active: number;
+    private lastTab: number;
+    private bookmarks: {};
+    private docId: number;
 
-    function($scope) {
-        "use strict";
-        $scope.currentLecturesList = [];
-        $scope.futureLecturesList = [];
-        $scope.pastLecturesList = [];
-        $scope.lectureQuestions = [];
-        $scope.materialQuestions = [];
-        $scope.users = Users;
-        $scope.bookmarks = $window.bookmarks; // from base.html
-        $scope.leftSide = $(".left-fixed-side");
+    constructor() {
+        this.currentLecturesList = [];
+        this.futureLecturesList = [];
+        this.pastLecturesList = [];
+        this.lectureQuestions = [];
+        this.materialQuestions = [];
+        this.users = Users;
+        this.bookmarks = $window.bookmarks; // from base.html
+        this.leftSide = $(".left-fixed-side");
 
-        $scope.active = -1;
+        this.active = -1;
         if ($window.showIndex) {
-            $scope.active = 0;
+            this.active = 0;
         } else if (Users.isLoggedIn()) {
             // make bookmarks tab active
-            $scope.active = 6;
+            this.active = 6;
         }
-        $scope.lastTab = $scope.active;
+        this.lastTab = this.active;
 
-        $scope.updateLeftSide = function() {
-            if ($("#menuTabs").is(":visible")) {
-                $scope.leftSide.css("min-width", "12em");
-            } else {
-                $scope.leftSide.css("min-width", "0");
-            }
-        };
+        this.updateLeftSide();
+        $($window).resize(() => this.updateLeftSide());
+    }
 
-        $scope.updateLeftSide();
-        $($window).resize($scope.updateLeftSide);
+    updateLeftSide() {
+        if ($("#menuTabs").is(":visible")) {
+            this.leftSide.css("min-width", "12em");
+        } else {
+            this.leftSide.css("min-width", "0");
+        }
+    }
 
-        $scope.bookmarkTabSelected = function(isSelected) {
-            const tabContent = $("#menuTabs").find(".tab-content");
-            if (isSelected) {
-                // The dropdown menu is clipped if it's near right side of the menu without applying this hack
-                // Also the dropdown menu causes vertical scrollbar to appear without specifying height
-                tabContent.css("height", "calc(100vh - 51.2833px)");
-                tabContent.css("overflow-x", "visible");
-                tabContent.css("overflow-y", "visible");
-            } else {
-                tabContent.css("height", "auto");
-                tabContent.css("overflow-x", "hidden");
-                tabContent.css("overflow-y", "auto");
-            }
-        };
+    bookmarkTabSelected(isSelected) {
+        const tabContent = $("#menuTabs").find(".tab-content");
+        if (isSelected) {
+            // The dropdown menu is clipped if it's near right side of the menu without applying this hack
+            // Also the dropdown menu causes vertical scrollbar to appear without specifying height
+            tabContent.css("height", "calc(100vh - 51.2833px)");
+            tabContent.css("overflow-x", "visible");
+            tabContent.css("overflow-y", "visible");
+        } else {
+            tabContent.css("height", "auto");
+            tabContent.css("overflow-x", "hidden");
+            tabContent.css("overflow-y", "auto");
+        }
+    }
 
-        /**
-         * FILL WITH SUITABLE TEXT
-         * @memberof module:sidebarMenuCtrl
-         */
-        $scope.showSidebar = function() {
-            const tabs = $("#menuTabs");
-            if (tabs.is(":visible")) {
-                if ($scope.active !== null) {
-                    $scope.lastTab = $scope.active;
-                    $scope.active = -1; // this will set the value to null and remove the "selected" state from tab
-                    if ($(".device-xs").is(":visible") || $(".device-sm").is(":visible")) {
-                        tabs.hide();
-                        $scope.leftSide.css("min-width", "0");
-                    }
-                } else {
-                    $scope.active = $scope.lastTab;
+    /**
+     * FILL WITH SUITABLE TEXT
+     * @memberof module:sidebarMenuCtrl
+     */
+    showSidebar() {
+        const tabs = $("#menuTabs");
+        if (tabs.is(":visible")) {
+            if (this.active !== null) {
+                this.lastTab = this.active;
+                this.active = -1; // this will set the value to null and remove the "selected" state from tab
+                if ($(".device-xs").is(":visible") || $(".device-sm").is(":visible")) {
+                    tabs.hide();
+                    this.leftSide.css("min-width", "0");
                 }
             } else {
-                tabs.show();
-                $scope.leftSide.css("min-width", "12em");
-                tabs.attr("class", "");
-                if ($scope.active === null) {
-                    $scope.active = $scope.lastTab || 0;
-                }
+                this.active = this.lastTab;
             }
-        };
+        } else {
+            tabs.show();
+            this.leftSide.css("min-width", "12em");
+            tabs.attr("class", "");
+            if (this.active === null) {
+                this.active = this.lastTab || 0;
+            }
+        }
+    }
 
-        /**
-         * FILL WITH SUITABLE TEXT
-         * @memberof module:sidebarMenuCtrl
-         */
-        $scope.toggleLectures = function() {
-            $http<{currentLectures, futureLectures, pastLectures}>({
-                url: "/getAllLecturesFromDocument",
-                method: "GET",
-                params: {doc_id: $scope.docId},
-            })
-                .then(function(response) {
-                    const lectures = response.data;
-                    $scope.currentLecturesList = lectures.currentLectures;
-                    $scope.futureLecturesList = lectures.futureLectures;
-                    $scope.pastLecturesList = lectures.pastLectures;
-                }, function() {
-                    $log.error("Couldn't fetch the lectures");
-                });
-        };
+    /**
+     * FILL WITH SUITABLE TEXT
+     * @memberof module:sidebarMenuCtrl
+     */
+    toggleLectures() {
+        $http<{currentLectures, futureLectures, pastLectures}>({
+            url: "/getAllLecturesFromDocument",
+            method: "GET",
+            params: {doc_id: this.docId},
+        })
+            .then(function(response) {
+                const lectures = response.data;
+                this.currentLecturesList = lectures.currentLectures;
+                this.futureLecturesList = lectures.futureLectures;
+                this.pastLecturesList = lectures.pastLectures;
+            }, function() {
+                $log.error("Couldn't fetch the lectures");
+            });
+    }
 
-        /**
-         * FILL WITH SUITABLE TEXT
-         * @memberof module:sidebarMenuCtrl
-         */
-        $scope.toggleQuestions = function() {
-            // Does not work anymore after changing questions part of document
-            $scope.lectureQuestions = [];
-            $http<Array<{question_id, questionjson}>>({
-                url: "/questions/" + $scope.docId,
-                method: "GET",
-            })
-                .then(function(response) {
-                    const questions = response.data;
-                    for (let i = 0; i < questions.length; i++) {
-                        const question = {
-                            questionId: questions[i].question_id,
-                            questionTitle: (JSON.parse(questions[i].questionjson)).questionTitle,
-                        };
-                        $scope.lectureQuestions.push(question);
-                    }
-                }, function() {
-                    $log.error("Couldn't fetch the questions");
-                });
-        };
+    /**
+     * FILL WITH SUITABLE TEXT
+     * @memberof module:sidebarMenuCtrl
+     */
+    toggleQuestions() {
+        // Does not work anymore after changing questions part of document
+        this.lectureQuestions = [];
+        $http<Array<{question_id, questionjson}>>({
+            url: "/questions/" + this.docId,
+            method: "GET",
+        })
+            .then(function(response) {
+                const questions = response.data;
+                for (let i = 0; i < questions.length; i++) {
+                    const question = {
+                        questionId: questions[i].question_id,
+                        questionTitle: (JSON.parse(questions[i].questionjson)).questionTitle,
+                    };
+                    this.lectureQuestions.push(question);
+                }
+            }, function() {
+                $log.error("Couldn't fetch the questions");
+            });
+    }
+}
+
+timApp.component("timSidebarMenu", {
+    controller: SidebarMenuCtrl,
+    require: {
+        lctrl: "?^timLectureView",
+        vctrl: "?^timView",
     },
-]);
+    template: "<div ng-transclude></div>",
+    transclude: true,
+});
