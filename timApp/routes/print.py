e7d4161aa6ef68560c2f181f1a3b46aee864cd7b
Topi Latva-Salo <tohesala@student.jyu.fi>
2017-07-09 23:18:15 +0300

    Restructure printing routes and stop recreation of already created doc prints
    The print is now created with a HTTP POST call to the printing
    endpoint, and the document can be viewed with a GET call to the same endpoint.

--- timApp/routes/print.py
+++ timApp/routes/print.py
@@ -5,7 +5,7 @@ import os
 from typing import Optional
 
 from datetime import datetime
-from flask import Blueprint, send_file, jsonify
+from flask import Blueprint, send_file, jsonify, json
 from flask import Response
 from flask import g
 from flask import abort
@@ -47,48 +47,85 @@ def pull_doc_path(endpoint, values):
             abort(404)
 
 
-@print_blueprint.route("/<path:doc_path>", methods=['GET'])
+@print_blueprint.route("/<path:doc_path>", methods=['POST'])
 def print_document(doc_path):
+
+    data = request.get_json(silent=True)
+    file_type = data.get('fileType')
+    template_doc_id = data.get('templateDocId')
+
+    if file_type is None:
+        abort(400, "No filetype selected.")
+
+    if template_doc_id is None:
+        abort(400, "No template doc selected.")
+
+    file_type = str(file_type)
+    template_doc_id = int(float(template_doc_id))
+
+    if (file_type.lower() not in [f.value for f in PrintFormat]):
+        abort(400, "The supplied parameter 'fileType' is invalid.")
+
     doc = g.doc_entry
+    template_doc = DocEntry.find_by_id(template_doc_id)
+    type = PrintFormat[file_type.upper()]
+
+    if get_document_print(doc_entry=doc, template=template_doc, file_type=type) is not None:
+        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+
     if os.environ.get('TIM_HOST', None) != request.url_root:
         os.environ['TIM_HOST'] = request.url_root
 
+    if template_doc is None:
+        abort(400, "The supplied parameter 'templateDocId' is invalid.")
+
+    try:
+        create_printed_doc(doc_entry=doc, file_type=type, template_doc=template_doc, temp=True)
+    except PrintingError as err:
+        abort(500, str(err))
+
+    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+
+
+@print_blueprint.route("/<path:doc_path>", methods=['GET'])
+def get_printed_document(doc_path):
+    doc = g.doc_entry
+
     file_type = request.args.get('file_type')
     template_doc_id = request.args.get('template_doc_id')
-    #print("file_type: %s" % file_type)
+
     if file_type is None or file_type.lower() not in [f.value for f in PrintFormat]:
-        abort(404, "The supplied parameter 'file_type' was not valid.")
+        abort(400, "The supplied query parameter 'file_type' was invalid.")
 
     if template_doc_id is None:
-        abort(404, "You need to supply a value for parameter 'template_doc_id'.")
+        abort(400, "The supplied query parameter 'template_doc_id' was invalid.")
 
     template_doc_id = int(float(template_doc_id))
-    template_doc = DocEntry.query.filter(DocEntry.id == template_doc_id).first()
+    template_doc = DocEntry.find_by_id(template_doc_id)
 
     if template_doc is None:
-        abort(404, "The supplied parameter 'template_doc_id' was not valid.")
+        abort(400, "The supplied parameter 'template_doc_id' was invalid.")
 
     type = PrintFormat[file_type.upper()]
 
-    print("document: %s\ntemplate_doc: %s" % (doc.name, template_doc.name))
-    path_to_doc = ""
-    try:
-        path_to_doc = create_printed_doc(doc_entry=doc, file_type=type, template_doc=template_doc, temp=True)
-    except PrintingError as err:
-        abort(404, str(err))
+    path_to_doc = get_document_print(doc_entry=doc, template=template_doc, file_type=type)
 
+    if path_to_doc is None:
+        abort(404, "The document you tried to fetch does not exist.")
 
     mime = get_mimetype_for_format(type)
 
     if mime is None:
-        abort(404, "The supplied parameter 'file_type' was not valid.")
+        abort(400, "An unexpected error occurred.")
 
     response = make_response(send_file(filename_or_fp=path_to_doc, mimetype=mime))
-    # response.headers['Last-Modified'] = datetime.now()
-    # TODO: not sure if all these are needed
+
+    # Add headers to stop the documents from caching
+    # This is needed for making sure the current version of the document is actually retrieved
     response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate'
     response.headers['Pragma'] = 'no-cache'
     response.headers['Expires'] = '-1'
+
     return response
 
 
@@ -110,18 +147,19 @@ def get_mimetype_for_format(file_type: PrintFormat):
         return None
 
 
-def fetch_document_from_db(doc_entry: DocEntry, file_type: PrintFormat) -> Optional[str]:
+def get_document_print(doc_entry: DocEntry, template: DocEntry, file_type: PrintFormat) -> Optional[str]:
     """
     Fetches the given document from the database.
 
     :param doc_entry:
+    :param template:
     :param file_type:
     :return:
     """
 
-    # TODO: Do something meaningful!
+    printer = DocumentPrinter(doc_entry=doc_entry, template_to_use=template)
 
-    return None
+    return printer.get_printed_document_path_from_db(file_type=file_type)
 
 
 def create_printed_doc(doc_entry: DocEntry, template_doc: DocEntry, file_type: PrintFormat, temp: bool) -> str:
@@ -143,9 +181,9 @@ def create_printed_doc(doc_entry: DocEntry, template_doc: DocEntry, file_type: P
 
     existing_doc_path = printer.get_printed_document_path_from_db(file_type=file_type)
 
-    #if existing_doc_path is not None:
-    #    print("already exists:\n" + existing_doc_path)
-    #    return existing_doc_path
+    if existing_doc_path is not None:
+        print("already exists:\n" + existing_doc_path)
+        return existing_doc_path
 
     try:
         path = printer.get_print_path(temp=temp, file_type=file_type)
@@ -174,4 +212,4 @@ def create_printed_doc(doc_entry: DocEntry, template_doc: DocEntry, file_type: P
 
         return p_doc.path_to_file
     except PrintingError as err:
-        abort(403, str(err))
+        abort(500, str(err))
