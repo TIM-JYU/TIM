a06104774e06f4b6708c8ee75576fab66a140a73
Mika Lehtinen <mikkalle@student.jyu.fi>
2017-09-04 16:22:28 +0300

    Include "query" column, exclude "psql" application

--- timApp/sql/activity.sql
+++ timApp/sql/activity.sql
@@ -1,4 +1,6 @@
 SELECT datname as db, pid, usesysid, usename, application_name as app_name,
   client_addr, client_port as port, backend_start,
-  xact_start, query_start, state_change, waiting, state, backend_xid, backend_xmin
-FROM pg_stat_activity;
+  xact_start, query_start, state_change, waiting, state, backend_xid, backend_xmin, query
+FROM pg_stat_activity
+WHERE application_name <> 'psql'
+;
