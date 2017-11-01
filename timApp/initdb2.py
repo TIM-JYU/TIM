e9b4a3c36f7ba523ea5ff65e16f762d9bb47f5be
Mika Lehtinen <mikkalle@student.jyu.fi>
2017-11-01 13:55:21 +0200

    Add missing import

--- timApp/initdb2.py
+++ timApp/initdb2.py
@@ -19,6 +19,7 @@ from timApp.logger import log_info, enable_loggers, log_error, log_warning
 from timApp.maintenance.migrate_to_postgre import perform_migration
 from timApp.tim_app import app
 from timApp.timdb import tempdb_models
+from timApp.timdb.documents import import_document_from_file
 from timApp.timdb.models.docentry import DocEntry
 from timApp.timdb.models.user import User
 from timApp.timdb.tim_models import AccessType, db
