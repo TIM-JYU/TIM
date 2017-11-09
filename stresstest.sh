1e4f7e5474c354c94a72160688a7b90957d5e48c
Mika Lehtinen <mikkalle@student.jyu.fi>
2017-11-09 09:56:58 +0200

    Make doc id configurable in stresstest script

--- stresstest.sh
+++ stresstest.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-if [ $# -ne "1" ]; then
+if [ $# -ne "2" ]; then
  echo Stress test for TIM instance. Makes n simultaneous wget calls.
- echo Usage examples:
- echo ./stresstest.sh 1
- echo ./stresstest.sh 128
+ echo "Usage: ./stresstest.sh <number of calls> <doc id or path>"
+ echo Examples:
+ echo ./stresstest.sh 1 1
+ echo ./stresstest.sh 128 1
  exit
 fi
 
@@ -13,7 +14,7 @@ dir=/tmp/stresstest
 mkdir -p ${dir}
 rm ${dir}/* 2>/dev/null
 time (
-eval "for i in {1..$1};do wget --timeout=0 tim:5000/view/1 -O ${dir}/stresstest.\$i & done" 2>/dev/null
+eval "for i in {1..$1};do wget --timeout=0 tim:5000/view/$2 -O ${dir}/stresstest.\$i & done" 2>/dev/null
 wait
 )
 echo "Errors: $(ls -la ${dir} | grep -c "  0")"
