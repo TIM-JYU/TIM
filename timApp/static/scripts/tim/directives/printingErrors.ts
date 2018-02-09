8ce940431f082694360198b619cfdd4cc6d21e04
Mika Lehtinen <mikkalle@student.jyu.fi>
2018-02-09 15:35:03 +0200

    Delete unused component

--- timApp/static/scripts/tim/directives/printingErrors.ts
+++
@@ -1,8 +0,0 @@
-import {timApp} from "../app";
-
-timApp.component("printingErrors", {
-    bindings: {
-        errorMsg: "<",
-    },
-    template: `<div><h4>An error occurred during printing!</h4><p>{{$ctrl.errorMsg}}</p></div>`,
-});
