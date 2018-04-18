1b859e332baa718a95c91337cd62cfea72e963d4
Mika Lehtinen <mikkalle@student.jyu.fi>
2018-04-18 09:34:27 +0300

    Fix wrong parameter name

--- timApp/static/scripts/tim/directives/pareditor.ts
+++ timApp/static/scripts/tim/directives/pareditor.ts
@@ -1005,7 +1005,7 @@ export function openEditorSimple(docId: number, text: string) {
             tags: [],
             touchDevice: false,
         }, previewCb: async (txt) => {
-            const resp = await $http.post<IPluginInfoResponse>(`/preview/${docId}`, {txt, isComment: true});
+            const resp = await $http.post<IPluginInfoResponse>(`/preview/${docId}`, {text: txt, isComment: true});
             return resp.data;
         },
         saveCb: async (txt, data) => {
