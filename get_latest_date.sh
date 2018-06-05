344c4c9b4fd9eb4d0492a3383c7dee6988ebab2b
Mika Lehtinen <mikkalle@student.jyu.fi>
2018-06-05 11:50:20 +0300

    Use full commit hash in get_latest_date
    Different Git versions abbreviate the commit hash differently, causing problems e.g. in GitLab CI. The full commit hash always has the same length.

--- get_latest_date.sh
+++ get_latest_date.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-git --git-dir=${DIR}/.git --work-tree=${DIR} log -1 --date=short --format="%cd-%h" $DIR/timApp/Dockerfile
+git --git-dir=${DIR}/.git --work-tree=${DIR} log -1 --date=short --format="%cd-%H" $DIR/timApp/Dockerfile
