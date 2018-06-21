2a6312050d73e13c6da27f8541eb0705fecbf9d3
Visa Naukkarinen <visa.naukkarinen@gmail.com>
2018-06-21 13:13:51 +0300

    Create 'teachers' group

---
+++ timApp/migrations/versions/010b7b2d8521_create_teachers_group.py
@@ -0,0 +1,27 @@
+"""Create teachers group
+
+Revision ID: 010b7b2d8521
+Revises: abcc9a7a0d91
+Create Date: 2018-06-21 09:52:22.512960
+
+"""
+
+# revision identifiers, used by Alembic.
+from timApp.user.usergroup import UserGroup
+
+revision = '010b7b2d8521'
+down_revision = 'abcc9a7a0d91'
+
+from alembic import op
+import sqlalchemy as sa
+from timApp.timdb.sqa import db
+
+
+def upgrade():
+    db.session.add(UserGroup(name="teachers"))
+    db.session.commit()
+
+
+def downgrade():
+    db.session.delete(UserGroup.get_by_name("teachers"))
+    db.session.commit()
