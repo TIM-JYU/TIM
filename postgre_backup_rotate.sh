72144b7e3511cb505b1510a9c84327f48b66de4c
Mika Lehtinen <mikkalle@student.jyu.fi>
2018-07-04 14:16:55 +0300

    Increase PostgreSQL backup rotation period

--- postgre_backup_rotate.sh
+++ postgre_backup_rotate.sh
@@ -4,5 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ${DIR}/postgre_backup.sh ${DIR}/pg_backup
 
-# delete all backups older than 2 days
-find ${DIR}/pg_backup/ -type f -mtime +2 -name '*.gz' -exec rm -- {} \;
+# delete all backups older than a week
+find ${DIR}/pg_backup/ -type f -mtime +6 -name '*.gz' -exec rm -- {} \;
