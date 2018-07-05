9fcf7cd7199df057e3fa0f5abf0130c8a37acc18
Visa Naukkarinen <visa.naukkarinen@gmail.com>
2018-07-05 13:11:22 +0300

    Add search options and saving them to local storage to search dialog

--- timApp/static/scripts/tim/item/tagSearchCtrl.ts
+++ timApp/static/scripts/tim/item/tagSearchCtrl.ts
@@ -7,6 +7,8 @@ import * as focusMe from "tim/ui/focusMe";
 import {DialogController, registerDialogComponent, showDialog} from "../ui/dialog";
 import {markAsUsed} from "../util/utils";
 import {IItem} from "./IItem";
+import {ngStorage} from "ngstorage";
+import {$localStorage} from "../util/ngimport";
 
 markAsUsed(focusMe);
 
@@ -17,9 +19,17 @@ export class ShowTagSearchController extends DialogController<{ params: IItem },
     private static $inject = ["$element", "$scope"];
     private enableSearch = true;
     private header = "";
+    private advancedOptions = false;
+    private caseSensitive = false;
+    private listDocTags = true;
+    private exactMatch = false;
+    private storage: ngStorage.StorageService & {searchOptionStorage: null | boolean[]};
 
     constructor(protected element: IRootElementService, protected scope: IScope) {
         super(element, scope);
+        this.storage = $localStorage.$default({
+            searchOptionStorage: null,
+        });
     }
 
     /*
@@ -29,7 +39,19 @@ export class ShowTagSearchController extends DialogController<{ params: IItem },
         super.$onInit();
         if (this.enableSearch) {
             this.header = "Input a tag name to search documents";
+            if (this.storage.searchOptionStorage && this.storage.searchOptionStorage.length === 3) {
+                this.caseSensitive = this.storage.searchOptionStorage[0];
+                this.exactMatch = this.storage.searchOptionStorage[1];
+                this.listDocTags = this.storage.searchOptionStorage[2];
         }
+        }
+    }
+
+    $onDestroy() {
+        this.storage.searchOptionStorage = [];
+        this.storage.searchOptionStorage.push(this.caseSensitive);
+        this.storage.searchOptionStorage.push(this.exactMatch);
+        this.storage.searchOptionStorage.push(this.listDocTags);
     }
 
     /*
@@ -50,8 +72,22 @@ registerDialogComponent("timSearchTags",
     </dialog-header>
     <dialog-body>
         <h4>{{$ctrl.header}}</h4>
-        <tagged-document-list enable-search="$ctrl.enableSearch" tag-filter="" exact-match="false"
-                              list-doc-tags="true" case-sensitive="false"></tagged-document-list>
+        <tagged-document-list enable-search="$ctrl.enableSearch" tag-filter=""
+        exact-match="$ctrl.exactMatch" list-doc-tags="$ctrl.listDocTags"
+        case-sensitive="$ctrl.caseSensitive"></tagged-document-list>
+    <div>
+        <label class="font-weight-normal"><input type="checkbox" ng-model="$ctrl.advancedOptions"
+        class="ng-pristine ng-untouched ng-valid ng-not-empty"> Advanced options</label>
+    </div>
+    <div ng-if="$ctrl.advancedOptions">
+        <label class="font-weight-normal"><input type="checkbox" ng-model="$ctrl.exactMatch"
+        class="ng-pristine ng-untouched ng-valid ng-not-empty"> Search exact words</label>
+        <label class="font-weight-normal"><input type="checkbox" ng-model="$ctrl.caseSensitive"
+        class="ng-pristine ng-untouched ng-valid ng-not-empty"> Case sensitive</label>
+        <label class="font-weight-normal"><input type="checkbox" ng-model="$ctrl.listDocTags"
+        class="ng-pristine ng-untouched ng-valid ng-not-empty"
+        title="List all document tags in search results"> List all tags</label>
+    </div>
     </dialog-body>
     <dialog-footer>
         <button class="timButton" ng-click="$ctrl.dismiss()">Close</button>
