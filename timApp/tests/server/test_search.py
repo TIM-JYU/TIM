e9924adb728b656e5868f2dca82fd4d527b40c9a
Visa Naukkarinen <visa.naukkarinen@gmail.com>
2018-07-20 12:26:30 +0300

    Update tests and add title search test

--- timApp/tests/server/test_search.py
+++ timApp/tests/server/test_search.py
@@ -11,37 +11,39 @@ class SearchTest(TimRouteTest):
         d = self.create_doc(initial_par=text_in_document)
         url = f'search?caseSensitive=false&folder=&ignorePluginsSettings=false&onlyfirst=999' \
               f'&query={text_to_search}&regex=false&searchDocNames=false&searchWords=true'
-        self.get(url, expect_status=200, expect_content=[{'doc': {'id': d.id,
-                                                                  'isFolder': False,
-                                                                  'location': d.location,
-                                                                  'modified': 'just now',
-                                                                  'name': d.short_name,
-                                                                  'owner': {'id': self.get_test_user_1_group_id(),
-                                                                            'name': u.name},
-                                                                  'path': d.path,
-                                                                  'public': True,
-                                                                  'rights': {'browse_own_answers': True,
-                                                                             'can_comment': True,
-                                                                             'can_mark_as_read': True,
-                                                                             'editable': True,
-                                                                             'manage': True,
-                                                                             'owner': True,
-                                                                             'see_answers': True,
-                                                                             'teacher': True},
-                                                                  'title': d.title,
-                                                                  'unpublished': True},
-                                                          'in_title': False,
-                                                          'match_end_index': 9,
-                                                          'match_start_index': 6,
-                                                          'match_word': text_to_search,
-                                                          'num_pars': 1,
-                                                          'num_pars_found': 1,
-                                                          'num_results': 1,
-                                                          'par': {'attrs': {},
-                                                                  'html': f'<p>{text_in_document}</p>',
-                                                                  'id': '02rrzIY0IaHV',
-                                                                  'md': text_in_document,
-                                                                  't': 'MHgzMTA3NTA0Zg=='}}])
+        self.get(url, expect_status=200, expect_content={'complete': True,
+                                                         'errors': [],
+                                                         'results': [{'doc': {'id': d.id,
+                                                                              'isFolder': False,
+                                                                              'location': d.location,
+                                                                              'modified': 'just now',
+                                                                              'name': d.short_name,
+                                                                              'owner': {
+                                                                                  'id': self.get_test_user_1_group_id(),
+                                                                                  'name': u.name},
+                                                                              'path': d.path,
+                                                                              'public': True,
+                                                                              'rights': {'browse_own_answers': True,
+                                                                                         'can_comment': True,
+                                                                                         'can_mark_as_read': True,
+                                                                                         'editable': True,
+                                                                                         'manage': True,
+                                                                                         'owner': True,
+                                                                                         'see_answers': True,
+                                                                                         'teacher': True},
+                                                                              'title': d.title,
+                                                                              'unpublished': True},
+                                                                      'in_title': False,
+                                                                      'match_end_index': 9,
+                                                                      'match_start_index': 6,
+                                                                      'match_word': text_to_search,
+                                                                      'par': {'attrs': {},
+                                                                              'html': f'<p>{text_in_document}</p>',
+                                                                              'id': '02rrzIY0IaHV',
+                                                                              'md': text_in_document,
+                                                                              't': 'MHgzMTA3NTA0Zg=='}}],
+                                                         'titleResultCount': 0,
+                                                         'wordResultCount': 1})
 
     def test_too_short_search(self):
         self.login_test1()
@@ -50,14 +52,18 @@ class SearchTest(TimRouteTest):
         url = f'search?caseSensitive=false&folder=&ignorePluginsSettings=false&onlyfirst=999' \
               f'&query={text_to_search}&regex=false&searchDocNames=false&searchWords=true'
         self.get(url, expect_status=400,
-                 expect_content={'error': 'Search text must be at least 2 characters long with whitespace stripped.'})
+                 expect_content={'error': 'Search text must be at least 3 character(s) long with whitespace stripped.'})
 
     def test_not_found_search(self):
         self.login_test1()
         text_to_search = 'Another text to search'
         url = f'search?caseSensitive=false&folder=&ignorePluginsSettings=false&onlyfirst=999' \
               f'&query={text_to_search}&regex=false&searchDocNames=false&searchWords=true'
-        self.get(url, expect_status=200, expect_content=[])
+        self.get(url, expect_status=200, expect_content={'complete': True,
+                                                         'errors': [],
+                                                         'results': [],
+                                                         'titleResultCount': 0,
+                                                         'wordResultCount': 0})
 
     def test_case_sensitive_search(self):
         self.login_test1()
@@ -67,4 +73,46 @@ class SearchTest(TimRouteTest):
         text_to_search_upper = 'TEXT TO SEARCH'
         url = f'search?caseSensitive={case_sensitive}&folder=&ignorePluginsSettings=false&onlyfirst=999' \
               f'&query={text_to_search_upper}&regex=false&searchDocNames=false&searchWords=true'
-        self.get(url, expect_status=200, expect_content=[])
+        self.get(url, expect_status=200, expect_content={'complete': True,
+                                                         'errors': [],
+                                                         'results': [],
+                                                         'titleResultCount': 0,
+                                                         'wordResultCount': 0})
+
+    def test_title_search(self):
+        u = self.test_user_1
+        self.login_test1()
+        d = self.create_doc()
+        search_titles = True
+        text_to_search = d.title
+        url = f'search?caseSensitive=false&folder=&ignorePluginsSettings=false&onlyfirst=999' \
+              f'&query={text_to_search}&regex=false&searchDocNames={search_titles}&searchWords=false'
+        self.get(url, expect_status=200, expect_content={'complete': True,
+                                                         'errors': [],
+                                                         'results': [{'doc': {'id': d.id,
+                                                                              'isFolder': False,
+                                                                              'location': d.location,
+                                                                              'modified': 'just now',
+                                                                              'name': d.short_name,
+                                                                              'owner': {
+                                                                                  'id': self.get_test_user_1_group_id(),
+                                                                                  'name': u.name},
+                                                                              'path': d.path,
+                                                                              'public': True,
+                                                                              'rights': {'browse_own_answers': True,
+                                                                                         'can_comment': True,
+                                                                                         'can_mark_as_read': True,
+                                                                                         'editable': True,
+                                                                                         'manage': True,
+                                                                                         'owner': True,
+                                                                                         'see_answers': True,
+                                                                                         'teacher': True},
+                                                                              'title': d.title,
+                                                                              'unpublished': True},
+                                                                      'in_title': True,
+                                                                      'match_end_index': len(d.title),
+                                                                      'match_start_index': 0,
+                                                                      'match_word': text_to_search,
+                                                                      'par': None}],
+                                                         'titleResultCount': 1,
+                                                         'wordResultCount': 0})
