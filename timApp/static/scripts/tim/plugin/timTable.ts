46dd76d3767a4fe33191b9a89fb957953bccfa3b
Rami Pasanen <rampastring@outlook.com>
2018-07-27 18:04:05 +0300

    Reduce width of empty cells to 1.5em

--- timApp/static/scripts/tim/plugin/timTable.ts
+++ timApp/static/scripts/tim/plugin/timTable.ts
@@ -864,7 +864,7 @@ export class TimTableController implements IController {
 
         if (this.getCellContentString(rowi, coli) === "") {
             styles["height"] = "2em";
-            styles["width"] = "2em";
+            styles["width"] = "1.5em";
         }
 
         const cell = this.cellDataMatrix[rowi][coli];
