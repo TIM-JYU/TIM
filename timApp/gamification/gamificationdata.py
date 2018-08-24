9f7fd49fe4dc52fd19a8e47407c2657da2f3d12e
Visa Naukkarinen <visa.naukkarinen@gmail.com>
2018-08-24 12:54:06 +0300

    Add error handling and remove unused code

--- timApp/gamification/gamificationdata.py
+++ timApp/gamification/gamificationdata.py
@@ -1,14 +1,13 @@
 """Collection of gamification functions."""
-from typing import List, Union, Tuple, Optional, Any, Dict
+from collections import defaultdict
 
 from flask import request
 
 import timApp.plugin.pluginControl
-from timApp.timdb.dbaccess import get_timdb
-from timApp.document.yamlblock import YamlBlock
 from timApp.auth.sessioninfo import get_current_user_id
-from timApp.document.docinfo import DocInfo
 from timApp.document.docentry import DocEntry
+from timApp.document.yamlblock import YamlBlock
+from timApp.timdb.dbaccess import get_timdb
 
 
 def gamify(initial_data: YamlBlock):
@@ -58,44 +57,7 @@ def get_doc_data(gamify_data: YamlBlock):
 
     demos = get_demo_data(demo_paths, default_max)
 
-    # Configure data of demo documents
-    # demos = []
-    # for path in demo_paths:
-    #     demo = DocEntry.find_by_path(path['path'])
-    #
-    #     if demo is not None:
-    #         doc = path.get('shortname', demo.short_name)
-    #         doc_max_points = path.get('max_points', None)
-    #         temp_dict = dict()
-    #         temp_dict['id'] = demo.id
-    #         temp_dict['name'] = doc
-    #         temp_dict['link'] = request.url_root + 'view/' + demo.path
-    #         if doc_max_points is None:
-    #             doc_set = demo.document.get_settings()
-    #             doc_max_points = doc_set.max_points() or default_max
-    #         if doc_max_points is not None:
-    #             temp_dict['maxPoints'] = doc_max_points
-    #         temp_dict['gotPoints'] = get_points_for_doc(demo)  # TODO: must pick all docs at once
-    #         demos.append(temp_dict)
-
     return lectures, demos, button_text
-
-
-def get_points(doc_id, task_info_list):
-    """
-    Get total points for one document from a list of all tasks.
-    :param doc_id: Demo document id.
-    :param task_info_list: All tasks including their document ids.
-    :return: Total points for the demo document.
-    """
-    user_points = 0
-    for entrys in task_info_list:
-        try:
-            if str(entrys['doc_id']) == str(doc_id):
-                user_points += (entrys['total_points'])
-        except KeyError:
-            continue
-    return user_points
 
 
 def get_demo_data(demos, default_max):
@@ -115,48 +77,34 @@ def get_demo_data(demos, default_max):
         if path is not None:
             demo_path_list.append(path)
             filtered_demos.append(demo)
-    demos = filtered_demos   # Leave out demos with None paths.
+    demos = filtered_demos  # Leave out demos with None paths.
     docs = DocEntry.query.filter(DocEntry.name.in_(demo_path_list)).all()
+
+    # If demos don't match the documents fetched from their paths, there's an error.
+    if len(demos) is not len(docs):
+        invalid_paths = []
+        for doc, demo in zip(docs, demos):
+            if demo.get('path') != doc.path:
+                invalid_paths.append(demo.get('path'))
+        raise GamificationException(f"Failed to fetch following demo document(s): {invalid_paths}")
 
     for doc in docs:
         task_id_list += (timApp.plugin.pluginControl.find_task_ids(doc.document.get_paragraphs()))[0]
     task_info_list = get_timdb().answers.get_users_for_tasks(task_id_list, [get_current_user_id()], group_by_doc=True)
-
+    task_info_dict = defaultdict(int)
+    for task in task_info_list:
+        task_info_dict[task.get('doc_id')] += int(task.get('total_points'))
     for doc, demo in zip(docs, demos):
         doc_max_points = demo.get('max_points', None)
-        temp_dict = dict()
-        temp_dict['id'] = doc.id
-        temp_dict['name'] = doc.short_name
-        temp_dict['link'] = request.url_root + 'view/' + doc.path
+        temp_dict = dict(id=doc.id, name=doc.short_name, link=doc.url)
         if doc_max_points is None:
             doc_set = doc.document.get_settings()
             doc_max_points = doc_set.max_points() or default_max
         if doc_max_points is not None:
             temp_dict['maxPoints'] = doc_max_points
-        temp_dict['gotPoints'] = get_points(doc.id, task_info_list)
+        temp_dict['gotPoints'] = task_info_dict[str(doc.id)]
         results.append(temp_dict)
     return results
-
-
-def get_points_for_doc(d: DocInfo):
-    """Finds the current users point information for a specific document.
-
-    :param d: The document.
-    :returns: The current users points for the document
-
-    """
-    document = d.document
-    timdb = get_timdb()
-    user_points = 0
-    task_id_list = (timApp.plugin.pluginControl.find_task_ids(document.get_paragraphs()))
-
-    users_task_info = timdb.answers.get_users_for_tasks(task_id_list[0], [get_current_user_id()])
-
-    for entrys in users_task_info:
-        if entrys['total_points'] is not None:
-            user_points += (entrys['total_points'])
-
-    return user_points
 
 
 class GamificationException(Exception):
