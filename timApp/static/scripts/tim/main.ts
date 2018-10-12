4a20cbb2b0fb8837f1c3b614c6cea45d25b62171
Mika Lehtinen <mikkalle@student.jyu.fi>
2018-10-12 09:51:39 +0300

    Improve names of main.ts imports

--- timApp/static/scripts/tim/main.ts
+++ timApp/static/scripts/tim/main.ts
@@ -1,45 +1,81 @@
 import angular from "angular";
-import ad from "bootstrap";
+import bootstrap from "bootstrap";
 import $ from "jquery";
-import * as q from "tim/answer/answerbrowser3";
-import * as n from "tim/answer/userlistController";
-import * as ah from "tim/bookmark/bookmarkFolderBox";
-import * as r from "tim/bookmark/bookmarks";
+import * as answerbrowser from "tim/answer/answerbrowser3";
+import * as userlistController from "tim/answer/userlistController";
+import * as bookmarkFolderBox from "tim/bookmark/bookmarkFolderBox";
+import * as bookmarks from "tim/bookmark/bookmarks";
 import * as templateList from "tim/document/editing/templateList";
-import * as g from "tim/document/question/questionController";
-import * as o from "tim/document/viewctrl";
-import * as v from "tim/editor/pareditor";
-import * as d from "tim/folder/indexCtrl";
-import * as m from "tim/frontpage/startController";
-import * as b from "tim/item/breadcrumbs";
-import * as t from "tim/item/createItem";
-import * as ab from "tim/item/manageCtrl";
-import * as x from "tim/item/rightsEditor";
-import * as ag from "tim/item/taggedDocumentList";
-import * as a from "tim/lecture/answerToQuestionController";
-import * as c from "tim/lecture/createLectureCtrl";
-import * as e from "tim/lecture/lectureController";
-import * as f from "tim/lecture/lectureInfoController";
+import * as questionController from "tim/document/question/questionController";
+import * as viewctrl from "tim/document/viewctrl";
+import * as pareditor from "tim/editor/pareditor";
+import * as indexCtrl from "tim/folder/indexCtrl";
+import * as startController from "tim/frontpage/startController";
+import * as loadMap from "tim/gamification/loadMap";
+import * as breadcrumbs from "tim/item/breadcrumbs";
+import * as createItem from "tim/item/createItem";
+import * as manageCtrl from "tim/item/manageCtrl";
+import * as rightsEditor from "tim/item/rightsEditor";
+import * as taggedDocumentList from "tim/item/taggedDocumentList";
+import * as answerToQuestionController from "tim/lecture/answerToQuestionController";
+import * as createLectureCtrl from "tim/lecture/createLectureCtrl";
+import * as lectureController from "tim/lecture/lectureController";
+import * as lectureInfoController from "tim/lecture/lectureInfoController";
 import * as lectureMenu from "tim/lecture/lectureMenu";
-import * as h from "tim/lecture/questionAskController";
-import * as j from "tim/lecture/showStatisticsToQuestionController";
-import * as ae from "tim/plugin/qstController";
-import * as af from "tim/plugin/timTable";
-import * as ak from "tim/plugin/tape";
-import * as ai from "tim/search/searchBox";
-import * as aj from "tim/gamification/loadMap";
-import * as k from "tim/sidebar/sidebarMenuCtrl";
-import * as s from "tim/ui/bootstrapPanel";
-import * as u from "tim/user/loginMenu";
-import * as ac from "tim/user/settingsCtrl";
+import * as questionAskController from "tim/lecture/questionAskController";
+import * as showStatisticsToQuestionController from "tim/lecture/showStatisticsToQuestionController";
+import * as qstController from "tim/plugin/qstController";
+import * as tape from "tim/plugin/tape";
+import * as timTable from "tim/plugin/timTable";
+import * as searchBox from "tim/search/searchBox";
+import * as sidebarMenuCtrl from "tim/sidebar/sidebarMenuCtrl";
+import * as bootstrapPanel from "tim/ui/bootstrapPanel";
+import * as loginMenu from "tim/user/loginMenu";
+import * as settingsCtrl from "tim/user/settingsCtrl";
 import {markAsUsed} from "tim/util/utils";
-import * as p from "tim/velp/annotation";
-import * as i from "tim/velp/reviewController";
-import * as y from "tim/velp/velpSelection";
+import * as annotation from "tim/velp/annotation";
+import * as reviewController from "tim/velp/reviewController";
+import * as velpSelection from "tim/velp/velpSelection";
 import {ParCompiler} from "./editor/parCompiler";
 import {insertLogDivIfEnabled, timLogInit, timLogTime} from "./util/timTiming";
 
-markAsUsed(a, b, c, d, e, f, g, h, i, j, k, m, n, o, p, q, r, s, t, u, v, x, y, ab, ac, ad, ae, ag, ah, ai, aj, lectureMenu, templateList, af, ak);
+markAsUsed(
+    annotation,
+    answerbrowser,
+    answerToQuestionController,
+    bookmarkFolderBox,
+    bookmarks,
+    bootstrap,
+    bootstrapPanel,
+    breadcrumbs,
+    createItem,
+    createLectureCtrl,
+    indexCtrl,
+    lectureController,
+    lectureInfoController,
+    lectureMenu,
+    loadMap,
+    loginMenu,
+    manageCtrl,
+    pareditor,
+    qstController,
+    questionAskController,
+    questionController,
+    reviewController,
+    rightsEditor,
+    searchBox,
+    settingsCtrl,
+    showStatisticsToQuestionController,
+    sidebarMenuCtrl,
+    startController,
+    taggedDocumentList,
+    tape,
+    templateList,
+    timTable,
+    userlistController,
+    velpSelection,
+    viewctrl,
+);
 
 timLogInit(document.location.search.slice(1));
 
