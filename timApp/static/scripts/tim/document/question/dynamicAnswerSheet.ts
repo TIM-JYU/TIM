6b4ad41d4df12c00f4e710561ec979bdd86920d1
Mika Lehtinen <mikkalle@student.jyu.fi>
2018-10-29 09:54:24 +0200

    Remove a usage of "any"

--- timApp/static/scripts/tim/document/question/dynamicAnswerSheet.ts
+++ timApp/static/scripts/tim/document/question/dynamicAnswerSheet.ts
@@ -311,9 +311,8 @@ class AnswerSheetController implements IController {
     }
 
     private getInputClass(rowIndex: number, colIndex: number) {
-        const pts:any = this.getPoints(rowIndex, colIndex);
-        const pt:any = pts *1;
-        return pt != NaN && pt > 0 ? "qst-correct" : "qst-normal";
+        const pts = this.getPoints(rowIndex, colIndex);
+        return pts != null && parseFloat(pts) > 0 ? "qst-correct" : "qst-normal";
     }
 
     private canShowExpl(): boolean {
