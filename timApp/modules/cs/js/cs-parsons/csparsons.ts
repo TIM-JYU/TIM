d51be78f9a4959f47e65b4f155129d94d3444a71
Mika Lehtinen <mikkalle@student.jyu.fi>
2018-11-27 19:54:57 +0200

    Apply TSLint to csparsons.ts

--- timApp/modules/cs/js/cs-parsons/csparsons.ts
+++ timApp/modules/cs/js/cs-parsons/csparsons.ts
@@ -1,182 +1,207 @@
 import $ from "jquery";
-import "./jquery.ui.touch-punch.min.js";
 import "./jquery-ui-sortable.min.js";
+import "./jquery.ui.touch-punch.min.js";
 
-type CsParsonsOptions = {
-    shuffle: boolean,
-    sortable: Element,
-    notordermatters: boolean,
-    words: boolean,
-    maxcheck: number,
-    separator?: string,
-    minWidth: string,
-    styleWords: string,
-    onChange?(widget: CsParsonsWidget);
-};
+interface CsParsonsOptions {
+    shuffle: boolean;
+    sortable: Element;
+    notordermatters: boolean;
+    words: boolean;
+    maxcheck: number | undefined;
+    separator?: string;
+    minWidth: string;
+    styleWords: string;
+
+    onChange?(widget: CsParsonsWidget): void;
+}
 
 export class CsParsonsWidget {
     options: CsParsonsOptions;
-    text: string;
-    lines: string[];
+    text: string = "";
+    lines: string[] = [];
 
     constructor(options: CsParsonsOptions) {
-        var defaults = {
-            separator : "\n",
-            styleWords: ""
+        const defaults = {
+            separator: "\n",
+            styleWords: "",
         };
 
         this.options = $.extend({}, defaults, options);
-    };
+    }
 
-    shuffle(lines) {
-        var result = lines.slice();
-        var n = lines.length;
-        for (var i = n-1; i >= 0; i--) {
-            var j = Math.floor(Math.random() * (i+1));
-            var tmp = result[i];
+    shuffle(lines: string[]) {
+        const result = lines.slice();
+        const n = lines.length;
+        for (let i = n - 1; i >= 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            const tmp = result[i];
             result[i] = result[j];
             result[j] = tmp;
         }
         return result;
-    };
+    }
 
-    init(text,userText) {
+    init(text: string, userText: string) {
         this.text = text;
-        if ( this.options.shuffle ) {
-            this.lines =  text.split("\n");
+        if (this.options.shuffle) {
+            this.lines = text.split("\n");
             this.lines = this.shuffle(this.lines);
+        } else {
+            this.lines = userText.split("\n");
         }
-        else
-            this.lines =  userText.split("\n");
-    };
+    }
 
     show() {
-        var classes = "sortable-code sortable-output";
-        var parsonsEditDiv = this.options.sortable;
-        var type = "div";
-        if ( this.options.words ) {
+        let classes = "sortable-code sortable-output";
+        const parsonsEditDiv = this.options.sortable;
+        let type = "div";
+        if (this.options.words) {
             classes += " sortable-words ";
             this.options.separator = " ";
             type = "span";
         }
-        var words = this.lines;
+        const words = this.lines;
         parsonsEditDiv.innerHTML = "";
-        for (var i = 0; i < words.length; i++) {
-            var div;
-            var typ=type;
-            var w = words[i];
-            if ( this.options.words && w === "" ) {
+        for (let i = 0; i < words.length; i++) {
+            let div;
+            let w = words[i];
+            if (this.options.words && w === "") {
                 div = document.createElement("div");
-                typ = div;
                 w = "\\n";
-            } else  {
+            } else {
                 div = document.createElement(type);
-                if ( this.options.words && this.options.minWidth && w.length < 3 )
-                    div.setAttribute('style',"width: " + this.options.minWidth );
+                if (this.options.words && this.options.minWidth && w.length < 3) {
+                    div.setAttribute("style", "width: " + this.options.minWidth);
+                }
             }
-            div.setAttribute('class',"sortitem");
+            div.setAttribute("class", "sortitem");
 
-            var t = document.createTextNode(w) as any;
+            const t = document.createTextNode(w) as any;
             t.title = w; // TODO this may be wrong; TS complains without "any"
             div.appendChild(t);
-            //var div2 = document.createElement(typ);
-            //div2.appendChild(div);
+            // var div2 = document.createElement(typ);
+            // div2.appendChild(div);
             parsonsEditDiv.appendChild(div);
-            if ( this.options.maxcheck && this.options.maxcheck == i+1) {
+            if (this.options.maxcheck && this.options.maxcheck == i + 1) {
                 div = document.createElement(type);
                 div.className = "parsonsstatic";
                 parsonsEditDiv.appendChild(div);
             }
-
         }
-        parsonsEditDiv.setAttribute('class',classes);
-        parsonsEditDiv.setAttribute('style',"float: left; width: 100%" + ";" + this.options.styleWords);
-        let a = $(parsonsEditDiv);
-        if ( this.options.maxcheck )
-            a.sortable( {
-                items: ':not(.parsonsstatic)',
-                start: function(){
-                    $('.parsonsstatic', this).each(function(){
-                        var $this = $(this);
-                        $this.data('pos', $this.index());
+        parsonsEditDiv.setAttribute("class", classes);
+        parsonsEditDiv.setAttribute("style", "float: left; width: 100%" + ";" + this.options.styleWords);
+        const a = $(parsonsEditDiv);
+        if (this.options.maxcheck) {
+            a.sortable({
+                items: ":not(.parsonsstatic)",
+                start: function() {
+                    $(".parsonsstatic", this).each(function() {
+                        const $this = $(this);
+                        $this.data("pos", $this.index());
                     });
                 },
-                change: function(){
-                    let $sortable = $(this);
-                    let $statics = $('.parsonsstatic', this).detach();
-                    let $helper = $('<'+type+'></'+type+'>').prependTo(this);
-                    $statics.each(function(){
-                        var $this = $(this);
-                        var target = $this.data('pos');
+                change: function() {
+                    const $sortable = $(this);
+                    const $statics = $(".parsonsstatic", this).detach();
+                    const $helper = $("<" + type + "></" + type + ">").prependTo(this as any);
+                    $statics.each(function() {
+                        const $this = $(this);
+                        const target = $this.data("pos");
 
                         $this.insertAfter($(type, $sortable).eq(target));
                     });
                     $helper.remove();
-                }
+                },
             });
-        else a.sortable();
-        var parson = this;
+        } else {
+            a.sortable();
+        }
         // a.sortable( "option", "axis", "x" );
-        a.on( "sortstop", function( event, ui ) {
-             if ( parson.options.onChange ) parson.options.onChange(parson);
-             parson.clear();
-          } );
-    };
+        a.on("sortstop", (event, ui) => {
+            if (this.options.onChange) {
+                this.options.onChange(this);
+            }
+            this.clear();
+        });
+    }
 
-    join(separator) {
-        var result = "";
-        var sep = "";
-        var div = this.options.sortable;
-        var nosep = false;
-        if ( !div ) return "";
-        for (var i = 0; i < div.childElementCount; i++) {
-            var node = div.childNodes[i];
-            var line = node.textContent;
-            if ( line === "" ) continue; // separatorline
-            if ( line === "\\n" ) { line = "\n"; sep = ""; nosep = true; }
+    join(separator: string) {
+        let result = "";
+        let sep = "";
+        const div = this.options.sortable;
+        let nosep = false;
+        if (!div) {
+            return "";
+        }
+        for (let i = 0; i < div.childElementCount; i++) {
+            const node = div.childNodes[i];
+            let line = node.textContent;
+            if (line === "") {
+                continue;
+            } // separatorline
+            if (line === "\\n") {
+                line = "\n";
+                sep = "";
+                nosep = true;
+            }
             result += sep + line;
             sep = separator;
-            if ( nosep ) sep = "";
+            if (nosep) {
+                sep = "";
+            }
             nosep = false;
         }
 
         return result;
     }
 
-    check(userText) {
-        var result = "";
-        var div = this.options.sortable;
-        var lines = this.text.split("\n");
-        var ulines = userText.split("\n");
-        if ( !div ) return "";
-        var maxn = div.childElementCount;
-        if ( this.options.maxcheck && this.options.maxcheck < div.childElementCount) maxn = this.options.maxcheck
-        for (var i = 0; i < maxn; i++) {
-            var node = div.children[i];
-            var nodeok = true;
-            if ( this.options.notordermatters ) {
+    check(userText: string) {
+        const result = "";
+        const div = this.options.sortable;
+        const lines = this.text.split("\n");
+        const ulines = userText.split("\n");
+        if (!div) {
+            return "";
+        }
+        let maxn = div.childElementCount;
+        if (this.options.maxcheck && this.options.maxcheck < div.childElementCount) {
+            maxn = this.options.maxcheck;
+        }
+        for (let i = 0; i < maxn; i++) {
+            const node = div.children[i];
+            let nodeok = true;
+            if (this.options.notordermatters) {
                 nodeok = false;
-                for (var j=0; j<maxn; j++) {
-                    if ( lines[j] === ulines[i] ) {
+                for (let j = 0; j < maxn; j++) {
+                    if (lines[j] === ulines[i]) {
                         lines[j] = "XXXXXXXXXXXXXX";
                         nodeok = true;
                         break;
                     }
                 }
-            } else { if ( lines[i] !== ulines[i] ) nodeok = false; }
-            if ( !nodeok ) node.setAttribute('style',"background-color: RED;");
-            else if ( this.options.maxcheck )  node.setAttribute('style',"background-color: LIGHTGREEN;");
+            } else {
+                if (lines[i] !== ulines[i]) {
+                    nodeok = false;
+                }
+            }
+            if (!nodeok) {
+                node.setAttribute("style", "background-color: RED;");
+            } else if (this.options.maxcheck) {
+                node.setAttribute("style", "background-color: LIGHTGREEN;");
+            }
         }
 
         return result;
     }
 
     clear() {
-        var div = this.options.sortable;
-        if ( !div ) return "";
+        const div = this.options.sortable;
+        if (!div) {
+            return "";
+        }
         for (let i = 0; i < div.childElementCount; i++) {
-            var node = div.children[i];
-            node.removeAttribute('style');
+            const node = div.children[i];
+            node.removeAttribute("style");
         }
     }
 }
