c53e4c776a96ebb5368589c2ec2ca232c5e801b2
Visa Naukkarinen <visa.naukkarinen@gmail.com>
2018-12-11 13:31:50 +0200

    Add default and inheritance tests

--- timApp/tests/server/test_relevance.py
+++ timApp/tests/server/test_relevance.py
@@ -3,10 +3,13 @@ from timApp.tests.server.timroutetest import TimRouteTest
 
 class RelevanceTest(TimRouteTest):
 
-    def test_get_relevance_none(self):
+    def test_get_relevance_default(self):
         self.login_test3()
         d = self.create_doc()
-        self.get(f'/items/relevance/get/{d.id}', expect_content=None)
+        self.get(f'/items/relevance/get/{d.id}', expect_content={
+            'default': True,
+            'inherited': False,
+            'relevance': {'block_id': d.id, 'relevance': 10}})
 
     def test_set_relevance_and_get(self):
         self.login_test3()
@@ -26,6 +29,24 @@ class RelevanceTest(TimRouteTest):
             'default': False,
             'inherited': False,
             'relevance': {'block_id': d.id, 'relevance': 50}})
+
+    def test_get_relevance_inherited(self):
+        self.login_test3()
+        d = self.create_doc(self.get_personal_item_path('a/b/doc'))
+
+        # Test inheriting changing the relevance.
+        self.json_post(f'/items/relevance/set/{d.parent.id}', {'value': 0})
+        self.get(f'/items/relevance/get/{d.id}', expect_content={
+            'default': False,
+            'inherited': True,
+            'relevance': {'block_id': d.parent.id, 'relevance': 0}})
+
+        # Test inheritance overridden by target item value.
+        self.json_post(f'/items/relevance/set/{d.id}', {'value': -100})
+        self.get(f'/items/relevance/get/{d.id}', expect_content={
+            'default': False,
+            'inherited': False,
+            'relevance': {'block_id': d.id, 'relevance': -100}})
 
     def test_set_relevance_no_manage_rights(self):
         self.login_test3()
