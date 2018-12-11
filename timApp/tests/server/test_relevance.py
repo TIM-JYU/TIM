39b8f3025691a95eac02f3a72b01d4dd33cb8359
Visa Naukkarinen <visa.naukkarinen@gmail.com>
2018-12-11 11:44:31 +0200

    Update tests

--- timApp/tests/server/test_relevance.py
+++ timApp/tests/server/test_relevance.py
@@ -12,14 +12,20 @@ class RelevanceTest(TimRouteTest):
         self.login_test3()
         d = self.create_doc()
         self.json_post(f'/items/relevance/set/{d.id}', {'value': 100})
-        self.get(f'/items/relevance/get/{d.id}', expect_content={'block_id': d.id, 'relevance': 100})
+        self.get(f'/items/relevance/get/{d.id}', expect_content={
+            'default': False,
+            'inherited': False,
+            'relevance': {'block_id': d.id, 'relevance': 100}})
 
     def test_set_and_replace_relevance_and_get(self):
         self.login_test3()
         d = self.create_doc()
         self.json_post(f'/items/relevance/set/{d.id}', {'value': 100})
         self.json_post(f'/items/relevance/set/{d.id}', {'value': 50})
-        self.get(f'/items/relevance/get/{d.id}', expect_content={'block_id': d.id, 'relevance': 50})
+        self.get(f'/items/relevance/get/{d.id}', expect_content={
+            'default': False,
+            'inherited': False,
+            'relevance': {'block_id': d.id, 'relevance': 50}})
 
     def test_set_relevance_no_manage_rights(self):
         self.login_test3()
