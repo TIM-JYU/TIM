d76e18edede294d6e271f55052b76ebe1449605b
Vesa Lappalainen <vesal@jyu.fi>
2018-12-25 21:31:26 +0200

    kirjoitusvirhe pois

--- timApp/modules/cs/languages.py
+++ timApp/modules/cs/languages.py
@@ -951,14 +951,14 @@ class Stack(Language):
         nosave = self.query.jso.get('input', {}).get('nosave', False)
         stack_data["seed"] = userseed
         q = stack_data.get("question","")
-        if not self.query.jso.get('markup').get('stackjsx') and q.find("[[jsxgraph ") >= 0:  # make jsxgraph replace
-            q = q.replace('[[jsxgraph ','[[jsxgraphapi ')
-            q = q.replace('[[/jsxgraph]]','[[/jsxgraphapi]]')
-
-            stack_data["question"] = q
+        # if not self.query.jso.get('markup').get('stackjsx') and q.find("[[jsxgraph ") >= 0:  # make jsxgraph replace
+        #    q = q.replace('[[jsxgraph ','[[jsxgraphapi ')
+        #    q = q.replace('[[/jsxgraph]]','[[/jsxgraphapi]]')
+        #
+        #    stack_data["question"] = q
 
         if nosave or get_task:
-            stack_data['score'] = Falseplo
+            stack_data['score'] = False
             stack_data['feedback'] = False
         stack_data["answer"] = data.get("answer")
         stack_data["prefix"] = data.get("prefix")
