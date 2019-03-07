ad403d519102e0004593c10b7f4678a06dd4b3db
jejoojal <jejoojal@student.jyu.fi>
2019-03-07 16:10:03 +0200

    Add more constructed report compiling

--- timApp/answer/feedbackanswer.py
+++ timApp/answer/feedbackanswer.py
@@ -81,13 +81,20 @@ def get_all_feedback_answers(task_ids: List[TaskId],
     qq: Iterable[Tuple[Answer, User, int]] = q
     cnt = 0
     #pt_dt = datetime(1, 1, 1, tzinfo='UTC')    #muistiinmenevä tehtävän tekoaiku
-    pt_dt = None
-    result = ""
+    pt_dt = None    #Previous Datetime
+    prev_ans = None     #Previous Answer
+    prev_user = None     #Previous User
+    temp_bool = True    #Temporary answer combination condition
+
+    #FOR STARTS FROM HERE
     for answer, user, n in qq:
 
+        """
         points = str(answer.points)
         if points == "None":
             points = ""
+        """
+        """
         name = user.name
         n = str(int(n))
         # header = f"{name};{answer.task_id};{str(answer.answered_on)};{n};{points}"
@@ -109,12 +116,38 @@ def get_all_feedback_answers(task_ids: List[TaskId],
 
         taskid = answer.task_id
         i = taskid.find(".")
+
         if i >= 0:
             taskid = taskid[i + 1:]
         result += f"{taskid};{answer_str};{str(math.floor(tasksecs))}"
         if (cnt % 2 == 0 and cnt > 0):
             results.append(result)
         cnt += 1
+        """
+        result = ""
+        if prev_user == None: prev_user = user
+        if prev_ans == None: prev_ans = answer
+        if temp_bool:
+            result += f"{prev_user};"
+            line = json.loads(prev_ans.content)
+            result += f"{line};"
+            line = json.loads(answer.content)
+            result += f"{line};"
+
+            if pt_dt != None:
+                tasksecs = (prev_ans.answered_on - pt_dt).total_seconds()
+            else:
+                tasksecs = 0.0
+
+            fbsecs = (answer.answered_on - prev_ans.answered_on).total_seconds()
+
+            result += f"{str(math.floor(tasksecs))};"
+            result += f"{str(math.floor(fbsecs))};"
+            if pt_dt != None : results.append(result)   #IF-CONDITION temporary for the sake of excluding the first sample item
+        pt_dt = prev_ans.answered_on
+        prev_ans = answer
+        temp_bool = not temp_bool
+
 
     return results
 
