3891803182823fbe0e053ed7c2cb60c6464f9ede
Visa Naukkarinen <visa.naukkarinen@gmail.com>
2019-03-15 12:24:38 +0200

    Save page language to local storage and reformat HTML

--- timApp/static/scripts/tim/frontpage/startController.ts
+++ timApp/static/scripts/tim/frontpage/startController.ts
@@ -5,31 +5,36 @@ import {timApp} from "../app";
 import {showCourseListDialog} from "../document/course/courseListDialogCtrl";
 import {ICourseSettings, IItem} from "../item/IItem";
 import {showMessageDialog} from "../ui/dialog";
-import {$http, $window} from "../util/ngimport";
+import {$http, $window, $localStorage} from "../util/ngimport";
 import {to} from "../util/utils";
 import {Users} from "../user/userService";
+import {ngStorage} from "ngstorage";
 
 markAsUsed(createItem);
 
 export class StartCtrl implements IController {
     private creatingNew: boolean;
     private docListOpen: boolean;
-    private language: string; // Page language
+    private defaultLanguage: string = "fi"; // Page default language.
+    private language: string = this.defaultLanguage;
     private bookmarks = {};
-    private userPersonalFolderPath?: string;
+    private storage: ngStorage.StorageService & {languageStorage: null | string};
 
     constructor() {
         this.creatingNew = false;
         this.docListOpen = false;
-        this.language = "fi";
         this.bookmarks = $window.bookmarks; // from base.html
-        this.userPersonalFolderPath = Users.getCurrentPersonalFolderPath();
+        this.storage = $localStorage.$default({languageStorage: null});
     }
 
     $onInit() {
         this.setLanguage();
     }
 
+    /**
+     * Picks the page language from url suffix or local storage, otherwise use default.
+     * Currently supported: fi, en.
+     */
     setLanguage() {
         const urlSuffix: string = window.location.pathname;
         switch (urlSuffix) {
@@ -42,13 +47,24 @@ export class StartCtrl implements IController {
                 break;
             }
             default: {
-                this.language = "fi";
-                //TODO: Local storage.
+                // Try local storage, otherwise use default.
+                if (this.storage.languageStorage) {
+                    this.language = this.storage.languageStorage;
+                } else {
+                    this.language = this.defaultLanguage;
+                }
                 break;
             }
         }
     }
 
+    getCurrentUserFolderPath() {
+        return Users.getCurrentPersonalFolderPath();
+    }
+
+    /**
+     * Check whether the current user is logged in.
+     */
     isLoggedIn() {
         return Users.isLoggedIn();
     }
@@ -62,12 +78,13 @@ export class StartCtrl implements IController {
     }
 
     /**
-     * Change page language.
+     * Change page language and save it to the local storage.
      * Currently supported: fi, en.
-     * @param changeTo
+     * @param changeTo New language abbreviation.
      */
     changeLanguage(changeTo: string) {
         this.language = changeTo;
+        this.storage.languageStorage = changeTo;
     }
 
     /**
@@ -106,7 +123,7 @@ timApp.component("timStart", {
                     <bookmark-folder-box bookmarks="$ctrl.bookmarks" bookmark-folder-name="My courses">
                     </bookmark-folder-box>
                     <a href="/view/tim/TIM-esittely">
-                        <img class="img-responsive" src="/static/images/responsive.jpg"/>
+                        <img class="img-responsive" alt="TIM-esittely" src="/static/images/responsive.jpg"/>
                     </a>
                 </div>
                 <div class="col-md-4">
@@ -115,36 +132,49 @@ timApp.component("timStart", {
                             <h3>Get started</h3>
                             <p ng-if="!$ctrl.isLoggedIn()">To log in or sign up, use the "Log in" button at the top.</p>
                             <ul class="list-unstyled">
-                                <li ng-if="$ctrl.isLoggedIn()" class="h5"><a href="/view/{{$ctrl.userPersonalFolderPath}}">My documents</a></li>
-                                <li class="h5"><a ng-click="$ctrl.openCourseListDialog()" href="#">Available courses</a></li>
+                                <li ng-if="$ctrl.isLoggedIn()" class="h5">
+                                    <a href="/view/{{$ctrl.getCurrentUserFolderPath()}}">My documents</a>
+                                </li>
+                                <li class="h5">
+                                    <a ng-click="$ctrl.openCourseListDialog()" href="#">Available courses</a>
+                                </li>
                                 <li class="h5"><a href="/view/">All documents</a></li>
-                                <li ng-if="$ctrl.isLoggedIn()" class="h5"><a ng-click="$ctrl.enableCreate()" href="#">Create a new document</a></li>
+                                <li ng-if="$ctrl.isLoggedIn()" class="h5">
+                                    <a ng-click="$ctrl.enableCreate()" href="#">Create a new document</a>
+                                </li>
                             </ul>
                             <bootstrap-panel ng-if="$ctrl.creatingNew"
                                              title="Create a new document"
                                              show-close="true"
                                              close-fn="$ctrl.cancelCreate()">
                                 <create-item item-title="My document"
-                                             item-location="{{$ctrl.userPersonalFolderPath}}"
+                                             item-location="{{$ctrl.getCurrentUserFolderPath()}}"
                                              item-type="document">
                                 </create-item>
                             </bootstrap-panel>
                         </div>
                         <div ng-switch-when="fi">
                             <h3>Aloitus</h3>
-                            <p ng-if="!$ctrl.isLoggedIn()">Kirjautuaksesi sisään käytä "Log in" -painiketta sivun ylälaidassa.</p>
+                            <p ng-if="!$ctrl.isLoggedIn()">Kirjautuaksesi sisään käytä "Log in"
+                             -painiketta sivun ylälaidassa.</p>
                             <ul class="list-unstyled">
-                                <li ng-if="$ctrl.isLoggedIn()" class="h5"><a href="/view/{{$ctrl.userPersonalFolderPath}}">Omat dokumentit</a></li>
-                                <li class="h5"><a ng-click="$ctrl.openCourseListDialog()" href="#">Saatavilla olevat kurssit</a></li>
+                                <li ng-if="$ctrl.isLoggedIn()" class="h5">
+                                    <a href="/view/{{$ctrl.getCurrentUserFolderPath()}}">Omat dokumentit</a>
+                                </li>
+                                <li class="h5">
+                                    <a ng-click="$ctrl.openCourseListDialog()" href="#">Saatavilla olevat kurssit</a>
+                                </li>
                                 <li class="h5"><a href="/view/">Selaa dokumentteja</a></li>
-                                <li ng-if="$ctrl.isLoggedIn()" class="h5"><a ng-click="$ctrl.enableCreate()" href="#">Luo uusi dokumentti</a></li>
+                                <li ng-if="$ctrl.isLoggedIn()" class="h5">
+                                    <a ng-click="$ctrl.enableCreate()" href="#">Luo uusi dokumentti</a>
+                                </li>
                             </ul>
                             <bootstrap-panel ng-if="$ctrl.creatingNew"
                                              title="Create a new document"
                                              show-close="true"
                                              close-fn="$ctrl.cancelCreate()">
                                 <create-item item-title="My document"
-                                             item-location="{{$ctrl.userPersonalFolderPath}}"
+                                             item-location="{{$ctrl.getCurrentUserFolderPath()}}"
                                              item-type="document">
                                 </create-item>
                             </bootstrap-panel>
@@ -166,8 +196,14 @@ timApp.component("timStart", {
                             <h4>Examples <sup>(F)</sup></h4>
                             <ul class="list-unstyled">
                                 <li><a href="/view/tim/Esimerkkeja-TIMin-mahdollisuuksista">TIM's possibilities</a></li>
-                                <li><a href="/view/tim/Eri-ohjelmointikielia">Programming languages</a> <sup ng-if="!$ctrl.isLoggedIn()">(*)</sup></li>
-                                <li><a href="/view/tim/muita-esimerkkeja">Usage in different subjects</a> <sup ng-if="!$ctrl.isLoggedIn()">(*)</sup></li>
+                                <li>
+                                    <a href="/view/tim/Eri-ohjelmointikielia">Programming languages</a>
+                                    <sup ng-if="!$ctrl.isLoggedIn()"> (*)</sup>
+                                </li>
+                                <li>
+                                    <a href="/view/tim/muita-esimerkkeja">Usage in different subjects</a>
+                                    <sup ng-if="!$ctrl.isLoggedIn()"> (*)</sup>
+                                </li>
                             </ul>
                         </div>
                     </div>
@@ -191,9 +227,17 @@ timApp.component("timStart", {
                         <div class="col-md-4">
                             <h4>Esimerkkejä</h4>
                             <ul class="list-unstyled">
-                                <li><a href="/view/tim/Esimerkkeja-TIMin-mahdollisuuksista">TIMin mahdollisuuksia</a></li>
-                                <li><a href="/view/tim/Eri-ohjelmointikielia">Ohjelmointikieliä</a> <sup ng-if="!$ctrl.isLoggedIn()">(*)</sup></li>
-                                <li><a href="/view/tim/muita-esimerkkeja">Käyttö eri oppiaineissa</a> <sup ng-if="!$ctrl.isLoggedIn()">(*)</sup></li>
+                                <li>
+                                    <a href="/view/tim/Esimerkkeja-TIMin-mahdollisuuksista">TIMin mahdollisuuksia</a>
+                                </li>
+                                <li>
+                                    <a href="/view/tim/Eri-ohjelmointikielia">Ohjelmointikieliä</a>
+                                    <sup ng-if="!$ctrl.isLoggedIn()"> (*)</sup>
+                                </li>
+                                <li>
+                                    <a href="/view/tim/muita-esimerkkeja">Käyttö eri oppiaineissa</a>
+                                    <sup ng-if="!$ctrl.isLoggedIn()"> (*)</sup>
+                                </li>
                             </ul>
                         </div>
                     </div>
