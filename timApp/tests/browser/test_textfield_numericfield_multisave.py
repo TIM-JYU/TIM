92ecae2deefe701daee8c39a971dd7288abef00f
sijualle <sijualle@student.jyu.fi>
2019-03-29 13:19:23 +0200

    Optimize textfield/numericfield/multisave test

--- timApp/tests/browser/test_textfield_numericfield_multisave.py
+++ timApp/tests/browser/test_textfield_numericfield_multisave.py
@@ -1,8 +1,3 @@
-from time import sleep
-
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
-
 from timApp.tests.browser.browsertest import BrowserTest
 
 
@@ -36,11 +31,9 @@ autosave: false
         self.save_element_screenshot(par, 'fields_before_answer') # is not needed once taken
         self.assert_same_screenshot(par, ['textfield/fields_before_answer'])
         runbutton = buttonPar.find_element_by_css_selector('button') # Find multisave button element
-        sleep(3) # sleep to ensure db put
         runbutton.click()
-        sleep(2) # sleep to ensure db put
         self.refresh() # refresh is created in browsertext.py
-        sleep(2) # sleep to ensure screenshot to match reload
+        self.wait_until_present('#pars')
         par = self.find_element_avoid_staleness('#pars') # recall for save screenshot
         self.wait_until_present('#t1 input' and '#t2 input')
         self.save_element_screenshot(par, 'fields_after_answer') # is not needed once taken (again)
@@ -51,7 +44,6 @@ autosave: false
 
         self.goto_document(d)
         self.refresh()  # refresh is created in browsertext.py
-        sleep(2)  # sleep to ensure screenshot to match reload
         self.wait_until_present('#t1 input')
         input = self.find_element_and_move_to('#t1 input')
         input.clear()
@@ -63,11 +55,9 @@ autosave: false
         self.find_element('.breadcrumb .active').click()
         buttonPar = self.find_element_avoid_staleness('#t3 > tim-plugin-loader > div')
         runbutton = buttonPar.find_element_by_css_selector('button') # Find multisave button element
-        sleep(3)  # sleep to ensure db put
         runbutton.click()
-        sleep(2)  # sleep to ensure db put
         self.refresh()  # refresh is created in browsertext.py
-        sleep(2)  # sleep to ensure screenshot to match reload
+        self.wait_until_present('#pars')
         par = self.find_element_avoid_staleness('#pars')  # recall for save screenshot
         self.wait_until_present('#t1 input' and '#t2 input')
         self.save_element_screenshot(par, 'fields_after_answer_switch')  # is not needed once taken (again)
