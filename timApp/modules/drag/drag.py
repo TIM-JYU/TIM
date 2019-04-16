bd6dacc3bf5de3bb34914e4ef527a9af7a718710
jejoojal <jejoojal@student.jyu.fi>
2019-04-16 11:05:51 +0300

    Set debug to False because of "Exec format error"

--- timApp/modules/drag/drag.py
+++ timApp/modules/drag/drag.py
@@ -245,5 +245,5 @@ if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
         port=5000,
-        debug=True,
+        debug=False,
     )
--- timApp/modules/dropdown/dropdown.py
+++ timApp/modules/dropdown/dropdown.py
@@ -226,5 +226,5 @@ if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
         port=5000,
-        debug=True,
+        debug=False,  # TODO True does not work for some reason (gives "Exec format error")
     )
