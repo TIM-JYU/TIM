9f1b3acbc801a744a6de211a2513f76934a331ed
Mika Lehtinen <mikkalle@student.jyu.fi>
2019-06-19 17:21:04 +0300

    Raise JSON body limit for jsrunner to 50 MB

--- timApp/modules/jsrunner/app.ts
+++ timApp/modules/jsrunner/app.ts
@@ -15,7 +15,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 app.use(logger("dev"));
-app.use(express.json());
+app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
