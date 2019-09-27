0255fdaf414c99a5e7cf525ddf3312ef976416d9
Mika Lehtinen <mikkalle@student.jyu.fi>
2019-09-27 15:22:39 +0300

    Properly report possible errors from build_plugin_js

--- build_plugin_js.sh
+++ build_plugin_js.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Builds JavaScript of all plugins.
 # Currently only needed for csPlugin and showfile.
+set -e
 
 ./run_command_workdir.sh . tsc --build timApp/modules/cs/js timApp/modules/svn/js timApp/modules/pali/js \
 timApp/modules/fields/js   \
