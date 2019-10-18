dcddc39de97c3e4da74f7505ae18e77465bbf49b
Visa Naukkarinen <visa.naukkarinen@gmail.com>
2019-10-18 13:36:38 +0300

    Refactor tests

--- timApp/tests/server/test_doc_partition.py
+++ timApp/tests/server/test_doc_partition.py
@@ -19,7 +19,6 @@ class DocPartitionTest(TimRouteTest):
 
 
     def test_calculating_part_indices(self):
-        self.make_admin(self.test_user_1)
         self.login_test1()
         self.json_post(url=f'/viewrange/set/piecesize',
                        json_data={'pieceSize': 5},
@@ -33,11 +32,11 @@ class DocPartitionTest(TimRouteTest):
         self.get(f'/viewrange/get/{d1.id}/0/1', expect_content={'b': 0, 'e': 0})
         # Begin index at the doc beginning.
         self.get(f'/viewrange/get/{d2.id}/0/{forwards}', expect_content={'b': 0, 'e': 5})
-        # Begin index is at the doc end, and is rounded to avoid a too short part.
+        # Begin index is at the doc end; rounded to avoid a too short part.
         self.get(f'/viewrange/get/{d2.id}/10/{forwards}', expect_content={'b': 8, 'e': 10})
         # Begin index is 5 and moving backwards.
         self.get(f'/viewrange/get/{d2.id}/5/{backwards}', expect_content={'b': 0, 'e': 5})
-        # Begin index is at doc beginning and moving backwards, the is rounded to avoid a too short part.
+        # Begin index is atthe doc beginning and moving backwards; rounded to avoid a too short part.
         self.get(f'/viewrange/get/{d2.id}/0/{backwards}', expect_content={'b': 0, 'e': 2})
         self.json_post(url=f'/viewrange/set/piecesize',
                        json_data={'pieceSize': 8},
@@ -48,14 +47,12 @@ class DocPartitionTest(TimRouteTest):
 
 
     def test_partitioning_document(self):
-        self.make_admin(self.test_user_1)
         self.login_test1()
         d = self.create_doc(initial_par=["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"])
 
         # No partitioning.
-        tree = self.get(f"view/{d.path}", as_tree=True)
-        pars = tree.cssselect('.parContent > p')
-        self.assertEqual(10, len(pars))
+        tree = self.get(d.url, as_tree=True)
+        self.assert_content(tree, ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"])
 
         self.json_post(url=f'/viewrange/set/piecesize',
                        json_data={'pieceSize': 5},
@@ -63,26 +60,19 @@ class DocPartitionTest(TimRouteTest):
                        expect_status=200)
 
         # Partitioning on, no URL parameters.
-        tree = self.get(f"view/{d.path}", as_tree=True)
-        pars = tree.cssselect('.parContent > p')
-        self.assertEqual(5, len(pars))
-        self.assertIn('2', pars[1].text)
+        tree = self.get(d.url, as_tree=True)
+        self.assert_content(tree, ["1","2","3","4","5"])
 
         # Partitioning with URL parameters, mid-document range.
-        tree = self.get(f"view/{d.path}?b=2&e=6", as_tree=True)
-        pars = tree.cssselect('.parContent > p')
-        self.assertEqual(4, len(pars))
-        self.assertIn('3', pars[0].text)
-        self.assertIn('6', pars[3].text)
+        tree = self.get(d.url, query_string={'b': 2, 'e': 6}, as_tree=True)
+        self.assert_content(tree, ["3", "4", "5", "6"])
 
         # Partitioning with URL parameters, whole document range.
-        tree = self.get(f"view/{d.path}?b=0&e=10", as_tree=True)
-        pars = tree.cssselect('.parContent > p')
-        self.assertEqual(10, len(pars))
+        tree = self.get(d.url, query_string={'b': 0, 'e': 10}, as_tree=True)
+        self.assert_content(tree, ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"])
 
 
     def test_partitioning_document_with_overflowing_range(self):
-        self.make_admin(self.test_user_1)
         self.login_test1()
         d = self.create_doc(initial_par=["1","Kissa","3","4","5","6","Koira","8","9","10"])
         self.json_post(url=f'/viewrange/set/piecesize',
@@ -91,20 +81,15 @@ class DocPartitionTest(TimRouteTest):
                        expect_status=200)
 
         # Overflowing range end.
-        tree = self.get(f"view/{d.path}?b=6&e=100", as_tree=True)
-        pars = tree.cssselect('.parContent > p')
-        self.assertEqual(4, len(pars))
-        self.assertIn(pars[0].text, 'Koira')
+        tree = self.get(d.url, query_string={'b': 6, 'e': 100}, as_tree=True)
+        self.assert_content(tree, ["Koira","8","9","10"])
 
         # Negative range begin.
-        tree = self.get(f"view/{d.path}?b=-100&e=4", as_tree=True)
-        pars = tree.cssselect('.parContent > p')
-        self.assertEqual(4, len(pars))
-        self.assertIn(pars[1].text, 'Kissa')
+        tree = self.get(d.url, query_string={'b': -100, 'e': 4}, as_tree=True)
+        self.assert_content(tree, ["1", "Kissa", "3", "4"])
 
 
     def test_partitioning_empty_document(self):
-        self.make_admin(self.test_user_1)
         self.login_test1()
         self.json_post(url=f'/viewrange/set/piecesize',
                        json_data={'pieceSize': 5},
@@ -112,13 +97,11 @@ class DocPartitionTest(TimRouteTest):
                        expect_status=200)
         d = self.create_doc()
 
-        tree = self.get(f"view/{d.path}?b=3&e=14", as_tree=True)
-        pars = tree.cssselect('.parContent > p')
-        self.assertEqual(0, len(pars))
+        tree = self.get(d.url, query_string={'b': 3, 'e': 14}, as_tree=True)
+        self.assert_content(tree, [])
 
 
     def test_partitioning_with_preambles(self):
-        self.make_admin(self.test_user_1)
         self.login_test1()
         self.json_post(url=f'/viewrange/set/piecesize',
                        json_data={'pieceSize': 5},
@@ -126,58 +109,44 @@ class DocPartitionTest(TimRouteTest):
                        expect_status=200)
         d = self.create_doc(
             initial_par=["1","Kissa","3","4","5","6","Koira","8","9","10"])
-        d_preamble = self.create_doc(
-            path=self.get_personal_item_path(f'{TEMPLATE_FOLDER_NAME}/{PREAMBLE_FOLDER_NAME}/preamble'),
-            initial_par=["Preamble par 1", "Preamble par 2"])
+        self.create_preamble_for(d, initial_par=["Preamble par 1", "Preamble par 2"])
 
         # Partitioning on, no URL parameters.
-        tree = self.get(f"view/{d.path}", as_tree=True)
-        pars = tree.cssselect('.parContent > p')
-        self.assertEqual(7, len(pars))
-        self.assertIn('Preamble paragraph 1', pars[0].text)
-        self.assertIn('Kissa', pars[3].text)
+        tree = self.get(d.url, as_tree=True)
+        self.assert_content(tree, ["Preamble par 1", "Preamble par 2", "1","Kissa","3","4","5"])
 
         # Partitioning with URL parameters, starting from doc beginning.
-        tree = self.get(f"view/{d.path}?b=0&e=6&preamble=true", as_tree=True)
-        pars = tree.cssselect('.parContent > p')
-        self.assertEqual(8, len(pars))
-        self.assertIn('Preamble par 1', pars[0].text)
-        self.assertIn('Kissa', pars[3].text)
+        tree = self.get(d.url, query_string={'b': 0, 'e': 6, 'preamble': 'true'}, as_tree=True)
+        self.assert_content(tree, ["Preamble par 1", "Preamble par 2", "1","Kissa","3","4","5", "6"])
 
         # Partitioning with URL parameters, starting from mid-document.
-        tree = self.get(f"view/{d.path}?b=6&e=10&preamble=true", as_tree=True)
-        pars = tree.cssselect('.parContent > p')
-        self.assertEqual(4, len(pars))
-        self.assertIn(pars[0].text, 'Koira')
+        tree = self.get(d.url, query_string={'b': 6, 'e': 9, 'preamble': 'true'}, as_tree=True)
+        self.assert_content(tree, ["Koira","8","9"])
 
 
     def test_partitioning_with_special_class_preambles(self):
-        self.make_admin(self.test_user_1)
-        self.login_test1()
+        self.login_test2()
         self.json_post(url=f'/viewrange/set/piecesize',
                        json_data={'pieceSize': 5},
                        expect_cookie=('r','5'),
                        expect_status=200)
-        d = self.create_doc(initial_par=["1","Kissa","3","4","5","6","Koira","8","9","10"])
-        d_preamble = self.create_doc(
-            path=self.get_personal_item_path(f'{TEMPLATE_FOLDER_NAME}/{PREAMBLE_FOLDER_NAME}/preamble'),
-            initial_par=["Preamble par 1", "Preamble par 2", """
+        d = self.create_doc(path=self.get_personal_item_path(f'2/test'),
+                            initial_par=["1","Kissa","3","4","5","6","Koira","8","9","10"])
+        self.create_preamble_for(d, initial_par=["Preamble par 1", "Preamble par 2", """
 #- {.includeInParts}
-Preamble par 3
-"""])
-
+Preamble par 3"""])
         # Partitioning on, no URL parameters; all pars should be included.
-        tree = self.get(f"view/{d.path}", as_tree=True)
-        pars = tree.cssselect('.parContent > p')
-        self.assertEqual(8, len(pars))
-        self.assertIn('Preamble par 1', pars[0].text)
-        self.assertIn('Kissa', pars[4].text)
+        tree = self.get(d.url, as_tree=True)
+        self.assert_content(tree, ["Preamble par 1", "Preamble par 2", "Preamble par 3", "1", "Kissa", "3", "4", "5"])
 
         # Partitioning with URL parameters, starting from mid-document; special par should be included.
-        tree = self.get(f"view/{d.path}?b=6&e=10&preamble=true", as_tree=True)
-        pars = tree.cssselect('.parContent > p')
-        self.assertEqual(5, len(pars))
-        self.assertIn(pars[0].text, 'Preamble par 3')
+        tree = self.get(d.url, query_string={'b': 6, 'e': 9, 'preamble': 'true'}, as_tree=True)
+        self.assert_content(tree, ["Preamble par 3","Koira","8","9"])
+
+        # Preamble loading disabled.
+        tree = self.get(d.url, query_string={'b': 5, 'e': 8, 'preamble': 'false'}, as_tree=True)
+        self.assert_content(tree, ["6","Koira","8"])
+
 
     def test_partitioning_range(self):
         # TODO: Range check not implemented yet.
