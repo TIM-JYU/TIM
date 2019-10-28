b49bf46abeb79806b7550b7659768df083e2a8cb
Visa Naukkarinen <visa.naukkarinen@gmail.com>
2019-10-28 14:01:59 +0200

    Add tests for partitioning and areas

--- timApp/tests/server/test_doc_partition.py
+++ timApp/tests/server/test_doc_partition.py
@@ -162,17 +162,209 @@ Preamble par 3"""])
 
         # Partitioning with URL parameters, starting from mid-document; special par should be included.
         tree = self.get(d.url, query_string={'b': 6, 'e': 9, 'preamble': 'true'}, as_tree=True)
-        self.assert_content(tree, ["Preamble par 3","Koira","8","9"])
+        self.assert_content(tree, ["Preamble par 3", "Koira", "8", "9"])
 
         # Preamble loading disabled.
         tree = self.get(d.url, query_string={'b': 5, 'e': 8, 'preamble': 'false'}, as_tree=True)
-        self.assert_content(tree, ["6","Koira","8"])
+        self.assert_content(tree, ["6", "Koira", "8"])
 
 
-    def test_partitioning_range(self):
+    def test_partitioning_document_areas(self):
         self.login_test1()
         self.json_post(url=f'/viewrange/set/piecesize',
                        json_data={'pieceSize': 5},
                        expect_cookie=('r', '5'),
                        expect_status=200)
+
+        # Normal area within the document. The view range is expected to adjust avoid cutting the area.
+        # Note: URL parameter ranges will take precedence and cut areas, so these tests are done without them.
+        d = self.create_doc(initial_par="""
+#-
+1
+
+#- {area="test"}
+
+#-
+Kissa
+
+#-
+3
+
+#-
+4
+
+#-
+5
+
+#-
+6
+
+#-
+Koira
+
+#- {area_end="test"}
+
+#-
+8
+
+#-
+9
+
+#-
+10
+""")
+        tree = self.get(d.url, as_tree=True)
+        self.assert_content(tree, ["1", '{"area": "test"}', "Kissa", "3", "4", "5", "6", "Koira",'{"area_end": "test"}'])
+
+        # Area inside area.
+        d2 = self.create_doc(initial_par="""
+#-
+1
+
+#- {area="test"}
+
+#-
+Kissa
+
+#-
+3
+
+#- {area="test2"}
+
+#-
+4
+
+#- {area_end="test2"}
+
+#-
+5
+
+#-
+6
+
+#-
+Koira
+
+#- {area_end="test"}
+
+#-
+8
+
+#-
+9
+
+#-
+10
+""")
+        self.assert_content(self.get(d2.url, as_tree=True), ["1", '{"area": "test"}', "Kissa", "3",
+                                                             '{"area": "test2"}', "4", '{"area_end": "test2"}',
+                                                             "5", "6", "Koira",'{"area_end": "test"}'])
+        # Whole document wide area.
+        d3 = self.create_doc(initial_par="""
+#- {area="test"}
+
+#-
+1
+
+#-
+2
+
+#-
+3
+
+#-
+4
+
+#-
+5
+
+#-
+6
+
+#- {area_end="test"}
+""")
+        self.assert_content(self.get(d3.url, as_tree=True), ['{"area": "test"}', '1', '2', '3', '4', '5', '6', '{"area_end": "test"}'])
+
+        # Two separate areas, the latter is partially within document part range.
+        d3 = self.create_doc(initial_par="""
+#-
+1 
+
+#- {area="test1"}
+
+#-
+2
+
+#- {area_end="test1"}
+
+#-
+3
+
+#- {area="test2"}
+
+#-
+4
+
+#- {area_end="test2"}
+
+#-
+5
+
+""")
+        self.assert_content(self.get(d3.url, as_tree=True), ['1', '{"area": "test1"}', '2', '{"area_end": "test1"}',
+                                                             '3', '{"area": "test2"}', '4', '{"area_end": "test2"}'])
+
+        # Empty area within document.
+        d3 = self.create_doc(initial_par="""
+#-
+1 
+
+#-
+2
+
+#-
+3
+
+#- {area="test"}
+
+#- {area_end="test"}
+
+#-
+5
+
+#-
+6
+
+#-
+7
+""")
+        self.assert_content(self.get(d3.url, as_tree=True), ['1', '2', '3', '{"area": "test"}', '{"area_end": "test"}', ])
+
+        # Broken area within document; handled like a normal par.
+        d3 = self.create_doc(initial_par="""
+#-
+1 
+
+#-
+2
+
+#-
+3
+
+#- {area="test"}
+
+#-
+5
+
+#-
+6
+
+#-
+7
+
+#-
+8
+""")
+        self.assert_content(self.get(d3.url, as_tree=True), ['1', '2', '3', '{"area": "test"}', '5', ])
+
     # TODO: Test areas + preambles & areas.
