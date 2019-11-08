ce2bd012f4c73af4449c13bf23a41436f41564a7
Mika Lehtinen <mikkalle@student.jyu.fi>
2019-11-08 10:26:14 +0200

    Add a script to fix invalid imagex freeHandData

---
+++ timApp/admin/fix_imagex_freehanddata.py
@@ -0,0 +1,40 @@
+import json
+from typing import List
+
+from timApp.admin.util import process_items, create_argparser, DryrunnableArguments
+from timApp.answer.answer import Answer
+from timApp.document.docinfo import DocInfo
+
+
+def fix_imagex_freehanddata(doc: DocInfo, args: DryrunnableArguments):
+    """Fixes invalid imagex freeHandData.
+    An invalid entry in freeHandData looks like: {'color': '#ff0', 'lines': [[None, None]], 'w': 3}
+    :param doc: The document to fix.
+    :param args: The arguments.
+    """
+    errors = 0
+    answers: List[Answer] = Answer.query.filter(Answer.task_id.startswith(f'{doc.id}.')).all()
+    for a in answers:
+        data = a.content_as_json
+        freehanddata = data.get('freeHandData')
+        if not freehanddata:
+            continue
+        print(f'Processing imagex answer: {a.id} {a.task_id} {a.answered_on}')
+        valid_data = []
+        answer_has_errors = False
+        for f in freehanddata:
+            lines = f['lines']
+            if len(lines) == 1 and lines[0][0] is None:
+                answer_has_errors = True
+                errors += 1
+                print(f'Invalid lines in answer {a.id}')
+            else:
+                valid_data.append(f)
+        if answer_has_errors:
+            data['freeHandData'] = valid_data
+            a.content = json.dumps(data)
+    return errors
+
+
+if __name__ == '__main__':
+    process_items(fix_imagex_freehanddata, create_argparser('Fixes invalid imagex freehanddata'))
