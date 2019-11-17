754483ae757022ae58b56727c0ea9ac89f18d47d
Vesa Lappalainen <vesal@jyu.fi>
2019-11-17 19:56:29 +0200

    one more try for msg

--- timApp/notification/notify.py
+++ timApp/notification/notify.py
@@ -178,13 +178,13 @@ def multi_send_email_impl(
                     send_extra = ''
                     if rcp == bcc:
                         send_extra = extra
-                    if reply_to:
-                        mime_msg.add_header('Reply-To', reply_to)
-                    mime_msg = MIMEText(msg)  # + flask_app.config['MAIL_SIGNATURE'])
+                    mime_msg = MIMEText(msg + send_extra)  # + flask_app.config['MAIL_SIGNATURE'])
                     mime_msg['Subject'] = subject
                     mime_msg['From'] = mail_from
                     mime_msg['Bcc'] = bccmail
                     mime_msg['To'] = rcp
+                    if reply_to:
+                        mime_msg.add_header('Reply-To', reply_to)
                     s.sendmail(mail_from, [rcp, bccmail], mime_msg.as_string() )
                 except (smtplib.SMTPSenderRefused,
                         smtplib.SMTPRecipientsRefused,
