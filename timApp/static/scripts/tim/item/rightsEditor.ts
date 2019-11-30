8d85c7a5a0612e2357f6685d17ab81e3469e796a
Vesa Lappalainen <vesal@jyu.fi>
2019-11-30 17:43:44 +0200

    try to get clipboard rights

--- timApp/static/scripts/tim/item/rightsEditor.ts
+++ timApp/static/scripts/tim/item/rightsEditor.ts
@@ -346,6 +346,7 @@ class RightsEditorController implements IController {
      * but could not read clipboard, so just clearInput
      */
     async paste() {
+        // navigator.permissions.query({name: "clipboard-read"});  // does not compile
         if ( !this.confirmExpire ) { return; }  // clearInput did not work?
         this.groupName = "";
         /*
