1b610adbcdd4db4f7b97690604b7b07ee679942f
Mika Lehtinen <mikkalle@jyu.fi>
2019-12-10 15:08:44 +0200

    Fix issues with JS building
    * Use "deleteOutputPath: false": When rebuilding JS in production, the old script versions should stay until they are overwritten.
    * Remove ts service because differential bundles don't support watch mode. Scripts must be rebuilt manually for now by running build_js.

--- build_js.sh
+++ build_js.sh
@@ -2,11 +2,4 @@
 
 set -e
 
-./run_command_workdir.sh timApp /bin/bash -c "npm install --unsafe-perm && npm run lint"
-echo "NPM libraries were installed and ESLint was run.
-
-If this is a development instance, you still have to start the 'bdw' NPM script e.g. from your IDE or by running
-'npm run bdw' in timApp directory.
-
-If this is not a development instance, run './restart.sh ts' to make sure the ts container picks up the changes.
-"
+./run_command_workdir.sh timApp /bin/bash -c "npm install --unsafe-perm && npm run lint && npm run b"
