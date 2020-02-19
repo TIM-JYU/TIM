bd348a432c02900ca3835acd16d1987c0a33caf8
Mika Lehtinen <mikkalle@jyu.fi>
2020-02-19 13:56:12 +0200

    fix_double_c: order answers by id, print also timestamp

--- timApp/admin/answer_cli.py
+++ timApp/admin/answer_cli.py
@@ -14,7 +14,7 @@ answer_cli = AppGroup('answer')
 @answer_cli.command('fix_double_c')
 @click.option('--dry-run/--no-dry-run', default=True)
 def create_docs(dry_run):
-    answers: List[Answer] = Answer.query.filter(Answer.answered_on > datetime(year=2020, month=1, day=1)).all()
+    answers: List[Answer] = Answer.query.filter(Answer.answered_on > datetime(year=2020, month=1, day=1)).order_by(Answer.id).all()
     count = 0
     for a in answers:
         cont = a.content_as_json
@@ -23,7 +23,7 @@ def create_docs(dry_run):
         c = cont.get('c')
         if isinstance(c, dict):
             if 'c' in c:
-                print(f'Modifying {a.id} ({a.task_id})')
+                print(f'Modifying {a.id} ({a.task_id}, {a.answered_on})')
                 count += 1
                 if not dry_run:
                     a.content = json.dumps(c)
