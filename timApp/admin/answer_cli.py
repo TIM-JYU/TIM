aa1b9badf3a481365db09073ffa6cbb0049c812f
Mika Lehtinen <mikkalle@jyu.fi>
2020-02-19 13:39:33 +0200

    Add a CLI command to fix double "c" in answers

---
+++ timApp/admin/answer_cli.py
@@ -0,0 +1,32 @@
+import json
+from datetime import datetime
+from typing import List
+
+import click
+from flask.cli import AppGroup
+
+from timApp.answer.answer import Answer
+from timApp.timdb.sqa import db
+
+answer_cli = AppGroup('answer')
+
+
+@answer_cli.command('fix_double_c')
+@click.option('--dry-run/--no-dry-run', default=True)
+def create_docs(dry_run):
+    answers: List[Answer] = Answer.query.filter(Answer.answered_on > datetime(year=2020, month=1, day=1)).all()
+    count = 0
+    for a in answers:
+        cont = a.content_as_json
+        if not isinstance(cont, dict):
+            continue
+        c = cont.get('c')
+        if isinstance(c, dict):
+            if 'c' in c:
+                print(f'Modifying {a.id} ({a.task_id})')
+                count += 1
+                if not dry_run:
+                    a.content = json.dumps(c)
+    print(f'Total {count}')
+    if not dry_run:
+        db.session.commit()
--- timApp/admin/cli.py
+++ timApp/admin/cli.py
@@ -1,3 +1,4 @@
+from timApp.admin.answer_cli import answer_cli
 from timApp.admin.item_cli import item_cli
 from timApp.admin.sisu_cli import sisu_cli
 from timApp.admin.user_cli import user_cli
@@ -5,6 +6,7 @@ from timApp.admin.user_cli import user_cli
 
 def register_clis(app):
     for c in [
+        answer_cli,
         item_cli,
         sisu_cli,
         user_cli,
