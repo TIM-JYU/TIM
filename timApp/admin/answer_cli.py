eb017958e7c0aadf15f4477e889aa6d31778e73a
Mika Lehtinen <mikkalle@jyu.fi>
2020-03-09 21:32:23 +0200

    Add a CLI command to clear answers in a document

--- timApp/admin/answer_cli.py
+++ timApp/admin/answer_cli.py
@@ -5,15 +5,17 @@ from typing import List
 import click
 from flask.cli import AppGroup
 
-from timApp.answer.answer import Answer
+from timApp.answer.answer import Answer, AnswerSaver
+from timApp.answer.answer_models import UserAnswer
+from timApp.document.docentry import DocEntry
 from timApp.timdb.sqa import db
 
 answer_cli = AppGroup('answer')
 
 
-@answer_cli.command('fix_double_c')
+@answer_cli.command()
 @click.option('--dry-run/--no-dry-run', default=True)
-def create_docs(dry_run):
+def fix_double_c(dry_run):
     answers: List[Answer] = (
         Answer.query
             .filter((Answer.answered_on > datetime(year=2020, month=2, day=9)) & Answer.content.startswith('{"c": {"c":'))
@@ -33,5 +35,23 @@ def create_docs(dry_run):
                 if not dry_run:
                     a.content = json.dumps(c)
     print(f'Total {count}')
+    if not dry_run:
+        db.session.commit()
+
+
+@answer_cli.command()
+@click.argument('doc')
+@click.option('--dry-run/--no-dry-run', default=True)
+def clear_all(doc, dry_run):
+    d = DocEntry.find_by_path(doc)
+    if not d:
+        click.echo(err=f'cannot find document "{doc}"')
+        return
+    ids = Answer.query.filter(Answer.task_id.startswith(f'{d.id}.')).with_entities(Answer.id)
+    cnt = ids.count()
+    UserAnswer.query.filter(UserAnswer.answer_id.in_(ids)).delete(synchronize_session=False)
+    AnswerSaver.query.filter(AnswerSaver.answer_id.in_(ids)).delete(synchronize_session=False)
+    Answer.query.filter(Answer.id.in_(ids)).delete(synchronize_session=False)
+    click.echo(f'Total {cnt}')
     if not dry_run:
         db.session.commit()
