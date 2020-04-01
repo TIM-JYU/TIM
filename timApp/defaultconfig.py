4f9f5bb33795bc49f5255155a4fdf5f35f570a18
Mika Lehtinen <mikkalle@jyu.fi>
2020-04-01 15:53:40 +0300

    Set SameSite=None for session cookie; needed for Aalto iframe

--- timApp/defaultconfig.py
+++ timApp/defaultconfig.py
@@ -95,3 +95,5 @@ HAKA_METADATA_URL_PROD = 'https://haka.funet.fi/metadata/haka-metadata.xml'
 HAKA_METADATA_FINGERPRINT_PROD = '6212391dfbd2874425a7fdd5ac8dd0bd5cd50d2f'
 
 HOME_ORGANIZATION = 'jyu.fi'
+SESSION_COOKIE_SAMESITE = 'None'  # Required for Aalto iframe to work.
+SESSION_COOKIE_SECURE = False  # Override to True in production.
