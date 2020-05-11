35e136a4ab47682734d7995f5672b39f3aec4866
sijualle <sijualle@student.jyu.fi>
2020-05-12 01:08:55 +0300

    Handle missing drawio savebutton

--- timApp/modules/cs/jsframe.py
+++ timApp/modules/cs/jsframe.py
@@ -447,7 +447,7 @@ class DrawIO(JSframe):
             ma["height"] = ma.get("width", 800) / (ar * 0.95)
         ma["initListener"] = ma.get("initListener", True)
         ma["iframeopts"] = ma.get("iframeopts", 'sandbox="allow-scripts allow-same-origin allow-popups"')
-        ma["saveButton"] = ma.get("saveButton", None)
+        ma["saveButton"] = ma.get("saveButton", "")
         # TODO: prevent user options if thereis
         ma["options"] = {'fullscreen': ma.get("fullscreen", True)}
         super().modify_query()
