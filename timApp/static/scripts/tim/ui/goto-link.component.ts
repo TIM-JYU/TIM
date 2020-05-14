7c9a3e44ed2cfb444dd5a4c6f62214ea2cfeca67
dezhidki <dezhidki@jyu.fi>
2020-05-14 10:38:28 +0300

    GotoLink: Clear timers after reset

--- timApp/static/scripts/tim/ui/goto-link.component.ts
+++ timApp/static/scripts/tim/ui/goto-link.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from "@angular/core";
+import Timeout = NodeJS.Timeout;
 
 @Component({
     selector: "tim-goto-link",
@@ -24,18 +25,26 @@ export class GotoLinkComponent {
     @Input() isButton = false;
     @Input() target = "_self";
     isGoing = false;
+    resetTimeout?: Timeout;
 
     startGoto() {
+        if (this.isGoing) { return; }
         this.isGoing = true;
         const waitTime = Math.random() * Math.max(this.maxWait, 0) * 1000;
         const realResetTime = Math.max(this.resetTime * 1000, waitTime);
 
         setTimeout(() => {
             window.open(this.href, this.target);
+            this.isGoing = false;
+            if (this.resetTimeout) {
+                clearTimeout(this.resetTimeout);
+                this.resetTimeout = undefined;
+            }
         }, waitTime);
 
-        setTimeout(() => {
+        this.resetTimeout = setTimeout(() => {
            this.isGoing = false;
+           this.resetTimeout = undefined;
         }, realResetTime);
     }
 }
