3d85e1729e6e5b4839cc45262e5ecbaf31cbd92c
dezhidki <dezhidki@jyu.fi>
2020-05-16 15:30:45 +0300

    GotoLink: Remove unused import

--- timApp/static/scripts/tim/ui/goto-link.component.ts
+++ timApp/static/scripts/tim/ui/goto-link.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from "@angular/core";
+import {Component, Input} from "@angular/core";
 import moment from "moment";
 import {$http} from "tim/util/ngimport";
 import {secondsToHHMMSS, to} from "tim/util/utils";
