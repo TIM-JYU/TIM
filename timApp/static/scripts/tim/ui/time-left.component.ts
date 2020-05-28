b511f37f2aef9131fd9de1b0eec3a398a43621ee
dezhidki <dezhidki@jyu.fi>
2020-05-28 16:46:33 +0300

    Add timeLeft docsetting to set low time threshold

--- timApp/static/scripts/tim/ui/time-left.component.ts
+++ timApp/static/scripts/tim/ui/time-left.component.ts
@@ -1,21 +1,29 @@
 import {Component, Input, OnInit} from "@angular/core";
 import {showMessageDialog} from "tim/ui/dialog";
+import {documentglobals} from "tim/util/globals";
+import {ITimeLeftSettings} from "tim/document/IDocSettings";
+
+const TIME_LEFT_DEFAULTS: ITimeLeftSettings = {
+  lowTimeThreshold: -1,
+};
 
 @Component({
   selector: "tim-time-left",
   template: `
     <span class="label label-default" [class.low-time]="isLowTime" i18n>
-      Time left: <tim-countdown [displayUnits]="['d']" [endTime]="endTime" [lowTimeThreshold]="60" (onFinish)="onTimeUp()" (onLowTime)="onLowTime()"></tim-countdown>
+      Time left: <tim-countdown [displayUnits]="['d']" [endTime]="endTime" [lowTimeThreshold]="settings.lowTimeThreshold" (onFinish)="onTimeUp()" (onLowTime)="onLowTime()"></tim-countdown>
     </span>
-    <span class="low-time-warn alert alert-danger" *ngIf="isLowTime">The time is about to run out, please save your answers.</span>
+    <span class="low-time-warn alert alert-danger" *ngIf="isLowTime">The time is about to run out, remember to save your answers.</span>
   `,
   styleUrls: ["./time-left.component.scss"],
 })
 export class TimeLeftComponent implements OnInit {
   @Input() endTime?: string;
   isLowTime = false;
+  settings: ITimeLeftSettings = {...TIME_LEFT_DEFAULTS, ...documentglobals()?.docSettings?.timeLeft};
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
