537f058539d8e9b34c0aef912dc0247e6d48ab41
dezhidki <dezhidki@jyu.fi>
2020-05-28 15:41:44 +0300

    TimeLeftComponent: Change style when time is less than 60 seconds

--- timApp/static/scripts/tim/ui/time-left.component.ts
+++ timApp/static/scripts/tim/ui/time-left.component.ts
@@ -4,21 +4,27 @@ import {showMessageDialog} from "tim/ui/dialog";
 @Component({
   selector: "tim-time-left",
   template: `
-    <span class="label label-default" i18n>
-      Time left: <tim-countdown [displayUnits]="['d']" [endTime]="endTime" (finishCallback)="onTimeUp()"></tim-countdown>
+    <span class="label label-default" [class.lowTime]="isLowTime" i18n>
+      Time left: <tim-countdown [displayUnits]="['d']" [endTime]="endTime" [lowTimeThreshold]="60" (onFinish)="onTimeUp()" (onLowTime)="onLowTime()"></tim-countdown>
     </span>
   `,
   styleUrls: ["./time-left.component.scss"],
 })
 export class TimeLeftComponent implements OnInit {
   @Input() endTime?: string;
+  isLowTime = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onLowTime() {
+    this.isLowTime = true;
+  }
+
   onTimeUp() {
+    this.isLowTime = false;
     showMessageDialog("Time is up. You can still save answers for a while, but they will be marked invalid by default.");
   }
 
