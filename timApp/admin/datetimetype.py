b2cf244a118b8994b1af1c5b7ec5f37318b0deb9
Mika Lehtinen <mikkalle@jyu.fi>
2020-06-04 11:19:29 +0300

    Fix typings

--- timApp/admin/datetimetype.py
+++ timApp/admin/datetimetype.py
@@ -9,7 +9,7 @@ class DateTimeType(click.ParamType):
     """A better datetime type to use with Click."""
     name = "datetime"
 
-    def convert(self, value: str, param: str, ctx: Any) -> datetime:
+    def convert(self, value: Any, param: Any, ctx: Any) -> datetime:
         try:
             return dateutil.parser.parse(value)
         except (ValueError, OverflowError) as e:
