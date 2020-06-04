343ee777d62cca907571dc684b037bb8d913c52d
sijualle <sijualle@student.jyu.fi>
2020-06-04 18:21:28 +0300

    Add missing semicolon

--- timApp/static/scripts/tim/answer/answerbrowser3.ts
+++ timApp/static/scripts/tim/answer/answerbrowser3.ts
@@ -249,12 +249,11 @@ export class PluginLoaderCtrl extends DestroyScope implements IController {
         if (this.viewctrl) {
             const timComp = this.viewctrl.getTimComponentByName(this.taskId);
             if (timComp) {
-                const fo = timComp.isForm()
+                const fo = timComp.isForm();
                 if (fo == FormModeOption.IsForm
                     || this.viewctrl.docSettings.form_mode && fo == FormModeOption.Undecided) {
                     return true;
                 }
-
             }
         }
         return false;
