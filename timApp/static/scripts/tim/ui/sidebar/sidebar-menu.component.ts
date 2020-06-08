8b5229ab573405ff213882b188cca1b6c1f6c912
dezhidki <dezhidki@jyu.fi>
2020-06-08 16:47:41 +0300

    SidebarMenu: Fix indent

--- timApp/static/scripts/tim/ui/sidebar/sidebar-menu.component.ts
+++ timApp/static/scripts/tim/ui/sidebar/sidebar-menu.component.ts
@@ -2,46 +2,48 @@ import {AfterViewInit, Component, OnInit, ViewChild} from "@angular/core";
 import {TabDirective, TabsetComponent} from "ngx-bootstrap/tabs";
 
 @Component({
-  selector: "app-sidebar-menu",
-  templateUrl: "./sidebar-menu.component.html",
-  styleUrls: ["./sidebar-menu.component.scss"],
+    selector: "app-sidebar-menu",
+    templateUrl: "./sidebar-menu.component.html",
+    styleUrls: ["./sidebar-menu.component.scss"],
 })
 export class SidebarMenuComponent implements OnInit, AfterViewInit {
-  private showSidebar = false;
-  currentElement?: HTMLElement;
-  @ViewChild("tabs") tabs!: TabsetComponent;
-  constructor() { }
+    private showSidebar = false;
+    currentElement?: HTMLElement;
+    @ViewChild("tabs") tabs!: TabsetComponent;
+    constructor() { }
 
-  ngOnInit(): void {
-  }
-
-  ngAfterViewInit() {
-    this.setSidebarState(false);
-  }
-
-  onTabSelect(tab: TabDirective) {
-    this.showSidebar = true;
-    this.currentElement = tab.elementRef.nativeElement as HTMLElement;
-  }
-
-  shouldRender(tab: HTMLElement) {
-    return this.currentElement == tab;
-  }
-
-  private setSidebarState(visible: boolean) {
-    this.showSidebar = visible;
-
-    for (const tab of this.tabs.tabs) {
-      if (!this.showSidebar) {
-        tab.active = false;
-      } else if (tab.elementRef.nativeElement == this.currentElement) {
-        tab.active = true;
-      }
+    ngOnInit(): void {
     }
-  }
 
-  toggleSidebar() {
-    this.setSidebarState(!this.showSidebar);
-  }
+    ngAfterViewInit() {
+        // By default, ngx-bootstrap tabset has first tab selected, so we
+        // unselect a tab on PC
+        // TODO: Replicate proper mobile behaviour
+        this.setSidebarState(false);
+    }
 
+    onTabSelect(tab: TabDirective) {
+        this.showSidebar = true;
+        this.currentElement = tab.elementRef.nativeElement as HTMLElement;
+    }
+
+    shouldRender(tab: HTMLElement) {
+        return this.currentElement == tab;
+    }
+
+    private setSidebarState(visible: boolean) {
+        this.showSidebar = visible;
+
+        for (const tab of this.tabs.tabs) {
+            if (!this.showSidebar) {
+                tab.active = false;
+            } else if (tab.elementRef.nativeElement == this.currentElement) {
+                tab.active = true;
+            }
+        }
+    }
+
+    toggleSidebar() {
+        this.setSidebarState(!this.showSidebar);
+    }
 }
