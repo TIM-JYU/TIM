1b154dbd9a59255225ec99dbec65dd32fd0648e2
dezhidki <dezhidki@jyu.fi>
2020-06-08 16:46:04 +0300

    SidebarMenu: Implement tab selection and menu toggle on desktop screens

--- timApp/static/scripts/tim/ui/sidebar/sidebar-menu.component.ts
+++ timApp/static/scripts/tim/ui/sidebar/sidebar-menu.component.ts
@@ -1,24 +1,47 @@
-import { Component, OnInit } from "@angular/core";
-import {TabDirective} from "ngx-bootstrap/tabs";
+import {AfterViewInit, Component, OnInit, ViewChild} from "@angular/core";
+import {TabDirective, TabsetComponent} from "ngx-bootstrap/tabs";
 
 @Component({
   selector: "app-sidebar-menu",
   templateUrl: "./sidebar-menu.component.html",
   styleUrls: ["./sidebar-menu.component.scss"],
 })
-export class SidebarMenuComponent implements OnInit {
+export class SidebarMenuComponent implements OnInit, AfterViewInit {
+  private showSidebar = false;
   currentElement?: HTMLElement;
+  @ViewChild("tabs") tabs!: TabsetComponent;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    this.setSidebarState(false);
+  }
+
   onTabSelect(tab: TabDirective) {
+    this.showSidebar = true;
     this.currentElement = tab.elementRef.nativeElement as HTMLElement;
   }
 
   shouldRender(tab: HTMLElement) {
     return this.currentElement == tab;
+  }
+
+  private setSidebarState(visible: boolean) {
+    this.showSidebar = visible;
+
+    for (const tab of this.tabs.tabs) {
+      if (!this.showSidebar) {
+        tab.active = false;
+      } else if (tab.elementRef.nativeElement == this.currentElement) {
+        tab.active = true;
+      }
+    }
+  }
+
+  toggleSidebar() {
+    this.setSidebarState(!this.showSidebar);
   }
 
 }
