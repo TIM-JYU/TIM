b5522d0f44b219e151008fbaea3a7037dcee0176
sijualle <sijualle@student.jyu.fi>
2020-06-15 15:21:05 +0300

    Iterate non-browser attributes

--- timApp/modules/jsrunner/server/routes/multihtml.ts
+++ timApp/modules/jsrunner/server/routes/multihtml.ts
@@ -7,41 +7,35 @@ const router = express.Router();
 router.post("/", (req, res, next) => {
     const htmls = [];
     for (const j of req.body) {
-        // TODO: Lift this up to pluginify / render_plugin
-        if (j['viewmode']) {
-            const siw = j['markup']['showInView'] || false;
-            if (!siw) {
-                htmls.push(``);
-                continue;
+            // TODO: Lift this up to pluginify / render_plugin
+            if (j.viewmode) {
+                const siw = j.markup.showInView || false;
+                if (!siw) {
+                    htmls.push(``);
+                    continue;
+                }
             }
-        }
-        const m = j['markup'];
-        if (m) {
-            // Delete everything that shouldn't be show in browser
-            // TODO check if comprehensive
-            delete m['autoadd'];
-            // delete m['autoUpdateTables'];
-            delete m['creditField'];
-            delete m['defaultPoints'];
-            delete m['failGrade'];
-            // delete m['fieldhelper'];
-            delete m['fields'];
-            delete m['gradeField'];
-            delete m['gradingScale'];
-            delete m['group'];
-            delete m['groups'];
-            // delete m['includeUsers'];
-            // delete m['selectIncludeUsers'];
-            // delete m['paramFields'];
-            delete m['postprogram'];
-            delete m['preprogram'];
-            delete m['program'];
-            delete m['overrideGrade'];
-            // delete m['showInView'];
-            // delete m['confirmText'];
-            // delete m['timeout'];
-            // delete m['updateFields'];
-        }
+            const m = j.markup;
+            if (m) {
+                const privateAttrs = ["autoadd",
+                    "creditField",
+                    "defaultPoints",
+                    "failGrade",
+                    "fields",
+                    "gradeField",
+                    "gradingScale",
+                    "group",
+                    "groups",
+                    "postprogram",
+                    "preprogram",
+                    "program",
+                    "overrideGrade"];
+                // Delete everything that shouldn't be shown in browser
+                // TODO check if comprehensive
+                for (const attr of privateAttrs){
+                    delete m[attr]
+                }
+            }
 
         let s = JSON.stringify(j);
         // Escape all non-ascii characters. The base64 string will eventually get passed to "atob" function in browser,
