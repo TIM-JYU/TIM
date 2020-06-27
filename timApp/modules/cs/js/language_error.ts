b74fc9414349efec488aa3c69d748b8c82b1a6ba
Tuomas Laine <tuomaslaine97@gmail.com>
2020-06-27 17:52:27 +0000

    csPlugin: update language_error.ts to angular 10

--- timApp/modules/cs/js/language_error.ts
+++ timApp/modules/cs/js/language_error.ts
@@ -1,28 +1,66 @@
-import {IScope} from "angular";
-import {pluginBindings} from "tim/plugin/util";
-import {csApp, CsBase} from "./csPlugin";
+import {
+        Component,
+        DoBootstrap,
+        NgModule,
+        StaticProvider,
+        ApplicationRef,
+    } from "@angular/core"
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserModule} from "@angular/platform-browser";
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+import {FormsModule} from "@angular/forms";
+import {CsBase} from "./csPlugin";
+import {createDowngradedModule, doDowngrade} from "tim/downgrade";
+import {TimUtilityModule} from "tim/ui/tim-utility.module";
 
-class CSErrorController extends CsBase {
+@Component({
+    selector: "cs-error",
+    template:`
+    <tim-markup-error *ngIf="markupError" [data]="markupError"></tim-markup-error>
+    <div *ngIf="!markupError" class="error">
+        <p>Error(s) initializing csPlugin:</p>
+        <pre *ngIf="error">{{error}}</pre>
+        <pre *ngIf="own_error">{{own_error}}</pre>
+    </div>`,
+})
+class CSErrorComponent extends CsBase {
 
-    private error?: string;
-    private own_error?: string;
+    error?: string;
+    own_error?: string;
 
-    $onInit() {
-        super.$onInit();
+    ngOnInit() {
+        super.ngOnInit();
 
         this.error = this.attrsall.error ?? "";
         this.own_error = this.attrsall.own_error ?? "";
     }
 }
 
-csApp.component("csError", {
-    bindings: pluginBindings,
-    controller: CSErrorController,
-    template: `
-        <tim-markup-error ng-if="$ctrl.markupError" [data]="$ctrl.markupError"></tim-markup-error>
-        <div ng-if="!$ctrl.markupError" class="error">
-            <p>Error(s) initializing csPlugin:</p>
-            <pre ng-if="$ctrl.error">{{$ctrl.error}}</pre>
-            <pre ng-if="$ctrl.own_error">{{$ctrl.own_error}}</pre>
-        </div>`,
-});
+// noinspection AngularInvalidImportedOrDeclaredSymbol
+@NgModule({
+    declarations: [
+        CSErrorComponent,
+    ],
+    exports: [
+        CSErrorComponent,
+    ],
+    imports: [
+        BrowserModule,
+        TimUtilityModule,
+        FormsModule,
+        HttpClientModule,
+    ],
+})
+export class CsErrorModule implements DoBootstrap {
+    ngDoBootstrap(appRef: ApplicationRef) {
+    }
+}
+
+const bootstrapFn = (extraProviders: StaticProvider[]) => {
+    const platformRef = platformBrowserDynamic(extraProviders);
+    return platformRef.bootstrapModule(CsErrorModule);
+};
+
+const angularJsModule = createDowngradedModule(bootstrapFn);
+doDowngrade(angularJsModule, "csError", CSErrorComponent);
+export const moduleDefs = [angularJsModule];
