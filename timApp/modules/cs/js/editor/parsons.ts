79c654bb6c0a9a8316bc16cb455632c61cc936cb
Tuomas Laine <tuomaslaine97@gmail.com>
2020-06-29 11:36:46 +0000

    csPlugin: add parsons editor component

---
+++ timApp/modules/cs/js/editor/parsons.ts
@@ -0,0 +1,70 @@
+import {
+    ElementRef,
+    ViewChild,
+    Component,
+    Input,
+    SimpleChanges,
+} from "@angular/core";
+import {IEditor} from "./editor";
+import { parseHostBindings } from '@angular/compiler';
+
+@Component({
+    selector: "cs-parsons-editor",
+    template: `<div #area class="no-popup-menu"></div>`,
+})
+export class ParsonsEditorComponent implements IEditor {
+    private parson?: {join:(str: string)=>string, clear:()=>void, check:(str:string)=>string};
+    private content_?: string;
+    @Input() private shuffle: boolean = false;
+    @Input() private maxcheck?: number;
+    @Input() private notordermatters: boolean = false;
+    @Input() private base: string = "";
+    @Input() private styleWords: string = "";
+    @Input() private words: boolean = false;
+    @ViewChild("area") area!: ElementRef;
+    
+    ngOnChanges(changes: SimpleChanges) {
+        if (this.parson) {
+            // TODO: something smarter than recreating the whole thing on changes
+            this.content = this.content; // creates a new parson
+        }
+    }
+    
+    async ngAfterViewInit() { // TODO: check if this gets called multiple times (import)
+        this.createParsons(this.content);
+    }
+    
+    get content(): string {
+        return this.content_ ?? this.base;
+    }
+    set content(str: string) {
+        this.parson?.clear();
+        
+        this.createParsons(str);
+    }
+    
+    async createParsons(content: string) {
+        this.content_ = content;
+        
+        const csp = await import("../cs-parsons/csparsons");
+        const parson = new csp.CsParsonsWidget({
+            sortable: this.area.nativeElement,
+            words: this.words,
+            minWidth: "40px",
+            shuffle: this.shuffle,
+            styleWords: this.styleWords,
+            maxcheck: this.maxcheck,
+            notordermatters: this.notordermatters,
+            onChange: (p) => {
+                this.content_ = p.join("\n");
+            },
+        });
+        parson.init(this.base, this.content_);
+        parson.show();
+        this.parson = parson;
+    }
+    
+    check(): string {
+        return this.parson?.check(this.content) ?? "";
+    }
+}
