83bb6a3ddc1c4672fb03105145d1618ca01f4e03
Tuomas Laine <tuomaslaine97@gmail.com>
2020-06-29 11:36:07 +0000

    csPlugin: add normal editor component

---
+++ timApp/modules/cs/js/editor/normal.ts
@@ -0,0 +1,96 @@
+import $ from "jquery";
+import {Ace} from "ace-builds/src-noconflict/ace";
+import {
+    ElementRef,
+    ViewChild,
+    Component,
+    Input,
+    SimpleChanges,
+} from "@angular/core";
+import {wrapText} from "tim/document/editing/utils";
+import {IEditor} from "./editor";
+import {countChars} from "../util";
+
+//ng-attr-placeholder="{{placeholder}}"
+@Component({
+    selector: "cs-normal-editor",
+    template: `
+        <textarea #area class="csRunArea csEditArea no-popup-menu"
+                [rows]="rows"
+                [(ngModel)]="content">
+        </textarea>`,
+})
+export class NormalEditorComponent implements IEditor {
+    
+    private content_: string = "";
+    rows: number = 1;
+    @Input() minRows: number = 1;
+    @Input() maxRows: number = 100;
+    @ViewChild("area") private area!: ElementRef;
+    
+    get content(): string {
+        return this.content_;
+    }
+    
+    set content(str: string) {
+        this.content_ = str;
+        this.rows = countChars(this.content, "\n") + 1;
+        this.checkRowBounds();
+    }
+    
+    ngOnChanges(changes: SimpleChanges) {
+        this.checkRowBounds();
+    }
+    
+    checkRowBounds() {
+        if (this.rows < this.minRows) {
+            this.rows = this.minRows;
+        }
+        else if (this.maxRows != -1 && this.rows > this.maxRows) {
+            this.rows = this.maxRows;
+        }
+    }
+
+    ngAfterViewInit() {
+        $(this.area.nativeElement).on("keydown", (event) => { // TODO: check that this gets disabled when not used
+            if (event.which === 9) { // tab key
+                event.preventDefault();
+                if (event.shiftKey) {
+                    return;
+                }
+                this.insert("    ");
+                return;
+            }
+        });
+    }
+    
+    insert(str: string, strPos?: number): void {
+        const txtarea = this.area.nativeElement;
+        const scrollPos = txtarea.scrollTop;
+        strPos = strPos ?? txtarea.selectionStart ?? 0;
+        
+        const cont = this.content;
+        this.content = cont.slice(0, strPos) + str + cont.slice(strPos);
+        
+        strPos = strPos! + str.length;
+        if (txtarea.selectionStart) {
+            txtarea.selectionStart = strPos;
+            txtarea.selectionEnd = strPos;
+            txtarea.focus();
+        }
+        txtarea.scrollTop = scrollPos;
+    }
+
+    wrap(wrap: number) {
+        const r = wrapText(this.content, wrap);
+        if (!r.modified) { return; }
+        
+        const element = this.area.nativeElement;
+        const start = element.selectionStart;
+
+        this.content = r.s;
+        element.value = r.s;
+        element.selectionStart = start;
+        element.selectionEnd = start;
+    }
+}
