037f209989aa3c6452bb267aa14c9455db8678f6
Tuomas Laine <tuomaslaine97@gmail.com>
2020-08-04 09:14:25 +0000

    csPlugin: break csplugin.ts into multiple files

--- timApp/modules/cs/js/csPlugin.ts
+++ timApp/modules/cs/js/csPlugin.ts
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-explicit-any,@typescript-eslint/tslint/config,no-underscore-dangle */
-import {IController} from "angular";
 import {
         Component,
         ViewChild,
@@ -135,7 +134,7 @@ class CWPD {
     }
 }
 
-const ConsolePWD = new CWPD();
+export const ConsolePWD = new CWPD();
 
 const csJSTypes = ["js", "glowscript", "vpython", "html", "processing", "wescheme"];
 
@@ -281,7 +280,7 @@ class LanguageTypes {
     }
 }
 
-const languageTypes = new LanguageTypes();
+export const languageTypes = new LanguageTypes();
 
 // =================================================================================================================
 
@@ -380,7 +379,7 @@ interface IUploadResponse {
  * and the plugin won't work until the problem is fixed.
  */
 
-const Example = t.type({
+export const Example = t.type({
     expr: t.string,
     title: t.string,
 });
@@ -3042,281 +3041,5 @@ Object.getPrototypeOf(document.createElement("canvas").getContext("2d")).fillCir
 export class CsRunnerComponent extends CsController {
     constructor(el: ElementRef<HTMLElement>, http: HttpClient, domSanitizer: DomSanitizer, cdr: ChangeDetectorRef) {
         super(el, http, domSanitizer, cdr);
-    }
-}
-
-
-@Component({
-    selector: "cs-text-runner",
-    template: `
-        <div [ngClass]="{csRunDiv: markup.borders}" class="csTinyDiv" style="text-align: left;">
-            <h4 *ngIf="header" [innerHTML]="header"></h4>
-            <span *ngIf="stem"
-                class="stem"
-                [innerHTML]="stem"></span>
-            <input class="csTinyText no-popup-menu"
-                [ngClass]="{warnFrame: isUnSaved()}"
-                *ngIf="!noeditor || viewCode"
-                [size]="cols"
-                [(ngModel)]="usercode"
-                [attr.placeholder]="placeholder"
-                (keypress)="runCodeIfCR($event)"/>
-            <button *ngIf="isRun"
-                    [attr.disabled]="isRunning || preventSave || (markup.disableUnchanged && !isUnSaved())"
-                    class = "timButton"
-                    title="(Ctrl-S)"
-                    (click)="runCode();"
-                    [innerHTML]="buttonText()"></button>
-            <a href="javascript:void(0)" *ngIf="undoButton && isUnSaved()" title="{{undoTitle}}"
-                    (click)="tryResetChanges();">
-                    &nbsp;{{undoButton}}
-                    </a>
-            <span *ngIf="savedText"
-                        class="savedText"
-                        [innerHTML]="savedText"></span>
-            <div *ngIf="connectionErrorMessage" class="error" style="font-size: 12px" [innerHTML]="connectionErrorMessage"></div>
-
-            &nbsp;&nbsp;<a href="javascript:void(0)"
-                        *ngIf="muokattu"
-                        (click)="initCode();">{{resetText}}</a>&nbsp;&nbsp;
-            <pre class="console"
-                *ngIf="result">{{result}}</pre>
-            <span class="csRunError"
-                *ngIf="runError"
-                [style]="tinyErrorStyle">{{error}}</span>
-            <div class="htmlresult"
-                *ngIf="htmlresult">
-                <span [innerHTML]="svgImageSnippet()"></span>
-            </div>
-        </div>`,
-})
-export class CsTextComponent extends CsController {
-    constructor(el: ElementRef<HTMLElement>, http: HttpClient, domSanitizer: DomSanitizer, cdr: ChangeDetectorRef) {
-        super(el, http, domSanitizer, cdr);
-    }
-}
-
-function trackByIndex(index: number, o: unknown) { return index; }
-
-@Component({
-    selector: "cs-console",
-    template: `
-    <div class="web-console no-popup-menu {{currentSize}} " (keydown)="handleKey($event)">
-        <code class="console-output">
-            <div class="console-output-elem" *ngFor="let item of history; trackBy: trackByIndex">
-                <span class="console-oldinput">
-                    <span class="console-in">{{item.istem}}</span>
-                    <span class="console-userInput">{{item.input}}</span>
-                </span>
-                <span class="console-oldresponse">
-                    <span *ngIf="!isShell">  <br/>
-                        <span class="console-out">{{item.ostem}}</span>
-                    </span>
-                    <span class="console-response" [ngClass]="{error: item.error}">
-                        <span [innerHTML]="item.response"></span>
-                    </span>
-                    <!-- Double span since [textContent] eats the innermost one -->
-                </span>
-            </div>
-            <span class="console-expander-sym" (click)="toggleSize()"></span>
-        </code>
-        <div class="console-examples-box">
-            <span class="examples-title" (click)="examplesVisible=!examplesVisible">    ▼ example expressions ▲</span>
-            <div>Click to load:</div>
-            <ul>
-                <li *ngFor="let example of examples; index as i">
-                    <a (click)="loadExample(i)" title="example.expr">{{example.title||example.expr}}</a>
-                </li>
-            </ul>
-        </div>
-        <div class="console-curIndex" *ngIf="isShell">{{pwd}}</div>
-        <span class="console-curIndex">in_{{cursor}}</span>
-        <input type="text" placeholder="type expressions here"
-                        class="console-input"
-                        [(ngModel)]="currentInput"/>
-        &nbsp;
-        <div class="console-buttons">
-            <button (click)="up()">↑</button>&nbsp;
-            <button (click)="down()">↓</button>&nbsp;
-            <button (click)="handler()">Enter</button>&nbsp;
-        </div>
-    </div>`,
-})
-export class CsConsoleComponent extends CsBase implements IController {
-    trackByIndex = trackByIndex;
-    // isShell: boolean; method
-    cursor: number;
-    currentSize: string;
-    // isHtml: boolean;
-    oldpwd!: string;
-    currentInput: string;
-    pwd!: string;
-    // byCode: string; method
-    // content: AttrType;
-    examples: Array<t.TypeOf<typeof Example>>;
-    examplesVisible: boolean = true;
-    history: Array<{istem: string, ostem: string, input: string, response: string, error?: string}>;
-    // savestate: string;
-    // path: string;
-    // type: string;
-
-    constructor(el: ElementRef<HTMLElement>, http: HttpClient, domSanitizer: DomSanitizer, public cdr: ChangeDetectorRef) {
-        super(el, http, domSanitizer);
-        this.examples = [];
-        this.history = [];
-        this.currentSize = "normal";
-        this.currentInput = "";
-        this.cursor = this.history.length; // this.history.length means new input is last command.
-    }
-
-    $postLink() {
-        // nothing to do
-    }
-
-    get isShell() {
-        return languageTypes.getRunType(this.type, "") === "shell";
-    }
-
-    get savestate() {
-        return this.markup.savestate;
-    }
-
-    ngOnInit() {
-        super.ngOnInit();
-
-        // This block could be re-used
-
-        // End of generally re-usable TIM stuff
-        if (this.markup.examples) {
-            this.examples = this.markup.examples;
-        }
-
-        this.pwd = ConsolePWD.getPWD(this);
-        this.oldpwd = this.pwd;
-        if (this.usercode === "" && this.byCode) {
-            this.usercode = this.byCode.split("\n")[0];
-        }
-        this.currentInput = this.usercode;
-        if (this.isShell) {
-            ConsolePWD.register(this);
-        }
-    }
-
-    setPWD(pwd: string) {
-        this.pwd = pwd;
-    }
-
-    loadExample(i: number) {
-        this.currentInput = this.examples[i].expr;
-        this.focusOnInput();
-    }
-
-    focusOnInput() {
-        const el: HTMLInputElement | null = this.getRootElement().querySelector(".console-input");
-        if (el) {
-            el.focus();
-        }
-    }
-
-    async handler() {
-        const url = this.pluginMeta.getAnswerUrl();
-        const ty = languageTypes.getRunType(this.type, "shell");
-        const ucode = this.currentInput;
-        const isInput = false;
-        const uargs = "";
-        const uinput = "";
-
-        const r = await this.httpPut<{web: {pwd?: string, error?: string, console?: string}}>(url,
-            {
-                input: {
-                    usercode: ucode,
-                    userinput: uinput,
-                    isInput: isInput,
-                    userargs: uargs,
-                    type: ty,
-                },
-            },
-        );
-        if (r.ok) {
-            const data = r.result;
-            let s = "";
-            this.oldpwd = this.pwd;
-            if (data.web.pwd) {
-                ConsolePWD.setPWD(data.web.pwd, this);
-            }
-            if (data.web.error) {
-                s = data.web.error;
-                s = "<pre>" + s + "</pre>";
-            } else {
-                s = data.web.console ?? "";
-                if (!this.markup.isHtml) {
-                    s = "<pre>" + s + "</pre>";
-                }
-            }
-            this.submit(s);
-        } else {
-            console.log(["protocol error", r.result.error]);
-            this.submit("Endless loop?");
-        }
-    }
-
-    toggleSize() {
-        if (this.currentSize === "normal") {
-            this.currentSize = "enlarged";
-        } else {
-            this.currentSize = "normal";
-        }
-    }
-
-    async submit(result: string) {
-        this.history.push({
-            istem: this.isShell ? this.history.length + " " + this.oldpwd + "$" : "in_" + this.history.length + ": ",
-            ostem: this.isShell ? "" : "out_" + this.history.length + ": ",
-            input: this.currentInput,
-            response: result,
-        });
-        this.currentInput = "";
-        this.cursor = this.history.length;
-        await $timeout();
-        const el = this.getRootElement().querySelector(".console-output");
-        if (el) {
-            el.scrollTop = el.scrollHeight;
-        }
-    }
-
-    load() {
-        if (this.cursor >= this.history.length) {
-            this.currentInput = "";
-            this.cursor = this.history.length;
-            return;
-        }
-        const norm = Math.min(this.history.length - 1, Math.max(0, this.cursor));
-        this.currentInput = this.history[norm].input;
-        this.cursor = norm;
-    }
-
-    up() {
-        if (!this.cursor) {
-            return;
-        }
-        this.cursor--;
-        this.load();
-    }
-
-    down() {
-        this.cursor++;
-        this.load();
-    }
-
-    handleKey(ev: KeyboardEvent) {
-        if (ev.which === 13) {
-            this.handler();
-        }
-        if (ev.which === 40) {
-            this.down();
-        }
-        if (ev.which === 38) {
-            this.up();
-        }
     }
 }
---
+++ timApp/modules/cs/js/text.ts
@@ -0,0 +1,58 @@
+import {
+    Component,
+    ChangeDetectorRef,
+    ElementRef,
+} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {DomSanitizer} from "@angular/platform-browser";
+import {CsController} from "./csPlugin";
+
+@Component({
+    selector: "cs-text-runner",
+    template: `
+        <div [ngClass]="{csRunDiv: markup.borders}" class="csTinyDiv" style="text-align: left;">
+            <h4 *ngIf="header" [innerHTML]="header"></h4>
+            <span *ngIf="stem"
+                class="stem"
+                [innerHTML]="stem"></span>
+            <input class="csTinyText no-popup-menu"
+                [ngClass]="{warnFrame: isUnSaved()}"
+                *ngIf="!noeditor || viewCode"
+                [size]="cols"
+                [(ngModel)]="usercode"
+                [attr.placeholder]="placeholder"
+                (keypress)="runCodeIfCR($event)"/>
+            <button *ngIf="isRun"
+                    [attr.disabled]="isRunning || preventSave || (markup.disableUnchanged && !isUnSaved())"
+                    class = "timButton"
+                    title="(Ctrl-S)"
+                    (click)="runCode();"
+                    [innerHTML]="buttonText()"></button>
+            <a href="javascript:void(0)" *ngIf="undoButton && isUnSaved()" title="{{undoTitle}}"
+                    (click)="tryResetChanges();">
+                    &nbsp;{{undoButton}}
+                    </a>
+            <span *ngIf="savedText"
+                        class="savedText"
+                        [innerHTML]="savedText"></span>
+            <div *ngIf="connectionErrorMessage" class="error" style="font-size: 12px" [innerHTML]="connectionErrorMessage"></div>
+
+            &nbsp;&nbsp;<a href="javascript:void(0)"
+                        *ngIf="muokattu"
+                        (click)="initCode();">{{resetText}}</a>&nbsp;&nbsp;
+            <pre class="console"
+                *ngIf="result">{{result}}</pre>
+            <span class="csRunError"
+                *ngIf="runError"
+                [style]="tinyErrorStyle">{{error}}</span>
+            <div class="htmlresult"
+                *ngIf="htmlresult">
+                <span [innerHTML]="svgImageSnippet()"></span>
+            </div>
+        </div>`,
+})
+export class CsTextComponent extends CsController {
+    constructor(el: ElementRef<HTMLElement>, http: HttpClient, domSanitizer: DomSanitizer, cdr: ChangeDetectorRef) {
+        super(el, http, domSanitizer, cdr);
+    }
+}
