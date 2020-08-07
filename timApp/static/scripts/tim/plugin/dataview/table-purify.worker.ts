3e0fef7efb734eebcf0d8bb49a0c25582a3305cc
dezhidki <dezhidki@jyu.fi>
2020-08-07 15:29:02 +0300

    TablePurifyWorker: document purpose

--- timApp/static/scripts/tim/plugin/dataview/table-purify.worker.ts
+++ timApp/static/scripts/tim/plugin/dataview/table-purify.worker.ts
@@ -1,5 +1,17 @@
 /// <reference lib="webworker" />
 
+/**
+ * Custom WebWorker that sanitizes given rows of a DataView
+ *
+ * Because web workers are isolated from the main thread, no DOM API is available, in which case
+ * using DOMPurify is impossible. As such, this web worker uses sanitize-html instead which is initially
+ * designed for running in isolated environments with no access to DOM (like NodeJS, for instance).
+ *
+ * Web workers are bundled separately from main app chunks, so sanitize-html is only bundled as part of this web worker.
+ * That means that the penalty of downloading an extra purifier comes only when tables are used with virtual scrolling
+ * enabled.
+ */
+
 import * as sanitizeHtml from 'sanitize-html';
 
 interface PurifyData {
