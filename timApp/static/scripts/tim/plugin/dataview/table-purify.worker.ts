a926b75947cf6624bece1f9bc2da0ef28bba07be
dezhidki <dezhidki@jyu.fi>
2020-08-13 13:20:48 +0300

    TablePurifyWorker: document bundle size and lifetime

--- timApp/static/scripts/tim/plugin/dataview/table-purify.worker.ts
+++ timApp/static/scripts/tim/plugin/dataview/table-purify.worker.ts
@@ -1,8 +1,10 @@
 /// <reference lib="webworker" />
 
 /**
- * Custom WebWorker that sanitizes given rows of a DataView. This is one of the primary methods to speed up TimTable
- * loading, as sanitizing initial data takes up to 50% of the loading time.
+ * Custom WebWorker that sanitizes given rows of a DataView during first load.
+ * This is one of the primary methods to speed up TimTable loading, as sanitizing initial data takes up to 50%
+ * of the loading time. The worker only does its job in DataView's virtual scrolling mode, as not all data
+ * is visible at load time.
  *
  * Because web workers are isolated from the main thread, no DOM API is available, in which case
  * using DOMPurify is impossible. As such, this web worker uses sanitize-html instead which is initially
@@ -10,7 +12,7 @@
  *
  * Web workers are bundled separately from main app chunks, so sanitize-html is only bundled as part of this web worker.
  * That means that the penalty of downloading an extra purifier comes only when tables are used with virtual scrolling
- * enabled.
+ * enabled. The size of the entire compiled worker is under 1kB which shouldn't cause any visible loading times.
  */
 
 import * as sanitizeHtml from 'sanitize-html';
