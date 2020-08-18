df53dc1d0c7187e2a88db951cf4054a251480d71
Mika Lehtinen <mikkalle@jyu.fi>
2020-08-18 15:06:04 +0300

    Add a CLI command for truncating large answers

--- timApp/admin/answer_cli.py
+++ timApp/admin/answer_cli.py
@@ -1,14 +1,18 @@
 import json
+import sys
 from datetime import datetime
 from typing import List, Tuple
 
 import click
 from flask.cli import AppGroup
+from sqlalchemy import func
+from sqlalchemy.orm import joinedload
 
 from timApp.admin.datetimetype import DateTimeType
 from timApp.answer.answer import Answer, AnswerSaver
 from timApp.answer.answer_models import UserAnswer
 from timApp.document.docentry import DocEntry
+from timApp.document.docinfo import DocInfo
 from timApp.timdb.sqa import db
 from timApp.user.user import User
 from timApp.user.usergroup import UserGroup
@@ -40,18 +44,14 @@ def fix_double_c(dry_run: bool) -> None:
                 if not dry_run:
                     a.content = json.dumps(c)
     print(f'Total {count}')
-    if not dry_run:
-        db.session.commit()
+    commit_if_not_dry(dry_run)
 
 
 @answer_cli.command()
 @click.argument('doc')
 @click.option('--dry-run/--no-dry-run', default=True)
 def clear_all(doc: str, dry_run: bool) -> None:
-    d = DocEntry.find_by_path(doc)
-    if not d:
-        click.echo(f'cannot find document "{doc}"', err=True)
-        return
+    d = get_doc_or_quit(doc)
     ids = Answer.query.filter(Answer.task_id.startswith(f'{d.id}.')).with_entities(Answer.id)
     cnt = ids.count()
     UserAnswer.query.filter(UserAnswer.answer_id.in_(ids)).delete(synchronize_session=False)
@@ -61,8 +61,7 @@ def clear_all(doc: str, dry_run: bool) -> None:
     anns.delete(synchronize_session=False)
     Answer.query.filter(Answer.id.in_(ids)).delete(synchronize_session=False)
     click.echo(f'Total {cnt}')
-    if not dry_run:
-        db.session.commit()
+    commit_if_not_dry(dry_run)
 
 
 @answer_cli.command()
@@ -72,10 +71,7 @@ def clear_all(doc: str, dry_run: bool) -> None:
 @click.option('--dry-run/--no-dry-run', default=True)
 @click.option('--may-invalidate/--no-may-invalidate', default=False)
 def revalidate(doc: str, deadline: datetime, group: str, dry_run: bool, may_invalidate: bool) -> None:
-    d = DocEntry.find_by_path(doc)
-    if not d:
-        click.echo(f'cannot find document "{doc}"', err=True)
-        return
+    d = get_doc_or_quit(doc)
     answers: List[Tuple[Answer, str]] = (
         Answer.query
             .filter(Answer.task_id.startswith(f'{d.id}.'))
@@ -100,7 +96,64 @@ def revalidate(doc: str, deadline: datetime, group: str, dry_run: bool, may_inva
     total = len(answers)
     click.echo(f'Changing {changed_to_valid} to valid, {changed_to_invalid} to invalid.')
     click.echo(f'Total answers in document for group: {total}')
+    commit_if_not_dry(dry_run)
+
+
+def commit_if_not_dry(dry_run: bool) -> None:
     if not dry_run:
         db.session.commit()
     else:
-        print('Dry run enabled, nothing changed.')
+        click.echo('Dry run enabled, nothing changed.')
+
+
+def get_doc_or_quit(doc: str) -> DocInfo:
+    d = DocEntry.find_by_path(doc)
+    if not d:
+        click.echo(f'cannot find document "{doc}"', err=True)
+        sys.exit(1)
+    return d
+
+
+@answer_cli.command()
+@click.argument('doc')
+@click.option('--limit', required=True, type=int)
+@click.option('--to', required=True, type=int)
+@click.option('--dry-run/--no-dry-run', default=True)
+def truncate_large(doc: str, limit: int, to: int, dry_run: bool) -> None:
+    if limit < to:
+        click.echo('limit must be >= to')
+        sys.exit(1)
+    d = get_doc_or_quit(doc)
+    anss: List[Answer] = (
+        Answer.query
+            .filter(Answer.task_id.startswith(f'{d.id}.'))
+            .filter(func.length(Answer.content) > limit)
+            .options(joinedload(Answer.users_all))
+            .all()
+    )
+    note = '\n\n(answer truncated)'
+    try_keys = ['usercode', 'c']
+    truncated = 0
+    for a in anss:
+        diff = len(a.content) - to
+        if diff > 0:
+            loaded = a.content_as_json
+            if not isinstance(loaded, dict):
+                continue
+            for k in try_keys:
+                c = loaded.get(k)
+                if c:
+                    c_diff = len(c) - to
+                    if c_diff <= 0:
+                        continue
+                    try:
+                        new_c = c[:-(c_diff + len(note))] + note
+                    except IndexError:
+                        continue
+                    print(f'Truncating: {a.task_id}, {a.users_all[0].name}, {a.answered_on}, length {len(a.content)}')
+                    truncated += 1
+                    loaded[k] = new_c
+                    a.content = json.dumps(loaded)
+                    break
+    print(f'Truncating {truncated} answers.')
+    commit_if_not_dry(dry_run)
