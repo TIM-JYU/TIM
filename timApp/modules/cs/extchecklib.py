673b8a578381cb1706dad1587aefe0822a02fa48
Tuomas Laine <tuomaslaine97@gmail.com>
2020-09-04 10:23:44 +0000

    remove extra field()s in extchecklib

--- timApp/modules/cs/extchecklib.py
+++ timApp/modules/cs/extchecklib.py
@@ -6,32 +6,32 @@ from loadable import Loadable
 
 @dataclass
 class AngularComponent(Loadable):
-    template: str = field(default="")
-    component: str = field(default="class {}")
+    template: str = ""
+    component: str = "class {}"
 
 @dataclass
 class AngularModule(Loadable):
     components: Dict[str, AngularComponent] = field(default_factory=dict)
-    entry: str = field(default="")
+    entry: str = ""
 
 @dataclass
 class DivContent(Loadable):
-    classes: str = field(default="")
-    content: str = field(default="")
+    classes: str = ""
+    content: str = ""
 
 @dataclass
 class OutputContainer(Loadable):
-    title: Optional[DivContent] = field(default=None)
-    text: Optional[DivContent] = field(default=None)
-    angular: Optional[AngularModule] = field(default=None)
-    hide: bool = field(default=False) # whether to hide by default
+    title: Optional[DivContent] = None
+    text: Optional[DivContent] = None
+    angular: Optional[AngularModule] = None
+    hide: bool = False # whether to hide by default
 
 @dataclass
 class RunResult(Loadable):
     output_boxes: List[OutputContainer] = field(default_factory=list)
     penalties: Dict[str, Union[bool, str]] = field(default_factory=dict)
-    points: float = field(default=0.0)
-    max_points: Optional[float] = field(default=None)
+    points: float = 0.0
+    max_points: Optional[float] = None
 
     def penalize(self, key: str) -> bool:
         return self.penalties and self.penalties.get(key, False)
