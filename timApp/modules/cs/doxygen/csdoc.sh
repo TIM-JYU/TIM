54bfcfdd143fe729d3ee1c0d3bc492f5a30f931b
Vesa Lappalainen <vesal@jyu.fi>
2020-11-15 19:35:39 +0200

    doxygen: prevent main window scroll from iframe

--- timApp/modules/cs/doxygen/csdoc.sh
+++ timApp/modules/cs/doxygen/csdoc.sh
@@ -5,3 +5,8 @@ mkdir -p $2
 rm csdoc
 ln -s $2 csdoc
 doxygen /cs/doxygen/Doxyfile
+# Poista se ettei scrollaa vaan pääikkunaa
+sed -i s!if.*updateLocation.*window.location.href=aname.!! csdoc/html/navtree.js # >csdoc/html/navtree.tmp
+# sed -i s!...#doc-content...scrollTop.0..!! csdoc/html/navtree.js
+# sed -i 's!(var o = new Object())!\1 return!' csdoc/html/navtree.js
+#cp csdoc/html/navtree.tmp csdoc/html/navtree.js
