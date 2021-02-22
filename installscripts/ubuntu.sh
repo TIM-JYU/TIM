260342fe6433907946cdaf8e53032b4018ade6a1
Mika Lehtinen <mikkalle@jyu.fi>
2021-02-22 14:15:30 +0200

    Update Ubuntu install script

--- installscripts/ubuntu.sh
+++ installscripts/ubuntu.sh
@@ -6,14 +6,14 @@ set -o pipefail
 
 # Install Docker.
 apt update
-apt install -y apt-transport-https ca-certificates curl software-properties-common
+apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt update
 apt install -y docker-ce
 
 # Install Docker Compose.
-curl -L https://github.com/docker/compose/releases/download/1.25.5/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.28.4/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # Run docker commands without sudo.
