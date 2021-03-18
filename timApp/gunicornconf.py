e590b8d7aada516481dc5f7c24272ffbf0fafe5c
Mika Lehtinen <mikkalle@jyu.fi>
2021-03-18 10:39:57 +0200

    Use psycogreen for Gunicorn

--- timApp/gunicornconf.py
+++ timApp/gunicornconf.py
@@ -1,5 +1,7 @@
 import multiprocessing
 
+from psycogreen.gevent import patch_psycopg
+
 # This file MUST NOT contain any TIM imports! Otherwise Gunicorn restart signal will not work properly.
 
 bind = "0.0.0.0:5000"
@@ -7,3 +9,7 @@ workers = multiprocessing.cpu_count()
 worker_class = "gevent"  # Possible values: sync, gevent, eventlet, tornado, gaiohttp, gthread
 limit_request_line = 0
 timeout = 600
+
+
+def post_fork(server, worker):
+    patch_psycopg()
