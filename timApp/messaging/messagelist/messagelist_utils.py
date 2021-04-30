512d1f0f0944153e2bdf6f59c98b7d6568ff1672
Tomi Lundberg <totalund@student.jyu.fi>
2021-04-30 18:34:05 +0300

    Finish message archiving

--- timApp/messaging/messagelist/messagelist_utils.py
+++ timApp/messaging/messagelist/messagelist_utils.py
@@ -1,11 +1,13 @@
 import re
 from dataclasses import dataclass, field
+from datetime import datetime
 from typing import Optional, List, Dict
 
 from timApp.document.create_item import create_document
 from timApp.folder.folder import Folder
 from timApp.item.block import Block
 from timApp.messaging.messagelist.messagelist_models import MessageListModel, Channel
+from timApp.timdb.sqa import db
 from timApp.util.flask.requesthelper import RouteException
 from timApp.util.utils import remove_path_special_chars
 
@@ -116,14 +118,33 @@ MESSAGE_LIST_ARCHIVE_FOLDER_PREFIX = "archives"
 
 def archive_message(message_list: MessageListModel, message: MessageTIMversalis) -> None:
     """Archive a message for a message list."""
-    # TODO: If there are multiple messages with same title, differentiate them.
-    archive_title = message.title
+    # TODO: If there are multiple messages with same title, differentiate them. FIXME MESSAGE TITLE IN BRACKETS
+    archive_title = f"{message.title}-{datetime.now()}"
     archive_folder_path = f"{MESSAGE_LIST_ARCHIVE_FOLDER_PREFIX}/{remove_path_special_chars(message_list.name)}"
+    archive_doc_path = f"{archive_folder_path}/{remove_path_special_chars(archive_title)}"
 
+    # From the archive folder, query all documents, sort them by created attribute. We do this to get the previously
+    # newest archived message, before we create a archive document for newest message.
     # Archive folder for message list.
-    archive_folder = Folder.find_by_location(archive_folder_path, message_list.name)
+    # archive_folder = Folder.find_by_location(archive_folder_path, message_list.name)
+    archive_folder = Folder.find_by_path(archive_folder_path)
 
-    archive_doc = create_document(archive_folder_path, archive_title)
+    all_archived_messages = []
+    if archive_folder is not None:
+        all_archived_messages = archive_folder.get_all_documents()
+    else:
+        # TODO: Set folder's owners to be message list's owners.
+        # FIXME USE BETTER METHOD HERE
+        manage_doc_block = Block.query.filter_by(id=message_list.manage_doc_id).one()
+        owners = manage_doc_block.owners
+        Folder.create(archive_folder_path, owner_groups=owners, title=f"{message_list.name}")
+
+    # if archive_folder is None:
+    #    archive_folder = Folder.create(archive_folder_path, owner_groups=None, title=message_list.name)
+
+    # Find suitable name for archive document.
+    # datetime.now()
+    archive_doc = create_document(archive_doc_path, archive_title)
 
     # Set header information for archived message.
     archive_doc.document.add_text(f"Title: {message.title}")
@@ -131,49 +152,41 @@ def archive_message(message_list: MessageListModel, message: MessageTIMversalis)
     archive_doc.document.add_text(f"Recipients: {message.recipients}")
 
     # Set message body for archived message.
+    archive_doc.document.add_text("Message body:")
     archive_doc.document.add_text(f"{message.message_body}")
 
-    # From the archive folder, query all documents, sort them by created attribute. We do this to get the previously
-    # newest archived message, before we create a archive document for newest message.
-    all_archived_messages = []
-    if archive_folder is not None:
-        all_archived_messages = archive_folder.get_all_documents()
-    else:
-        # TODO: Set folder's owners to be message list's owners.
-        manage_doc_block = Block.query.filter_by(id=message_list.manage_doc_id).one()
-        owners = manage_doc_block.owners()
-        Folder.create(archive_folder_path, owner_groups=owners, title=f"{message_list.name}")
-
     # If there is only one message, we don't need to add links to any other messages.
-    if len(all_archived_messages) > 1:
+    if len(all_archived_messages):
         # TODO: Extract linkings to their own functions.
         sorted_messages = sorted(all_archived_messages, key=lambda document: document.block.created, reverse=True)
-        previous_doc = sorted_messages[1]
+        previous_doc = sorted_messages[0]
 
-        # Set footer information for archived message. Footer information is not set for the very first message,
-        # it get's it's link to next message when a second message is archived.
+        # Set the "Next message" link for the previous newest message.
+        next_doc_title = f"Next message: {archive_title}"
+        next_doc_link = f"{archive_doc.url}"
+        next_message_link = f"[{next_doc_title}]({next_doc_link})"
+        previous_doc.document.add_text(next_message_link)
 
-        # VIESTIM: Do we need other type of URL to previous_doc and archive_doc? Is url attribute enough?
-        previous_doc_title = "Previous message"
+        # Set the "Previous message" for the newest message.
+        previous_doc_title = f"Previous message: {previous_doc.title}"
         previous_doc_link = f"{previous_doc.url}"
         previous_message_link = f"[{previous_doc_title}]({previous_doc_link})"
         archive_doc.document.add_text(f"{previous_message_link}")
 
-        next_doc_title = "Next message"
-        next_doc_link = f"{archive_doc.url}"
-        previous_doc.document.add_text(f"[{next_doc_title}]({next_doc_link})")
-
     # TODO: Set proper rights to the document. The message sender owns the document. Owners of the list get at least a
     #  view right. Other rights depend on the message list's archive policy.
+    db.session.commit()
+    return
 
 
 def parse_mailman_message(original: Dict, msg_list: MessageListModel) -> MessageTIMversalis:
     """Modify an email message sent from Mailman to TIM's universal message format."""
     # VIESTIM: original message is of form specified in https://pypi.org/project/mail-parser/
-
-    visible_recipients = (original.get("to", []))
-    visible_recipients.extend(original.get("cc", []))
-    visible_recipients.extend(original.get("bcc", []))
+    # msg = Mailparser(original)
+    visible_recipients: List[str] = []
+    visible_recipients.append(original.get("to", []))
+    visible_recipients.append(original.get("cc", []))
+    visible_recipients.append(original.get("bcc", []))
     # VIESTIM: How should we differentiate with cc and bcc in TIM's context? bcc recipients should still get messages
     #  intented for them.
 
