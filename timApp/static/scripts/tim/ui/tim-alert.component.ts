08e344fe2def4b6f21129bdf9560845cf426231e
dezhidki <dezhidki@jyu.fi>
2021-05-18 18:26:30 +0300

    TimAlertComponent: add closeable attribute

--- timApp/static/scripts/tim/ui/tim-alert.component.ts
+++ timApp/static/scripts/tim/ui/tim-alert.component.ts
@@ -4,14 +4,18 @@ import {AlertSeverity} from "tim/ui/formErrorMessage";
 @Component({
     selector: "tim-alert",
     template: `
-        <div class="alert alert-{{ severity }}">
+        <div *ngIf="open" class="alert alert-{{ severity }}">
             <span class="glyphicon glyphicon-{{getIcon()}}"></span>&nbsp;
             <ng-content></ng-content>
+            <i *ngIf="closeable" role="button" class="glyphicon glyphicon-remove" (click)="open = false"></i>
         </div>
     `,
+    styleUrls: ["tim-alert.component.scss"],
 })
 export class TimAlertComponent {
     @Input() severity: AlertSeverity = "danger";
+    @Input() closeable: boolean = false;
+    open = true;
 
     getIcon() {
         switch (this.severity) {
