3af1532e87e1c3bfbdb389c325e30494a67b7f82
Tuuli Veini <tuuliveini@gmail.com>
2021-06-01 20:55:16 +0300

    Fix DocEntry query and reformat file

--- timApp/tests/server/test_tim_message.py
+++ timApp/tests/server/test_tim_message.py
@@ -1,7 +1,7 @@
 from timApp.auth.accesstype import AccessType
 from timApp.document.docentry import DocEntry
 from timApp.folder.folder import Folder
-from timApp.messaging.timMessage.internalmessage_models import InternalMessageDisplay
+from timApp.messaging.timMessage.internalmessage_models import InternalMessageDisplay, InternalMessage
 from timApp.tests.server.timroutetest import TimRouteTest
 
 
@@ -12,34 +12,48 @@ class UrlTest(TimRouteTest):
         self.create_folder(self.get_personal_item_path("testfolder"))
 
         self.login_test1()
-        self.json_post('/timMessage/url_check', {"urls": "http://www.google.com"}, expect_status=404)
+        self.json_post('/timMessage/url_check',
+                       {"urls": "http://www.google.com"},
+                       expect_status=404)
         self.create_doc(self.get_personal_item_path("testdoc"))
         self.create_folder(self.get_personal_item_path("testfolder"))
-        self.json_post('/timMessage/url_check', {"urls": "http://localhost/view/users/test-user-1/testdoc"},
+        self.json_post('/timMessage/url_check',
+                       {"urls": "http://localhost/view/users/test-user-1/testdoc"},
                        expect_status=200)
-        self.json_post('/timMessage/url_check', {"urls": "http://localhost/view/users/test-user-1/testdoc"},
+        self.json_post('/timMessage/url_check',
+                       {"urls": "http://localhost/view/users/test-user-1/testdoc"},
                        expect_content={"shortened_urls": "users/test-user-1/testdoc"})
-        self.json_post('/timMessage/url_check', {"urls": "http://localhost/view/users/test-user-1/testfolder"},
+        self.json_post('/timMessage/url_check',
+                       {"urls": "http://localhost/view/users/test-user-1/testfolder"},
                        expect_status=200)
-        self.json_post('/timMessage/url_check', {"urls": "http://localhost/view/users/test-user-2/testdoc"},
+        self.json_post('/timMessage/url_check',
+                       {"urls": "http://localhost/view/users/test-user-2/testdoc"},
                        expect_status=401)
-        self.json_post('/timMessage/url_check', {"urls": "http://localhost/view/users/test-user-2/testfolder"},
+        self.json_post('/timMessage/url_check',
+                       {"urls": "http://localhost/view/users/test-user-2/testfolder"},
                        expect_status=401)
-        self.json_post('/timMessage/url_check', {
+        self.json_post('/timMessage/url_check',
+                       {
             "urls": "http://localhost/teacher/users/test-user-1/testdoc\nhttp://localhost/teacher/users/test-user-1"},
                        expect_status=200)
-        self.json_post('/timMessage/url_check', {
+        self.json_post('/timMessage/url_check',
+                       {
             "urls": "http://localhost/teacher/users/test-user-1/testdoc\nhttp://localhost/teacher/users/test-user-1"},
                        expect_content={"shortened_urls": "users/test-user-1/testdoc\nusers/test-user-1"})
-        self.json_post('/timMessage/url_check', {"urls": "  http://localhost/view/users/test-user-1/testdoc"},
+        self.json_post('/timMessage/url_check',
+                       {"urls": "  http://localhost/view/users/test-user-1/testdoc"},
                        expect_status=200)
-        self.json_post('/timMessage/url_check', {"urls": "http://localhost/view/users/test-user-1/testdoc  "},
+        self.json_post('/timMessage/url_check',
+                       {"urls": "http://localhost/view/users/test-user-1/testdoc  "},
                        expect_status=200)
-        self.json_post('/timMessage/url_check', {"urls": "http://localhost/view/users/test-user-1/testdoc#jjndsg"},
+        self.json_post('/timMessage/url_check',
+                       {"urls": "http://localhost/view/users/test-user-1/testdoc#jjndsg"},
                        expect_status=200)
-        self.json_post('/timMessage/url_check', {"urls": "http://localhost/view/users/test-user-1/testfolder/"},
+        self.json_post('/timMessage/url_check',
+                       {"urls": "http://localhost/view/users/test-user-1/testfolder/"},
                        expect_status=200)
-        self.json_post('/timMessage/url_check', {"urls": "http://localhost/view/users/test-user-1/testfolder/testdoc"},
+        self.json_post('/timMessage/url_check',
+                       {"urls": "http://localhost/view/users/test-user-1/testfolder/testdoc"},
                        expect_status=404)
 
 
@@ -70,12 +84,9 @@ class SendMessageTest(TimRouteTest):
                        expect_status=200)
 
         self.login_test1()
-        self.get('/view/users/test-user-1/testfolder', expect_status=200)
-        self.get('/view/messages/tim-messages', expect_status=200)
+        self.get('/view/messages/tim-messages', expect_status=200)  # tim-messages folder created successfully
 
         display = InternalMessageDisplay.query.filter_by(usergroup_id=self.get_test_user_1_group_id()).first()
-        print(display.display_doc_id)
-        print(display.usergroup_id)
-        msg_doc = DocEntry.query.filter_by(id=display.display_doc_id).first()
-        if msg_doc:
-            print(msg_doc.name)
+        msg = InternalMessage.query.filter_by(id=display.message_id).first()
+        msg_doc = DocEntry.query.filter_by(id=msg.doc_id).first()
+        self.get(f'/view/{msg_doc.name}', expect_status=200)  # document for message created successfully
