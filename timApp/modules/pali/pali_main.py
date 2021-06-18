1bce407e07a833be2ea7c0cd6cf77d8edad22b7b
dezhidki <dezhidki@jyu.fi>
2021-06-18 22:51:39 +0300

    Adjust COMPOSE_PROFILES parsing

--- timApp/modules/pali/pali_main.py
+++ timApp/modules/pali/pali_main.py
@@ -174,7 +174,7 @@ cols: 20
     }
 
     # Show pali templates only in development mode.
-    if os.environ.get('COMPOSE_PROFILES') == "dev":
+    if "dev" in os.environ.get('COMPOSE_PROFILES'):
         result['editor_tabs'] = editor_tabs
 
     return result
