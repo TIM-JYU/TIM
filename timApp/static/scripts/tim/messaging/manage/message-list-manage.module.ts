3088fee41e9ac44ff6890cbf3ce27780483957ae
dezhidki <dezhidki@jyu.fi>
2021-07-06 22:25:20 +0300

    Move tim-message-send to its own module
    Message sending does not belong to TimTable module

--- timApp/static/scripts/tim/messaging/manage/message-list-manage.module.ts
+++ timApp/static/scripts/tim/messaging/manage/message-list-manage.module.ts
@@ -3,14 +3,19 @@ import {CommonModule} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
 import {TimUtilityModule} from "../../ui/tim-utility.module";
-import {TableFormModule} from "../../plugin/tableForm";
 import {createDowngradedModule, doDowngrade} from "../../downgrade";
+import {TimMessageSendModule} from "../tim-message-send.component";
 import {MessageListAdminComponent} from "./message-list-admin.component";
 
 @NgModule({
     declarations: [MessageListAdminComponent],
     exports: [MessageListAdminComponent],
-    imports: [CommonModule, FormsModule, TimUtilityModule, TableFormModule],
+    imports: [
+        CommonModule,
+        FormsModule,
+        TimUtilityModule,
+        TimMessageSendModule,
+    ],
 })
 export class MessageListManageModule implements DoBootstrap {
     ngDoBootstrap(appRef: ApplicationRef): void {}
