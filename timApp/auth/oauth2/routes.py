ec0b53a45e3d9a441691d1f4ae5ee3431defb0d0
dezhidki <dezhidki@jyu.fi>
2021-08-10 12:19:23 +0300

    OAuth2: Add initial profile check

--- timApp/auth/oauth2/routes.py
+++ timApp/auth/oauth2/routes.py
@@ -1,3 +1,4 @@
+from authlib.integrations.flask_oauth2 import current_token
 from authlib.oauth2.rfc6749 import BaseGrant
 from flask import Response, render_template
 
@@ -6,6 +7,8 @@ from timApp.auth.oauth2.models import OAuth2Client, describe_scope, Scope
 from timApp.auth.oauth2.oauth2 import auth_server, require_oauth
 from timApp.auth.sessioninfo import get_current_user_object, logged_in
 from timApp.tim_app import csrf
+from timApp.user.user import User
+from timApp.util.flask.responsehelper import json_response
 from timApp.util.flask.typedblueprint import TypedBlueprint
 
 oauth = TypedBlueprint('oauth', __name__, url_prefix='/oauth')
@@ -36,5 +39,16 @@ def issue_token() -> Response:
 @oauth.get('profile')
 @require_oauth(Scope.profile.name)
 def get_user_profile() -> Response:
-    print("hit!")
-    raise Exception("Hit!")
+    user: User = current_token.user
+    # Implement profile output here to make sure not too much information is given out for this scope
+    return json_response({
+        "id": user.id,
+        "emails": [{  # First email is considered primary
+            "email": user.email,
+            "verified": True,
+        }],
+        "last_name": user.last_name,
+        "given_name": user.given_name,
+        "real_name": user.real_name,
+        "username": user.name
+    })
