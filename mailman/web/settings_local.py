5109d85545cc986c7e391c9802dcd1341d1ea444
dezhidki <dezhidki@jyu.fi>
2021-08-17 12:54:54 +0300

    Add default dev configuration for mailman oauth login

--- mailman/web/settings_local.py
+++ mailman/web/settings_local.py
@@ -2,3 +2,18 @@ EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = '/tmp/mailman-messages'
 ACCOUNT_DEFAULT_HTTP_PROTOCOL = 'http'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+
+MAILMAN_WEB_SOCIAL_AUTH = [
+    'allauth_tim',
+]
+
+SOCIALACCOUNT_PROVIDERS = {
+    'tim': {
+        'SCOPE': ['profile'],
+        "APP": {
+            "client_id": "mailman_tim",
+            "secret": "test123",
+        },
+    },
+}
