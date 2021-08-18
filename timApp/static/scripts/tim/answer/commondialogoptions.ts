03310716f84b0f3a1275f424e209f69e97286e56
dezhidki <dezhidki@jyu.fi>
2021-08-18 17:14:48 +0300

    Move DateFromString io-ts helper to attributes.ts

--- timApp/static/scripts/tim/answer/commondialogoptions.ts
+++ timApp/static/scripts/tim/answer/commondialogoptions.ts
@@ -1,18 +1,6 @@
-// from https://github.com/gcanti/io-ts/blob/master/index.md#custom-types
-import {maybeUndefined} from "tim/plugin/attributes";
+import {DateFromString, maybeUndefined} from "tim/plugin/attributes";
 import * as t from "io-ts";
-import {either} from "fp-ts/Either";
 
-const DateFromString = new t.Type<Date, string, unknown>(
-    "DateFromString",
-    (u): u is Date => u instanceof Date,
-    (u, c) =>
-        either.chain(t.string.validate(u, c), (s) => {
-            const d = new Date(s);
-            return isNaN(d.getTime()) ? t.failure(u, c) : t.success(d);
-        }),
-    (a) => a.toISOString()
-);
 export const CommonDialogOptions = t.type({
     period: t.keyof({
         whenever: null,
