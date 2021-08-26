c87748038d410ae3d47ee68a10480458d0eb9ff8
Mika Lehtinen <mikkalle@jyu.fi>
2021-08-26 15:46:36 +0300

    Avoid db access when instantiating a Plugin
    This improves performance of loading Programming 1 document
    by about 0.5s on my local machine (2.8s -> 2.3s).

--- timApp/plugin/plugintype.py
+++ timApp/plugin/plugintype.py
@@ -12,25 +12,44 @@ CONTENT_FIELD_NAME_MAP = {
 }
 
 
+class PluginTypeBase:
+    def get_type(self) -> str:
+        raise NotImplementedError
+
+    def get_content_field_name(self) -> str:
+        return CONTENT_FIELD_NAME_MAP.get(self.get_type(), 'content')
+
+    def can_give_task(self) -> bool:
+        plugin_class = timApp.plugin.containerLink.get_plugin(self.get_type())  # type: ignore[attr-defined]
+        return plugin_class.can_give_task
+
+    def to_json(self) -> Dict[str, Any]:
+        return {
+            'type': self.get_type()
+        }
+
+
 # TODO: Right now values are added dynamically to the table when saving answers. Instead add them on TIM start.
-class PluginType(db.Model):
+class PluginType(db.Model, PluginTypeBase):
     __tablename__ = 'plugintype'
     id = db.Column(db.Integer, primary_key=True)
 
     type = db.Column(db.Text, nullable=False, unique=True)
 
-    def get_content_field_name(self) -> str:
-        return CONTENT_FIELD_NAME_MAP.get(self.type, 'content')
-
-    def can_give_task(self) -> bool:
-        plugin_class = timApp.plugin.containerLink.get_plugin(self.type)  # type: ignore[attr-defined]
-        return plugin_class.can_give_task
-
     @staticmethod
     def resolve(p_type: str) -> 'PluginType':
         return PluginType.query.filter_by(type=p_type).first() or PluginType(type=p_type)
 
-    def to_json(self) -> Dict[str, Any]:
-        return {
-            'type': self.type
-        }
+    def get_type(self) -> str:
+        return self.type
+
+
+@dataclass
+class PluginTypeLazy(PluginTypeBase):
+    type: str
+
+    def get_type(self) -> str:
+        return self.type
+
+    def resolve(self) -> PluginType:
+        return PluginType.resolve(self.type)
