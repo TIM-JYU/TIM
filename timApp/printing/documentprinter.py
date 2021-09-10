2669afa95bb1fba75a4b7da727f416b5dc75b284
Mika Lehtinen <mikkalle@jyu.fi>
2021-09-10 16:01:46 +0300

    Optimize DocParagraph internal attribute handling

--- timApp/printing/documentprinter.py
+++ timApp/printing/documentprinter.py
@@ -233,7 +233,7 @@ class DocumentPrinter:
                                                                                                           par_infos):
             md = p.get_final_dict(view_ctx)['md']
             if not p.is_plugin() and not p.is_question():
-                if not p.no_macros() and not self.texplain and not self.textplain:
+                if not p.get_nomacros() and not self.texplain and not self.textplain:
                     md = expand_macros(
                         text=md,
                         macros=pdoc_macros,
