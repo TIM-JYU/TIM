6a94c4661734d36b7b54936f28f31985efc2b7fc
dezhidki <dezhidki@jyu.fi>
2021-12-03 20:35:10 +0200

    Fix xlf-merge to handle whitespace better

--- timApp/i18n/xlf-merge.js
+++ timApp/i18n/xlf-merge.js
@@ -7,10 +7,27 @@ const fs = require("fs");
 
 const LOCALES = ["fi"];
 
-function read(path) {
-    const messagesStr = fs.readFileSync(path, {encoding: "utf8"});
+function read(path, translationsList) {
+    const messagesXLIF = fs.readFileSync(path, {encoding: "utf8"});
+    const tl = translationsList;
+    // The actual translations are inside source and target elements. Those elements must preserve whitespace fully
+    // On the other hand, we would like to format our XML file
+    // Because of that, we grab and replace the translations with an ID
+    // Later, we insert translations back into formatted XML
+    const messagesStr = messagesXLIF
+        .replace(/<source>((.|\s)+?)<\/source>/g, (match, m1) => {
+            const i = tl.push(m1) - 1;
+            return `<source>${i.toString()}</source>`;
+        })
+        .replace(
+            /<target state="([^"]*)">((.|\s)+?)<\/target>/g,
+            (match, m1, m2) => {
+                const i = tl.push(m2) - 1;
+                return `<target state="${m1}">${i.toString()}</target>`;
+            }
+        );
     // Don't use compact to ensure order is preserved
-    const messages = convert.xml2js(messagesStr, { compact: false, trim: true });
+    const messages = convert.xml2js(messagesStr, {compact: false, trim: true});
 
     const transUnits = messages.elements[0].elements[0].elements[0].elements;
     const itemSources = new Map();
@@ -23,7 +40,13 @@ function read(path) {
         itemKeys.add(unit.attributes.id);
     }
 
-    return {messages, transUnits, items, itemSources, itemKeys};
+    return {
+        messages,
+        transUnits,
+        items,
+        itemSources,
+        itemKeys,
+    };
 }
 
 function setDiff(setA, setB) {
@@ -39,47 +62,56 @@ function setDiff(setA, setB) {
     return res;
 }
 
-function de(obj1, obj2) {
-    if (typeof obj1 !== typeof obj2) {
-        return false;
-    }
-    if (typeof obj1 === "string") {
-        return obj1.replace(/\s/g, "") === obj2.replace(/\s/g, "");
-    }
-    if (obj1 instanceof Array) {
-        if (obj1.length !== obj2.length) {
-            return false;
-        }
-        for (let i = 0; i < obj1.length; i++) {
-            if (!de(obj1[i], obj2[i])) {
-                return false;
-            }
-        }
-    }
-    if (obj1 instanceof Object) {
-        for (const k of Object.keys(obj1)) {
-            if (!obj2.hasOwnProperty(k)) {
-                return false;
-            }
-            if (!de(obj1[k], obj2[k])) {
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
 function addTarget(item) {
-    const target = {...item.elements[0], name: "target", attributes: { state: "new" }};
+    const target = {
+        ...item.elements[0],
+        name: "target",
+        attributes: {state: "new"},
+    };
     item.elements.splice(1, 0, target);
     return item;
 }
 
+const htmlEntities = {
+    nbsp: " ",
+    cent: "¢",
+    pound: "£",
+    yen: "¥",
+    euro: "€",
+    copy: "©",
+    reg: "®",
+    lt: "<",
+    gt: ">",
+    quot: '"',
+    amp: "&",
+    apos: "'",
+};
+
+// Adapted from https://stackoverflow.com/a/39243641
+function decodeEntities(str) {
+    return str.replace(/&([^;]+);/g, (entity, entityCode) => {
+        let match;
+        if (entityCode in htmlEntities) {
+            return htmlEntities[entityCode];
+        } else if ((match = entityCode.match(/^#x([\da-fA-F]+)$/))) {
+            return String.fromCharCode(parseInt(match[1], 16));
+        } else if ((match = entityCode.match(/^#(\d+)$/))) {
+            return String.fromCharCode(parseInt(match[1], 10));
+        } else {
+            return entity;
+        }
+    });
+}
+
+function hashTranslation(text) {
+    return decodeEntities(text).replace(/\s/g, "");
+}
 
 function mergeTranslations(locale) {
     console.log(`Merging main with "${locale}"`);
-    const newMessages = read("messages.xlf");
-    const messagesFi = read(`i18n/messages.${locale}.xlf`);
+    const translationsList = [];
+    const newMessages = read("messages.xlf", translationsList);
+    const messagesFi = read(`i18n/messages.${locale}.xlf`, translationsList);
 
     const addedMessages = setDiff(newMessages.itemKeys, messagesFi.itemKeys);
     const removedMessages = setDiff(messagesFi.itemKeys, newMessages.itemKeys);
@@ -101,8 +133,15 @@ function mergeTranslations(locale) {
         if (!newMessages.itemKeys.has(msgId)) {
             continue;
         }
-        const newSrc = newMessages.itemSources.get(msgId);
-        if (de(newSrc, src)) {
+        const srcText = hashTranslation(
+            translationsList[+src.elements[0].text]
+        );
+        const newSrc = hashTranslation(
+            translationsList[
+                +newMessages.itemSources.get(msgId).elements[0].text
+            ]
+        );
+        if (srcText === newSrc) {
             continue;
         }
         console.log(`Updated translation: ${msgId}`);
@@ -113,7 +152,16 @@ function mergeTranslations(locale) {
         messagesFi.transUnits[itemIndex] = addTarget(newItem);
     }
 
-    const newTlStr = convert.js2xml(messagesFi.messages, { compact: false, spaces: 2 });
+    const newTlStr = convert.js2xml(messagesFi.messages, {
+        compact: false,
+        spaces: 2,
+        textFn: (value, elName) => {
+            if (elName === "source" || elName === "target") {
+                return translationsList[+value];
+            }
+            return value;
+        },
+    });
     fs.writeFileSync(`i18n/messages.${locale}.xlf`, newTlStr, {
         encoding: "utf8",
     });
