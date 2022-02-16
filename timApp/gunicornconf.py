b4f36259838cecc4851ce84c28a284daa1e378b4
dezhidki <dezhidki@jyu.fi>
2022-02-16 20:31:51 +0200

    Reduce max_requests option for gunicorn to test for memory leaks

--- timApp/gunicornconf.py
+++ timApp/gunicornconf.py
@@ -12,8 +12,8 @@ worker_class = (
 )
 limit_request_line = 0
 timeout = 600
-max_requests = 5000
-max_requests_jitter = 500
+max_requests = 2000
+max_requests_jitter = 200
 
 
 def post_fork(server: Any, worker: Any) -> None:
