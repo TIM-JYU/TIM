60e9e9b3468e157f39f01094ecea19e383fb6e4e
Noora Jokela <noora.e.jokela@student.jyu.fi>
2022-03-09 10:35:13 +0200

    Comment out a function that's making pipeline fail

--- timApp/static/scripts/tim/sidebarmenu/tabs/settings-tab.component.ts
+++ timApp/static/scripts/tim/sidebarmenu/tabs/settings-tab.component.ts
@@ -384,12 +384,12 @@ export class SettingsTabComponent implements OnInit {
         );
     }
 
-    async markTranslationChecked() {
+    /* async markTranslationChecked() {
         await this.confirmPost(
             $localize`This will mark the translations in all paragraphs in this document as checked. Continue?`,
             `/markChecked/${this.item!.id}`
         );
-    }
+    }*/
 
     private async confirmPost(message: string, url: string) {
         if (!this.item) {
