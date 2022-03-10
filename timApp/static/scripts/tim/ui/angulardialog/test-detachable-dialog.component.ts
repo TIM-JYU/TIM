4d2fe91c60721204a3447a6b75d9334e88cee929
dezhidki <dezhidki@jyu.fi>
2022-03-10 15:10:22 +0200

    wip

---
+++ timApp/static/scripts/tim/ui/angulardialog/test-detachable-dialog.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+    selector: "tim-test-detachable-dialog",
+    template: `
+    <tim-dialog-frame anchor="absolute">
+        <ng-container header>
+            Test
+        </ng-container>
+        <ng-container body>
+            This is a body!
+        </ng-container>
+    </tim-dialog-frame>
+  `,
+    styles: [],
+})
+export class TestDetachableDialogComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
