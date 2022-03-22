710a2f03f6718ddf5357b2ab1b3bf2bfc6d02461
Lehkonen <ripelehk@jyu.fi>
2022-03-22 14:45:53 +0200

    unit tests fixed for exception in case incorrect API key

--- timApp/tests/unit/test_translator_deepl.py
+++ timApp/tests/unit/test_translator_deepl.py
@@ -6,16 +6,30 @@ from timApp.document.translation import translator
 
 class TestTranslatorDeepl(unittest.TestCase):
     def test_incorrect_key(self):
-        deepltrans = routes.init_deepl_translator("FI", "EN")
-        translator.DeepLTranslator.api_key = "incorrect key"
-        translatedtext = ["first str", "second str", "third str"]
-        with self.assertRaises(Exception) as context:
-            routes.translate(deepltrans, "translate text", "FI", "EN")
+        with self.assertRaises(Exception) as e:
+            routes.init_deepl_translator("FI", "EN")
 
-            self.assertTrue(
-                "DeepL API / languages responded with 403" in context.exception
-            )
-        # add assertion here
+        self.assertEqual(
+            e.exception.args[0],
+            "DeepL API / languages responded with 403",
+        )
+
+
+# code in preparation for when the initialization changes
+#        deepltrans = routes.init_deepl_translator("FI", "EN")
+#        translator.DeepLTranslator.api_key = "incorrect key"
+#        translatedtext = ["first str", "second str", "third str"]
+#
+#        with self.assertRaises(Exception) as e:
+#            routes.translate(deepltrans, "translate text", "FI", "EN")
+#
+#        self.assertEqual(
+#            e.exception.args[0],
+#            "DeepL API / languages responded with 403",
+#        )
+
+
+# add assertion here
 
 
 if __name__ == "__main__":
