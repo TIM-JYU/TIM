80d9561ad0796f9697cf1e593f3dd480fa757bd1
Lehkonen <ripelehk@jyu.fi>
2022-03-23 15:35:56 +0200

    LaTeX protection allows certain text areas to be translated from within

--- timApp/document/translation/translationparser.py
+++ timApp/document/translation/translationparser.py
@@ -14,24 +14,47 @@ x=0\;\;\;\\text{tai}\;\;\;&x^2-49=0 &&|\\text{ ratkaistaan x}\\
 
 class TranslationParser:
     def latexparse(self, text: str, translator: str):
+        # TODO add table for all the text areas to translate, currently only translates \text{}
+        # TODO add table for all the formulas, currently only does \begin{} \end{} -pair
+
         newtext = text
-        # TODO currently match and match2 have overlaps
-        # finds between singular $-signs correctly but also some $$
+        # TODO currently dollartag and doubledollartag have overlaps
+        # finds between singular $-signs correctly but also some $$.
         # areas as well
-        match = re.findall(r"(?<!\\)\$\S.*?(?<!\\)\S\$", newtext)
-        # finds between double $-signs correctly
-        match2 = re.findall(r"(?<!\\)\$(?<!\\)\$.+?(?<!\\)\$(?<!\\)\$", newtext)
-        # TODO only finds begin/end for latex
-        match3 = re.findall(r"(\\begin{.*?})(?s).*?(\\end{.*?})", newtext)
+        dollartag = re.findall(r"(?<!\\)\$\S.*?(?<!\\)\S\$", newtext)
+        # finds between double $-signs correctly.
+        doubledollartag = re.findall(
+            r"(?<!\\)\$(?<!\\)\$.+?(?<!\\)\$(?<!\\)\$", newtext
+        )
+        # TODO only finds BEGIN/END tags for LaTeX
+        # saves the area as 3 variables within a tuple.
+        # First is begin tag
+        # Second is text within the LaTeX area
+        # Third is end tag
+        croptags = re.findall(r"(\\begin{.*?})(?s)(.*?)(\\end{.*?})", newtext)
+
         # TODO make it self.translator specific
-        for word in match:
-            newtext = newtext.replace(word, "<protect>" + word + "</protect>")
-        # replaces at the start and end of found begin and end tags.
-        for tags in match3:
+        for formula in dollartag:
+            newtext = newtext.replace(formula, "<protect>" + formula + "</protect>")
+        # protects at the start and end of found begin and end tags.
+        for tags in croptags:
             for tuplepair in tags:
-                if tags.index(tuplepair) % 2 == 0:
+                # begin tag indicates protection start
+                if tags.index(tuplepair) == 0:
                     newtext = newtext.replace(tuplepair, "<protect>" + tuplepair)
-                elif tags.index(tuplepair) % 2 == 1:
+                # the translation protection removal for text areas within the protected areas
+                # currently separates text area as { text }
+                if tags.index(tuplepair) == 1:
+                    textcrop = re.findall(r"(?s)\\text({.*?})", tuplepair)
+                    # removing possible duplicates from the textareas to ensure format
+                    textcrop = tuple(set(textcrop))
+                    for textarea in textcrop:
+                        # protection is swapped, since it is within another protection
+                        newtext = newtext.replace(
+                            textarea, "</protect>" + textarea + "<protect>"
+                        )
+                # ending tag indicates protection end
+                elif tags.index(tuplepair) == 2:
                     newtext = newtext.replace(tuplepair, tuplepair + "</protect>")
 
         return newtext
