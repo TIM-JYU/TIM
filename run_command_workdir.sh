5fde1c61b4cf848f3ac63a5e0d226ebdf858de5c
dezhidki <dezhidki@jyu.fi>
2022-03-25 14:14:06 +0200

    run_command_workdir: add --dc-no-deps option to prevent creating extra containers when just building

--- run_command_workdir.sh
+++ run_command_workdir.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
 # Runs the given command in the given working directory inside TIM container with the repository's root path being mapped at /service inside container.
-# Usage: ./run_command_workdir.sh <working dir> <command> [arguments...]
+# Usage: ./run_command_workdir.sh [--dc-no-deps] <working dir> <command> [arguments...]
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# parse all args using getopts and check for --dc-no-deps
+docker_args=""
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        --dc-no-deps)
+            docker_args="--no-deps"
+            shift
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
+
 # Git Bash needs extra "/" in workdir.
-. ${DIR}/docker-compose.sh run --rm --workdir="//service/$1" tim "${@:2}"
+. ${DIR}/docker-compose.sh run --rm $docker_args --workdir="//service/$1" tim "${@:2}"
