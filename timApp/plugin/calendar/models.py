24f0f4e03ee2155c9bb349fc5f9e5e39a1a12449
Miika Immonen <immomipe@student.jyu.fi>
2022-03-29 12:59:55 +0300

    added relationships

--- timApp/plugin/calendar/models.py
+++ timApp/plugin/calendar/models.py
@@ -1,10 +1,18 @@
 from timApp.timdb.sqa import db
+from timApp.user.usergroup import UserGroup
 
 
 class Eventgroup(db.Model):
     __tablename__ = "eventgroup"
     event_id = db.Column(db.Integer, db.ForeignKey("event.event_id"), primary_key=True)
     usergroup_id = db.Column(db.Integer, db.ForeignKey("usergroup.id"), primary_key=True)
+
+
+class Enrollment(db.Model):
+    __tablename__ = "enrollment"
+    event_id = db.Column(db.Integer, db.ForeignKey("event.event_id"), primary_key=True)
+    usergroup_id = db.Column(db.Integer, db.ForeignKey("usergroup.id"), primary_key=True)
+    enroll_type_id = db.Column(db.Integer, db.ForeignKey("enrollmenttype.enroll_type_id"), nullable=False)
 
 class Event(db.Model):
     __tablename__ = "event"
@@ -19,15 +27,18 @@ class Event(db.Model):
     signup_before = db.Column(db.DateTime(timezone=True))
     creator_user_id = db.Column(db.Integer, db.ForeignKey("useraccount.id"), nullable=False)
 
+    enrolled_users: list[UserGroup] = db.relationship(UserGroup,
+                                     Enrollment.__table__,
+                                     primaryjoin=event_id==Enrollment.event_id,
+                                     lazy="select")
 
-
-class Enrollment(db.Model):
-    __tablename__ = "enrollment"
-    event_id = db.Column(db.Integer, db.ForeignKey("event.event_id"), primary_key=True)
-    usergroup_id = db.Column(db.Integer, db.ForeignKey("usergroup.id"), primary_key=True)
-    enroll_type_id = db.Column(db.Integer, db.ForeignKey("enrollmenttype.enroll_type_id"), nullable=False)
+    groups_in_events: list[UserGroup] = db.relationship(UserGroup,
+                                     Eventgroup.__table__,
+                                     primaryjoin=event_id==Eventgroup.event_id,
+                                     lazy="select")
 
 class Enrollmenttype(db.Model):
     __tablename__ = "enrollmenttype"
     enroll_type_id = db.Column(db.Integer, primary_key=True)
     enroll_type = db.Column(db.Text, nullable=False)
+    
