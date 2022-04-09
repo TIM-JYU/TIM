4a1b821b0b09e5f666f5e49d83408d571ad41795
sijualle <simo.lehtinen@hotmail.com>
2022-04-10 01:23:24 +0300

    Update peer_review test to match current implementation

--- timApp/tests/server/test_peer_review.py
+++ timApp/tests/server/test_peer_review.py
@@ -3,6 +3,7 @@
 from timApp.peerreview.peerreview import PeerReview
 from timApp.tests.server.timroutetest import TimRouteTest
 from timApp.timdb.sqa import db
+from timApp.user.usergroup import UserGroup
 
 
 class PeerReviewTest(TimRouteTest):
@@ -28,7 +29,7 @@ class PeerReviewTest(TimRouteTest):
             "Not enough users to form pairs (1 but at least 2 users needed)", r
         )
         self.assertEqual(0, len(rq.all()))
-        self.add_answer(d, "t", "x", user=self.test_user_2)
+        self.add_answer(d, "t2", "x", user=self.test_user_2)
         db.session.commit()
         r = self.get(d.get_url_for_view("review"))
         self.assertNotIn(
@@ -40,21 +41,32 @@ class PeerReviewTest(TimRouteTest):
                 PeerReview.query.filter_by(block_id=d.id)
                 .order_by(
                     PeerReview.reviewer_id,
-                    PeerReview.task_name,
                 )
                 .all()
             )
             self.assertEqual(2, len(prs))
-            self.assertEqual("t", prs[0].task_name)
+            self.assertEqual(None, prs[0].task_name)
             self.assertEqual(self.test_user_1.id, prs[0].reviewer_id)
             self.assertEqual(self.test_user_2.id, prs[0].reviewable_id)
-            self.assertEqual("t", prs[1].task_name)
+            self.assertEqual(None, prs[1].task_name)
             self.assertEqual(self.test_user_2.id, prs[1].reviewer_id)
             self.assertEqual(self.test_user_1.id, prs[1].reviewable_id)
 
+        # pairing without group works using everyone who answered the document
         check_peerreview_rows()
         rq.delete()
+        d.document.add_setting("group", "testusers1")
         db.session.commit()
-        d.document.add_setting("group", "testuser1")
         self.get(d.get_url_for_view("review"))
+        # pairing with group ignores group members who haven't answered the document
+        check_peerreview_rows()
+        rq.delete()
+        self.add_answer(d, "t", "x", user=self.test_user_3)
+        ug = UserGroup.create("testuser3isnothere")
+        ug.users.append(self.test_user_1)
+        ug.users.append(self.test_user_2)
+        d.document.add_setting("group", "testuser3isnothere")
+        db.session.commit()
+        self.get(d.get_url_for_view("review"))
+        # pairing with group setting ignores users who answered but aren't in the group
         check_peerreview_rows()
