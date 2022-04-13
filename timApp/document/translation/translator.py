2fbc176a4404118c9cd1cbc0c54835f0b3ce8b6e
Sami Viitanen <samantvi@student.jyu.fi>
2022-04-13 11:05:45 +0300

    Remove unnecessary class DeeplPlaceholderTranslationService

--- timApp/document/translation/translator.py
+++ timApp/document/translation/translator.py
@@ -413,12 +413,6 @@ class DeeplProTranslationService(DeeplTranslationService):
     __mapper_args__ = {"polymorphic_identity": "DeepL Pro"}
 
 
-# TODO Remove this when crisis is over
-class DeeplPlaceholderTranslationService(DeeplTranslationService):
-    # TODO Make the value an enum like with Verification?
-    __mapper_args__ = {"polymorphic_identity": "DeepL"}
-
-
 def init_translate(
     translator: TranslationService,
     source_lang: Language,
