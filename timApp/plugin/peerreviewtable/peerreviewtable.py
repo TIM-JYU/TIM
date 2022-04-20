088332463fe41c744b1293ba50643ad18c0019d3
alsoderg90 <alexander.sodergard@gmail.com>
2022-04-20 19:15:35 +0300

    demo versio finished

--- timApp/plugin/peerreviewtable/peerreviewtable.py
+++ timApp/plugin/peerreviewtable/peerreviewtable.py
@@ -1,8 +1,11 @@
 from dataclasses import dataclass
 
 from flask import Response
-
+from collections import defaultdict
+from timApp.user.user import User
+from timApp.auth.accesshelper import get_doc_or_abort
 from timApp.auth.sessioninfo import get_current_user_object
+from timApp.peerreview.peerreview_utils import get_all_reviews
 from timApp.util.flask.responsehelper import json_response
 from timApp.util.flask.typedblueprint import TypedBlueprint
 from tim_common.markupmodels import GenericMarkupModel
@@ -44,6 +47,38 @@ def reqs_handle() -> PluginReqs:
         "js": ["peerReviewTable"],
         "multihtml": True
     }
+
+
+@my_plugin.get("/<int:doc_id>/test")
+def test(doc_id: int) -> Response:
+    d = get_doc_or_abort(doc_id)
+    reviewers = get_all_reviews(d)
+    result = list(map(lambda r: r.to_json, reviewers))
+    for r in result:
+        r['reviewer'] = User.get_by_id(r.get("reviewer_id")).pretty_full_name()
+        r['reviewable'] = []
+        r['reviewable'].append({
+            "name": User.get_by_id(r.get("reviewable_id")).pretty_full_name(),
+            "id": r["reviewable_id"]
+        })
+        r.pop("reviewable_id", None)
+        r.pop("reviewer_id", None)
+        r.pop("answer_id", None)
+        r.pop("block_id", None)
+        r.pop("end_time", None)
+        r.pop("start_time", None)
+        r.pop("task_name", None)
+
+    listt = []
+    print(result)
+    for r in result:
+        if any([r['reviewer'] in d.keys() for d in listt]):
+            listt.append(r)
+        else:
+            el = list(filter(lambda e: e['reviewer'] == e['reviewer'], listt))
+            print(el)
+
+    return json_response(listt)
 
 #/myplugin/reviews
 @my_plugin.get("/reviews")
