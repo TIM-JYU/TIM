d2c14813de613cf2f65c15c7c3fd522ce1975a17
alsoderg90 <alexander.sodergard@gmail.com>
2022-04-28 13:15:18 +0300

    peerreviewer can be changed

--- timApp/answer/routes.py
+++ timApp/answer/routes.py
@@ -1303,9 +1303,7 @@ def preprocess_jsrunner_answer(
         answerdata["peerreviews"] = get_reviews_for_document(d)
     else:
         answerdata["peerreviews"] = get_reviews_for_document(d)
-
     answerdata["velps"] = get_annotations_with_comments_in_document(curr_user, d, False)
-
     answerdata.pop(
         "paramComps", None
     )  # This isn't needed by jsrunner server, so don't send it.
