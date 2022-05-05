8f0f43d08a0cf4538abed8e4fe347dd777171d98
dezhidki <dezhidki@jyu.fi>
2022-05-05 11:33:56 +0300

    defaultconfig: move git tools to a separate util file

--- timApp/defaultconfig.py
+++ timApp/defaultconfig.py
@@ -1,13 +1,13 @@
 import logging
 import multiprocessing
 import os
-import subprocess
 from datetime import timedelta
 from pathlib import Path
 
 from celery.schedules import crontab
 
 from timApp.user.special_group_names import TEACHERS_GROUPNAME
+from timApp.util.git_utils import get_latest_commit_timestamp, get_current_branch
 
 # NOTE: If you are a different organization (other than JYU), please don't modify this file directly.
 # This avoids merge conflicts. Override the values with prodconfig.py instead.
@@ -87,32 +87,8 @@ WUFF_EMAIL = "wuff@tim.jyu.fi"
 NOREPLY_EMAIL = "no-reply@tim.jyu.fi"
 GLOBAL_NOTIFICATION_FILE = "/tmp/global_notification.html"
 
-# Ensure the main dir is marked as safe
-# See https://github.blog/2022-04-12-git-security-vulnerability-announced/
-subprocess.run(
-    [
-        "git",
-        "config",
-        "--global",
-        "--add",
-        "safe.directory",
-        os.path.dirname(os.getcwd()),
-    ]
-)
-
-GIT_LATEST_COMMIT_TIMESTAMP = (
-    subprocess.run(
-        ["git", "log", "-1", "--date=format:%d.%m.%Y %H:%M:%S", "--format=%cd"],
-        stdout=subprocess.PIPE,
-    )
-    .stdout.decode()
-    .strip()
-)
-GIT_BRANCH = (
-    subprocess.run(["git", "rev-parse", "--abbrev-ref", "HEAD"], stdout=subprocess.PIPE)
-    .stdout.decode()
-    .strip()
-)
+GIT_LATEST_COMMIT_TIMESTAMP = get_latest_commit_timestamp()
+GIT_BRANCH = get_current_branch()
 
 CELERY_BROKER_URL = "redis://redis:6379"
 CELERY_RESULT_BACKEND = "redis://redis:6379"
