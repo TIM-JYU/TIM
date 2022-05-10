b3f7b6331361430aaa834a61f914ae3877c2ccd7
dezhidki <dezhidki@jyu.fi>
2022-05-10 21:06:37 +0300

    Merge DB with upsteam

---
+++ timApp/migrations/versions/d096668f9a63_merge_with_upstream.py
@@ -0,0 +1,22 @@
+"""Merge with upstream
+
+Revision ID: d096668f9a63
+Revises: ('32cfa170c7b6', 'c22914d260b3')
+Create Date: 2022-05-10 18:05:49.595147
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'd096668f9a63'
+down_revision = ('32cfa170c7b6', 'c22914d260b3')
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
