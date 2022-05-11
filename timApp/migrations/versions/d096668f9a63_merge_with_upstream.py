4f382c710ee95a1736fea1cf1488d8a6c0f5ec29
dezhidki <dezhidki@jyu.fi>
2022-05-11 10:29:59 +0300

    Fix formatting

--- timApp/migrations/versions/d096668f9a63_merge_with_upstream.py
+++ timApp/migrations/versions/d096668f9a63_merge_with_upstream.py
@@ -7,8 +7,8 @@ Create Date: 2022-05-10 18:05:49.595147
 """
 
 # revision identifiers, used by Alembic.
-revision = 'd096668f9a63'
-down_revision = ('32cfa170c7b6', 'c22914d260b3')
+revision = "d096668f9a63"
+down_revision = ("32cfa170c7b6", "c22914d260b3")
 
 from alembic import op
 import sqlalchemy as sa
