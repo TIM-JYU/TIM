1fc3ea04448b2793330899d6264cd28a352a308c
Elsi <eljosilv@student.jyu.fi>
2022-05-13 18:15:15 +0300

    Change peer review times to use utc

--- timApp/peerreview/peerreview_utils.py
+++ timApp/peerreview/peerreview_utils.py
@@ -15,6 +15,7 @@ from timApp.plugin.taskid import TaskId
 from timApp.timdb.sqa import db
 from timApp.user.user import User
 from timApp.user.usergroup import UserGroup
+import dateutil.parser
 import pytz
 
 
@@ -200,14 +201,16 @@ def check_review_grouping(doc: DocInfo) -> bool:
 def is_peerreview_enabled(doc: DocInfo) -> bool:
     settings = doc.document.get_settings()
 
-    # TODO: create better solution
-    tz = pytz.timezone("Europe/Helsinki")
-
     if not settings.peer_review_start() or not settings.peer_review_stop():
         return doc.document.get_settings().peer_review()
 
-    start = tz.localize((settings.peer_review_start()), is_dst=None)
-    stop = tz.localize((settings.peer_review_stop()), is_dst=None)
+    start = dateutil.parser.parse(settings.peer_review_start().isoformat()).astimezone(
+        pytz.UTC
+    )
+    stop = dateutil.parser.parse(settings.peer_review_stop().isoformat()).astimezone(
+        pytz.UTC
+    )
+
     current_time = datetime.now(pytz.timezone("UTC"))
 
     if not start or not stop:
