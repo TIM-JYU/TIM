671f7cbdc3d59ecbedbcba2095490b1c69dc62d2
Miika Immonen <immomipe@student.jyu.fi>
2022-05-16 16:38:23 +0300

    handles multiple migration heads, new migration file

---
+++ timApp/migrations/versions/0e143b0206f7_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 0e143b0206f7
+Revises: ('0212bfd7beee', 'a99becb28386')
+Create Date: 2022-05-16 13:35:10.954468
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "0e143b0206f7"
+down_revision = ("0212bfd7beee", "a99becb28386")
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
