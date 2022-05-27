47a15f5117c622e0afe0adc81c29fe8314fe4725
alsoderg90 <alexander.sodergard@gmail.com>
2022-05-27 17:21:18 +0300

    bugfixes and template editing

--- timApp/modules/jsrunner/server/routes/tools.ts
+++ timApp/modules/jsrunner/server/routes/tools.ts
@@ -132,6 +132,12 @@ const ABSOLUTE_FIELD_REGEX = /^[0-9]+\./;
 
 type Users = Record<string | number, string>;
 
+interface PeerReviewerUser {
+    id: number;
+    name: string;
+    points: number[];
+}
+
 interface Point {
     x: number;
     y: number;
@@ -1444,14 +1450,14 @@ export class Tools extends ToolsBase {
     /**
      * Print reviewers and received points of current user
      */
-    getReviews(task: string, usersObject: Users): object[] {
+    getReviews(task: string, usersObject: Users): PeerReviewerUser[] {
         const users = usersObject ? usersObject : this.users;
         const peerreviewers = this.getPeerReviewsForUser()
             .filter((pr) => pr.task_name === task)
             .map((pr) => {
                 return {
-                    id: pr.reviewer_id,
-                    name: users[pr.reviewer_id],
+                    id: pr?.reviewer_id,
+                    name: users[pr?.reviewer_id],
                     points: [0],
                 };
             });
@@ -1465,7 +1471,7 @@ export class Tools extends ToolsBase {
             }));
 
         //
-        velps.filter((v) =>
+        velps?.filter((v) =>
             peerreviewers.forEach((pr) => {
                 return pr.id == v.id;
             })
