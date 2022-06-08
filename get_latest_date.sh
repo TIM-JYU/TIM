5e42efd624d8b2d7d68a2def05febf598d05d01c
dezhidki <dezhidki@jyu.fi>
2022-06-08 16:53:44 +0300

    Remove old GitLab CI files

--- get_latest_date.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-git --git-dir=${DIR}/.git --work-tree=${DIR} log -1 --date=short --format="%cd-%H" $DIR/timApp/Dockerfile
