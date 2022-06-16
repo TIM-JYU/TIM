b1726504a6c3f624283f45f28eff67273dde0f48
dezhidki <dezhidki@jyu.fi>
2022-06-16 11:36:06 +0300

    RoleInfoComponent: Allow hiding the access lock alert

--- timApp/static/scripts/tim/header/role-info.component.ts
+++ timApp/static/scripts/tim/header/role-info.component.ts
@@ -6,11 +6,16 @@ import {
     accessTypeDisplayNames,
 } from "tim/item/access-role.service";
 import {showMessageDialog} from "tim/ui/showMessageDialog";
+import {TimStorage} from "tim/util/utils";
+import * as t from "io-ts";
 
 @Component({
     selector: "tim-role-info",
     template: `
-    <tim-alert severity="info" *ngIf="lockedAccess">
+    <tim-alert severity="info"
+               *ngIf="lockedAccess && !hideAccessLockAlert"
+               (closing)="hideAccessLockAlertMessage()"
+               [closeable]="true">
         <p i18n>
             You are previewing the page with access limited to level "<strong>{{lockedAccessName}}</strong>".
         </p>
@@ -24,15 +29,28 @@ import {showMessageDialog} from "tim/ui/showMessageDialog";
 export class RoleInfoComponent implements OnInit {
     lockedAccess?: AccessType;
     lockedAccessName?: string;
+    private hideAlert = new TimStorage("hideAccessLockAlert", t.boolean);
 
     constructor(private access: AccessRoleService) {}
 
+    get hideAccessLockAlert() {
+        return this.hideAlert.get() ?? false;
+    }
+
+    hideAccessLockAlertMessage() {
+        this.hideAlert.set(true);
+    }
+
     ngOnInit(): void {
         if (!Users.isLoggedIn()) {
+            this.hideAlert.set(false);
             return;
         }
         const currentUser = Users.getCurrent();
         this.lockedAccess = currentUser.locked_access;
+        if (!this.lockedAccess) {
+            this.hideAlert.set(false);
+        }
         this.lockedAccessName = this.lockedAccess
             ? accessTypeDisplayNames[this.lockedAccess]
             : undefined;
