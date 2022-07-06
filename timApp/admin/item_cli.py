68d825023d0d46fecc5f1eadeeffa34d0c5fbbf0
dezhidki <dezhidki@jyu.fi>
2022-07-06 09:30:35 +0300

    item_cli: Improve bookmark cleanup CLI
    * Add progress bar
    * Add error handling for missing documents
    * Add max processing cap

--- timApp/admin/item_cli.py
+++ timApp/admin/item_cli.py
@@ -12,6 +12,7 @@ from timApp.admin.util import commit_if_not_dry
 from timApp.document.docentry import DocEntry
 from timApp.document.translation.translation import Translation
 from timApp.item.block import Block, BlockType
+from timApp.notification.notification import Notification
 from timApp.notification.pending_notification import PendingNotification
 from timApp.readmark.readparagraph import ReadParagraph
 from timApp.timdb.dbaccess import get_files_path
@@ -44,43 +45,84 @@ def cleanup_default_right_doc_names() -> None:
 
 @item_cli.command()
 @click.option("--dry-run/--no-dry-run", default=True)
-def cleanup_bookmark_docs(dry_run: bool) -> None:
+@click.option("--prompt-before-commit/--no-prompt-before-commit", default=False)
+@click.option("--max-docs", default=None, type=int)
+def cleanup_bookmark_docs(
+    dry_run: bool, prompt_before_commit: bool, max_docs: int | None
+) -> None:
     new_bookmark_users: list[User] = User.query.filter(
         User.prefs.contains('"bookmarks":')
     ).all()
     docs_to_delete = set()
-    for u in new_bookmark_users:
-        prefs = u.get_prefs()
-        if not prefs.bookmarks:
-            continue
-        f = u.get_personal_folder()
-        bm_doc = f.get_document("Bookmarks")
-        if not bm_doc:
-            continue
-        print(f"Deleting unused bookmarks document of {u.name}")
-        docs_to_delete.add(bm_doc.id)
-        block = bm_doc.block
-        block.accesses = {}
-        Translation.query.filter_by(doc_id=bm_doc.id).delete()
-        ReadParagraph.query.filter_by(doc_id=bm_doc.id).delete()
-        PendingNotification.query.filter_by(doc_id=bm_doc.id).delete()
-        VelpGroupsInDocument.query.filter_by(doc_id=bm_doc.id).delete()
-        db.session.delete(bm_doc)
-        db.session.delete(block)
+    processed_users = 0
+
+    click.echo("Collecting bookmark documents")
+    with click.progressbar(new_bookmark_users) as bar:
+        for u in bar:  # type: User
+            prefs = u.get_prefs()
+            if not prefs.bookmarks:
+                continue
+            f = u.get_personal_folder()
+            bm_doc = f.get_document("Bookmarks")
+            if not bm_doc:
+                continue
+            docs_to_delete.add((u.name, bm_doc))
+            processed_users += 1
+            if max_docs and processed_users >= max_docs:
+                click.echo(
+                    f"Stopped at {processed_users} users because of max-docs cap"
+                )
+                break
+
+    click.echo(f"Found {len(docs_to_delete)} bookmark documents to delete.")
+    with click.progressbar(docs_to_delete) as bar:
+        for u, bm_doc in bar:  # type: str, DocEntry
+            click.echo(f"Deleting unused bookmarks document of {u}")
+            block = bm_doc.block
+            block.accesses = {}
+            Translation.query.filter_by(doc_id=bm_doc.id).delete()
+            ReadParagraph.query.filter_by(doc_id=bm_doc.id).delete()
+            PendingNotification.query.filter_by(doc_id=bm_doc.id).delete()
+            VelpGroupsInDocument.query.filter_by(doc_id=bm_doc.id).delete()
+            Notification.query.filter_by(block_id=bm_doc.id).delete()
+            db.session.delete(bm_doc)
+            db.session.delete(block)
     if dry_run:
-        print("Dry run enabled; nothing changed.")
+        click.echo("Dry run enabled; nothing changed.")
         return
+
+    if prompt_before_commit:
+        if not click.confirm(
+            f"{len(docs_to_delete)} users will be affected. Commit changes?"
+        ):
+            return
     db.session.commit()
+
     fp = get_files_path()
-    deleted_docs = fp / "deleted" / "docs"
-    deleted_pars = fp / "deleted" / "pars"
+    deleted_folder = fp / "deleted"
+    deleted_docs = deleted_folder / "docs"
+    deleted_pars = deleted_folder / "pars"
     deleted_pars.mkdir(parents=True, exist_ok=True)
     deleted_docs.mkdir(parents=True, exist_ok=True)
-    for d in docs_to_delete:
-        doc_dir = fp / "docs" / str(d)
-        pars_dir = fp / "pars" / str(d)
-        shutil.move(doc_dir.as_posix(), deleted_docs)
-        shutil.move(pars_dir.as_posix(), deleted_pars)
+
+    click.echo(f"Done, moving document files to {deleted_folder.as_posix()}...")
+    failed_to_remove = []
+    with click.progressbar(docs_to_delete) as bar:
+        for u, bm_doc in bar:  # type: str, DocEntry
+            d = bm_doc.id
+            click.echo(f"Deleting bookmark document {d} (owned by {u})")
+            doc_dir = fp / "docs" / str(d)
+            pars_dir = fp / "pars" / str(d)
+            try:
+                shutil.move(doc_dir.as_posix(), deleted_docs)
+                shutil.move(pars_dir.as_posix(), deleted_pars)
+            except Exception as e:
+                failed_to_remove.append(d)
+                click.echo(f"Failed to remove {d}: {e}")
+    click.echo(f"Removed {len(docs_to_delete) - len(failed_to_remove)} documents.")
+    if failed_to_remove:
+        click.echo("Failed to remove the following documents:")
+        click.echo(failed_to_remove)
 
 
 @item_cli.command()
