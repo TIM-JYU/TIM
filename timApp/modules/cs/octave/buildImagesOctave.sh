9c5f3e6a7be1a8b6d6ca2e95e5bf6a0d858156ef
dezhidki <dezhidki@jyu.fi>
2022-07-14 12:30:44 +0300

    Remove unused Dockerfiles
    * imagex uses csplugin image
    * octave is part of csplugin and not a separate service
    * timagent is based on tim's Dockerfile

--- timApp/modules/cs/octave/buildImagesOctave.sh
+++
@@ -1,1 +0,0 @@
-docker build --tag="timimages/octave" -f DockerfileOctave .
--- timApp/modules/cs/octave/urso.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-dockername="cso"
-dockerImage="timimages/octave"
- 
-dockerOptions="--name $dockername --net=timnet -p 56001:5000 -v /var/run/docker.sock:/var/run/docker.sock   -v /opt/cs:/cs/:ro -v /opt/cs/images/cs:/csimages/ -v /tmp/uhome:/tmp2/ -w /cs $dockerImage " 
-
-docker run  --rm=true  -t -i $dockerOptions 
