fc3dbb358321c534bffe0bcd8c31af576e2066f3
dezhidki <dezhidki@jyu.fi>
2022-07-25 17:28:49 +0300

    CLI: Fix docker-compose.yml being emitted multiple times

--- cli/docker/compose.py
+++ cli/docker/compose.py
@@ -4,8 +4,13 @@ from typing import Optional
 from cli.config import get_config
 from cli.util.template import PyTemplate
 
+_compose_refreshed = False
+
 
 def init_compose(profile: Optional[str] = None) -> None:
+    global _compose_refreshed
+    if _compose_refreshed:
+        return
     config = get_config()
 
     docker_compose_target = Path.cwd() / "docker-compose.yml"
@@ -17,3 +22,4 @@ def init_compose(profile: Optional[str] = None) -> None:
 
     dotenv_str = PyTemplate("docker/.env.tmpl").render(ctx)
     dotenv_target.write_text(dotenv_str, encoding="utf-8")
+    _compose_refreshed = True
