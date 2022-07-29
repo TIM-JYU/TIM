0afc6a8cfdec5c35882bbf8cee5b0eee4a731d2d
dezhidki <dezhidki@jyu.fi>
2022-07-29 19:03:26 +0300

    CLI: Add --shell option to `pg restore`
    The option allows to use native zcat if available

--- cli/commands/pg/restore.py
+++ cli/commands/pg/restore.py
@@ -1,12 +1,15 @@
 import gzip
+import subprocess
 from argparse import ArgumentParser
 from pathlib import Path
 from subprocess import Popen, PIPE
+from typing import List
 
 from cli.docker.run import get_compose_cmd
 from cli.util.errors import CLIError
 from cli.util.iter import iter_chunked
 from cli.util.logging import log_info, log_warning, log_debug
+from cli.util.proc import sh_join
 
 info = {
     "help": "Restore the database from a gzipped SQL dump",
@@ -19,7 +22,31 @@ The operation will overwrite the existing database.
 
 
 class Arguments:
+    shell: bool
     input: str
+
+
+def restore_python(input_file: Path, compose_cmd: List[str]) -> None:
+    with Popen(compose_cmd, stdin=PIPE) as proc:
+        assert proc.stdin is not None
+        log_debug(f"Opening gzip {input_file}")
+        with gzip.open(input_file, "rb") as f:
+            log_debug("Writing gzip to stdin")
+            for chunk in iter_chunked(f):
+                proc.stdin.write(chunk)
+                proc.stdin.flush()
+        log_debug("Closing stdin and waiting")
+        proc.stdin.close()
+        proc.wait()
+        if proc.returncode != 0:
+            raise CLIError(f"Failed to restore the database", proc.returncode)
+
+
+def restore_shell(input_file: Path, compose_cmd: List[str]) -> None:
+    command = f"zcat {input_file.as_posix()} | {sh_join(compose_cmd)}"
+    res = subprocess.run(command, shell=True)
+    if res.returncode != 0:
+        raise CLIError(f"Failed to restore the database", res.returncode)
 
 
 def run(args: Arguments) -> None:
@@ -51,21 +78,18 @@ def run(args: Arguments) -> None:
             "postgres",
         ]
     )
-    with Popen(compose_cmd, stdin=PIPE) as proc:
-        assert proc.stdin is not None
-        log_debug(f"Opening gzip {input_file}")
-        with gzip.open(input_file, "rb") as f:
-            log_debug("Writing gzip to stdin")
-            for chunk in iter_chunked(f):
-                proc.stdin.write(chunk)
-                proc.stdin.flush()
-        log_debug("Closing stdin and waiting")
-        proc.stdin.close()
-        proc.wait()
-        if proc.returncode != 0:
-            raise CLIError(f"Failed to restore the database", proc.returncode)
+    if args.shell:
+        restore_shell(input_file, compose_cmd)
+    else:
+        restore_python(input_file, compose_cmd)
     log_info(f"Restore complete")
 
 
 def init(parser: ArgumentParser) -> None:
+    parser.add_argument(
+        "--shell",
+        help="Whether to use shell and native zcat command to write the command."
+        "This assumes that `zcat` command is installed and available.",
+        action="store_true",
+    )
     parser.add_argument("input", help="Input SQL file")
