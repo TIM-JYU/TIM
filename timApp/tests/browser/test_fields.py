80b8d4bf2361ba5525a47b587fb90b7c76d2a8a2
sijualle <simo.lehtinen@hotmail.com>
2022-10-24 18:54:55 +0300

    Add missing initial drag tooltip check

--- timApp/tests/browser/test_fields.py
+++ timApp/tests/browser/test_fields.py
@@ -146,7 +146,7 @@ copy: target
         self.wait_until_hidden("#dropdown button")
         selector = Select(field)
         selector.select_by_index(1)
-        self.wait_until_present_and_vis(".plugindropdown bs-tooltip-container")
+        self.wait_until_present_and_vis("#dropdown bs-tooltip-container")
         self.wait_until_present_and_vis("#dragsource li")
         self.wait_until_present_and_vis("#dragtarget .dropword")
         drag_piece = self.find_element("#dragsource li")
@@ -154,6 +154,7 @@ copy: target
         # Regular selenium drag-drop doesn't trigger all the events here, https://stackoverflow.com/a/40608989
         JS_HTML5_DND = 'function e(e,t,n,i){var r=a.createEvent("DragEvent");r.initMouseEvent(t,!0,!0,o,0,0,0,c,g,!1,!1,!1,!1,0,null),Object.defineProperty(r,"dataTransfer",{get:function(){return d}}),e.dispatchEvent(r),o.setTimeout(i,n)}var t=arguments[0],n=arguments[1],i=arguments[2]||0,r=arguments[3]||0;if(!t.draggable)throw new Error("Source element is not draggable.");var a=t.ownerDocument,o=a.defaultView,l=t.getBoundingClientRect(),u=n?n.getBoundingClientRect():l,c=l.left+(l.width>>1),g=l.top+(l.height>>1),s=u.left+(u.width>>1)+i,f=u.top+(u.height>>1)+r,d=Object.create(Object.prototype,{_items:{value:{}},effectAllowed:{value:"all",writable:!0},dropEffect:{value:"move",writable:!0},files:{get:function(){return this._items.Files}},types:{get:function(){return Object.keys(this._items)}},setData:{value:function(e,t){this._items[e]=t}},getData:{value:function(e){return this._items[e]}},clearData:{value:function(e){delete this._items[e]}},setDragImage:{value:function(e){}}});if(n=a.elementFromPoint(s,f),!n)throw new Error("The target element is not interactable and need to be scrolled into the view.");u=n.getBoundingClientRect(),e(t,"dragstart",101,function(){var i=n.getBoundingClientRect();c=i.left+s-u.left,g=i.top+f-u.top,e(n,"dragenter",1,function(){e(n,"dragover",101,function(){n=a.elementFromPoint(c,g),e(n,"drop",1,function(){e(t,"dragend",1,callback)})})})})'
         self.drv.execute_script(JS_HTML5_DND, drag_piece, drag_target)
+        self.wait_until_present_and_vis("#dragtarget bs-tooltip-container")
 
         check_hover("textfield")
         check_hover("numericfield")
