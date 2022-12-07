9430a3983d07b4a011537d915f8aacc7659780dc
sijualle <simo.lehtinen@hotmail.com>
2022-12-07 17:14:48 +0200

    Test that selecting user without valid answers load default plugin  state

--- timApp/tests/browser/test_teacher.py
+++ timApp/tests/browser/test_teacher.py
@@ -76,3 +76,47 @@ form: false
             "#t .textfieldNoSaveDiv input"
         ).get_attribute("value")
         self.assertEqual(d2_ans_table[selected_username], selected_answer)
+
+    def test_userchange_only_invalid_answers(self):
+        # Check that changing to user with invalid answers only does not show previous user's answer
+        self.login_browser_quick_test1()
+        self.login_test1()
+        ug = UserGroup.create("testusers1")
+        ug.users.append(self.test_user_1)
+        ug.users.append(self.test_user_2)
+        ug.admin_doc = self.create_doc().block
+        d1 = self.create_doc(
+            initial_par="""
+#- {plugin=textfield #t}
+form: false
+initword: empty
+                """,
+            settings={"group": "testusers1"},
+        )
+        self.add_answer(d1, "t", "tu1 valid")
+        self.add_answer(d1, "t", "tu2 invalid", valid=False, user=self.test_user_2)
+        db.session.commit()
+
+        def do_test(focus_first: bool):
+            self.goto_document(d1, "teacher")
+            input_selector = "#t .textfieldNoSaveDiv input"
+            self.wait_until_present_and_vis(input_selector)
+            ele = self.find_element(input_selector)
+            self.assertEqual(ele.get_attribute("value"), "tu1 valid")
+            if focus_first:
+                self.find_element_and_move_to(input_selector)
+                self.wait_until_present_and_vis("answerbrowser .point-form")
+                velp_hider = self.find_element("velp-selection i.glyphicon-minus")
+                velp_hider.click()
+            tu_2_selector = self.find_element('div[title = "Test user 2"]')
+            tu_2_selector.click()
+            self.find_element_and_move_to(input_selector)
+            self.wait_until_present_and_vis("answerbrowser .onlyValid input")
+            valid_only = self.find_element("answerbrowser .onlyValid input")
+            self.assertTrue(valid_only.is_selected())
+            self.wait_until_hidden("answerbrowser .updating")
+            ele = self.find_element_and_move_to(input_selector)
+            self.assertEqual(ele.get_attribute("value"), "empty")
+
+        do_test(True)
+        do_test(False)
