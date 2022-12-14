1e4ce09ad3169bf8090e9f9cc3780847fcfb64e7
Sami Viitanen <sami.a.viitanen@outlook.com>
2022-12-14 15:28:02 +0200

    Update tests

--- timApp/tests/server/test_settings.py
+++ timApp/tests/server/test_settings.py
@@ -104,7 +104,8 @@ class SettingsTest(TimRouteTest):
                     '"style_doc_ids": [], "last_answer_fetch": {}, '
                     '"auto_mark_all_read": false, "bookmarks": [{"Last edited": '
                     '[{"document 2": "/view/users/test-user-1/doc1"}]}], '
-                    '"max_uncollapsed_toc_items": null}',
+                    '"max_uncollapsed_toc_items": null, '
+                    '"use_minimal_velp_menu_ui": false}',
                     "real_name": "Test user 1",
                 },
                 "velps": [],
@@ -214,6 +215,7 @@ type: python
                 "remember_last_sidebar_menu_tab": False,
                 "style_doc_ids": [],
                 "use_document_word_list": False,
+                "use_minimal_velp_menu_ui": False,
                 "word_list": "",
             },
         )
@@ -247,6 +249,7 @@ type: python
                 "disable_menu_hover": True,
                 "remember_last_sidebar_menu_state": True,
                 "remember_last_sidebar_menu_tab": True,
+                "use_minimal_velp_menu_ui": False,
                 "auto_mark_all_read": True,
                 "bookmarks": None,
             },
@@ -281,6 +284,7 @@ type: python
                 "remember_last_sidebar_menu_tab": True,
                 "auto_mark_all_read": False,
                 "max_uncollapsed_toc_items": None,
+                "use_minimal_velp_menu_ui": False,
                 "bookmarks": None,
             },
         )
