acc6cc3c3dcf50267f53c69eb1a64b4ec0bf3058
dezhidki <dezhidki@jyu.fi>
2023-01-11 09:11:00 +0200

    BootstrapPanelComponent: Don't add id attribute when anchorID is missing

--- timApp/static/scripts/tim/ui/bootstrap-panel.component.ts
+++ timApp/static/scripts/tim/ui/bootstrap-panel.component.ts
@@ -11,7 +11,7 @@ import {AlertSeverity} from "tim/ui/formErrorMessage";
     selector: "bootstrap-panel",
     template: `
         <div class="panel panel-{{ severity ? severity : 'default' }}" [hidden]="show === false">
-            <div class="panel-heading" id="{{ anchorID }}">
+            <div class="panel-heading" [attr.id]="anchorID">
                 <ng-container *ngIf="title">{{ title }}</ng-container>
                 <ng-container *ngIf="titleTemplate">
                     <ng-container *ngTemplateOutlet="titleTemplate"></ng-container>
@@ -19,7 +19,7 @@ import {AlertSeverity} from "tim/ui/formErrorMessage";
                 <a *ngIf="showClose">
                     <tim-close-button (click)="close()"></tim-close-button>
                 </a>
-                <span class="headerlink" *ngIf="showHeadingAnchors">
+                <span class="headerlink" *ngIf="showHeadingAnchors && anchorID">
                     <a href="#{{ anchorID }}" title="Permanent link to paragraph" class="">
                         <span class="header-anchor">#</span>
                     </a>
