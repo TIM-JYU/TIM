72107985db1b540513955005e723ae353d27a28c
Sami Viitanen <sami.a.viitanen@outlook.com>
2023-02-20 12:02:48 +0200

    Fix Pipe for searching/filtering Velp labels

--- timApp/static/scripts/tim/velp/label-content-filter.pipe.ts
+++ timApp/static/scripts/tim/velp/label-content-filter.pipe.ts
@@ -11,41 +11,22 @@ import type {ILabelUI} from "tim/velp/velptypes";
 @Injectable()
 export class LabelContentFilter implements PipeTransform {
     transform(
-        labels?: ILabelUI[],
+        labels: ILabelUI[],
+        searchString?: string,
         advancedOn?: boolean
-    ): ILabelUI[] | undefined {
-        // TODO for now a copy of velp filtering by label
-        const selectedLabels = [];
+    ): ILabelUI[] {
+        const returnLabels = [];
 
-        if (!advancedOn) {
+        if (!advancedOn || !searchString) {
             return labels;
         }
 
-        if (labels != null) {
-            for (const l of labels) {
-                if (l.selected) {
-                    // selectedLabels.push(l.id);
-                    selectedLabels.push(l);
-                }
+        for (const l of labels) {
+            if (l.content.includes(searchString)) {
+                returnLabels.push(l);
             }
         }
 
-        // const selectedArray = [];
-        // const returnVelps = [];
-
-        // for (const sv in selectedVelps) {
-        //     if (selectedVelps.hasOwnProperty(sv)) {
-        //         selectedArray.push(selectedVelps[sv]);
-        //     }
-        // }
-
-        // selectedArray.sort((a, b) => b[1] - a[1]);
-
-        // for (const s of selectedArray) {
-        //     returnVelps.push(s[0]);
-        // }
-
-        // return returnVelps;
-        return selectedLabels;
+        return returnLabels;
     }
 }
