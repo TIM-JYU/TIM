dd5de6314e6d84cf46869875bb7b21a87dec9902
Jaakko Palm <jaakkop1998@gmail.com>
2023-03-30 17:18:42 +0300

    Remove one unused import from symbol-button-menu.component.ts

--- timApp/modules/cs/js/editor/math-editor/symbol-button-menu.component.ts
+++ timApp/modules/cs/js/editor/math-editor/symbol-button-menu.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from "@angular/core";
+import {Component, EventEmitter, Output} from "@angular/core";
 import formulas from "./latex-commands";
 
 @Component({
