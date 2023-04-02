8a2e3e0770b497d681d1cb43008c46eb63005803
Juha Reinikainen <juha.a.reinikainen@student.jyu.fi>
2023-04-02 15:42:03 +0300

    Fix commands starting with linebreak not working

--- timApp/modules/cs/js/editor/math-editor/formula-editor.component.ts
+++ timApp/modules/cs/js/editor/math-editor/formula-editor.component.ts
@@ -704,7 +704,7 @@ export class FormulaEditorComponent {
                 activeField.mathField.typedText(formulaInput.command);
                 formula = formulaInput.command;
             }
-            if (~formula.indexOf("\\")) {
+            if (formula.includes("\\")) {
                 activeField.mathField.keystroke("Spacebar");
             }
             setTimeout(() => {
