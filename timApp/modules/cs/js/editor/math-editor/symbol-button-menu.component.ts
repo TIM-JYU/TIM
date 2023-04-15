4ee4fd0a1e4fd82da18b854a61c30d5d5ab12b5c
Juha Reinikainen <juha.a.reinikainen@student.jyu.fi>
2023-04-15 20:47:35 +0300

    Fix link

--- timApp/modules/cs/js/editor/math-editor/symbol-button-menu.component.ts
+++ timApp/modules/cs/js/editor/math-editor/symbol-button-menu.component.ts
@@ -78,7 +78,7 @@ enum ButtonMenuState {
                     </button>                       
                    </ng-template>
                     
-                    <a href="/view/kurssit/tie/proj/2023/timath/dokumentit/ohjeet/kayttoohjeet"
+                    <a href="https://tim.jyu.fi/view/kurssit/tie/proj/2023/timath/dokumentit/ohjeet/kayttoohjeet"
                        target="_blank">
                         <span class="glyphicon glyphicon-question-sign help-icon" title="Instructions"
                               i18n-title></span>
