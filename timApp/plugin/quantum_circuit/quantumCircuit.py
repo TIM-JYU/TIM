caaacb6c0261071a823e2f131a2880a3aedd18cc
juanrein <juha.reinikainen1@gmail.com>
2023-08-03 13:04:46 +0300

    Add qulacs checking

--- timApp/plugin/quantum_circuit/quantumCircuit.py
+++ timApp/plugin/quantum_circuit/quantumCircuit.py
@@ -3,6 +3,9 @@ from typing import Union
 
 from flask import render_template_string
 
+from qulacs import Observable, QuantumCircuit, QuantumState
+from qulacs.gate import Y, CNOT, merge
+
 from timApp.tim_app import csrf
 from tim_common.markupmodels import GenericMarkupModel
 from tim_common.pluginserver_flask import (
@@ -147,6 +150,22 @@ class QuantumCircuitAnswerModel(
 
 def answer(args: QuantumCircuitAnswerModel) -> PluginAnswerResp:
     # initial_circuit = args.markup.initialCircuit
+
+    state = QuantumState(3)
+    state.set_Haar_random_state()
+
+    circuit = QuantumCircuit(3)
+    circuit.add_X_gate(0)
+    merged_gate = merge(CNOT(0, 1), Y(1))
+    circuit.add_gate(merged_gate)
+    circuit.add_RX_gate(1, 0.5)
+    circuit.update_quantum_state(state)
+
+    observable = Observable(3)
+    observable.add_operator(2.0, "X 2 Y 1 Z 0")
+    observable.add_operator(-3.0, "Z 2")
+    value = observable.get_expectation_value(state)
+    print(value)
 
     model_circuit = args.markup.modelCircuit
     model_input = args.markup.modelInput
