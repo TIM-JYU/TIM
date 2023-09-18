2ad1affe31b19cf9f8a4038f37882a101ea265cd
Sami Viitanen <sami.a.viitanen@outlook.com>
2023-09-18 08:55:31 +0300

    wip [skip ci]

---
+++ timApp/auth/login_code/__init__.py
@@ -0,0 +1,8 @@
+"""Module for managing simple login codes.
+
+Login codes can be used to offer users a simplified login interface where the user may log in to TIM
+by inputting a temporary login code instead of the usual user credentials.
+
+.. note:: Login codes are intended as only a temporary login method! They should always have a limited
+lifetime, and be purged once no longer needed.
+"""
