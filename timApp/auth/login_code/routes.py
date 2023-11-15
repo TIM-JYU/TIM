57cee56e129116ee41c52ed1c974a9c347ec584c
Sami Viitanen <sami.a.viitanen@outlook.com>
2023-11-15 10:25:35 +0200

    Add permission checks, use urlsafe_b64encode/decode

--- timApp/auth/login_code/routes.py
+++ timApp/auth/login_code/routes.py
@@ -1,7 +1,7 @@
 import base64
 import json
 import time
-from base64 import b64decode, standard_b64decode
+from base64 import urlsafe_b64decode, urlsafe_b64encode
 from flask import Response, request
 from sqlalchemy import select
 
@@ -9,11 +9,13 @@ from timApp.auth.accesshelper import (
     get_doc_or_abort,
     verify_ownership,
     get_origin_from_request,
+    verify_admin,
 )
 from timApp.document.docentry import DocEntry
 from timApp.document.docinfo import DocInfo
 from timApp.folder.folder import Folder
 from timApp.timdb.sqa import db, run_sql
+from timApp.user.groups import verify_groupadmin
 from timApp.user.user import User, UserInfo
 from timApp.user.usergroup import UserGroup, get_groups_by_names
 from timApp.user.usergroupdoc import UserGroupDoc
@@ -31,6 +33,9 @@ def get_managers(doc_id: int) -> Response:
     """
     :return: UserGroups that are listed as managers in the docsetting `groupManagement`
     """
+
+    # TODO check permissions
+
     mgmnt_doc = get_doc_or_abort(doc_id).document
     groupnames: list[str] = mgmnt_doc.get_settings().group_management_settings()[
         "managers"
@@ -62,7 +67,16 @@ def get_groups(doc_id: int) -> Response:
     """
 
     show_all_groups: str | None = request.args.get("showAllGroups")
-    mgmnt_doc = get_doc_or_abort(doc_id).document
+    mgmnt_docinfo = get_doc_or_abort(doc_id)
+
+    # Only admins, group admins, and specified management doc owners should be able to view the groups
+    if not (verify_ownership(mgmnt_docinfo) or verify_admin() or verify_groupadmin()):
+        return json_response(
+            status_code=403,
+            jsondata={"result": {"error": f"Insufficient permissions."}},
+        )
+
+    mgmnt_doc = mgmnt_docinfo.document
     # TODO currently only one group path should be taken into account
     path: list[str] = mgmnt_doc.get_settings().group_management_settings()["groupsPath"]
     fpath = None
@@ -83,8 +97,7 @@ def get_groups(doc_id: int) -> Response:
         tmp = [ug for ug in ug_docs if curr_user in ug.owners]
         ug_docs = tmp
 
-    # Mypy doesn't recognize the correct typing here :(
-    groups = (
+    groups: list[UserGroup] = list(
         run_sql(
             select(UserGroup).filter(
                 UserGroupDoc.doc_id.in_([doc.id for doc in ug_docs])
@@ -114,6 +127,20 @@ def get_members(group_id: int) -> Response:
     """
     :return: Group members of the specified group
     """
+
+    # Only admins, group admins, and specified management doc owners should be able to view group members
+    ug_doc_id: int = (
+        run_sql(select(UserGroupDoc.doc_id).filter_by(group_id=group_id).limit(1))
+        .scalars()
+        .first()
+    )
+    ug_doc = get_doc_or_abort(ug_doc_id)
+    if not (verify_ownership(ug_doc) or verify_admin() or verify_groupadmin()):
+        return json_response(
+            status_code=403,
+            jsondata={"error": f"Insufficient permissions."},
+        )
+
     ug: UserGroup | None = (
         run_sql(select(UserGroup).filter_by(id=group_id).limit(1)).scalars().first()
     )
@@ -149,7 +176,9 @@ def check_ownership(doc_id: int) -> Response:
             status_code=200, jsondata={"has_ownership": has_ownership is not None}
         )
     else:
-        return json_response(status_code=403, jsondata={})
+        return json_response(
+            status_code=403, jsondata={"result": {"error": "Insufficient permissions."}}
+        )
 
 
 @login_code.get("/checkRequest")
@@ -179,23 +208,29 @@ def decode_name(name: str) -> str:
     """
     if name.startswith("b64_"):
         group_name = name.removeprefix("b64_")
-        decoded = str(standard_b64decode(group_name.encode()), encoding="utf-8")
+        decoded = str(urlsafe_b64decode(group_name.encode()), encoding="utf-8")
         return decoded.rsplit("/", maxsplit=1)[-1].rsplit("_", maxsplit=1)[0]
     return name
 
 
 @login_code.post("/addMembers/<group_name>")
 def create_users(group_name: str) -> Response:
-    # TODO get user data from request
-    # TODO create UserInfo object
-    # TODO call user.create_with_group with UserInfo
-    # TODO get internal group name from plain group name param
-    # TODO add user to internal group
-    # TODO commit to database
     from timApp.tim import get_current_user_object
+    from timApp.user.groups import verify_groupadmin
+
+    # TODO mass import from CSV, probably want a dedicated function for it
+    current_user = get_current_user_object()
+    # Check for permission to create users
+    # Since we do not want to add every group manager (teacher) to Group admins groups,
+    # we will allow them to create users for their own groups if they have ownership of the group
+    if not verify_groupadmin(
+        user=current_user, action=f"Creating new user"
+    ) or not verify_ownership(require=False, b=UserGroup.get_by_name(group_name)):
+        return json_response(
+            status_code=403, jsondata={"result": {"error": "Insufficient permissions."}}
+        )
 
     u: dict = request.get_json()
-    # FIXME: workaround for Mypy errors
     username: str = str(u.get("username"))
     given_name: str = str(u.get("given_name"))
     surname: str = str(u.get("surname"))
@@ -204,7 +239,7 @@ def create_users(group_name: str) -> Response:
     # Create dummy password (for now), we do not want teacher-managed users to have
     # their personal passwords as we are using temporary login codes.
     dummy_pass: str = str(
-        base64.standard_b64encode(f"{email}{username}{time.time_ns()}".encode()),
+        base64.urlsafe_b64encode(f"{email}{username}{time.time_ns()}".encode()),
         encoding="utf-8",
     )
 
@@ -219,7 +254,7 @@ def create_users(group_name: str) -> Response:
 
     user, _ = User.create_with_group(ui)
 
-    groups = (
+    groups: list[UserGroup] = list(
         run_sql(
             select(UserGroup).where(
                 (UserGroup.name == group_name) | UserGroup.name.like("b64_%")
@@ -228,7 +263,7 @@ def create_users(group_name: str) -> Response:
         .scalars()
         .all()
     )
-    group = None
+    group = UserGroup | None
     for g in groups:
         dec = decode_name(g.name)
         if g.name == group_name or dec == group_name:
@@ -239,15 +274,15 @@ def create_users(group_name: str) -> Response:
             status_code=404,
             jsondata={"result": {"error": f"No matching group: {group_name}."}},
         )
-
-    user.add_to_group(group, get_current_user_object())
-    db.session.commit()
-    return json_response(
-        status_code=200,
-        jsondata={
-            "id": user.id,
-            "name": user.name,
-            "email": user.email,
-            "real_name": user.real_name,
-        },
-    )
+    else:
+        user.add_to_group(group, current_user)
+        db.session.commit()
+        return json_response(
+            status_code=200,
+            jsondata={
+                "id": user.id,
+                "name": user.name,
+                "email": user.email,
+                "real_name": user.real_name,
+            },
+        )
