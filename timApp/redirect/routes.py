488a4c7227440b07b4d9f0c25a2dd2a44f993a16
Vesa Lappalainen <vesal@jyu.fi>
2024-08-15 11:10:49 +0300

    redirect: macro expansion and error handling

--- timApp/redirect/routes.py
+++ timApp/redirect/routes.py
@@ -1,6 +1,10 @@
-from flask import Response, redirect
-
+from flask import Response, redirect, current_app
+# import requests
+import re
+from timApp.auth.sessioninfo import user_context_with_logged_in_or_anon
 from timApp.document.docentry import DocEntry
+from timApp.document.viewcontext import default_view_ctx
+from timApp.markdown.markdownconverter import expand_macros_info
 from timApp.util.flask.requesthelper import RouteException
 from timApp.util.flask.typedblueprint import TypedBlueprint
 # from timApp.printing.print import get_printed_document, pull_doc_path
@@ -12,22 +16,39 @@ redirect_route = TypedBlueprint("redirect", __name__, url_prefix="")
 def redirect_by_alias_file(
     alias: str
 ) -> Response:
-    # Seuraavasta tulee max retries:
-    # hostname = current_app.config["TIM_HOST"]
-    # res = requests.get(f"http://{hostname}/print/redirect/{alias}")
+    # Seuraavasta tulee max retries vaikka manuaalisesti olisi
+    # suora linkki.  localhostin ulkopuolelle toimii
+    hostname = current_app.config["TIM_HOST"]
+    # res = requests.get(f"{hostname}/print/redirect/{alias}")
     # res = requests.get("http://localhost/print/redirect/ohj1")
+    # res = requests.get("http://users.jyu.fi/~vesal/")
 
     # seuraava ei oikein toimi, mistä saisi itse tuloksen?
     # pull_doc_path('print.get_printed_document', {'doc_path': f'redirect/{alias}'})
     # res = get_printed_document(f"redirect/{alias}")
 
-    # Seuraavalla toimii, mutta makroja ei suoriteta
-    # mutta jostakin syystä niitä ei suoriteta edes print-reitissä?
     doc_entry = DocEntry.find_by_path(f"redirect/{alias}")
     if not doc_entry:
         raise RouteException(f"Alias '{alias}' does not exist!")
 
     res = doc_entry.document.export_markdown(export_ids=False, export_settings=False).strip("\n")
+
+    # Hakee käyttäjäkontekstin (mm. käyttäjäkohtaiset makrot)
+    user_ctx = user_context_with_logged_in_or_anon()
+    # Haetaan makroasetukset dokumentille
+    settings = doc_entry.document.get_settings()
+    macro_info = settings.get_macroinfo(default_view_ctx, user_ctx)
+    # Lasketaan makrot auki
+    res = expand_macros_info(res, macro_info, ignore_errors=True).strip("\n").strip(" ")
+    alias_doc_name = f"{hostname}/view/redirect/{alias}"
+
+    if res.find("\n") >= 0:
+        # should make new errortypes to get better html with link
+        raise RouteException(f'Alias {alias_doc_name}-file should include only one line! Now:\n\n{res}')
+
+    match = re.match("^(http)|(/).*$", res)
+    if not match:
+        raise RouteException(f'Alias {alias_doc_name}-file should start with http or /! Now:\n\n{res}')
 
     return redirect(res)
 
