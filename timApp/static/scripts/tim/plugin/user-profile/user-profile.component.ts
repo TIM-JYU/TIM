8dc021f5d46ef2d308b9b769905e8a308b4add91
ylivuoto <ylivuotokanava@gmail.com>
2024-09-20 11:47:09 +0300

    Basic idea ready for profile component

--- timApp/static/scripts/tim/plugin/user-profile/user-profile.component.ts
+++ timApp/static/scripts/tim/plugin/user-profile/user-profile.component.ts
@@ -1,28 +1,49 @@
 import type {OnInit} from "@angular/core";
 import {Component} from "@angular/core";
-import {getURLParameter} from "tim/util/utils";
-import {inIframe} from "tim/plugin/util";
-import {$httpParamSerializer} from "tim/util/ngimport";
-import {showLoginDialog} from "tim/user/showLoginDialog";
-import {Users} from "tim/user/userService";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @Component({
-    selector: "user-profile",
+    selector: "tim-user-profile",
     template: `
-        <div [innerHTML]="'Say hello'">
-            Hello
-            <button (click)="clickThis()"></button>
+        <div class="profile-heading">
+            <h2>About</h2>
+            <button class="btn btn-profile" type="button" (click)="modifyUserProfile()">Modify profile <span class="glyphicon glyphicon-wrench"></span></button>
+        </div>
+        <div class="container">
+            <div class="left-column">
+                <img [src]="profilePictureUrl" alt="profile-picture"/>
+            </div>
+            <div class="right-column">
+                <p>
+                    Paragraph inside
+                </p>
+            </div>
         </div>
     `,
     styleUrls: ["./user-profile.component.scss"],
 })
 export class UserProfileComponent implements OnInit {
-    ngOnInit() {
-        console.log("Kisu123");
+    pictureUrl = "http://localhost/images/68/Riikola_Olli-Pekka.png";
+    profileUrl: string = "";
+
+    constructor(private http: HttpClient) {}
+
+    async ngOnInit() {
+        const data = await this.getProfileData();
+        console.log(data);
     }
 
-    clickThis() {
-        console.log("Hello123");
+    async getProfileData() {
+        const data = this.http.get("/profile");
+        return data;
+    }
+    modifyUserProfile() {
+        this.profileUrl = "URL not set.";
+        console.log("Go to profile document: " + this.profileUrl);
+    }
+
+    get profilePictureUrl() {
+        return this.pictureUrl;
     }
 }
 /*
