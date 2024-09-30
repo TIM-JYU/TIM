aa2857d7a193dcabd43bceb5dfacf7a4694943a7
Vesa Lappalainen <vesal@jyu.fi>
2024-09-30 20:07:44 +0300

    populated to its own file

--- timApp/idesupport/ide_languages.py
+++ timApp/idesupport/ide_languages.py
@@ -8,6 +8,7 @@ from typing import TypeVar, Union
 
 from timApp.idesupport.files import SupplementaryFile, is_in_filename
 from timApp.modules.cs.modifiers import Modifier
+from tim_common.cs_utils import populated
 
 DOTNET_VERSION = "net8.0"
 
@@ -194,41 +195,6 @@ class Java(Language):
     def __init__(self, plugin_json: Any):
         super().__init__(plugin_json)
         self.fileext = "java"
-
-
-T = TypeVar("T", bound=Union[type[Language], type[Modifier]])
-
-
-def populated(
-    base_class: T,
-) -> dict[str, T]:
-    dictionary: dict[str, T] = {}
-    classes = [base_class] + base_class.all_subclasses()
-
-    def add(new_cls: T, new_ttype: str) -> None:
-        if new_ttype in dictionary:
-            raise Exception(
-                f"{base_class.__name__} {cls.__name__} has a duplicate ttype ({new_ttype}) with {dictionary[new_ttype].__name__}"
-            )
-        dictionary[new_ttype] = new_cls
-
-    for cls in classes:
-        if not hasattr(cls, "ttype"):
-            raise Exception(
-                f"{base_class.__name__} {cls.__name__} hasn't defined ttype"
-            )
-        if cls.ttype is None:
-            continue
-        if isinstance(cls.ttype, list):
-            if len(cls.ttype) == 0:
-                raise Exception(
-                    f"{base_class.__name__} {cls.__name__} hasn't defined ttype"
-                )
-            for ttype in cls.ttype:
-                add(cls, ttype.lower())
-        else:
-            add(cls, cls.ttype.lower())
-    return dictionary
 
 
 languages = populated(Language)
---
+++ tim_common/cs_utils.py
@@ -0,0 +1,36 @@
+# T = TypeVar("T", bound=Union[type[Language], type[Modifier]])
+from typing import TypeVar
+
+T = TypeVar("T")
+
+
+def populated(
+    base_class: T,
+) -> dict[str, T]:
+    dictionary: dict[str, T] = {}
+    classes = [base_class] + base_class.all_subclasses()
+
+    def add(new_cls: T, new_ttype: str) -> None:
+        if new_ttype in dictionary:
+            raise Exception(
+                f"{base_class.__name__} {cls.__name__} has a duplicate ttype ({new_ttype}) with {dictionary[new_ttype].__name__}"
+            )
+        dictionary[new_ttype] = new_cls
+
+    for cls in classes:
+        if not hasattr(cls, "ttype"):
+            raise Exception(
+                f"{base_class.__name__} {cls.__name__} hasn't defined ttype"
+            )
+        if cls.ttype is None:
+            continue
+        if isinstance(cls.ttype, list):
+            if len(cls.ttype) == 0:
+                raise Exception(
+                    f"{base_class.__name__} {cls.__name__} hasn't defined ttype"
+                )
+            for ttype in cls.ttype:
+                add(cls, ttype.lower())
+        else:
+            add(cls, cls.ttype.lower())
+    return dictionary
