ab33651023033a2d0ee441382b551229a4b6e30f
Sakari Hurmerinta <shurmeri@exam-agc3313-19.it.jyu.fi>
2025-02-11 13:47:16 +0200

    lisätty http pyyntö testikomponenttiin

--- timApp/static/scripts/tim/todo/todo-component.ts
+++ timApp/static/scripts/tim/todo/todo-component.ts
@@ -2,14 +2,48 @@ import type {OnInit} from "@angular/core";
 import {Component, NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {FormsModule} from "@angular/forms";
+import {Users} from "tim/user/userService";
+import {HttpClient} from "@angular/common/http";
+import {toPromise} from "tim/util/utils";
+import type {IAnswer} from "tim/answer/IAnswer";
 
 @Component({
     selector: "tim-todo",
-    template: "<p>Hello world!</p>",
+    template: `
+        <p>Hello world! id: {{userID}}</p>
+        <div *ngFor="let answer of answers">{{answer.points}}</div>
+    `,
     styleUrls: [],
 })
 export class TodoListComponent implements OnInit {
-    ngOnInit() {}
+    userID?: number;
+    answers?: IAnswer[];
+    constructor(private http: HttpClient) {}
+
+    ngOnInit() {
+        if (Users.isLoggedIn()) {
+            this.userID = Users.getCurrent().id;
+        }
+        this.fetchAnswers();
+    }
+
+    async fetchAnswers() {
+        const response = toPromise(
+            this.http.get<IAnswer[]>(
+                "/exportAnswers/users/hurmerinta-sakari/todo_test"
+            )
+        );
+
+        // Get result from response of the endpoint.
+        const result = await response;
+
+        if (result.ok) {
+            this.answers = result.result;
+            console.log(this.answers);
+        }
+
+        return result;
+    }
 }
 
 @NgModule({
