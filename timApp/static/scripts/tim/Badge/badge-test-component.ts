7117f4877cd6a03ce924f3828c8105b07c037ae1
Sakari Hurmerinta <shurmeri@exam-agc3313-19.it.jyu.fi>
2025-02-19 16:24:12 +0200

    cAdd angular/html to badge-test-component

--- timApp/static/scripts/tim/Badge/badge-test-component.ts
+++ timApp/static/scripts/tim/Badge/badge-test-component.ts
@@ -12,12 +12,30 @@ interface IBadge {
     template: `
         <p>Hello world</p>
         <div *ngFor='let badge of badges'>{{badge.text}}</div>
+        <p></p>
+        <div class="lisaaBadge">
+            <span> 
+                <input type="text" [(ngModel)]="newBadge"/> 
+                <button class="addBadgeButton" (click)="addBadge()">Lisää badge</button>
+            </span>
+        </div>
+        
     `,
     styleUrls: [],
 })
 export class BadgeTestComponent implements OnInit {
     badges: IBadge[] = [{text: "badge1"}, {text: "badge2"}, {text: "badge3"}];
+    newBadge: string = "";
     ngOnInit() {}
+
+    addBadge() {
+        if (this.newBadge) {
+            this.badges.push({
+                text: this.newBadge,
+            });
+        }
+        this.newBadge = "";
+    }
 }
 
 @NgModule({
