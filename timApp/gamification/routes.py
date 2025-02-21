d44748dd371a9b7dd3202a2705dbde727ea9354d
Smiboi <jylha.sami@hotmail.com>
2025-02-21 15:08:38 +0200

    Add routes and modify some hard coded routes dynamic for badge

--- timApp/gamification/routes.py
+++ timApp/gamification/routes.py
@@ -1,15 +1,16 @@
+from flask import Response
 from sqlalchemy import select
 
 from timApp.gamification.badges import Badge, BadgeGiven
 from timApp.timdb.sqa import db, run_sql
-from timApp.util.flask.responsehelper import ok_response
+from timApp.util.flask.responsehelper import ok_response, json_response
 from timApp.util.flask.typedblueprint import TypedBlueprint
 
 badges_blueprint = TypedBlueprint("badges", __name__)
 
 
 @badges_blueprint.get("/all_badges")
-def get_badges() -> list[dict]:
+def get_badges() -> Response:
     badges = run_sql(select(Badge)).all()
 
     badges_json = []
@@ -26,7 +27,7 @@ def get_badges() -> list[dict]:
             }
         )
 
-    return badges_json
+    return json_response(badges_json)
 
 
 @badges_blueprint.get("/create_badge")
@@ -47,30 +48,54 @@ def create_badge():
 # def delete_badge():
 
 
-# @badges_blueprint.get("/groups_badges")
-# def get_groups_badges() -> list[dict]:
-#     groups_badges = run_sql(select(BadgeGiven).filter(BadgeGiven.group_id == 1)).all()
-#
-#     badge_ids_json = []
-#
-#     for badge in groups_badges:
-#         badge_ids_json.append(
-#             {
-#                 "badge_id": badge._data[0].badge_id,
-#             }
-#         )
-#
-#     badges_json = []
-#
-#     for badge_id in badge_ids_json:
-#         badges_json.append()
-#
-#     return badge_ids_json
+@badges_blueprint.get("/groups_badges/<group_id>")
+def get_groups_badges(group_id: int) -> Response:
+    groups_badges_given = run_sql(
+        select(BadgeGiven).filter(BadgeGiven.group_id == group_id)
+    ).all()
+
+    badge_ids_json = []
+
+    for badge in groups_badges_given:
+        badge_ids_json.append(badge._data[0].badge_id)
+
+    groups_badges = run_sql(select(Badge).filter(Badge.id.in_(badge_ids_json))).all()
+
+    badges_json = []
+
+    for badge in groups_badges:
+        badges_json.append(
+            {
+                "id": badge._data[0].id,
+                "title": badge._data[0].title,
+                "description": badge._data[0].description,
+                "color": badge._data[0].color,
+                "shape": badge._data[0].shape,
+                "image": badge._data[0].image,
+            }
+        )
+
+    return json_response(badges_json)
 
 
-@badges_blueprint.get("/give_badge")
-def give_badge():
-    badge_given = BadgeGiven(badge_id=8, group_id=1)
+@badges_blueprint.get("/give_badge/<group_id>/<badge_id>")
+def give_badge(group_id: int, badge_id: int) -> Response:
+    badge_given = BadgeGiven(group_id=group_id, badge_id=badge_id)
+    db.session.add(badge_given)
+    db.session.commit()
+    return ok_response()
+
+
+@badges_blueprint.get("/withdraw_badge/<badge_given_id>")
+def withdraw_badge(badge_given_id: int) -> Response:
+    try:
+        badge_given = BadgeGiven(group_id=badge_given_id)
+        withdrawn_badge = run_sql(
+            select(BadgeGiven).filter(BadgeGiven.id == badge_given_id)
+        ).first()
+    except:
+        return
+    badge_given = BadgeGiven(group_id=group_id, badge_id=badge_id)
     db.session.add(badge_given)
     db.session.commit()
     return ok_response()
