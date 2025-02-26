9d93864c0eadc7412429557a14937154e89bc16a
Andreas Luoto <luotajyz@exam-agc3313-7.it.jyu.fi>
2025-02-26 15:22:38 +0200

    This commit will add description text when howering over created badge

--- timApp/static/scripts/tim/Badge/Badge-component.ts
+++ timApp/static/scripts/tim/Badge/Badge-component.ts
@@ -1,15 +1,59 @@
-import type {OnInit} from "@angular/core";
-import {Component, NgModule} from "@angular/core";
+import type {OnInit, OnChanges} from "@angular/core";
+import {Component, Input, NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 
 @Component({
     selector: "tim-badge",
-    template: "<p>Hello world!</p>",
-    styleUrls: [],
+    template: `
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+    <div class="badge-container" [ngClass]="['badge', color, shape]">
+    <div class="circle">
+        <span class="material-symbols-outlined">{{ icon }}</span>
+    </div>
+    <div class="ribbon">{{ title }}</div>
+    <!-- Tooltip for description -->
+    <div class="tooltip" *ngIf="description">{{ description }}</div>
+        <div class="tooltip" *ngIf="message">{{ message }}</div>
+</div>
+
+  `,
+    styleUrls: ["badge.component.css"],
 })
-export class BadgeComponent implements OnInit {
+export class BadgeComponent implements OnInit, OnChanges {
+    @Input() id?: number;
+    @Input() title?: string;
+    @Input() color?: string;
+    @Input() shape?: string;
+    @Input() image?: number;
+    @Input() description?: string;
+    @Input() message?: string;
+    icon?: string;
+
     ngOnInit() {}
+
+    ngOnChanges() {
+        this.icon = "help";
+
+        if (this.image == 1) {
+            this.icon = "diversity_3";
+        }
+        if (this.image == 2) {
+            this.icon = "code";
+        }
+        if (this.image == 3) {
+            this.icon = "trophy";
+        }
+        if (this.image == 4) {
+            this.icon = "editor_choice";
+        }
+        if (this.image == 5) {
+            this.icon = "bug_report";
+        }
+        if (this.image == 6) {
+            this.icon = "liquor";
+        }
+    }
 }
 
 @NgModule({
