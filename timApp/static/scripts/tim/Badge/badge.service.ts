b6d85914ec5fe3b1eb8e5ec7099d6935860ec248
Onni Karjalainen <ontajoka@student.jyu.fi>
2025-03-06 13:17:32 +0200

    Update badge service

--- timApp/static/scripts/tim/Badge/badge.service.ts
+++ timApp/static/scripts/tim/Badge/badge.service.ts
@@ -1,8 +1,26 @@
 import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {lastValueFrom, Observable} from "rxjs";
+import {toPromise} from "tim/util/utils";
+
+class Badge {}
 
 @Injectable({
     providedIn: "root",
 })
 export class BadgeService {
-    constructor() {}
+    private all_badges: any[] = [];
+    constructor(private http: HttpClient) {}
+
+    async getAllBadges(): Promise<Badge[]> {
+        try {
+            const result = await lastValueFrom(
+                this.http.get<Badge[]>("/all_badges")
+            );
+            return result ?? []; // Ensure result is always an array
+        } catch (error) {
+            console.error("Error fetching badges:", error);
+            return []; // Return an empty array in case of error
+        }
+    }
 }
