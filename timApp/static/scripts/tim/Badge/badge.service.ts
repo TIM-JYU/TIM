23bd6693cfede3e0318367f07cc58c10a8080eed
Onni Karjalainen <ontajoka@student.jyu.fi>
2025-03-07 11:57:39 +0200

    simplify badge service data request
    * comment code

--- timApp/static/scripts/tim/Badge/badge.service.ts
+++ timApp/static/scripts/tim/Badge/badge.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
-import {lastValueFrom, Observable} from "rxjs";
-import {toPromise} from "tim/util/utils";
+import {lastValueFrom} from "rxjs";
 
 class Badge {}
 
@@ -14,13 +13,18 @@ export class BadgeService {
 
     async getAllBadges(): Promise<Badge[]> {
         try {
-            const result = await lastValueFrom(
-                this.http.get<Badge[]>("/all_badges")
-            );
-            return result ?? []; // Ensure result is always an array
+            // Create an HTTP request and wait for response
+            const response = this.http.get<Badge[]>("/all_badges");
+
+            // Transform Observable to a Promise and wait for data
+            const result = await lastValueFrom(response);
+
+            // Return fetched data or an empty array if database is empty
+            return result ?? [];
         } catch (error) {
             console.error("Error fetching badges:", error);
-            return []; // Return an empty array in case of error
+            // Return an empty array in case of an error
+            return [];
         }
     }
 }
