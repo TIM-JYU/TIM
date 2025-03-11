72fe3347bf3992c6c74e3d680c164e39051c1390
Andreas Luoto <luotajyz@exam-agc3313-7.it.jyu.fi>
2025-03-11 12:17:22 +0200

    This commit will add badge preview start

--- timApp/static/scripts/tim/Badge/badge-viewer-component.ts
+++ timApp/static/scripts/tim/Badge/badge-viewer-component.ts
@@ -9,15 +9,41 @@ import {to2} from "tim/util/utils";
 @Component({
     selector: "tim-badge-viewer",
     template: `
-        <ng-container>
-            <p>{{userName}}'s badges: </p>
-            <div class="main-wrapper">
-              <div *ngFor='let badge of badges' class="{{badge.name}}">
-                <div class="circle"> </div>
-                <div class="ribbon">{{badge.text}}</div>
-              </div>
+        <ng-container *ngIf="badges.length > 0">
+            <p>{{selectedUserName}}'s badges</p>
+            <div class="user_badges">
+                <tim-badge *ngFor="let badge of badges" 
+                           title="{{badge.title}}" 
+                           color="{{badge.color}}" 
+                           shape="{{badge.shape}}"
+                           [image]="badge.image"
+                           description="{{badge.description}}"
+                           message="{{badge.message}}"
+                           (click)="selectBadge(badge)">
+                    
+                    
+                </tim-badge>
             </div>
         </ng-container>
+        <ng-container *ngIf="badges.length == 0">
+         <p>{{selectedUserName}} has no badges</p>
+        </ng-container>
+        
+        <!-- Preview of the selected badge -->
+        <div *ngIf="selectedBadge" class="badge-preview">
+            <h3>Selected Badge Preview</h3>
+            <div class="badge-info">
+                <h4>{{ selectedBadge.title }}</h4>
+                <p><strong>Description:</strong> {{ selectedBadge.description }}</p>
+                <p><strong>Message:</strong> {{ selectedBadge.message }}</p>
+                <img [src]="getBadgeImageUrl(selectedBadge.image)" alt="{{ selectedBadge.title }}" class="badge-image" />
+            </div>
+        </div>
+        
+        <!-- Delete button, only shown when a badge is selected -->
+        <div *ngIf="showDeleteButton">
+          <button (click)="removeBadge()">Delete</button>
+        </div>
         `,
     styleUrls: ["badge-viewer-component.scss"],
 })
@@ -30,16 +56,13 @@ export class BadgeViewerComponent implements OnInit {
 
     private async getBadges(id: number) {
         const response = toPromise(this.http.get<[]>("/groups_badges/" + id));
-
         const result = await response;
-
         if (result.ok) {
             if (result.result != undefined) {
                 for (const alkio of result.result) {
                     const json = JSON.stringify(alkio);
                     const obj = JSON.parse(json);
                     this.badges.push(obj);
-                    this.badgeIDs.push(obj.id);
                 }
             }
         }
@@ -51,6 +74,21 @@ export class BadgeViewerComponent implements OnInit {
             this.userID = Users.getCurrent().id;
         }
         this.getBadges();
+    }
+
+    // Select a badge to show the delete button
+    selectBadge(badge: IBadge) {
+        this.selectedBadge = badge;
+        this.showDeleteButton = true;
+    }
+
+    removeBadge() {
+        const response = toPromise(
+            this.http.get(
+                `/withdraw_badge/${this.selectedBadge}/${this.userID}`
+            )
+        );
+        console.log("badgeId: ", this.selectedBadge);
     }
 }
 
