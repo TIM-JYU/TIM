ea17f0346b95b956419026f91537b264af87b2a0
Andreas Luoto <luotajyz@exam-agc3313-7.it.jyu.fi>
2025-04-01 15:38:27 +0300

    add the dialog back with more information

--- timApp/static/scripts/tim/gamification/badge/badge.component.ts
+++ timApp/static/scripts/tim/gamification/badge/badge.component.ts
@@ -2,6 +2,9 @@ import type {OnInit, OnChanges} from "@angular/core";
 import {Component, Input, NgModule, SimpleChanges} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {FormsModule} from "@angular/forms";
+import {BadgeService} from "tim/gamification/badge/badge.service";
+import {angularDialog} from "tim/ui/angulardialog/dialog.service";
+import {MessageDialogComponent} from "tim/ui/message-dialog.component";
 
 @Component({
     selector: "tim-badge",
@@ -31,6 +34,7 @@ export class BadgeComponent implements OnInit, OnChanges {
     @Input() image?: number;
     @Input() description?: string;
     @Input() message?: string;
+    @Input() preventDialog: boolean = false;
 
     icon?: string;
 
@@ -48,6 +52,39 @@ export class BadgeComponent implements OnInit, OnChanges {
         11: "loop",
         12: "money",
     };
+
+    // hakee dialogServicen BadgeServicesta
+    constructor(private dialogService: BadgeService) {}
+
+    async openDialog(): Promise<void> {
+        if (this.preventDialog) {
+            this.dialogService.closeActiveDialog();
+            return;
+        }
+        // Close any open dialog
+        this.dialogService.closeActiveDialog();
+
+        // Open a new dialog
+        this.dialogService.activeDialogRef = await angularDialog.open(
+            MessageDialogComponent,
+            {
+                message: `
+            <b>${this.title}</b><br><br>
+            <b>Description:</b> ${this.description}<br>
+            <b>Message:</b> ${this.message}
+            <b>Color:</b> ${this.color}
+            <b>Icon:</b> ${this.icon}
+            <b>Shape:</b> ${this.shape}
+            
+        `,
+                modal: false,
+            }
+        );
+
+        // Wait for the dialog to close
+        await this.dialogService.activeDialogRef.result;
+        this.dialogService.activeDialogRef = null; // Reset the reference after closing
+    }
 
     ngOnInit(): void {
         this.setIcon();
