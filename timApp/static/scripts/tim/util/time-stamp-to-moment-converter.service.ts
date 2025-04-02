c1ce0552fa5384af5a60185779f9e39cf83802bd
dezhidki <dezhidki@jyu.fi>
2025-04-02 12:59:59 +0300

    TimeStampToMomentConverter: convert error objects too
    Angular.JS's response transformer transforms date values of error responses. The same did not happen for Angular's HTTPInterceptor. This commit aligns the behaviour of both

--- timApp/static/scripts/tim/util/time-stamp-to-moment-converter.service.ts
+++ timApp/static/scripts/tim/util/time-stamp-to-moment-converter.service.ts
@@ -4,11 +4,13 @@ import type {
     HttpInterceptor,
     HttpRequest,
 } from "@angular/common/http";
+import {HttpErrorResponse} from "@angular/common/http";
 import {HttpResponse} from "@angular/common/http";
 import type {Observable} from "rxjs/internal/Observable";
 import {Injectable} from "@angular/core";
-import {map} from "rxjs/operators";
+import {catchError, map} from "rxjs/operators";
 import {convertDateStringsToMoments} from "tim/util/utils";
+import {throwError} from "rxjs";
 
 @Injectable()
 export class TimeStampToMomentConverter implements HttpInterceptor {
@@ -16,18 +18,37 @@ export class TimeStampToMomentConverter implements HttpInterceptor {
         req: HttpRequest<unknown>,
         next: HttpHandler
     ): Observable<HttpEvent<unknown>> {
-        return next.handle(req).pipe(
-            map((event: HttpEvent<unknown>) => {
-                if (
-                    event instanceof HttpResponse &&
-                    event.headers.get("No-Date-Conversion") !== "true"
-                ) {
-                    event = event.clone({
-                        body: convertDateStringsToMoments(event.body),
-                    });
-                }
-                return event;
-            })
-        );
+        return next
+            .handle(req)
+            .pipe(
+                map((event: HttpEvent<unknown>) => {
+                    if (
+                        event instanceof HttpResponse &&
+                        event.headers.get("No-Date-Conversion") !== "true"
+                    ) {
+                        event = event.clone({
+                            body: convertDateStringsToMoments(event.body),
+                        });
+                    }
+                    return event;
+                })
+            )
+            .pipe(
+                catchError((error: unknown) => {
+                    if (
+                        error instanceof HttpErrorResponse &&
+                        error.headers.get("No-Date-Conversion") !== "true"
+                    ) {
+                        error = new HttpErrorResponse({
+                            headers: error.headers,
+                            status: error.status,
+                            statusText: error.statusText,
+                            url: error.url ?? undefined,
+                            error: convertDateStringsToMoments(error.error),
+                        });
+                    }
+                    return throwError(() => error);
+                })
+            );
     }
 }
