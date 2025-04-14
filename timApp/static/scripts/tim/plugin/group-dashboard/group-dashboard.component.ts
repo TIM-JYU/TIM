e3fbe829b932b9fe2af51d873a15892faf93297e
Onni Karjalainen <ontajoka@student.jyu.fi>
2025-04-14 13:19:15 +0300

    Add member fetching and displaying
    * attach group name changer to setting section

--- timApp/static/scripts/tim/plugin/group-dashboard/group-dashboard.component.ts
+++ timApp/static/scripts/tim/plugin/group-dashboard/group-dashboard.component.ts
@@ -3,26 +3,48 @@ import {CommonModule} from "@angular/common";
 import {BadgeService} from "tim/gamification/badge/badge.service";
 import {IManageGlobals, manageglobals} from "tim/util/globals";
 import {IFolder, IFullDocument} from "tim/item/IItem";
+import {GroupNameModule} from "tim/gamification/badge/group-name.component";
 
 @Component({
     selector: "tim-group-dashboard",
     template: `
         <ng-container>
-            <h1>{{ this.displayName }}'s Dashboard</h1>
-        </ng-container>`,
+    <h1>{{ displayName }}'s Dashboard</h1>
+
+    <div class="member-list">
+        <div class="member-card" *ngFor="let member of members">
+            <div class="member-info">
+                <span class="member-name">{{ member.name }}</span>
+
+                <div class="badges">
+                    <span *ngFor="let badge of member.badges" class="badge">
+                        🏅 {{ badge.name }}
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="settings">
+        <h2>Settings</h2>
+        <h3>Change group name:</h3>
+        <tim-group-name [group]="group"></tim-group-name>
+    </div>            
+</ng-container>`,
     styleUrls: ["./group-dashboard.component.scss"],
 })
 export class GroupDashboardComponent implements OnInit {
-    private item: IFullDocument | IFolder | undefined;
     constructor(private badgeService: BadgeService) {}
 
     @Input() group!: string;
+    private item: IFullDocument | IFolder | undefined;
     displayName: string | undefined;
     groupId: number | undefined;
+    members: any[] = [];
     title: string | undefined;
     ngOnInit(): void {
         if (this.group) {
             this.getGroupName();
+            this.fetchMembers();
         }
         this.item = manageglobals().curr_item;
     }
@@ -35,11 +57,20 @@ export class GroupDashboardComponent implements OnInit {
             this.displayName = fetchedGroup.description || "";
         }
     }
+
+    async fetchMembers() {
+        const members = await this.badgeService.getUsersFromGroup(this.group);
+
+        if (members) {
+            console.log("Jäsenet: ", members);
+            this.members = members;
+        }
+    }
 }
 
 @NgModule({
     declarations: [GroupDashboardComponent],
     exports: [GroupDashboardComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, GroupNameModule],
 })
 export class GroupDashboardModule {}
