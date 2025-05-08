5db0d214999a37d02609e0a00d90924dbfde22e0
Sakari Hurmerinta <shurmeri@exam-agc3313-19.it.jyu.fi>
2025-05-08 15:30:40 +0300

    resolved conflicts

--- timApp/gamification/badge/routes.py
+++ timApp/gamification/badge/routes.py
@@ -144,7 +144,7 @@ def verify_access(
 @badges_blueprint.get("/all_badges/<context_group>")
 def all_badges(context_group: str) -> Response:
     """
-    Fetches all badges in specific context_group. Sorted by created-timestamp.
+    Fetches all badges in specific context_group. Sorted by created-timestamp.!res
     :param context_group: Context group to get badges from
     :return: Badges in json response format
     """
