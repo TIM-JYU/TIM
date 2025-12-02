243eae701f97647814076fc07115773e9c849f35
Sami Viitanen <sami.a.viitanen@outlook.com>
2025-12-02 12:21:15 +0200

    Revert workarounds for CI failures in tests.
    This reverts commit c163a05208d902191304517cdf273d1953d8e1fb.

--- timApp/tests/browser/test_velps.py
+++ timApp/tests/browser/test_velps.py
@@ -138,11 +138,7 @@ saveButton: Tallenna
         vg.velps[new_velp.id] = new_velp
         db.session.commit()
         self.goto_document(d)
-        self.wait.until(
-            expected_conditions.visibility_of_element_located(
-                (By.CSS_SELECTOR, "jsframe-runner iframe")
-            )
-        )
+        self.wait_until_present("jsframe-runner iframe")
         par = self.find_element_avoid_staleness(".par.csPlugin")
         parid = par.get_attribute("id")
         t = par.get_attribute("t")
