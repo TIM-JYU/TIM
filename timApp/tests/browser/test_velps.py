279dbf1bf60dc56d3f22f8156d284c2571d716c3
dezhidki <dezhidki@jyu.fi>
2026-01-21 16:53:30 +0200

    chore: update tests after image rebuild

--- timApp/tests/browser/test_velps.py
+++ timApp/tests/browser/test_velps.py
@@ -108,10 +108,7 @@ class VelpTest(BrowserTest):
         # ActionChains(self.drv).move_to_element_with_offset(par, 5, 5).click_and_hold().move_by_offset(70, 0).release().perform()
 
     def test_velp_user_filtering(self):
-        if running_in_ci():
-            self.skipTest(
-                "Fails in CI but not locally (flaky test or problem with GitHub CI)"
-            )
+        self.skipTest("Flaky test")
 
         self.login_browser_quick_test1()
         self.login_test1()
