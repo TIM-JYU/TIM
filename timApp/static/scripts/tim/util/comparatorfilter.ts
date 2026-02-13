eca1fee7e5e53e4c34590c6ea919fdc897c993fd
Vesa Lappalainen <vesal@jyu.fi>
2026-02-13 22:25:02 +0200

    filter do not compare number to empty string

--- timApp/static/scripts/tim/util/comparatorfilter.ts
+++ timApp/static/scripts/tim/util/comparatorfilter.ts
@@ -2,21 +2,6 @@ export const timDateRegex =
     /^(?:\d{4}-\d{2}-\d{2}(?:[ T]\d{2}:\d{2}(?::\d{2})?)?|\d{2}:\d{2}(?::\d{2})?)$/;
 export const timNumberRegex = /^-?\d+([.,:]\d+)?([eE]-?\d+)?$/;
 
-/**
- * NumFilter class for filtering numbers.
- *
- * <2     => find all numbers less than 2
- * <=2    => find all numbers less or equal than 2
- * >2     => find all numbers greter than 2
- * >=2    => find all numbers greter or equal than 2
- * =2     => find all numbers equal to 2
- * >2<4   => find all numbers in range ]2,4[
- * >=2<=4 => find all numbers in range [2,4]
- * !>2<4  => find all numbers not in range ]2,4[
- * See: https://regex101.com/r/yfHbaH/3
- */
-// const numFilterEx: RegExp = /([<=>!]=?) *(-?[\w.,]*) *(!?) */g;
-
 type NumStr = number | string;
 
 export function getCompareValue(vs: string): [NumStr, boolean] {
@@ -33,72 +18,120 @@ export function getCompareValue(vs: string): [NumStr, boolean] {
 }
 
 type FilterComparator = (a: NumStr, b: NumStr) => boolean;
+type FilterItem = {
+    func: FilterComparator;
+    value: NumStr;
+    strValue: string;
+    isStr: boolean;
+};
+type FilterList = FilterItem[];
+
 const filterComparatorOperators = {
-    "<": (a: NumStr, b: NumStr) => a < b,
+    "!==": (a: NumStr, b: NumStr) => a !== b,
+    "!=": (a: NumStr, b: NumStr) => a !== b,
     "<=": (a: NumStr, b: NumStr) => a <= b,
-    "=": (a: NumStr, b: NumStr) => a === b,
     "==": (a: NumStr, b: NumStr) => a === b,
-    // "!==": (a: NumStr, b: NumStr) => a !== b,
-    // "!=": (a: NumStr, b: NumStr) => a !== b,
-    // "!": (a: NumStr, b: NumStr) => a !== b,
-    ">": (a: NumStr, b: NumStr) => a > b,
+    "!!": (a: NumStr, b: NumStr) => a == b,
     ">=": (a: NumStr, b: NumStr) => a >= b,
+    "<": (a: NumStr, b: NumStr) => a < b,
+    "=": (a: NumStr, b: NumStr) => a === b,
+    ">": (a: NumStr, b: NumStr) => a > b,
+    "!": (a: NumStr, b: NumStr) => a !== b,
 };
 
-function buildOperatorRegex(operators: Record<string, unknown>): RegExp {
-    const escapedOps = Object.keys(operators)
-        .sort((a, b) => b.length - a.length)
-        .map((op) => op.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"));
-    const opPattern = escapedOps.join("|");
+const filterComparatorOperatorsKeys = Object.keys(filterComparatorOperators);
 
-    // Prefix ! optional, operator valinnainen (puuttuva = regex), value valinnainen
-    return new RegExp(`(${opPattern})?([^<>=]*)(?=(?:${opPattern})|$)`, "g");
+function parseFilter(fltr: string): FilterList {
+    const ops = filterComparatorOperatorsKeys;
+
+    const results: FilterList = [];
+
+    if (fltr === "") {
+        return [
+            {
+                func: filterComparatorOperators["="],
+                value: "",
+                strValue: "",
+                isStr: true,
+            },
+        ];
+    }
+
+    let i = 0;
+    while (i < fltr.length) {
+        let func: FilterComparator | null = null;
+        let operator = "";
+
+        // is beginning of the operator
+        for (const op of ops) {
+            if (fltr.startsWith(op, i)) {
+                func =
+                    filterComparatorOperators[
+                        op as keyof typeof filterComparatorOperators
+                    ];
+                i += op.length;
+                operator = op;
+                break;
+            }
+        }
+
+        // Find the index of the next operator
+        let nextOpIndex = fltr.length;
+
+        for (const op of ops) {
+            const idx = fltr.indexOf(op, i);
+            if (idx !== -1 && idx < nextOpIndex) {
+                nextOpIndex = idx;
+            }
+        }
+
+        // Cut the value
+        const value = fltr.slice(i, nextOpIndex);
+        const strValue = value.toLowerCase();
+        const [val, isString] = getCompareValue(value);
+        if (value === "" && !func) {
+            // If no operator and value is empty, use equality operator
+            func = filterComparatorOperators["="];
+        }
+        if (!func || operator === "!!") {
+            const re = new RegExp(value, "i");
+            func = (a: NumStr, _: NumStr) => {
+                return re.test("" + a);
+            };
+        }
+        if (operator === "!") {
+            const re = new RegExp(value, "i");
+            func = (a: NumStr, _: NumStr) => {
+                return !re.test("" + a);
+            };
+        }
+
+        results.push({
+            func: func,
+            value: val,
+            strValue: strValue,
+            isStr: isString,
+        });
+
+        i = nextOpIndex;
+    }
+
+    return results;
 }
-
-const numFilterEx = buildOperatorRegex(filterComparatorOperators);
-
 export class ComparatorFilter {
-    values: NumStr[] = [];
-    strings: string[] = [];
-    funcs: FilterComparator[] = [];
+    filterItems: FilterList;
     negate: boolean = false;
-    // m: RegExpExecArray |
-    // null = [];
     constructor(fltr: string) {
-        // this.m = numFilterEx.exec(fltr);
-        // if ( !this.m ) { return; }
-        if (fltr.startsWith("!")) {
-            fltr = fltr.replace("!", "");
+        // Check for negation at the beginning of the filter.
+        if (
+            fltr.startsWith("!") &&
+            !fltr.startsWith("!=") &&
+            !fltr.startsWith("!!")
+        ) {
+            fltr = fltr.slice(1);
             this.negate = true;
         }
-        numFilterEx.lastIndex = 0;
-        let result = numFilterEx.exec(fltr);
-        while (result !== null) {
-            // this.negate = this.negate || !!result[1]; // If "!" is present before the operator, set negate to true.
-            let op = result[1] as keyof typeof filterComparatorOperators; // We know that it'll be one of the operators.
-            const vs = result[2];
-            if (!op && vs === "") {
-                op = "=";
-            }
-            if (op) {
-                this.funcs.push(filterComparatorOperators[op]);
-            } else {
-                // If no operator is specified, default to regexp.
-                const re = new RegExp(vs, "i");
-                this.funcs.push((a: NumStr, _: NumStr) => {
-                    return re.test("" + a);
-                });
-            }
-            this.values.push(getCompareValue(vs)[0]);
-            this.strings.push(vs.toLowerCase());
-            if (fltr === "" || result[0] === "") {
-                break;
-            }
-            result = numFilterEx.exec(fltr);
-            if (result == null || result[0] === "") {
-                break;
-            }
-        }
+        this.filterItems = parseFilter(fltr);
     }
 
     public static makeNumFilter(s: string): ComparatorFilter {
@@ -109,15 +142,19 @@ export class ComparatorFilter {
         const [n, forceString] = getCompareValue(s);
         const lcs = s.toLowerCase();
         let res = true;
-        for (let i = 0; i < this.values.length; i++) {
-            let v = this.values[i];
+        for (const item of this.filterItems) {
+            if (s === "" && !item.isStr) {
+                res = false;
+                break;
+            }
+            let v = item.value;
             let ns = n;
             if (forceString) {
-                v = this.strings[i];
-            } else if (typeof v === "string") {
+                v = item.strValue;
+            } else if (item.isStr) {
                 ns = lcs;
             }
-            if (!this.funcs[i](ns, v)) {
+            if (!item.func(ns, v)) {
                 res = false;
                 break;
             }
