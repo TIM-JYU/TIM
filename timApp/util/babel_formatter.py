868e4dd3e0e446165e6f91932824e4d9ede33561
pekMant <116541374+pekMant@users.noreply.github.com>
2026-02-27 15:52:29 +0200

    New features for Task summary (#3989)
    * add possibility of changing to comma in task summary expl
    
    * add working debug version. needs heavy refactoring
    
    * refactor the text variable passing. Remove debugging helpers
    
    * add check to ensure preformat_points is set. Add field to tests
    
    * refactor into a proper string formatter
    
    * allow pointsumrule text contain html; add sanitization, remove preformat_points
    
    * babel_formatter: use bf ending to format decimal
    
    * babel_formatter: use lf ending to format decimal using the locale
    
    * babel_formatter: add missing annotation
    
    ---------
    
    Co-authored-by: pekMant <pekka.j.mantysaari@jyu.fi>
    Co-authored-by: dezhidki <dezhidki@jyu.fi>

---
+++ timApp/util/babel_formatter.py
@@ -0,0 +1,27 @@
+import string
+from typing import Any
+
+from babel.numbers import format_decimal
+
+from timApp.util.locale import get_locale
+
+
+class BabelFormatter(string.Formatter):
+    def format_field(self, value: Any, format_spec: str) -> str:
+        """
+        Additional formatter that can handle Babel number formatting.
+        If the format spec ends with 'b', it will use Babel to format the number according to the current locale.
+
+        :param value:
+        :param format_spec:
+        :return:
+        """
+        if format_spec.endswith("lf"):
+            format_spec = format_spec[:-1]
+            return format_decimal(
+                value, format=format_spec or None, locale=get_locale()
+            )
+        return super().format_field(value, format_spec)
+
+
+babel_fmt = BabelFormatter()
