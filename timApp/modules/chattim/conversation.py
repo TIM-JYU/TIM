e643e32127d5babde5ec53deb67560a382b1e47e
lepton9 <tonihirvonen00@gmail.com>
2026-03-16 22:20:05 +0200

    list user conversations with a plugin instance

--- timApp/modules/chattim/conversation.py
+++ timApp/modules/chattim/conversation.py
@@ -1,21 +1,25 @@
 from __future__ import annotations
 
-import dataclasses
 import os
 import json
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from timApp.defaultconfig import FILES_PATH
 
-# from timApp.modules.chattim.model import Message
+# from timApp.modules.chattim.model import Message, Usage
 
 
 @dataclass
 class ChatMessage:
+    content: str
+    """Content of the message."""
+    # TODO: better to use a date for readability?
+    timestamp: int
+    """Timestamp of when the message was sent."""
     # role: Message.Role
     role: str
-    timestamp: int
-    content: str
+    """Role of the message sender."""
     # usage: Usage | None = None
+    """Tokens used for generating the message."""
 
 
 class ConversationManager:
@@ -26,19 +30,21 @@ class ConversationManager:
     # TODO: convo history in mem
     def __init__(self):
         # TODO: change root path naming
+        # TODO: get the `FILES_PATH` as an argument
         root_path = os.path.join(FILES_PATH, "history", "chattim")
+        # TODO: keep a cache for recent conversations?
         self.store = ConversationStore(root_path)
 
-    def append_message(
+    def append_messages(
         self,
         plugin_id: str,
         user_id: str,
         conversation_id: str,
-        message: ChatMessage,
+        messages: list[ChatMessage],
     ):
-        """Append a message to the history of the specified conversation."""
+        """Append messages to the history of the specified conversation."""
         # TODO: update cache
-        self.store.append_message(plugin_id, user_id, conversation_id, message)
+        self.store.append_messages(plugin_id, user_id, conversation_id, messages)
 
     def get_history(
         self,
@@ -52,6 +58,10 @@ class ConversationManager:
         messages = self.store.load_messages(plugin_id, user_id, conversation_id, last_n)
         return messages or []
 
+    def user_conversations(self, plugin_id: str, user_id: str) -> list[str]:
+        """Get all the conversations of the specified user with the plugin."""
+        return self.store.user_conversations(plugin_id, user_id)
+
 
 class ConversationStore:
     """Handles disk IO for storing the conversations."""
@@ -61,24 +71,26 @@ class ConversationStore:
     def __init__(self, root_path: str):
         self.root_path = root_path
 
-    def append_message(
+    def append_messages(
         self,
         plugin_id: str,
         user_id: str,
         conversation_id: str,
-        message: ChatMessage,
+        messages: list[ChatMessage],
     ):
         """
-        Append one `ChatMessage` to the JSONL file.
+        Append a list of `ChatMessage` to the JSONL file.
         Creates the file path for the conversation file if not found.
         """
-        file_path = self.resolve_conversation_file_path(
-            plugin_id, user_id, conversation_id
-        )
+        file_path = self.resolve_conversation_path(plugin_id, user_id, conversation_id)
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
-        msg_dict = dataclasses.asdict(message)
         with open(file_path, "a", encoding="utf-8") as f:
-            f.write(json.dumps(msg_dict) + "\n")
+            for message in messages:
+                try:
+                    msg_dict = asdict(message)
+                    f.write(json.dumps(msg_dict) + "\n")
+                except TypeError:
+                    continue
 
     def load_messages(
         self,
@@ -98,9 +110,7 @@ class ConversationStore:
         """
         if last_n is not None and last_n <= 0:
             return []
-        file_path = self.resolve_conversation_file_path(
-            plugin_id, user_id, conversation_id
-        )
+        file_path = self.resolve_conversation_path(plugin_id, user_id, conversation_id)
         try:
             out: list[ChatMessage] = []
 
@@ -120,12 +130,27 @@ class ConversationStore:
         except FileNotFoundError:
             return None
 
-    def resolve_conversation_file_path(
+    def user_conversations(
         self,
         plugin_id: str,
         user_id: str,
-        conversation_id: str,
+    ) -> list[str]:
+        """Return the conversation IDs of the user."""
+        conversations_path = self.resolve_conversation_path(plugin_id, user_id)
+        out: list[str] = []
+        for e in os.scandir(conversations_path):
+            if e.is_file():
+                out.append(e.name.split(".")[0])
+        return out
+
+    def resolve_conversation_path(
+        self,
+        plugin_id: str,
+        user_id: str,
+        conversation_id: str | None = None,
     ) -> str:
         """Resolve the path to the conversation JSONL file."""
+        if conversation_id is None:
+            return os.path.join(self.root_path, plugin_id, user_id)
         file_name = f"{conversation_id}.jsonl"
         return os.path.join(self.root_path, plugin_id, user_id, file_name)
