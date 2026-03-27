cef3c28eb95b861e2a139bd1e5e95422607c8322
Sami Viitanen <sami.a.viitanen@outlook.com>
2026-03-27 14:47:27 +0200

    tests: Appease the linter gods

--- timApp/modules/svn/js/vtt-parser.ts
+++ timApp/modules/svn/js/vtt-parser.ts
@@ -27,7 +27,9 @@ export function formatVttTimestamp(ts: string): string {
     const m = Math.floor((seconds % 3600) / 60);
     const s = Math.floor(seconds % 60);
     if (h > 0) {
-        return `${h}:${m.toString().padStart(2, "0")}:${s.toString().padStart(2, "0")}`;
+        return `${h}:${m.toString().padStart(2, "0")}:${s
+            .toString()
+            .padStart(2, "0")}`;
     }
     return `${m}:${s.toString().padStart(2, "0")}`;
 }
