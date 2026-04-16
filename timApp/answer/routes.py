309e7fca6c48106a01c834225af864c88ca36fba
Vesa Lappalainen <vesal@jyu.fi>
2026-04-16 20:07:33 +0300

    feature prevent jsrunner write

--- timApp/answer/routes.py
+++ timApp/answer/routes.py
@@ -856,7 +856,9 @@ def post_answer_impl(
                 raise PluginException(f"User {user_id} not found")
             users = [ctx_user]  # TODO: Vesa's hack to save answer to student
 
-    view_ctx = ViewContext(ViewRoute.View, False, urlmacros=urlmacros, origin=origin)
+    view_ctx = ViewContext(
+        ViewRoute.PostAnswer, False, urlmacros=urlmacros, origin=origin
+    )
     (
         vr,
         answerinfo,
@@ -1444,9 +1446,9 @@ def preprocess_jsrunner_answer(
         runner_req.input.paramComps
     ):  # TODO: add paramComps to the interface, so no need to manipulate source code
         preprg = runnermarkup.preprogram or ""
-        plugin.values[
-            "preprogram"
-        ] = f"gtools.params = {json.dumps(runner_req.input.paramComps)};\n{preprg}"
+        plugin.values["preprogram"] = (
+            f"gtools.params = {json.dumps(runner_req.input.paramComps)};\n{preprg}"
+        )
     siw = runnermarkup.showInView
     markup_include_opt = value_or_default(
         runnermarkup.includeUsers, MembershipFilter.Current
