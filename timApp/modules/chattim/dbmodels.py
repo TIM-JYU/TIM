6d88445c3068731f3f2df6b4f3b7e12512e6e060
Anni Kinnunen <anjakinn@ws-c3313-8.it.jyu.fi>
2026-04-20 11:11:41 +0300

    Fix database model relationships and missing keys.

--- timApp/modules/chattim/dbmodels.py
+++ timApp/modules/chattim/dbmodels.py
@@ -2,7 +2,6 @@ from sqlalchemy import Integer, String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.dialects.postgresql import ARRAY
 from timApp.timdb.sqa import db
-from timApp.user.usergroup import UserGroup
 
 
 class LLMRule(db.Model):
@@ -18,19 +17,22 @@ class LLMRule(db.Model):
     # apikey: Mapped[str] = mapped_column(String)
     # publickey: Mapped[str] = mapped_column(String)
     teachers: Mapped[list[int]] = mapped_column(ARRAY(Integer))
-    # teachers: Mapped[list["UserGroup"]] = relationship("UserGroup", back_populates="llm_rule")
     current_mode: Mapped[str] = mapped_column(String, default="summarizing") # summarizing or creative
     total_tokens_spent: Mapped[int] = mapped_column(Integer, default=0)
     indexed_chunk_ids: Mapped[list[int]] = mapped_column(ARRAY(Integer))
     agent: Mapped[str] = mapped_column(String)
     conv_time_window: Mapped[int] = mapped_column(Integer)
+    policy: Mapped[list["Policy"]] = relationship("Policy", back_populates="llm_rule")
+    usage: Mapped[list["Usage"]] = relationship("Usage", back_populates="llm_rule")
 
 
 class Policy(db.Model):
     """Defines usage limits for the instance."""
     __tablename__ = "policy"
 
+    id: Mapped[int] = mapped_column(primary_key=True)
     for_user: Mapped[int] = mapped_column(ForeignKey("user_group.id"))
+    llm_rule_id: Mapped[int] = mapped_column(ForeignKey("llm_rule.id"))
     llm_rule: Mapped["LLMRule"] = relationship("LLMRule", back_populates="policy")
     token_time_window_type: Mapped[str] = mapped_column(String, nullable=False) # 'd','h','min','sec'
     token_time_window_num: Mapped[int] = mapped_column(Integer, nullable=False) # 5 of type
@@ -43,7 +45,9 @@ class Usage(db.Model):
     """Associates a user and a conversation with an LLMRule context and tracks token usage."""
     __tablename__ = "usage"
 
+    id: Mapped[int] = mapped_column(primary_key=True)
     user: Mapped[int] = mapped_column(ForeignKey("user_group.id"))
     conversation_id: Mapped[int] = mapped_column(Integer)
-    context: Mapped[LLMRule] = relationship("LLMRule", back_populates="usage")
+    llm_rule_id: Mapped[int] = mapped_column(ForeignKey("llm_rule.id"))
+    llm_rule: Mapped[LLMRule] = relationship("LLMRule", back_populates="usage")
     used_tokens: Mapped[int] = mapped_column(Integer)
