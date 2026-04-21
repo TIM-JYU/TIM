92f2f6234abae33d78f8b5066ff39fe073544b87
Anni Kinnunen <anjakinn@exam-agc3313-11.it.jyu.fi>
2026-04-21 11:31:47 +0300

    Add apikey provider to the LLMRule class and fix formatting issues.

--- timApp/modules/chattim/dbmodels.py
+++ timApp/modules/chattim/dbmodels.py
@@ -10,14 +10,12 @@ class LLMRule(db.Model):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     document_id: Mapped[int] = mapped_column(ForeignKey("json.id"))
-    for_documents: Mapped[int] = mapped_column(Integer)
     owner: Mapped[int] = mapped_column(ForeignKey("user_group.id"))
+    apikey_provider: Mapped[str] = mapped_column(String)
+    apikey: Mapped[list[str]] = mapped_column(ARRAY(String))  # list  of apikeys and publickeys
     chosen_key: Mapped[str] = mapped_column(String)
-    apikey: Mapped[list[str]] = mapped_column(ARRAY(String))
-    # apikey: Mapped[str] = mapped_column(String)
-    # publickey: Mapped[str] = mapped_column(String)
     teachers: Mapped[list[int]] = mapped_column(ARRAY(Integer))
-    current_mode: Mapped[str] = mapped_column(String, default="summarizing") # summarizing or creative
+    current_mode: Mapped[str] = mapped_column(String)  # summarizing or creative
     total_tokens_spent: Mapped[int] = mapped_column(Integer, default=0)
     indexed_chunk_ids: Mapped[list[int]] = mapped_column(ARRAY(Integer))
     agent: Mapped[str] = mapped_column(String)
@@ -34,11 +32,11 @@ class Policy(db.Model):
     for_user: Mapped[int] = mapped_column(ForeignKey("user_group.id"))
     llm_rule_id: Mapped[int] = mapped_column(ForeignKey("llm_rule.id"))
     llm_rule: Mapped["LLMRule"] = relationship("LLMRule", back_populates="policy")
-    token_time_window_type: Mapped[str] = mapped_column(String, nullable=False) # 'd','h','min','sec'
-    token_time_window_num: Mapped[int] = mapped_column(Integer, nullable=False) # 5 of type
-    time_window_tokens: Mapped[int] = mapped_column(Integer, nullable=False) # token limit for the window
+    token_time_window_type: Mapped[str] = mapped_column(String, nullable=False)  # 'd','h','min','sec'
+    token_time_window_num: Mapped[int] = mapped_column(Integer, nullable=False)  # 5 of type
+    time_window_tokens: Mapped[int] = mapped_column(Integer, nullable=False)  # token limit for the window
     max_tokens: Mapped[int] = mapped_column(Integer)
-    policy_type: Mapped[str] = mapped_column(String, default="global") # global or user
+    policy_type: Mapped[str] = mapped_column(String)  # global or user
 
 
 class Usage(db.Model):
