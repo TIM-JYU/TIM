68d3d027084672d41d66d81298343e2dfb1c387a
Anni Kinnunen <anjakinn@exam-agc3313-11.it.jyu.fi>
2026-04-21 11:47:13 +0300

    Fix formatting and spelling errors.

--- timApp/modules/chattim/dbmodels.py
+++ timApp/modules/chattim/dbmodels.py
@@ -12,7 +12,7 @@ class LLMRule(db.Model):
     document_id: Mapped[int] = mapped_column(ForeignKey("json.id"))
     owner: Mapped[int] = mapped_column(ForeignKey("user_group.id"))
     apikey_provider: Mapped[str] = mapped_column(String)
-    apikey: Mapped[list[str]] = mapped_column(ARRAY(String))  # list  of apikeys and publickeys
+    apikey: Mapped[list[str]] = mapped_column(ARRAY(String))  # list of apikeys and publickeys
     chosen_key: Mapped[str] = mapped_column(String)
     teachers: Mapped[list[int]] = mapped_column(ARRAY(Integer))
     current_mode: Mapped[str] = mapped_column(String)  # summarizing or creative
@@ -32,7 +32,7 @@ class Policy(db.Model):
     for_user: Mapped[int] = mapped_column(ForeignKey("user_group.id"))
     llm_rule_id: Mapped[int] = mapped_column(ForeignKey("llm_rule.id"))
     llm_rule: Mapped["LLMRule"] = relationship("LLMRule", back_populates="policy")
-    token_time_window_type: Mapped[str] = mapped_column(String, nullable=False)  # 'd','h','min','sec'
+    token_time_window_type: Mapped[str] = mapped_column(String, nullable=False)  # d,h,min,sec
     token_time_window_num: Mapped[int] = mapped_column(Integer, nullable=False)  # 5 of type
     time_window_tokens: Mapped[int] = mapped_column(Integer, nullable=False)  # token limit for the window
     max_tokens: Mapped[int] = mapped_column(Integer)
