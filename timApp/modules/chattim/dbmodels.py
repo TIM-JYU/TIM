a51e0573ab9f08c460e7b0907dc10a219a3fb84e
Anni Kinnunen <anjakinn@ws-c3313-8.it.jyu.fi>
2026-04-22 12:27:36 +0300

    Add tables LLMRule, Policy and Usage to the database.

--- timApp/modules/chattim/dbmodels.py
+++ timApp/modules/chattim/dbmodels.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from sqlalchemy import Integer, String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.dialects.postgresql import ARRAY
@@ -9,8 +10,8 @@ class LLMRule(db.Model):
     __tablename__ = "llm_rule"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    document_id: Mapped[int] = mapped_column(ForeignKey("json.id"))
-    owner: Mapped[int] = mapped_column(ForeignKey("user_group.id"))
+    document_id: Mapped[int] = mapped_column(Integer)
+    owner: Mapped[int] = mapped_column(ForeignKey("useraccount.id"))
     apikey_provider: Mapped[str] = mapped_column(String)
     apikey: Mapped[list[str]] = mapped_column(ARRAY(String))  # list of apikeys and publickeys
     chosen_key: Mapped[str] = mapped_column(String)
@@ -20,8 +21,8 @@ class LLMRule(db.Model):
     indexed_chunk_ids: Mapped[list[int]] = mapped_column(ARRAY(Integer))
     agent: Mapped[str] = mapped_column(String)
     conv_time_window: Mapped[int] = mapped_column(Integer)
-    policy: Mapped[list["Policy"]] = relationship("Policy", back_populates="llm_rule")
-    usage: Mapped[list["Usage"]] = relationship("Usage", back_populates="llm_rule")
+    policy: Mapped[list["Policy"]] = relationship("Policy", back_populates="llm_rule", cascade="all, delete-orphan")
+    usage: Mapped[list["Usage"]] = relationship("Usage", back_populates="llm_rule", cascade="all, delete-orphan")
 
 
 class Policy(db.Model):
@@ -29,14 +30,14 @@ class Policy(db.Model):
     __tablename__ = "policy"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    for_user: Mapped[int] = mapped_column(ForeignKey("user_group.id"))
+    for_user: Mapped[Optional[int]] = mapped_column(ForeignKey("useraccount.id"), nullable=True)
     llm_rule_id: Mapped[int] = mapped_column(ForeignKey("llm_rule.id"))
     llm_rule: Mapped["LLMRule"] = relationship("LLMRule", back_populates="policy")
     token_time_window_type: Mapped[str] = mapped_column(String, nullable=False)  # d,h,min,sec
     token_time_window_num: Mapped[int] = mapped_column(Integer, nullable=False)  # 5 of type
     time_window_tokens: Mapped[int] = mapped_column(Integer, nullable=False)  # token limit for the window
     max_tokens: Mapped[int] = mapped_column(Integer)
-    policy_type: Mapped[str] = mapped_column(String)  # global or user
+    policy_type: Mapped[str] = mapped_column(String)  # global or student
 
 
 class Usage(db.Model):
@@ -44,7 +45,7 @@ class Usage(db.Model):
     __tablename__ = "usage"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    user: Mapped[int] = mapped_column(ForeignKey("user_group.id"))
+    user: Mapped[int] = mapped_column(ForeignKey("useraccount.id"))
     conversation_id: Mapped[int] = mapped_column(Integer)
     llm_rule_id: Mapped[int] = mapped_column(ForeignKey("llm_rule.id"))
     llm_rule: Mapped[LLMRule] = relationship("LLMRule", back_populates="usage")
