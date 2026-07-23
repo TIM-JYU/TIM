3235507c58ebca1df9eb72efebb4035f78061a0f
Sami Viitanen <sami.a.viitanen@outlook.com>
2026-07-23 14:12:33 +0300

    Fix mimetype for Markdown documents

--- timApp/upload/uploadedfile.py
+++ timApp/upload/uploadedfile.py
@@ -294,6 +294,7 @@ SCRIPT_SAFE_MIMETYPES = {
     "video/webm",
     "application/msword",
     "text/plain",
+    "text/markdown",
     "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
     "application/vnd.openxmlformats-officedocument.wordprocessingml.template",
     "application/vnd.ms-access",
@@ -335,7 +336,7 @@ ALLOWED_DOC_IMPORT_EXT_MIMETYPES = {
     "docx": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
     "odt": "application/vnd.oasis.opendocument.text",
     "txt": "text/plain",
-    "md": "text/plain",
+    "md": "text/markdown",
     "tex": "text/plain",
 }
 
